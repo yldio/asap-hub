c9768dc661c77647850dcff8ab904ba163ad3d8d
johnytiago <joaosao10@gmail.com>
2020-06-30 16:15:45 +0100

    fix: test coverage

--- apps/users-service/src/handlers/fetch-users.ts
+++ apps/users-service/src/handlers/fetch-users.ts
@@ -1,35 +1,22 @@
-import Boom from '@hapi/boom';
 import Joi from '@hapi/joi';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import * as lambda from '../framework/lambda';
 import Users from '../controllers/users';
 
-const fetchById = async (request: lambda.Request): Promise<lambda.Response> => {
-  const paramsSchema = Joi.object({
-    id: Joi.string().required(),
-  }).required();
-
-  const params = lambda.validate('params', request.params, paramsSchema) as {
-    id: string;
-  };
-
-  const users = new Users();
-  const res = await users.fetchById(params.id);
-  return {
-    payload: res,
-  };
-};
-
 export const handler: APIGatewayProxyHandler = lambda.http(
   async (request: lambda.Request): Promise<lambda.Response> => {
-    const { method } = request;
-    if (method === 'get') {
-      return fetchById(request);
-    }
+    const paramsSchema = Joi.object({
+      id: Joi.string().required(),
+    }).required();
 
-    // API Gateway ensures the right HTTP methods are invoked.
-    // In theory the following lines are dead code.
-    // istanbul ignore next
-    throw Boom.methodNotAllowed();
+    const params = lambda.validate('params', request.params, paramsSchema) as {
+      id: string;
+    };
+
+    const users = new Users();
+    const res = await users.fetchById(params.id);
+    return {
+      payload: res,
+    };
   },
 );

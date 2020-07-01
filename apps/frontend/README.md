eb0dfd64c7619e1cbd722b5cf870b9b79959dd32
Filipe Pinheiro <filipe@yld.io>
2020-07-01 16:03:54 +0100

    wip: use envvar on create react app build

--- apps/frontend/README.md
+++ apps/frontend/README.md
@@ -12,6 +12,10 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.<br />
 You will also see any lint errors in the console.
 
+#### Environment Variables
+
+- `REACT_APP_API_BASE_URL` - Set the api endpoint, default value is https://api.asap.yld.io.
+
 ### `yarn test`
 
 Launches the test runner in the interactive watch mode.<br />
--- apps/frontend/src/config.ts
+++ apps/frontend/src/config.ts
@@ -1,20 +1,4 @@
 export const STORAGE_PREFIX = 'asapHub.';
 export const STORAGE_KEY_INVITATION_CODE = `${STORAGE_PREFIX}invitationCode`;
-
-let baseUrl: string;
-switch (window.location.host.split('.').length) {
-  /* istanbul ignore next */
-  case 3: {
-    baseUrl = `${window.location.protocol}//api.${window.location.host}`;
-    break;
-  }
-  /* istanbul ignore next */
-  case 4: {
-    baseUrl = `${window.location.protocol}//api-${window.location.host}`;
-    break;
-  }
-  default: {
-    baseUrl = 'http://localhost:3333/development';
-  }
-}
-export const API_BASE_URL = baseUrl.toString();
+export const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || 'https://api.asap.yld.io/';

65cbd06661289afca1e38a6722600810b77dc3f0
johnytiago <joaosao10@gmail.com>
2020-07-20 17:33:24 +0100

    chore: merge fails

--- packages/model/src/research-output.ts
+++ packages/model/src/research-output.ts
@@ -1,30 +1,4 @@
-<<<<<<< HEAD:packages/model/src/romp.ts
-export type ResearchOutputType =
-  | 'dataset'
-  | 'code'
-  | 'protocol'
-  | 'resource'
-  | 'preprint'
-  | 'other';
-export type ResearchOutputAccessLevel = 'private' | 'team' | 'public';
-
-export interface ResearchOutputFormData {
-  readonly url: string;
-  readonly doi: string;
-
-  readonly type: ResearchOutputType;
-  readonly title: string;
-  readonly description: string;
-  readonly authors: ReadonlyArray<string>;
-  readonly publishDate?: Date;
-
-  readonly accessLevel: ResearchOutputAccessLevel;
-}
-
-export interface Romp {
-=======
 export interface ResearchOutput {
->>>>>>> 53d2417... chore: romp > research-output:packages/model/src/research-output.ts
   id: string;
   created: string;
   url: string;
--- packages/model/src/romp.ts
+++ packages/model/src/romp.ts
@@ -1,17 +1,21 @@
-export interface ResearchOutput {
-  id: string;
-  created: string;
-  url: string;
-  doi: string;
-  outputType: string;
-  title: string;
-  description: string;
-  authors: [
-    {
-      name: string;
-      id: string;
-    },
-  ];
-  publishDate: string;
-  createdBy: string;
+export type ResearchOutputType =
+  | 'dataset'
+  | 'code'
+  | 'protocol'
+  | 'resource'
+  | 'preprint'
+  | 'other';
+export type ResearchOutputAccessLevel = 'private' | 'team' | 'public';
+
+export interface ResearchOutputFormData {
+  readonly url: string;
+  readonly doi: string;
+
+  readonly type: ResearchOutputType;
+  readonly title: string;
+  readonly description: string;
+  readonly authors: ReadonlyArray<string>;
+  readonly publishDate?: Date;
+
+  readonly accessLevel: ResearchOutputAccessLevel;
 }

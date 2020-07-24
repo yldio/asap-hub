d905c68167704e31ee87d52f49b448777a0da7d5
Filipe Pinheiro <fampinheiro@users.noreply.github.com>
2020-07-24 17:22:20 +0100

    feat: require authentication when fetching users (#107)

--- apps/asap-server/src/handlers/users/fetch-by-id.ts
+++ apps/asap-server/src/handlers/users/fetch-by-id.ts
@@ -2,6 +2,7 @@ import Joi from '@hapi/joi';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { framework as lambda } from '@asap-hub/services-common';
 import Users from '../../controllers/users';
+import validateUser from '../../utils/validate-user';
 
 export const handler: APIGatewayProxyHandler = lambda.http(
   async (request: lambda.Request): Promise<lambda.Response> => {
@@ -12,6 +13,8 @@ export const handler: APIGatewayProxyHandler = lambda.http(
     const params = lambda.validate('params', request.params, paramsSchema) as {
       id: string;
     };
+
+    await validateUser(request);
 
     const users = new Users();
     const res = await users.fetchById(params.id);
--- apps/asap-server/test/handlers/users/fetch.test.ts
+++ apps/asap-server/test/handlers/users/fetch.test.ts
@@ -1,15 +1,22 @@
 import nock from 'nock';
+import Chance from 'chance';
+
 import { APIGatewayProxyResult } from 'aws-lambda';
 
+import { config as authConfig } from '@asap-hub/auth';
 import { handler } from '../../../src/handlers/users/fetch';
 import { cms } from '../../../src/config';
 import { apiGatewayEvent } from '../../helpers/events';
 import { createRandomUser } from '../../helpers/create-user';
 
+const chance = new Chance();
+
 describe('GET /users', () => {
   afterEach(() => nock.cleanAll());
 
   test('returns 200 when no users exist', async () => {
+    nock(`https://${authConfig.domain}`).get('/userinfo').reply(200);
+
     nock(cms.baseUrl)
       .post('/identity-server/connect/token')
       .reply(200, { access_token: 'token' })
@@ -25,6 +32,9 @@ describe('GET /users', () => {
     const result = (await handler(
       apiGatewayEvent({
         httpMethod: 'get',
+        headers: {
+          Authorization: `Bearer ${chance.string()}`,
+        },
       }),
       null,
       null,
@@ -36,11 +46,15 @@ describe('GET /users', () => {
   });
 
   test('returns 200 when users exist', async () => {
+    nock(`https://${authConfig.domain}`).get('/userinfo').reply(200);
     await Promise.all(new Array(3).fill(async () => await createRandomUser()));
 
     const result = (await handler(
       apiGatewayEvent({
         httpMethod: 'get',
+        headers: {
+          Authorization: `Bearer ${chance.string()}`,
+        },
       }),
       null,
       null,

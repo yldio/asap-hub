e4290f8f61cbce8ffdc5a92c9f66538597a4a274
Filipe Pinheiro <filipe@yld.io>
2020-08-03 17:42:01 +0100

    test: remove mocked router from check auth tests

--- apps/frontend/src/auth/__tests__/CheckAuth.test.tsx
+++ apps/frontend/src/auth/__tests__/CheckAuth.test.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { MemoryRouter, Route } from 'react-router-dom';
 import { render, waitFor } from '@testing-library/react';
 import { authTestUtils } from '@asap-hub/react-components';
 
@@ -7,12 +6,11 @@ import CheckAuth from '../CheckAuth';
 
 it('renders a loading indicator while Auth0 is initializing', async () => {
   const { getByText, queryByText } = render(
-    <MemoryRouter initialEntries={['/secure']}>
-      <authTestUtils.Auth0Provider>
-        <CheckAuth>content</CheckAuth>
-      </authTestUtils.Auth0Provider>
-    </MemoryRouter>,
+    <authTestUtils.Auth0Provider>
+      <CheckAuth>content</CheckAuth>
+    </authTestUtils.Auth0Provider>,
   );
+
   expect(getByText(/loading/i)).toBeVisible();
   await waitFor(() => expect(queryByText(/loading/i)).not.toBeInTheDocument());
 });
@@ -20,11 +18,9 @@ it('renders a loading indicator while Auth0 is initializing', async () => {
 it('redirects to the root if the user is not authenticated', async () => {
   const { findByText } = render(
     <authTestUtils.Auth0Provider>
-      <MemoryRouter initialEntries={['/secure']}>
-        <CheckAuth>
-          <Route exact path="/secure" render={() => 'secure'} />
-        </CheckAuth>
-      </MemoryRouter>
+      <CheckAuth>
+        <p>secure</p>
+      </CheckAuth>
     </authTestUtils.Auth0Provider>,
   );
   expect(await findByText('Log in')).toBeVisible();
@@ -33,12 +29,10 @@ it('redirects to the root if the user is not authenticated', async () => {
 it('renders the children if the user is authenticated', async () => {
   const { findByText } = render(
     <authTestUtils.Auth0Provider>
-      <authTestUtils.LoggedIn user={undefined}>
-        <MemoryRouter initialEntries={['/secure']}>
-          <CheckAuth>
-            <Route exact path="/secure" render={() => 'secure'} />
-          </CheckAuth>
-        </MemoryRouter>
+      <authTestUtils.LoggedIn user={{}}>
+        <CheckAuth>
+          <p>secure</p>
+        </CheckAuth>
       </authTestUtils.LoggedIn>
     </authTestUtils.Auth0Provider>,
   );

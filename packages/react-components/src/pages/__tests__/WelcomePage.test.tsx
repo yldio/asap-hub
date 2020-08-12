2dd83465fb2bc9c939825a56bbc81617c1e49acb
Filipe Pinheiro <filipe@yld.io>
2020-08-12 11:47:41 +0100

    test: raise coverage with welcome page test

---
+++ packages/react-components/src/pages/__tests__/WelcomePage.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import WelcomePage from '../WelcomePage';
+
+it('renders the signin page ', () => {
+  const handleClick = jest.fn();
+  const { getByRole } = render(<WelcomePage onClick={handleClick} />);
+  expect(getByRole('button').textContent).toMatchInlineSnapshot(`"Sign in"`);
+});
+
+it('renders the signup page', () => {
+  const handleClick = jest.fn();
+  const { getByRole } = render(<WelcomePage signup onClick={handleClick} />);
+  expect(getByRole('button').textContent).toMatchInlineSnapshot(
+    `"Create account"`,
+  );
+});
--- packages/react-components/src/templates/ProfileSkills.tsx
+++ packages/react-components/src/templates/ProfileSkills.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import css from '@emotion/css';
 import { Tag, Card, Headline2 } from '../atoms';
-import { Container } from '../molecules';
 
 type SkillsProps = {
   readonly skills: string[];
@@ -23,18 +22,16 @@ const elementStyle = css({
 
 const Skills: React.FC<SkillsProps> = ({ skills = [] }) => {
   return (
-    <Container>
-      <Card>
-        <Headline2>Skills and Expertise</Headline2>
-        <div css={containerStyles}>
-          {skills.map((s) => (
-            <div css={elementStyle}>
-              <Tag>{s}</Tag>
-            </div>
-          ))}
-        </div>
-      </Card>
-    </Container>
+    <Card>
+      <Headline2>Skills and Expertise</Headline2>
+      <div css={containerStyles}>
+        {skills.map((s) => (
+          <div css={elementStyle}>
+            <Tag>{s}</Tag>
+          </div>
+        ))}
+      </div>
+    </Card>
   );
 };
 

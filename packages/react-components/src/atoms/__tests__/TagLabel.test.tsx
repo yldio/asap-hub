2b8da140c760e59786a86e448406eb550762c696
Filipe Pinheiro <filipe@yld.io>
2020-08-13 10:59:47 +0100

    test: add test to tag label

---
+++ packages/react-components/src/atoms/__tests__/TagLabel.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import TagLabel from '../TagLabel';
+
+it('renders a tag label with content', () => {
+  const { container } = render(<TagLabel>Text</TagLabel>);
+  expect(container.textContent).toEqual('Text');
+});

00eb5b901de3d5f4d587309668c7ebf58f03ba4c
johnytiago <joaosao10@gmail.com>
2020-08-19 12:16:34 +0100

    fix: frontend tests

--- apps/frontend/src/users/__tests__/Profile.test.tsx
+++ apps/frontend/src/users/__tests__/Profile.test.tsx
@@ -14,6 +14,7 @@ import { API_BASE_URL } from '../../config';
 
 const user: UserResponse = {
   id: '42',
+  lastModifiedDate: '2020-08-19T11:11:06.989Z',
   displayName: 'John Doe',
   email: 'john.doe@example.com',
   institution: 'Unknown Institution',

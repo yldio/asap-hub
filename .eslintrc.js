65980940b06a4a440e4fbe6fcfd53346a637afdf
Tim Seckinger <tim.seckinger@yld.io>
2020-08-19 19:32:08 +0200

    fix format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   root: true,
   extends: require.resolve('@asap-hub/eslint-config-asap-hub'),
-  ignorePatterns: "apps/auth0-connect-user"
+  ignorePatterns: 'apps/auth0-connect-user',
 };

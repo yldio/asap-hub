9f37d93a3c5d49c0ed757bd9dbfaa9fc27aa6784
johnytiago <joaosao10@gmail.com>
2020-08-20 16:48:15 +0100

    wip: add ts

---
+++ apps/auth0-connect-user/src/__tests__/index.test.ts
@@ -0,0 +1,25 @@
+async function connectUser(user, context, callback) {
+  if (!context.invitationCode) {
+    return callback(null, user, context);
+  }
+
+  const got = require('got');
+
+  try {
+    await got
+      .post(`${configuration.APP_ORIGIN}/webhook/users/connections`, {
+        json: {
+          code: context.invitationCode,
+          userId: user.user_id,
+        },
+        headers: {
+          Authorization: `Basic ${configuration.API_SHARED_SECRET}`,
+        },
+        timeout: 10000,
+      })
+      .json();
+    return callback(null, user, context);
+  } catch (err) {
+    return callback(new Error(err));
+  }
+}

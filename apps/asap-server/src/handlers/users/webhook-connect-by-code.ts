b94e3ad9668314ef2422ae4d24aa9b1c3e273689
johnytiago <joaosao10@gmail.com>
2020-08-20 12:23:18 +0100

    feat: updates connect webhook to use generic validate fn

--- apps/asap-server/src/handlers/users/webhook-connect-by-code.ts
+++ apps/asap-server/src/handlers/users/webhook-connect-by-code.ts
@@ -1,33 +1,27 @@
-import Boom from '@hapi/boom';
 import Joi from '@hapi/joi';
 import { framework as lambda } from '@asap-hub/services-common';
 
 import Users from '../../controllers/users';
 import { Handler } from '../../utils/types';
-import { auth0SharedSecret } from '../../config';
+import validateRequest from '../../utils/validate-auth0-request';
 
 export const handler: Handler = lambda.http(
   async (request: lambda.Request): Promise<lambda.Response> => {
+    await validateRequest(request);
+
     const bodySchema = Joi.object({
       code: Joi.string().required(),
       userId: Joi.string().required(),
-      secret: Joi.string().required(),
     }).required();
 
-    const { code, userId, secret } = lambda.validate(
+    const { code, userId } = lambda.validate(
       'body',
       request.payload,
       bodySchema,
     ) as {
       code: string;
       userId: string;
-      secret: string;
     };
-
-    // TODO: address variable sharing
-    if (secret !== auth0SharedSecret) {
-      throw Boom.forbidden();
-    }
 
     const users = new Users();
     await users.connectByCode(code, userId);

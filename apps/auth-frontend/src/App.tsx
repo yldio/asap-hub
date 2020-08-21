ba7f13f677509c4047b88b91adf4dba9d2904e28
Tim Seckinger <seckinger.tim@gmail.com>
2020-08-21 15:15:37 +0100

    fix(auth-frontend): re-add missing layout
    That app was completely overlooked in 8469ef6

--- apps/auth-frontend/src/App.tsx
+++ apps/auth-frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Layout } from '@asap-hub/react-components';
 
 import Login from './login/Login';
 
 const App: React.FC<{}> = () => (
   <BrowserRouter>
-    <Switch>
-      <Route path="/login" component={Login} />
-      <Route render={() => 'This route does not exist'} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route>Not Found</Route>
+      </Switch>
+    </Layout>
   </BrowserRouter>
 );
 

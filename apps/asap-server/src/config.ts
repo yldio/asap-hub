fe3bb9e0bd6e01ff28789b42a6db825dcecb7338
Filipe Pinheiro <filipe@yld.io>
2020-09-01 12:25:11 +0100

    chore: use gitlab-ci to define envars

--- apps/asap-server/src/config.ts
+++ apps/asap-server/src/config.ts
@@ -1,29 +1,24 @@
 /* istanbul ignore file */
 
 const {
-  GLOBAL_TOKEN,
   APP_ORIGIN,
-  NODE_ENV,
-  CMS_BASE_URL,
-  CMS_APP_NAME,
-  CMS_CLIENT_ID,
-  CMS_CLIENT_SECRET,
   AUTH0_SHARED_SECRET,
+  AWS_SES_ENDPOINT,
+  GLOBAL_TOKEN,
+  SQUIDEX_APP_NAME,
+  SQUIDEX_BASE_URL,
+  SQUIDEX_CLIENT_ID,
+  SQUIDEX_CLIENT_SECRET,
 } = process.env;
 
 export const globalToken = GLOBAL_TOKEN || 'change_me_when_we_have_admins';
 export const origin = APP_ORIGIN || 'http://localhost:3000';
 export const cms = {
-  clientId: CMS_CLIENT_ID || '5eec9b8133e8330001a8aae9',
+  clientId: SQUIDEX_CLIENT_ID || '5eec9b8133e8330001a8aae9',
   clientSecret:
-    CMS_CLIENT_SECRET || 'iwsrcubobxxxrnropuj5k0xe4gjxip42tflvx5pv1a8x',
-  appName: CMS_APP_NAME || 'asap-local',
-  baseUrl:
-    NODE_ENV !== 'production'
-      ? CMS_BASE_URL || 'http://localhost:4004'
-      : CMS_BASE_URL || 'https://cloud.squidex.io',
+    SQUIDEX_CLIENT_SECRET || 'iwsrcubobxxxrnropuj5k0xe4gjxip42tflvx5pv1a8x',
+  appName: SQUIDEX_APP_NAME || 'asap-local',
+  baseUrl: SQUIDEX_BASE_URL || 'http://localhost:4004',
 };
-export const sesEndpoint =
-  NODE_ENV !== 'production' ? 'http://localhost:4566' : undefined;
-export const auth0SharedSecret =
-  NODE_ENV === 'production' ? AUTH0_SHARED_SECRET : 'auth0_shared_secret';
+export const sesEndpoint = AWS_SES_ENDPOINT;
+export const auth0SharedSecret = AUTH0_SHARED_SECRET || 'auth0_shared_secret';
--- ci/env-create.sh
+++ ci/env-create.sh
@@ -2,15 +2,15 @@
 
 CURPATH=`dirname "$0"`
 
-SLS_STAGE=${SLS_STAGE:-"dev"}
+ENV=${CI_EXTERNAL_PULL_REQUEST_IID:-${CI_COMMIT_REF_SLUG:-"dev"}}
 HOSTNAME=${BASE_HOSTNAME:-"hub.asap.science"}
 
 APP_HOSTNAME=$HOSTNAME
 API_HOSTNAME="api.$HOSTNAME"
 
-if [ $SLS_STAGE != "production" ]; then
-    API_HOSTNAME="api-${SLS_STAGE}.${APP_HOSTNAME}"
-    APP_HOSTNAME="${SLS_STAGE}.${APP_HOSTNAME}"
+if [ $ENV != "master" ]; then
+    API_HOSTNAME="api-${ENV}.${APP_HOSTNAME}"
+    APP_HOSTNAME="${ENV}.${APP_HOSTNAME}"
 fi
 
 echo "APP_HOSTNAME=${APP_HOSTNAME}"

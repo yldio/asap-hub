bbe666791682acde181e3db29ba1198c49721e36
Filipe Pinheiro <filipe@yld.io>
2020-09-02 10:12:11 +0100

    chore: create dev env on dev.hub.asap.science

--- ci/env-create.sh
+++ ci/env-create.sh
@@ -2,13 +2,13 @@
 
 CURPATH=`dirname "$0"`
 
-ENV=${CI_EXTERNAL_PULL_REQUEST_IID:-${CI_COMMIT_REF_SLUG:-"dev"}}
+ENV=${CI_EXTERNAL_PULL_REQUEST_IID:-${SLS_STAGE:-"dev"}}
 HOSTNAME=${BASE_HOSTNAME:-"hub.asap.science"}
 
 APP_HOSTNAME=$HOSTNAME
 API_HOSTNAME="api.$HOSTNAME"
 
-if [ $ENV != "master" ]; then
+if [ $ENV != "production" ]; then
     API_HOSTNAME="api-${ENV}.${APP_HOSTNAME}"
     APP_HOSTNAME="${ENV}.${APP_HOSTNAME}"
 fi

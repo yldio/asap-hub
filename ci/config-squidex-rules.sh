70f84115386a457a5c1c0b3645d37e03199ccd52
johnytiago <joaosao10@gmail.com>
2020-09-03 15:43:37 +0100

    feat: add script to set squidex rules urls

---
+++ ci/config-squidex-rules.sh
@@ -0,0 +1,5 @@
+find dev/squidex/rules -type f \( -name "*.json" -not -name "__rule.json" \) -print0 | xargs -t -0 -I @file \
+  sed -i '/sharedSecret/c\    \"sharedSecret\" : \"'$SQUIDEX_SHARED_SECRET'\",' @file
+
+find dev/squidex/rules -type f \( -name "*.json" -not -name "__rule.json" \) -print0 | xargs -t -0 -I @file \
+  sed -i -E 's#https?\:\/\/(www\.)?[[:alpha:]|.]+#'$API_HOSTNAME'#' @file

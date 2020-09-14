cfdad2342dc6d45a182862261ae74663262edf3d
Thomas Waterfall <thomas.waterfall@gmail.com>
2020-09-14 16:14:19 +0100

    PR Amends

--- packages/react-components/src/molecules/__tests__/TagList.test.tsx
+++ packages/react-components/src/molecules/__tests__/TagList.test.tsx
@@ -3,57 +3,65 @@ import { render } from '@testing-library/react';
 
 import TagList from '../TagList';
 
-it('shows all skills when there are few', () => {
-  const { getAllByRole } = render(
-    <TagList
-      summarize
-      tags={['Neurological Diseases', 'Clinical Neurology']}
-    />,
-  );
-  expect(
-    getAllByRole('listitem').map(({ textContent }) => textContent),
-  ).toEqual(['Neurological Diseases', 'Clinical Neurology']);
+describe('Summarized', () => {
+  it('shows all tags when there are few', () => {
+    const { getAllByRole } = render(
+      <TagList
+        summarize
+        tags={['Neurological Diseases', 'Clinical Neurology']}
+      />,
+    );
+    expect(
+      getAllByRole('listitem').map(({ textContent }) => textContent),
+    ).toEqual(['Neurological Diseases', 'Clinical Neurology']);
+  });
+
+  it('shows only the first tags when there are many', () => {
+    const { getAllByRole } = render(
+      <TagList
+        summarize
+        tags={[
+          'Neurological Diseases',
+          'Clinical Neurology',
+          'Adult Neurology',
+          'Neuroimaging',
+        ]}
+      />,
+    );
+    expect(
+      getAllByRole('listitem').map(({ textContent }) => textContent),
+    ).toEqual([
+      'Neurological Diseases',
+      'Clinical Neurology',
+      'Adult Neurology',
+    ]);
+  });
 });
 
-it('shows all tags when not summarized', () => {
-  const { getAllByRole } = render(
-    <TagList
-      tags={[
-        'Neurological Diseases',
-        'Clinical Neurology',
-        'Adult Neurology',
-        'Neuroimaging',
-      ]}
-    />,
-  );
-  expect(
-    getAllByRole('listitem').map(({ textContent }) => textContent),
-  ).toEqual([
-    'Neurological Diseases',
-    'Clinical Neurology',
-    'Adult Neurology',
-    'Neuroimaging',
-  ]);
-});
+describe('Full tag list', () => {
+  it('shows all tags when not summarized', () => {
+    const { getAllByRole } = render(
+      <TagList
+        tags={[
+          'Neurological Diseases',
+          'Clinical Neurology',
+          'Adult Neurology',
+          'Neuroimaging',
+        ]}
+      />,
+    );
+    expect(
+      getAllByRole('listitem').map(({ textContent }) => textContent),
+    ).toEqual([
+      'Neurological Diseases',
+      'Clinical Neurology',
+      'Adult Neurology',
+      'Neuroimaging',
+    ]);
+  });
 
-it('shows only the first skills when there are many', () => {
-  const { getAllByRole } = render(
-    <TagList
-      summarize
-      tags={[
-        'Neurological Diseases',
-        'Clinical Neurology',
-        'Adult Neurology',
-        'Neuroimaging',
-      ]}
-    />,
-  );
-  expect(
-    getAllByRole('listitem').map(({ textContent }) => textContent),
-  ).toEqual(['Neurological Diseases', 'Clinical Neurology', 'Adult Neurology']);
-});
-
-it('hides skills when there are none', () => {
-  const { queryAllByRole } = render(<TagList tags={[]} />);
-  expect(queryAllByRole('list')).toEqual([]);
+  it('hides tags when there are none', () => {
+    const { queryAllByRole } = render(<TagList tags={[]} />);
+    expect(queryAllByRole('list')).toEqual([]);
+  });
 });
--- packages/react-components/src/organisms/TeamCard.tsx
+++ packages/react-components/src/organisms/TeamCard.tsx
@@ -1,56 +1,12 @@
 import React from 'react';
 import css from '@emotion/css';
 
-import { Card, Link, Tag, Paragraph, Headline2 } from '../atoms';
-import { perRem, tabletScreen } from '../pixels';
+import { Card, Link, Paragraph, Headline2 } from '../atoms';
+import { perRem } from '../pixels';
 import { TeamMember } from '../../../model/src';
 import { teamMembersIcon } from '../icons';
+import { TagList } from '../molecules';
 import { lead } from '../colors';
-
-const listStyles = css({
-  padding: 0,
-  marginBlockStart: 0,
-  listStyle: 'none',
-
-  display: 'flex',
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-  alignItems: 'center',
-
-  '> .overflow': {
-    display: 'none', // can later be activated by previous list items
-  },
-});
-
-const normalListItemStyles = css({
-  counterIncrement: 'skills -1',
-
-  ':not(:nth-last-of-type(2))': {
-    paddingRight: `${12 / perRem}em`,
-  },
-
-  ':nth-of-type(n + 4)': {
-    display: 'none',
-    '~ .overflow': {
-      display: 'unset',
-    },
-  },
-
-  [`@media (max-width: ${tabletScreen.min - 1}px)`]: {
-    ':nth-of-type(n + 3)': {
-      display: 'none',
-      '~ .overflow': {
-        display: 'unset',
-      },
-    },
-  },
-});
-
-const overflowContentStyles = css({
-  '::after': {
-    content: '"+" counter(skills)',
-  },
-});
 
 const teamMemberStyles = css({
   color: lead.rgb,
@@ -83,20 +39,7 @@ const TeamCard: React.FC<TeamCardProps> = ({
       <Card>
         <Headline2 styleAsHeading={4}>{displayName}</Headline2>
         <Paragraph accent="lead">{projectSummary}</Paragraph>
-        {!!skills.length && (
-          <ul css={[listStyles, { counterReset: `skills ${skills.length}` }]}>
-            {skills.map((skill, index) => (
-              <li key={index} css={normalListItemStyles}>
-                <Tag>{skill}</Tag>
-              </li>
-            ))}
-            <li key="overflow" className="overflow">
-              <Tag>
-                <span css={overflowContentStyles}></span>
-              </Tag>
-            </li>
-          </ul>
-        )}
+        <TagList summarize tags={skills} />
         <span css={teamMemberStyles}>
           <span css={iconStyles}>{teamMembersIcon} </span>
           {members.length} Team Member

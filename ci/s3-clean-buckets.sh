7ed2fcc467728d569f1e5a940213fb8d3bf58155
johnytiago <joaosao10@gmail.com>
2020-09-16 15:20:57 +0100

    chore: use awk instead of tr

--- ci/s3-clean-buckets.sh
+++ ci/s3-clean-buckets.sh
@@ -2,5 +2,5 @@
 
 # use this script if you need to clean up feature environments
 
-aws s3 ls |  tr " " "\n" | grep '^asap-hub-[[:digit:]]' | xargs -I % aws s3 rb s3://% --force
+aws s3 ls | awk '{print $3}' | grep '^asap-hub-[[:digit:]]' | xargs -I % aws s3 rb s3://% --force
 

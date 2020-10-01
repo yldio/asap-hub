67df4bce73c6a6687cbcf206f51ec220ff7ac226
Filipe Pinheiro <fampinheiro@users.noreply.github.com>
2020-10-01 15:58:18 +0100

    fix: amend orcid works (#312)
    * fix: ammend orcid works
    
    * test: create tests for publish date
    
    * fix: use flatMap instead of reduce

--- apps/storybook/src/ProfileRecentWorks.stories.tsx
+++ apps/storybook/src/ProfileRecentWorks.stories.tsx
@@ -9,7 +9,6 @@ export const Normal = () => (
   <ProfileRecentWorks
     orcidWorks={[
       {
-        id: '42',
         doi: 'https://doi.org/10.7554/elife.07083',
         title:
           'Recognizing the importance of new tools and resources for research',
@@ -19,6 +18,17 @@ export const Normal = () => (
           month: '3',
         },
         lastModifiedDate: '1478865224685',
+      },
+      {
+        title:
+          'Systems genetics analysis identifies calcium-signaling defects as novel cause of congenital heart disease.',
+        type: 'JOURNAL_ARTICLE',
+        publicationDate: {
+          year: '2020',
+          month: '08',
+          day: '28',
+        },
+        lastModifiedDate: '1600246394769',
       },
     ]}
   />

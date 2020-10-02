26fd2b88a2702620ba02141e4f838f8ab3422954
Filipe Pinheiro <fampinheiro@users.noreply.github.com>
2020-10-02 13:44:21 +0100

    feat: redirect user to previous page on login (#318)

--- apps/frontend/src/auth/Signin.tsx
+++ apps/frontend/src/auth/Signin.tsx
@@ -6,9 +6,13 @@ const Home: React.FC<{}> = () => {
   const { loginWithRedirect } = useAuth0();
 
   const signin = () => {
-    // TODO pass a redirect_uri with a query param containing the current page;
-    // and send the user back to that original page in handleRedirectCallback
-    return loginWithRedirect({ prompt: 'login' });
+    const { pathname, search, hash } = window.location;
+    return loginWithRedirect({
+      prompt: 'login',
+      appState: {
+        targetUrl: pathname + search + hash,
+      },
+    });
   };
   return <WelcomePage onClick={signin} />;
 };

946fd7524c3416cf8e4cab2e20221686669b67ea
johnytiago <joaosao10@gmail.com>
2020-10-07 15:32:38 +0100

    refactor: orcid tests & fixtures

--- apps/asap-server/src/handlers/webhooks/cronjob-sync-orcid.ts
+++ apps/asap-server/src/handlers/webhooks/cronjob-sync-orcid.ts
@@ -1,15 +1,31 @@
 import pLimit from 'p-limit';
-import { framework as lambda } from '@asap-hub/services-common';
+import { framework as lambda, Squidex } from '@asap-hub/services-common';
 
-import { CMS } from '../../cms';
 import Users from '../../controllers/users';
+import { CMSUser } from '../../entities/user';
 
 export const handler = async (): Promise<lambda.Response> => {
-  const limit = pLimit(5);
-  const cms = new CMS();
-  const users = new Users();
-  const outdatedUsers = await cms.users.fetchWithOrcidSorted();
   const ONE_MONTH = 1000 * 60 * 60 * 24 * 31;
+
+  const limit = pLimit(5);
+  const users = new Users();
+  const squidexUsers: Squidex<CMSUser> = new Squidex('users');
+
+  const { items: outdatedUsers } = await squidexUsers.fetch({
+    take: 30,
+    filter: {
+      path: 'data.orcid.iv',
+      op: 'contains',
+      value: '-',
+    },
+    sort: [
+      {
+        path: 'data.orcidLastSyncDate.iv',
+        order: 'ascending',
+      },
+    ],
+  });
+
   await Promise.all(
     outdatedUsers
       .filter(

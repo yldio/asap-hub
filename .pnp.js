2d15821b2c3e4fe979acfc0ffb0fc511c431ae9e
Thomas Waterfall <Thomas.Waterfall@gmail.com>
2020-10-15 17:52:37 +0100

    Fixtures (#356)

--- .pnp.js
+++ .pnp.js
@@ -91,6 +91,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         "reference": "workspace:packages/eslint-config-asap-hub"
       },
       {
+        "name": "@asap-hub/fixtures",
+        "reference": "workspace:packages/fixtures"
+      },
+      {
         "name": "@asap-hub/model",
         "reference": "workspace:packages/model"
       },
@@ -116,6 +120,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
       ["@asap-hub/auth0-rules", ["workspace:apps/auth0-rules"]],
       ["@asap-hub/dom-test-utils", ["workspace:packages/dom-test-utils"]],
       ["@asap-hub/eslint-config-asap-hub", ["workspace:packages/eslint-config-asap-hub"]],
+      ["@asap-hub/fixtures", ["workspace:packages/fixtures"]],
       ["@asap-hub/frontend", ["workspace:apps/frontend"]],
       ["@asap-hub/import-data", ["workspace:apps/import-data"]],
       ["@asap-hub/model", ["workspace:packages/model"]],
@@ -165,6 +170,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
       [
         "@asap-hub/eslint-config-asap-hub",
         "workspace:packages/eslint-config-asap-hub"
+      ],
+      [
+        "@asap-hub/fixtures",
+        "workspace:packages/fixtures"
       ],
       [
         "@asap-hub/frontend",
@@ -8616,6 +8625,21 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "SOFT",
         }]
       ]],
+      ["@asap-hub/fixtures", [
+        ["workspace:packages/fixtures", {
+          "packageLocation": "./packages/fixtures/",
+          "packageDependencies": [
+            ["@asap-hub/fixtures", "workspace:packages/fixtures"],
+            ["@asap-hub/model", "workspace:packages/model"],
+            ["@babel/runtime-corejs3", "npm:7.11.2"],
+            ["core-js", "npm:3.6.5"],
+            ["date-fns", "npm:2.16.1"],
+            ["ramda", "npm:0.27.1"],
+            ["uuid", "npm:8.3.0"]
+          ],
+          "linkType": "SOFT",
+        }]
+      ]],
       ["@asap-hub/frontend", [
         ["workspace:apps/frontend", {
           "packageLocation": "./apps/frontend/",
@@ -8624,6 +8648,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@asap-hub/auth", "workspace:packages/auth"],
             ["@asap-hub/dom-test-utils", "workspace:packages/dom-test-utils"],
             ["@asap-hub/eslint-config-asap-hub", "workspace:packages/eslint-config-asap-hub"],
+            ["@asap-hub/fixtures", "workspace:packages/fixtures"],
             ["@asap-hub/model", "workspace:packages/model"],
             ["@asap-hub/react-components", "virtual:141d36d44af4b6731166e6172e8780f914aeab37a6dcbae67fcdbe0ff29aae79810737fc4482e7f5495801c3c6b8686f4717e34bde53971e4965b26dfb1e760e#workspace:packages/react-components"],
             ["@asap-hub/react-context", "virtual:a4c6bb32dfbbf947d13a0197041c2234b825c97508e81d202840e37113aeb8ad67038badebb8556b1d55925bb52ab345eef4de3ef6a2e7cabdbb4b290eac13b5#workspace:packages/react-context"],

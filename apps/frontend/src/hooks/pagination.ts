9401e727ea97f2cd5611340a6b64fbbb9a75ec22
Tim Seckinger <tim.seckinger@yld.io>
2020-10-23 16:30:39 +0200

    fix: page, search, and filter reset cases

--- apps/frontend/src/hooks/pagination.ts
+++ apps/frontend/src/hooks/pagination.ts
@@ -4,34 +4,46 @@ import { useHistory, useLocation } from 'react-router-dom';
 const DEFAULT_PAGE_SIZE = 10;
 
 export const usePaginationParams = (pageSize = DEFAULT_PAGE_SIZE) => {
+  const history = useHistory();
   const searchParams = new URLSearchParams(useLocation().search);
   const currentPage = Number(searchParams.get('currentPage')) ?? 0;
-  return { currentPage, pageSize };
+
+  const resetPage = () => {
+    const newSearchParams = new URLSearchParams(history.location.search);
+    newSearchParams.delete('currentPage');
+    history.replace({ search: newSearchParams.toString() });
+  };
+  return { currentPage, pageSize, resetPage };
 };
 
 export const usePagination = (numberOfItems: number, pageSize: number) => {
   const history = useHistory();
   const searchParams = new URLSearchParams(useLocation().search);
+
   const currentPage = Number(searchParams.get('currentPage')) ?? 0;
   const lastAllowedPage = Math.max(0, Math.ceil(numberOfItems / pageSize) - 1);
   const numberOfPages = Math.max(currentPage, lastAllowedPage) + 1;
+
   const renderPageHref = (page: number) => {
+    const newSearchParams = new URLSearchParams(history.location.search);
+
     if (page === currentPage) return '';
-    if (page === 0) searchParams.delete('currentPage');
-    else searchParams.set('currentPage', String(page));
-    const newParams = searchParams.toString();
+    if (page === 0) newSearchParams.delete('currentPage');
+    else newSearchParams.set('currentPage', String(page));
+
+    const newParams = newSearchParams.toString();
     return `${newParams.length ? '?' : '.'}${newParams}`;
   };
+
   useEffect(() => {
     if (numberOfItems && currentPage > lastAllowedPage)
       history.replace({
         search: renderPageHref(lastAllowedPage),
       });
   });
+
   return {
     numberOfPages,
     renderPageHref,
   };
 };
-
-export default usePagination;

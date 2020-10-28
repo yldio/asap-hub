0a83952b466a674106c339a7121484a8ae767523
johnytiago <joaosao10@gmail.com>
2020-10-28 16:38:07 +0000

    fix: issues with key selection

--- packages/auth/src/decode-token.ts
+++ packages/auth/src/decode-token.ts
@@ -1,20 +1,23 @@
-import { default as jwt, JwtHeader } from 'jsonwebtoken';
-import { promisify } from 'util';
+import { default as jwt, JwtHeader, SigningKeyCallback } from 'jsonwebtoken';
 import { Auth0User } from '.';
 
 import pubKeys from './pubKeys';
 
-const getPublicKey = (header: JwtHeader, cb: Function): string => {
-  const key = pubKeys.find(({ kid }) => header.kid);
-  return key ? cb(null, key) : cb(new Error());
+const getPublicKey = (header: JwtHeader, cb: SigningKeyCallback): void => {
+  const key = pubKeys.find(({ kid }) => kid === header.kid)?.x5c;
+  return key ? cb(null, key[0]) : cb(new Error());
 };
 
-const decodeToken = async (token: string): Promise<Auth0User> => {
-  const verify = promisify(jwt.verify);
-  const { payload } = (await verify(token, getPublicKey)) as {
-    payload: Auth0User;
-  };
-  return payload;
+const decodeToken = (token: string): Promise<Auth0User> => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, getPublicKey, { algorithms: ['RS256'] }, (err, res) => {
+      if (err) {
+        return reject(err);
+      }
+      const { payload } = res as { payload: Auth0User };
+      return resolve(payload);
+    });
+  });
 };
 
 export default decodeToken;

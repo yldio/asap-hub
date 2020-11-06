ffa2064a5a8bab858eea193b53f5c137fb0b81b6
johnytiago <joaosao10@gmail.com>
2020-11-06 13:51:22 +0000

    fix: replace any for unknown

--- apps/asap-server/src/controllers/users.ts
+++ apps/asap-server/src/controllers/users.ts
@@ -179,8 +179,7 @@ export default class Users {
     const cleanUpdate = Object.entries(update).reduce((acc, [key, value]) => {
       acc[key] = { iv: value };
       return acc;
-      // eslint-disable @typescript-eslint/no-explicit-any
-    }, {} as { [key: string]: { iv: any } });
+    }, {} as { [key: string]: { iv: unknown } });
 
     const user = await this.users.patch(id, cleanUpdate);
     return parseUser(user);

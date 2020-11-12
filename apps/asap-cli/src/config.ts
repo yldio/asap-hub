f936cc1ef4fc4c60524e5ad242de26f7699d1cf9
Filipe Pinheiro <filipe@yld.io>
2020-11-12 09:43:59 +0000

    feat: change from email on welcome emails

--- apps/asap-cli/src/config.ts
+++ apps/asap-cli/src/config.ts
@@ -3,3 +3,5 @@
 const { APP_ORIGIN } = process.env;
 
 export const origin = APP_ORIGIN || 'https://hub.asap.science';
+export const asapFromEmail = 'no-reply@hub.asap.science';
+export const grantsFromEmail = 'grants@parkinsonsroadmap.org';

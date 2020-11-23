22a0649024e98431fa8640532c96de453f3658ec
Tim Seckinger <seckinger.tim@gmail.com>
2020-11-23 14:51:50 +0000

    fix(storybook): flag knobs for pages with multiple flags

--- apps/storybook/src/flags.tsx
+++ apps/storybook/src/flags.tsx
@@ -1,28 +1,30 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { DecoratorFn } from '@storybook/react';
 import { boolean } from '@storybook/addon-knobs';
-import type { Flag } from '@asap-hub/flags';
-import { LiveFlagsProvider, useFlags } from '@asap-hub/react-context';
+import { Flag } from '@asap-hub/flags';
+import { FlagsContext, useFlags } from '@asap-hub/react-context';
 
-const DisableFlag: React.FC<{ flag: Flag }> = ({ flag }) => {
-  const { disable, reset } = useFlags();
-  useEffect(() => {
-    disable(flag);
-    return () => {
-      reset();
-    };
-  }, [flag, disable, reset]);
-  return null;
+const DisableFlag: React.FC<{ flag: Flag }> = ({ flag, children }) => {
+  const { isEnabled, disable, reset } = useFlags();
+  return (
+    <FlagsContext.Provider
+      value={{
+        disable,
+        reset,
+        isEnabled: (f) => (f === flag ? false : isEnabled(f)),
+      }}
+    >
+      {children}
+    </FlagsContext.Provider>
+  );
 };
 export const makeFlagDecorator = (name: string, flag: Flag): DecoratorFn => (
   storyFn,
   context,
 ) => {
   const enabled = boolean(name, true);
-  return (
-    <LiveFlagsProvider>
-      {enabled || <DisableFlag flag={flag} />}
-      {storyFn({ ...context })}
-    </LiveFlagsProvider>
-  );
+  const Provider: React.ComponentType = enabled
+    ? React.Fragment
+    : ({ children }) => <DisableFlag flag={flag}>{children}</DisableFlag>;
+  return <Provider>{storyFn({ ...context })}</Provider>;
 };

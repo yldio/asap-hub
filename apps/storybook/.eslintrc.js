3e43777d2acf4d804beaf4ba4e9a0df9765367bd
Tim Seckinger <tim.seckinger@yld.io>
2020-12-04 14:47:21 +0000

    feat(toast): unclosable mode (#502)

--- apps/storybook/.eslintrc.js
+++ apps/storybook/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@asap-hub/eslint-config-asap-hub/react'],
   rules: {
+    '@typescript-eslint/no-empty-function': 'off',
     'no-console': 'off',
   },
 };
--- packages/react-components/src/organisms/__tests__/Toast.test.tsx
+++ packages/react-components/src/organisms/__tests__/Toast.test.tsx
@@ -3,18 +3,30 @@ import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Toast from '../Toast';
+import { noop } from '../../utils';
 
 it('renders the children', () => {
   const { getByText } = render(<Toast>error message</Toast>);
   expect(getByText('error message')).toBeVisible();
 });
 
-it('emits close events', () => {
-  const handleClose = jest.fn();
-  const { getByTitle } = render(
-    <Toast onClose={handleClose}>error message</Toast>,
-  );
+it('does not render a close button by default', () => {
+  const { queryByText } = render(<Toast>error message</Toast>);
+  expect(queryByText(/close/i)).not.toBeInTheDocument();
+});
+describe('when closable', () => {
+  it('renders a close button', () => {
+    const { getByTitle } = render(<Toast onClose={noop}>error message</Toast>);
+    expect(getByTitle(/close/i)).toBeInTheDocument();
+  });
 
-  userEvent.click(getByTitle(/close/i));
-  expect(handleClose).toHaveBeenCalled();
+  it('emits close events', () => {
+    const handleClose = jest.fn();
+    const { getByTitle } = render(
+      <Toast onClose={handleClose}>error message</Toast>,
+    );
+
+    userEvent.click(getByTitle(/close/i));
+    expect(handleClose).toHaveBeenCalled();
+  });
 });

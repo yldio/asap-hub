e89770c1d36c576c8a727d31dd17d49dd578829d
johnytiago <joaosao10@gmail.com>
2021-01-11 18:13:25 +0000

    refactor: replaces CMS typings with Generic

--- apps/asap-server/src/entities/news-and-events.ts
+++ apps/asap-server/src/entities/news-and-events.ts
@@ -1,41 +1,9 @@
-import { NewsOrEventResponse, NewsAndEventsType } from '@asap-hub/model';
-import { GraphqlNewsOrEvent } from '@asap-hub/squidex';
+import { NewsOrEventResponse } from '@asap-hub/model';
+import { GraphqlNewsOrEvent, RestNewsOrEvent } from '@asap-hub/squidex';
 import { parseDate, createURL } from '../utils/squidex';
 
-export interface CMSNewsAndEvents {
-  id: string;
-  created: string;
-  data: {
-    type: {
-      iv: NewsAndEventsType;
-    };
-    title: { iv: string };
-    shortText: { iv: string };
-    thumbnail: { iv: ({ id: string } | string)[] };
-    text: { iv: string };
-    link?: { iv: string };
-    linkText?: { iv: string };
-  };
-}
-
-export interface CMSGraphQLNewsAndEvents {
-  id: string;
-  created: string;
-  flatData: {
-    type: NewsAndEventsType;
-    title: string;
-    shortText: string;
-    thumbnail: {
-      id: string;
-    }[];
-    text: string;
-    link?: string;
-    linkText?: string;
-  };
-}
-
 export const parseNewsAndEvents = (
-  item: CMSNewsAndEvents,
+  item: RestNewsOrEvent,
 ): NewsOrEventResponse =>
   ({
     id: item.id,
@@ -44,17 +12,7 @@ export const parseNewsAndEvents = (
     text: item.data.text?.iv,
     link: item.data.link?.iv,
     linkText: item.data.linkText?.iv,
-    thumbnail:
-      item.data.thumbnail &&
-      createURL(
-        item.data.thumbnail?.iv.map((t) => {
-          // this handles thumbnails fetched with graphql
-          if (typeof t === 'string') {
-            return t as string;
-          }
-          return t.id;
-        }),
-      )[0],
+    thumbnail: item.data.thumbnail && createURL(item.data.thumbnail?.iv)[0],
     title: item.data.title.iv,
     type: item.data.type.iv,
   } as NewsOrEventResponse);
--- packages/squidex/src/entities/news-or-event.ts
+++ packages/squidex/src/entities/news-or-event.ts
@@ -2,17 +2,17 @@ import { Rest, Entity, Graphql } from './common';
 
 export type NewsOrEventType = 'News' | 'Event' | 'Training';
 
-interface NewsOrEvent {
+interface NewsOrEvent<TThumbnail = string> {
   type: NewsOrEventType;
   title: string;
   shortText: string;
-  thumbnail: {
-    id: string;
-  }[];
+  thumbnail: TThumbnail[];
   text: string;
   link?: string;
   linkText?: string;
 }
 
 export interface RestNewsOrEvent extends Entity, Rest<NewsOrEvent> {}
-export interface GraphqlNewsOrEvent extends Entity, Graphql<NewsOrEvent> {}
+export interface GraphqlNewsOrEvent
+  extends Entity,
+    Graphql<NewsOrEvent<{ id: string }>> {}

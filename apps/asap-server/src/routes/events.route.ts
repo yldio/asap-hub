6a2813aa509ec0ec13ef478b02560cc66e2ca0e8
Piotr Szpak <piotr.szpak@news.co.uk>
2021-01-20 15:55:57 +0000

    added events POST route

--- apps/asap-server/src/routes/events.route.ts
+++ apps/asap-server/src/routes/events.route.ts
@@ -5,3 +5,7 @@ export const eventRoutes = Router();
 eventRoutes.get('/events', async (req, res) => {
   res.json({ response: 'OK' });
 });
+
+eventRoutes.post('/events', async (req, res) => {
+  res.status(201).json({ response: 'OK' });
+});

316c85a1abfbb4e77358a56cb8e39bb39e1fe13d
Piotr Szpak <piotr.szpak@news.co.uk>
2021-01-26 16:42:29 +0000

    added fetch-groups-by-user controller tests

--- apps/asap-server/test/controllers/groups.test.ts
+++ apps/asap-server/test/controllers/groups.test.ts
@@ -21,7 +21,7 @@ describe('Group controller', () => {
   });
 
   describe('Fetch method', () => {
-    test('empty response', async () => {
+    test('Should return an empty result', async () => {
       nock(config.baseUrl)
         .post(`/api/content/${config.appName}/graphql`, {
           query: buildGraphQLQueryFetchGroups(''),
@@ -36,10 +36,10 @@ describe('Group controller', () => {
         });
 
       const result = await groups.fetch({});
-      expect(result).toStrictEqual({ items: [], total: 0 });
+      expect(result).toEqual({ items: [], total: 0 });
     });
 
-    test('returns groups - with filters', async () => {
+    test('Should query with filters and return the groups', async () => {
       const fetchOptions: FetchOptions = {
         take: 12,
         skip: 2,
@@ -62,14 +62,12 @@ describe('Group controller', () => {
         .reply(200, fixtures.response);
 
       const result = await groups.fetch(fetchOptions);
-      expect(JSON.parse(JSON.stringify(result))).toStrictEqual(
-        fixtures.expectation,
-      );
+      expect(result).toEqual(fixtures.expectation);
     });
   });
 
   describe('Fetch-by-team-ID method', () => {
-    test('empty response', async () => {
+    test('Should return an empty result', async () => {
       const teamUUID = 'eb531b6e-195c-46e2-b347-58fb86715033';
       const filter = `data/teams/iv eq '${teamUUID}'`;
 
@@ -87,7 +85,7 @@ describe('Group controller', () => {
         });
 
       const result = await groups.fetchByTeamId(teamUUID, {});
-      expect(result).toStrictEqual({ items: [], total: 0 });
+      expect(result).toEqual({ items: [], total: 0 });
     });
 
     test('returns groups - with filters', async () => {
@@ -107,9 +105,73 @@ describe('Group controller', () => {
         .reply(200, fixtures.response);
 
       const result = await groups.fetchByTeamId(teamUUID, fetchOptions);
-      expect(JSON.parse(JSON.stringify(result))).toStrictEqual(
-        fixtures.expectation,
+      expect(result).toEqual(fixtures.expectation);
+    });
+  });
+
+  describe('Fetch-by-user-ID method', () => {
+    test('Should apply the filters and return an empty response', async () => {
+      const userUUID = 'eb531b6e-195c-46e2-b347-58fb86715033';
+
+      const userFilter = `data/leaders/iv/user eq '${userUUID}'`;
+      const teamFilter = `data/teams/iv in ['team-id-1', 'team-id-3']`;
+
+      nock(config.baseUrl)
+        .post(`/api/content/${config.appName}/graphql`, {
+          query: buildGraphQLQueryFetchGroups(userFilter),
+        })
+        .reply(200, {
+          data: {
+            queryGroupsContentsWithTotal: {
+              total: 0,
+              items: [],
+            },
+          },
+        })
+        .post(`/api/content/${config.appName}/graphql`, {
+          query: buildGraphQLQueryFetchGroups(teamFilter),
+        })
+        .reply(200, {
+          data: {
+            queryGroupsContentsWithTotal: {
+              total: 0,
+              items: [],
+            },
+          },
+        });
+
+      const result = await groups.fetchByUserId(
+        userUUID,
+        ['team-id-1', 'team-id-3'],
+        {},
       );
+      expect(result).toEqual({ items: [], total: 0 });
+    });
+
+    test('Should return the deduped result', async () => {
+      const userUUID = 'user-id-1';
+
+      const userFilter = `data/leaders/iv/user eq '${userUUID}'`;
+      const teamFilter = `data/teams/iv in ['team-id-1', 'team-id-3']`;
+
+      // same response since the user is group leader and member a team
+      nock(config.baseUrl)
+        .post(`/api/content/${config.appName}/graphql`, {
+          query: buildGraphQLQueryFetchGroups(userFilter),
+        })
+        .reply(200, fixtures.response)
+        .post(`/api/content/${config.appName}/graphql`, {
+          query: buildGraphQLQueryFetchGroups(teamFilter),
+        })
+        .reply(200, fixtures.response);
+
+      const result = await groups.fetchByUserId(
+        userUUID,
+        ['team-id-1', 'team-id-3'],
+        {},
+      );
+
+      expect(result).toEqual(fixtures.expectation);
     });
   });
 });

288e3a2011e7ace746d9963bf9f9457d38e95aaf
Joao Tiago <joao.tiago@yld.io>
2021-01-29 11:34:00 +0000

    feat: add rule and fix templating of rule values (#593)

--- ci/config-squidex-rules.sh
+++ ci/config-squidex-rules.sh
@@ -1,5 +1,11 @@
-find dev/squidex/rules -type f \( -name "*.json" -not -name "__rule.json" \) -print0 | xargs -t -0 -I @file \
-  sed -i '/sharedSecret/c\    \"sharedSecret\" : \"'$SQUIDEX_SHARED_SECRET'\",' @file
+# Replace PLACEHOLDERS with variable available on the CI
+find packages/squidex/schema/rules \
+  -type f \( -name "*.json" -not -name "__rule.json" \) \
+  -print0 | xargs -t -0 -I @file \
+  sed -i 's/SQUIDEX_SHARED_SECRET/'"$SQUIDEX_SHARED_SECRET"'/' @file
 
-find dev/squidex/rules -type f \( -name "*.json" -not -name "__rule.json" \) -print0 | xargs -t -0 -I @file \
-  sed -i -E 's#https?\:\/\/(www\.)?[[:alpha:]|.]+#ASAP_API_URL#' @file
+# Replace PLACEHOLDERS with variable available on the CI
+find packages/squidex/schema/rules \
+  -type f \( -name "*.json" -not -name "__rule.json" \) \
+  -print0 | xargs -t -0 -I @file \
+  sed -i 's/ASAP_API_URL/'"$ASAP_API_URL"'/' @file

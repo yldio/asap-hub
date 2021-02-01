9c55fbeee9eb6711c554addf890c3d07f85a8bb2
Piotr Szpak <piotr.szpak@news.co.uk>
2021-02-01 13:57:38 +0000

    added dashboard route

--- apps/asap-server/test/controllers/dashboard.test.ts
+++ apps/asap-server/test/controllers/dashboard.test.ts
@@ -2,6 +2,7 @@ import nock from 'nock';
 import Dashboard from '../../src/controllers/dashboard';
 import { config } from '@asap-hub/squidex';
 import { identity } from '../helpers/squidex';
+import { dashboardResponse } from "../fixtures/dashboard.fixtures";
 
 describe('Dashboard controller', () => {
   const dashboard = new Dashboard();
@@ -88,33 +89,9 @@ describe('Dashboard controller', () => {
           },
         });
 
-      const expectedItems = [
-        {
-          id: 'news-and-events-1',
-          title: 'News 1',
-          type: 'News',
-          shortText: 'Short text of news 1',
-          text: '<p>text</p>',
-          thumbnail: `${config.baseUrl}/api/assets/${config.appName}/thumbnail-uuid1`,
-          created: '2020-09-08T16:35:28.000Z',
-        },
-        {
-          id: 'news-and-events-2',
-          title: 'Event 2',
-          type: 'Event',
-          shortText: 'Short text of event 2',
-          text: '<p>text</p>',
-          thumbnail: `${config.baseUrl}/api/assets/${config.appName}/thumbnail-uuid2`,
-          created: '2020-09-16T14:31:19.000Z',
-        },
-      ];
-
       const result = await dashboard.fetch();
 
-      expect(result).toEqual({
-        newsAndEvents: expectedItems,
-        pages: [],
-      });
+      expect(result).toEqual(dashboardResponse);
     });
   });
 });

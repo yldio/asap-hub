7167d839811d18d29901c28d428aa83912bd9ce0
Piotr Szpak <piotr.szpak@news.co.uk>
2021-02-02 16:39:29 +0000

    added /calendars route

---
+++ apps/asap-server/src/routes/calendars.route.ts
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+import { framework } from '@asap-hub/services-common';
+import Joi from '@hapi/joi';
+import { CalendarController } from '../controllers/calendars';
+
+export const calendarRouteFactory = (
+  calendarController: CalendarController,
+): Router => {
+  const calendarRoutes = Router();
+
+  calendarRoutes.get('/calendars', async (req, res) => {
+    const parameters = req.query;
+
+    const query = (framework.validate(
+      'query',
+      parameters,
+      querySchema,
+    ) as unknown) as {
+      take: number;
+      skip: number;
+    };
+
+    const result = await calendarController.fetch(query);
+
+    res.json(result);
+  });
+
+  return calendarRoutes;
+};
+
+const querySchema = Joi.object({
+  take: Joi.number(),
+  skip: Joi.number(),
+}).required();
---
+++ apps/asap-server/test/routes/calendars.test.ts
@@ -0,0 +1,78 @@
+import supertest from 'supertest';
+import { appFactory } from '../../src/app';
+import { authHandlerMock } from '../mocks/auth-handler.mock';
+import { calendarControllerMock } from '../mocks/calendar-controller.mock';
+import { ListCalendarResponse } from '@asap-hub/model';
+
+describe('/calendars/ route', () => {
+  const app = appFactory({
+    calendarController: calendarControllerMock,
+    authHandler: authHandlerMock,
+  });
+
+  describe('GET /calendars', () => {
+    test('Should return 200 when no calendars are found', async () => {
+      calendarControllerMock.fetch.mockResolvedValueOnce({
+        total: 0,
+        items: [],
+      });
+
+      const response = await supertest(app).get('/calendars');
+
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual({ total: 0, items: [] });
+    });
+
+    test('Should return the results correctly', async () => {
+      calendarControllerMock.fetch.mockResolvedValueOnce(listCalendarResponse);
+
+      const response = await supertest(app).get('/calendars');
+
+      expect(response.body).toEqual(listCalendarResponse);
+    });
+
+    test('Should call the controller with the right parameters', async () => {
+      calendarControllerMock.fetch.mockResolvedValueOnce({
+        items: [],
+        total: 0,
+      });
+
+      await supertest(app).get('/calendars/').query({
+        take: 15,
+        skip: 5,
+      });
+
+      const expectedParams = {
+        take: 15,
+        skip: 5,
+      };
+      expect(calendarControllerMock.fetch).toBeCalledWith(expectedParams);
+    });
+
+    describe('Parameter validation', () => {
+      test('Should return a validation error when the arguments are not valid', async () => {
+        const response = await supertest(app).get('/calendars/').query({
+          take: 'invalid param',
+        });
+
+        expect(response.status).toBe(400);
+      });
+    });
+  });
+});
+
+const listCalendarResponse: ListCalendarResponse = {
+  total: 2,
+  items: [
+    {
+      id: 'calendar-id-1',
+      color: '#5C1158',
+      name: 'Kubernetes Meetups',
+    },
+    {
+      id: 'calendar-id-2',
+      color: '#B1365F',
+      name: 'Service Mesh Conferences',
+    },
+  ],
+};

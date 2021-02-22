b4d2c0be2bf4b75788cc8a58a05807139db41d59
Filipe Pinheiro <fampinheiro@users.noreply.github.com>
2021-02-22 13:27:28 +0000

    feat: remove welcome invite validation (#654)

--- apps/frontend/src/welcome/Welcome.tsx
+++ apps/frontend/src/welcome/Welcome.tsx
@@ -1,35 +1,32 @@
-import React, { useEffect } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
+import React from 'react';
+import { useParams } from 'react-router-dom';
 import { WelcomePage } from '@asap-hub/react-components';
 import { useAuth0 } from '@asap-hub/react-context';
 
-import { API_BASE_URL } from '../config';
-
 const Welcome: React.FC<Record<string, never>> = () => {
-  const history = useHistory();
   const { code } = useParams<{ code: string }>();
 
   const { loginWithRedirect } = useAuth0();
 
   // Check for an invalid code - fire and forget and don't delay rendering since
   // this is not security-critical, it's just nicer to fail now instead of after registration.
-  useEffect(() => {
-    const requestController = new AbortController();
-    fetch(`${API_BASE_URL}/users/invites/${code}`, {
-      headers: {
-        accept: 'application/json',
-      },
-      signal: requestController.signal,
-    })
-      .then(({ status }) => {
-        if (status >= 400 && status < 500) {
-          history.replace('/');
-        }
-      })
-      .catch();
-
-    return () => requestController.abort();
-  }, [history, code]);
+  // useEffect(() => {
+  //   const requestController = new AbortController();
+  //   fetch(`${API_BASE_URL}/users/invites/${code}`, {
+  //     headers: {
+  //       accept: 'application/json',
+  //     },
+  //     signal: requestController.signal,
+  //   })
+  //     .then(({ status }) => {
+  //       if (status >= 400 && status < 500) {
+  //         history.replace('/');
+  //       }
+  //     })
+  //     .catch();
+  //
+  //   return () => requestController.abort();
+  // }, [history, code]);
 
   const createAccount = () => {
     loginWithRedirect({

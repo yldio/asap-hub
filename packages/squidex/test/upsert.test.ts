ae8b0b34d148474175eb31345fc7f22dc6d78c52
Joao Tiago <joao.tiago@yld.io>
2021-03-01 08:46:11 +0000

    fix: upsert updates event when doesnt it exist (#676)

--- packages/squidex/test/upsert.test.ts
+++ packages/squidex/test/upsert.test.ts
@@ -26,7 +26,7 @@ describe('squidex wrapper - upsert', () => {
 
   it('returns 400 when squidex returns bad request', async () => {
     nock(config.baseUrl)
-      .post(`/api/content/${config.appName}/${collection}/${contentId}`)
+      .patch(`/api/content/${config.appName}/${collection}/${contentId}`)
       .query(() => true)
       .reply(400, {
         details: ['Request  body has an invalid format'],
@@ -52,7 +52,7 @@ describe('squidex wrapper - upsert', () => {
 
   it('returns 403 when squidex returns with credentials error', async () => {
     nock(config.baseUrl)
-      .post(`/api/content/${config.appName}/${collection}/${contentId}`)
+      .patch(`/api/content/${config.appName}/${collection}/${contentId}`)
       .query(() => true)
       .reply(400, {
         details: 'invalid_client',
@@ -72,7 +72,7 @@ describe('squidex wrapper - upsert', () => {
 
   it('returns 409 when squidex returns conflict', async () => {
     nock(config.baseUrl)
-      .post(`/api/content/${config.appName}/${collection}/${contentId}`)
+      .patch(`/api/content/${config.appName}/${collection}/${contentId}`)
       .query(() => true)
       .reply(409, {
         details: 'user with same email already exists',
@@ -92,7 +92,7 @@ describe('squidex wrapper - upsert', () => {
 
   it('returns 500 when squidex returns error', async () => {
     nock(config.baseUrl)
-      .post(`/api/content/${config.appName}/${collection}/${contentId}`)
+      .patch(`/api/content/${config.appName}/${collection}/${contentId}`)
       .query(() => true)
       .reply(500);
     const client = new Squidex<Content>(collection);
@@ -108,7 +108,7 @@ describe('squidex wrapper - upsert', () => {
 
   it('upserts a specific document as published', async () => {
     nock(config.baseUrl)
-      .post(
+      .patch(
         `/api/content/${config.appName}/${collection}/${contentId}?publish=true`,
         {
           string: {
@@ -144,7 +144,7 @@ describe('squidex wrapper - upsert', () => {
 
   it('upserts a specific document as draft', async () => {
     nock(config.baseUrl)
-      .post(
+      .patch(
         `/api/content/${config.appName}/${collection}/${contentId}?publish=false`,
         {
           string: {

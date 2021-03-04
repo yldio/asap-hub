4f44b55c564f4ba9ff8c9bf521c08cca070950f0
Thomas Waterfall <thomas.waterfall@gmail.com>
2021-03-04 15:00:50 +0000

    PR Amends

--- apps/storybook/src/EventInfo.stories.tsx
+++ apps/storybook/src/EventInfo.stories.tsx
@@ -1,6 +1,6 @@
 import React, { ComponentProps } from 'react';
 import { EventInfo } from '@asap-hub/react-components';
-import { date, number, text } from '@storybook/addon-knobs';
+import { date, number, text, boolean } from '@storybook/addon-knobs';
 import { createGroupResponse } from '@asap-hub/fixtures';
 
 export default {
@@ -9,10 +9,11 @@ export default {
 };
 
 const props = (): ComponentProps<typeof EventInfo> => ({
-  thumbnail: text('Thumbnail', 'https://via.placeholder.com/150'),
+  thumbnail: text('Thumbnail', 'https://placekitten.com/150/150'),
   startDate: new Date(
     date('Start Date', new Date(2021, 8, 6, 18)),
   ).toISOString(),
+  titleLimit: boolean('Title unlimited', false) ? null : undefined,
   endDate: new Date(date('End Date', new Date(2021, 8, 6, 20))).toISOString(),
   title: text('Title', 'GBA/LRRK2 Convergence workshops'),
   groups: Array.from({ length: number('Group Count', 1) }).map((_, index) => ({

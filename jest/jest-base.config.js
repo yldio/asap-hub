4fb1f577c02d8bcced08292205de9c6e3e3d8495
Piotr S <piotr.lukasz.szpak@gmail.com>
2021-03-10 10:01:57 +0000

    chore(devops): add e2e verification step before prod deployment (#705)
    * add dependency for the prod deployment
    * ignore e2e tests from basic test run

--- jest/jest-base.config.js
+++ jest/jest-base.config.js
@@ -6,6 +6,7 @@ const root = dirname(require.resolve('asap-hub/package.json'), '..');
 const testPathIgnorePatterns = [
   '\\.build-output-test\\.(js|jsx|ts|tsx)$',
   '\\.browser-test\\.(js|jsx|ts|tsx)$',
+  '\\.e2e\\.(js|jsx|ts|tsx)$',
 ];
 
 module.exports = {

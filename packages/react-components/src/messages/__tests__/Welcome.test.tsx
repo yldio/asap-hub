cecd05f4fd0c4de5d79b1cb37f03b1704a078250
Miguel Carvalho <migcarva@users.noreply.github.com>
2021-03-19 15:50:19 +0000

    feat: change welcome email's copy (#751)

---
+++ packages/react-components/src/messages/__tests__/Welcome.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render, RenderResult } from '@testing-library/react';
+
+import Welcome from '../Welcome';
+
+describe('welcome email template', () => {
+  let result!: RenderResult;
+
+  beforeEach(() => {
+    result = render(
+      <Welcome
+        firstName="John Doe"
+        link="https://example.com"
+        privacyPolicyHref={'https://hub.asap.science/privacy-policy'}
+        termsHref={'https://hub.asap.science/terms-and-conditions'}
+      />,
+    );
+  });
+
+  it('renders the correct grantee name', () => {
+    const heading = result.getByRole('heading');
+    expect(heading.textContent).toContain('John Doe');
+  });
+
+  it('renders the correct link on the Activate Account CTA', () => {
+    const cta = result.getByText(/activate account/i);
+    expect(cta.closest('a')).toHaveAttribute('href', 'https://example.com');
+  });
+
+  it('renders the correct mail to on the get in touch mailto link', () => {
+    const mailToSupport = result.getByRole('link', {
+      name: /get in touch/i,
+    }) as HTMLAnchorElement;
+
+    expect(mailToSupport.protocol).toBe('mailto:');
+    expect(mailToSupport.pathname).toBe('techsupport@asap.science');
+  });
+});

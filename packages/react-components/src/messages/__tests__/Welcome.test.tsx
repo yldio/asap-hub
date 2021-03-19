66e88165464326459e3216c6c38b692537d83bff
Miguel Carvalho <emece.pt@gmail.com>
2021-03-19 14:40:28 +0000

    Adds one logic unit check per test case

--- packages/react-components/src/messages/__tests__/Welcome.test.tsx
+++ packages/react-components/src/messages/__tests__/Welcome.test.tsx
@@ -1,28 +1,40 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
 
 import Welcome from '../Welcome';
 
-it('renders the welcome template with name and link', () => {
-  const { getByRole } = render(
-    <Welcome
-      firstName="John Doe"
-      link="https://example.com"
-      privacyPolicyHref={'https://hub.asap.science/privacy-policy'}
-      termsHref={'https://hub.asap.science/terms-and-conditions'}
-    />,
-  );
+describe('welcome email template', () => {
+  let result!: RenderResult;
 
-  const heading = getByRole('heading');
-  const cta = getByRole('link', {
-    name: /activate account/i,
-  }) as HTMLAnchorElement;
-  const mailToSupport = getByRole('link', {
-    name: /get in touch/i,
-  }) as HTMLAnchorElement;
+  beforeEach(() => {
+    result = render(
+      <Welcome
+        firstName="John Doe"
+        link="https://example.com"
+        privacyPolicyHref={'https://hub.asap.science/privacy-policy'}
+        termsHref={'https://hub.asap.science/terms-and-conditions'}
+      />,
+    );
+  });
 
-  expect(heading.textContent).toContain('John Doe');
-  expect(cta.href).toBe('https://example.com/');
-  expect(mailToSupport.protocol).toBe('mailto:');
-  expect(mailToSupport.pathname).toBe('techsupport@asap.science');
+  it('renders the correct grantee name', () => {
+    const heading = result.getByRole('heading');
+    expect(heading.textContent).toContain('John Doe');
+  });
+
+  it('renders the correct link on the Activate Account CTA', () => {
+    const cta = result.getByRole('link', {
+      name: /activate account/i,
+    }) as HTMLAnchorElement;
+    expect(cta.href).toBe('https://example.com/');
+  });
+
+  it('renders the correct mail to on the get in touch mailto link', () => {
+    const mailToSupport = result.getByRole('link', {
+      name: /get in touch/i,
+    }) as HTMLAnchorElement;
+
+    expect(mailToSupport.protocol).toBe('mailto:');
+    expect(mailToSupport.pathname).toBe('techsupport@asap.science');
+  });
 });

f03057a15678ee4243e12b5596433de437ddc53b
Miguel Carvalho <emece.pt@gmail.com>
2021-03-24 11:35:25 +0000

    feat: add main navigation check active tests

--- packages/react-components/src/organisms/__tests__/MainNavigation.test.tsx
+++ packages/react-components/src/organisms/__tests__/MainNavigation.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StaticRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 
 import MainNavigation from '../MainNavigation';
@@ -14,4 +15,89 @@ it('renders the navigation items', () => {
     expect.stringMatching(/calendar/i),
     expect.stringMatching(/discover/i),
   ]);
+});
+
+describe('Main Navigation shows correct active item when', () => {
+  const renderWithLocation = (location: string) =>
+    render(
+      <StaticRouter location={location}>
+        <MainNavigation />
+      </StaticRouter>,
+    );
+
+  it('the location matches a menu item href path', () => {
+    const { getByRole } = renderWithLocation('/network');
+
+    const networkLink = getByRole('link', {
+      name: /network/i,
+    }) as HTMLAnchorElement;
+    const researchLink = getByRole('link', {
+      name: /research/i,
+    }) as HTMLAnchorElement;
+    const newsLink = getByRole('link', {
+      name: /news/i,
+    }) as HTMLAnchorElement;
+    const calendarLink = getByRole('link', {
+      name: /calendar/i,
+    }) as HTMLAnchorElement;
+    const discoverLink = getByRole('link', {
+      name: /discover/i,
+    }) as HTMLAnchorElement;
+
+    expect(networkLink.classList.contains('active-link')).toBe(true);
+    expect(researchLink.classList.contains('active-link')).toBe(false);
+    expect(newsLink.classList.contains('active-link')).toBe(false);
+    expect(calendarLink.classList.contains('active-link')).toBe(false);
+    expect(discoverLink.classList.contains('active-link')).toBe(false);
+  });
+
+  it('the location matches a menu item href "CHILDS" path', () => {
+    const { getByRole } = renderWithLocation(
+      '/events/6021f36e-2931-418a-8179-f93c1e185772',
+    );
+
+    const networkLink = getByRole('link', {
+      name: /network/i,
+    }) as HTMLAnchorElement;
+    const researchLink = getByRole('link', {
+      name: /research/i,
+    }) as HTMLAnchorElement;
+    const newsLink = getByRole('link', { name: /news/i }) as HTMLAnchorElement;
+    const calendarLink = getByRole('link', {
+      name: /calendar/i,
+    }) as HTMLAnchorElement;
+    const discoverLink = getByRole('link', {
+      name: /discover/i,
+    }) as HTMLAnchorElement;
+
+    expect(networkLink.classList.contains('active-link')).toBe(false);
+    expect(researchLink.classList.contains('active-link')).toBe(false);
+    expect(newsLink.classList.contains('active-link')).toBe(false);
+    expect(calendarLink.classList.contains('active-link')).toBe(true);
+    expect(discoverLink.classList.contains('active-link')).toBe(false);
+  });
+
+  it('the location DO NOT matches a menu item href path', () => {
+    const { getByRole } = renderWithLocation('/');
+
+    const networkLink = getByRole('link', {
+      name: /network/i,
+    }) as HTMLAnchorElement;
+    const researchLink = getByRole('link', {
+      name: /research/i,
+    }) as HTMLAnchorElement;
+    const newsLink = getByRole('link', { name: /news/i }) as HTMLAnchorElement;
+    const calendarLink = getByRole('link', {
+      name: /calendar/i,
+    }) as HTMLAnchorElement;
+    const discoverLink = getByRole('link', {
+      name: /discover/i,
+    }) as HTMLAnchorElement;
+
+    expect(networkLink.classList.contains('active-link')).toBe(false);
+    expect(researchLink.classList.contains('active-link')).toBe(false);
+    expect(newsLink.classList.contains('active-link')).toBe(false);
+    expect(calendarLink.classList.contains('active-link')).toBe(false);
+    expect(discoverLink.classList.contains('active-link')).toBe(false);
+  });
 });

6559b3ca172ab5aed87de664462569a4c2bdeb12
Tim Seckinger <seckinger.tim@gmail.com>
2021-03-24 12:18:54 +0100

    cleanup duplicate URL

--- apps/messages/src/templates/welcome.tsx
+++ apps/messages/src/templates/welcome.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { MessageLayout, WelcomeMessage } from '@asap-hub/react-components';
 import { welcome } from '@asap-hub/message-templates';
+import { URL } from 'url';
+
+import { APP_ORIGIN } from '../config';
 
 export default (
   <MessageLayout
-    privacyPolicyHref={'https://hub.asap.science/privacy-policy'}
-    termsHref={'https://hub.asap.science/terms-and-conditions'}
+    privacyPolicyHref={new URL('/privacy-policy', APP_ORIGIN).href}
+    termsHref={new URL('/terms-and-conditions', APP_ORIGIN).href}
   >
     <WelcomeMessage {...welcome} />
   </MessageLayout>

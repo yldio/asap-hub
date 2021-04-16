1e7af5cc4b915ca5f389c0e6897147c7513c6085
Piotr Szpak <piotr.szpak@news.co.uk>
2021-04-16 14:40:55 +0100

    added the migration

---
+++ apps/asap-server/src/migrations/1618575365-move-short-text-to-admin-notes.ts
@@ -0,0 +1,56 @@
+/* istanbul ignore file */
+import { RestResearchOutput, Results, Squidex } from '@asap-hub/squidex';
+import { Migration } from '../handlers/webhooks/webhook-run-migrations';
+
+export default class MoveResearchOutputTextToDescription extends Migration {
+  up = async (): Promise<void> => {
+    const squidexClient = new Squidex<RestResearchOutput>('research-outputs', {
+      unpublished: true,
+    });
+
+    let pointer = 0;
+    let result: Results<RestResearchOutput>;
+
+    do {
+      result = await squidexClient.fetch({
+        $top: 10,
+        $skip: pointer,
+        $orderby: 'created asc',
+      });
+
+      for (const researchOutput of result.items) {
+        await squidexClient.patch(researchOutput.id, {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          adminNotes: (researchOutput.data as any).shortText,
+        });
+      }
+
+      pointer += 10;
+    } while (pointer < result.total);
+  };
+  down = async (): Promise<void> => {
+    const squidexClient = new Squidex<RestResearchOutput>('research-outputs', {
+      unpublished: true,
+    });
+
+    let pointer = 0;
+    let result: Results<RestResearchOutput>;
+
+    do {
+      result = await squidexClient.fetch({
+        $top: 10,
+        $skip: pointer,
+        $orderby: 'created asc',
+      });
+
+      for (const researchOutput of result.items) {
+        await squidexClient.patch(researchOutput.id, {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          adminNotes: null as any, // types dont cover null values
+        });
+      }
+
+      pointer += 10;
+    } while (pointer < result.total);
+  };
+}

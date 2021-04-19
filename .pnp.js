a64bd60c810d0b13ef9ed3143be5d0735977a5a4
Piotr Szpak <piotr.szpak@news.co.uk>
2021-04-19 15:10:04 +0100

    moved hapi/joi to asap server

--- .pnp.js
+++ .pnp.js
@@ -920,7 +920,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
       ],
       [
         "@hapi/joi",
-        "npm:17.1.1"
+        "npm:16.1.8"
       ],
       [
         "@hapi/mimos",
@@ -964,7 +964,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
       ],
       [
         "@hapi/topo",
-        "npm:5.0.0"
+        "npm:3.1.6"
       ],
       [
         "@hapi/vise",
@@ -9358,7 +9358,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@babel/preset-typescript", "virtual:d0e958afa23d83fcbe1e8341a17fed5245116729be9039e4a7f723d5645a3b4a0db7dc084c7f30fa2114faa4c170592f819708d736b1e487b4ae7cd58824e005#npm:7.12.7"],
             ["@babel/runtime-corejs3", "npm:7.12.5"],
             ["@emotion/jest", "virtual:d0e958afa23d83fcbe1e8341a17fed5245116729be9039e4a7f723d5645a3b4a0db7dc084c7f30fa2114faa4c170592f819708d736b1e487b4ae7cd58824e005#npm:11.1.0"],
-            ["@hapi/joi", "npm:17.1.1"],
             ["@jeysal/babel-plugin-ramda", "npm:2.0.1"],
             ["@peculiar/webcrypto", "npm:1.1.1"],
             ["@serverless/typescript", "npm:2.34.0"],
@@ -23428,7 +23427,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@babel/preset-typescript", "virtual:d0e958afa23d83fcbe1e8341a17fed5245116729be9039e4a7f723d5645a3b4a0db7dc084c7f30fa2114faa4c170592f819708d736b1e487b4ae7cd58824e005#npm:7.12.7"],
             ["@babel/runtime-corejs3", "npm:7.12.5"],
             ["@emotion/jest", "virtual:d0e958afa23d83fcbe1e8341a17fed5245116729be9039e4a7f723d5645a3b4a0db7dc084c7f30fa2114faa4c170592f819708d736b1e487b4ae7cd58824e005#npm:11.1.0"],
-            ["@hapi/joi", "npm:17.1.1"],
             ["@jeysal/babel-plugin-ramda", "npm:2.0.1"],
             ["@peculiar/webcrypto", "npm:1.1.1"],
             ["@serverless/typescript", "npm:2.34.0"],

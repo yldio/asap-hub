0eb381881780d1bb1ff9d2847a8511a7a23b5c4f
Joao Tiago <joao.tiago@yld.io>
2021-05-07 15:33:49 +0100

    chore(config): app origin default points to dev (#847)

--- apps/asap-server/src/config.ts
+++ apps/asap-server/src/config.ts
@@ -4,7 +4,6 @@ const {
   APP_ORIGIN,
   AUTH0_SHARED_SECRET,
   AWS_SES_ENDPOINT,
-  GLOBAL_TOKEN,
   SQUIDEX_SHARED_SECRET,
   LIGHTSTEP_TOKEN,
   ENVIRONMENT,
@@ -17,8 +16,7 @@ const {
   NODE_ENV,
 } = process.env;
 
-export const globalToken = GLOBAL_TOKEN || 'change_me_when_we_have_admins';
-export const origin = APP_ORIGIN || 'http://localhost:3000';
+export const origin = APP_ORIGIN || 'https://dev.hub.asap.science';
 export const sesEndpoint = AWS_SES_ENDPOINT;
 export const lightstepToken = LIGHTSTEP_TOKEN;
 export const environment = ENVIRONMENT

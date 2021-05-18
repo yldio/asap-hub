373b081320f91b278e6704bfbf536dac5d968b56
Thomas Waterfall <Thomas.Waterfall@gmail.com>
2021-05-18 17:24:24 +0100

    Profile skills placeholder (#858)
    Co-authored-by: Tim Seckinger <tim.seckinger@yld.io>

--- packages/react-components/src/organisms/ProfileSkills.tsx
+++ packages/react-components/src/organisms/ProfileSkills.tsx
@@ -4,6 +4,7 @@ import { UserProfileContext } from '@asap-hub/react-context';
 import { Card, Headline2, Paragraph } from '../atoms';
 import { TagList } from '../molecules';
 import UserProfilePlaceholderCard from './UserProfilePlaceholderCard';
+import { perRem } from '../pixels';
 
 type ProfileSkillsProps = {
   readonly skills: string[];
@@ -25,7 +26,31 @@ const ProfileSkills: React.FC<ProfileSkillsProps> = ({
       ) : (
         <Paragraph>{skillsDescription}</Paragraph>
       )}
-      <TagList tags={skills} />
+      {skills.length ? (
+        <TagList tags={skills} />
+      ) : (
+        <div css={{ marginTop: `${24 / perRem}em` }}>
+          <UserProfilePlaceholderCard title="Add tags to your profile">
+            Help other researchers find you by adding tags about your skills,
+            techniques, resources, and tools. Completing this section will make
+            you more likely to come up in search results.
+            <TagList
+              tags={[
+                'Structural biology',
+                'Autophagy',
+                'Transcriptional regulation',
+                'Electron Microscopy',
+                'Multi-omics',
+                'Long-read sequencing',
+                'LRRK2',
+                'Alpha-synuclein',
+                'VPS35',
+              ]}
+              enabled={false}
+            />
+          </UserProfilePlaceholderCard>
+        </div>
+      )}
     </Card>
   ) : null;
 };

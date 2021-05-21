f85b0e9252dc67acd6edf7ee6c9fb975c768e30e
Piotr Szpak <piotr.szpak@news.co.uk>
2021-05-21 14:20:36 +0100

    updated user integration test

--- apps/asap-server/test/integration/fetch-users.integration-test.ts
+++ apps/asap-server/test/integration/fetch-users.integration-test.ts
@@ -1,4 +1,5 @@
-import { Role } from '@asap-hub/model';
+import { Role, UserResponse } from '@asap-hub/model';
+import { User } from '@asap-hub/squidex';
 import Users from '../../src/controllers/users';
 import { createUser } from '../helpers/users';
 
@@ -6,7 +7,7 @@ const users = new Users();
 
 describe('Users', () => {
   test('Should create and fetch a user', async () => {
-    const user = {
+    const user: Partial<User> = {
       firstName: 'John',
       lastName: 'Doe',
       jobTitle: 'Project Manager',
@@ -14,29 +15,31 @@ describe('Users', () => {
       institution: 'Instituto Superior Tecnico',
       email: 'john.doe@asap.science',
       role: 'Grantee' as Role,
+      degree: 'MPH',
     };
 
     await createUser(user);
 
     const result = await users.fetch({});
 
+    const expectedResponse: Partial<UserResponse> = {
+      firstName: 'John',
+      lastName: 'Doe',
+      displayName: 'John Doe',
+      jobTitle: 'Project Manager',
+      orcid: '0000-0002-9079-593X',
+      institution: 'Instituto Superior Tecnico',
+      email: 'john.doe@asap.science',
+      role: 'Grantee',
+      degree: 'MPH',
+      teams: [],
+      questions: [],
+      skills: [],
+    };
+
     expect(result).toEqual({
       total: 1,
-      items: [
-        expect.objectContaining({
-          firstName: 'John',
-          lastName: 'Doe',
-          displayName: 'John Doe',
-          jobTitle: 'Project Manager',
-          orcid: '0000-0002-9079-593X',
-          institution: 'Instituto Superior Tecnico',
-          email: 'john.doe@asap.science',
-          role: 'Grantee',
-          teams: [],
-          questions: [],
-          skills: [],
-        }),
-      ],
+      items: [expect.objectContaining(expectedResponse)],
     });
   });
 });

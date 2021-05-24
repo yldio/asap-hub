2e10ac61009185f5b14324f5e06395af96dc38b7
Tim Seckinger <seckinger.tim@gmail.com>
2021-05-24 18:23:15 +0100

    feat(shared-research-output): additional information

--- packages/react-components/src/templates/__tests__/SharedResearchOutput.test.tsx
+++ packages/react-components/src/templates/__tests__/SharedResearchOutput.test.tsx
@@ -77,3 +77,44 @@ it('displays access instructions when data provided', () => {
   expect(getByText(/access instructions/i)).toBeVisible();
   expect(getByText(/some data/i)).toBeVisible();
 });
+
+describe('additional information', () => {
+  it('contains the sharing status', () => {
+    const { getByText } = render(
+      <SharedResearchOutput {...props} sharingStatus="Network Only" />,
+    );
+    expect(getByText('Network Only')).toBeVisible();
+  });
+
+  it('contains the publication use status', () => {
+    const { getByText, rerender } = render(
+      <SharedResearchOutput {...props} usedInPublication />,
+    );
+    expect(getByText(/used in pub/i).closest('li')).toHaveTextContent(/yes/i);
+
+    rerender(<SharedResearchOutput {...props} usedInPublication={false} />);
+    expect(getByText(/used in pub/i).closest('li')).toHaveTextContent(/no/i);
+  });
+  it('omits the publication use status if unknown', () => {
+    const { queryByText } = render(
+      <SharedResearchOutput {...props} usedInPublication={undefined} />,
+    );
+    expect(queryByText(/used in pub/i)).not.toBeInTheDocument();
+  });
+
+  it('contains the ASAP funding status', () => {
+    const { getByText, rerender } = render(
+      <SharedResearchOutput {...props} asapFunded />,
+    );
+    expect(getByText(/asap.funded/i).closest('li')).toHaveTextContent(/yes/i);
+
+    rerender(<SharedResearchOutput {...props} asapFunded={false} />);
+    expect(getByText(/asap.funded/i).closest('li')).toHaveTextContent(/no/i);
+  });
+  it('omits the ASAP funding status if unknown', () => {
+    const { queryByText } = render(
+      <SharedResearchOutput {...props} asapFunded={undefined} />,
+    );
+    expect(queryByText(/asap.funded/i)).not.toBeInTheDocument();
+  });
+});

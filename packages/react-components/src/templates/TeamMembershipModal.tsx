da98705b7b8a00d5dfe34044cbe4c151405072b2
Miguel Carvalho <emece.pt@gmail.com>
2021-06-11 15:05:37 +0100

    Remove commented prop

--- packages/react-components/src/templates/TeamMembershipModal.tsx
+++ packages/react-components/src/templates/TeamMembershipModal.tsx
@@ -100,9 +100,6 @@ const TeamMembershipModal: React.FC<TeamMembershipModalProps> = ({
               required
               title="Your responsibilities"
               placeholder="Example: Randy will be responsible for applying basic principles he developed from studies of a simple eukaryote, yeast, to investigate the mechanisms of intracellular vesicular transport and biogenesis of extracellular vesicles (exosomes) in cultured human cells. His team's current work is devoted to understanding how proteins and RNA are sorted into extracellular vesicles and how these molecules may be delivered to target cells in relation to normal and pathological functions."
-              // customValidationMessage={
-              //   !newResponsibilities ? 'Please add your responsibilities.' : ''
-              // }
               getValidationMessage={() => 'Please add your responsibilities.'}
               maxLength={500}
               enabled={!isSaving}

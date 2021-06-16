ad11854d94171cda2d2b77a5c8bad1ff9aa733e8
Piotr Szpak <piotr.szpak@news.co.uk>
2021-06-16 14:16:22 +0100

    added test error page REMOVE THIS COMMIT

--- apps/asap-server/src/routes/pages.route.ts
+++ apps/asap-server/src/routes/pages.route.ts
@@ -4,6 +4,14 @@ import { PageController } from '../controllers/pages';
 export const pageRouteFactory = (pageController: PageController): Router => {
   const pageRoutes = Router();
 
+  pageRoutes.get('/pages/error', async () => {
+    throw new Error('some error');
+  });
+
+  pageRoutes.get('/pages/error2', async () => {
+    throw new Error('some other error');
+  });
+
   pageRoutes.get('/pages/:path', async (req, res) => {
     const { params } = req;
 

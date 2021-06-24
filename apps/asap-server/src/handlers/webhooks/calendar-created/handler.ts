03297574dcdc24e034a0e504270a8ea30a70d3d0
Piotr Szpak <piotr.szpak@news.co.uk>
2021-06-24 13:11:04 +0100

    ignore coverage check from calendar handler for serverless

--- apps/asap-server/src/handlers/webhooks/calendar-created/handler.ts
+++ apps/asap-server/src/handlers/webhooks/calendar-created/handler.ts
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 import * as Sentry from '@sentry/serverless';
 import { sentryDsn, environment, currentRevision } from '../../../config';
 import Calendars from '../../../controllers/calendars';

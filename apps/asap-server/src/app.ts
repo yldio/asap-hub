37263479455fcc318b4d08d84f703dc782e277d0
Piotr Szpak <piotr.szpak@news.co.uk>
2021-07-05 13:54:25 +0100

    extended payload limit to 10MB

--- apps/asap-server/src/app.ts
+++ apps/asap-server/src/app.ts
@@ -111,7 +111,7 @@ export const appFactory = (libs: Libs = {}): Express => {
   app.use(httpLogger);
   app.use(tracingHandler);
   app.use(cors());
-  app.use(express.json());
+  app.use(express.json({ limit: '10MB' }));
 
   /**
    * Public routes --->

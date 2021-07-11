9c41752701b8de0fc2b4adccb662ee3fa22454bc
Miguel Carvalho <emece.pt@gmail.com>
2021-07-11 22:18:56 +0100

    wip

--- packages/react-components/src/icons/padlock.tsx
+++ packages/react-components/src/icons/padlock.tsx
@@ -9,23 +9,23 @@ const padlock = (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M6 20.3364H18.916V10.8154H6V20.3364Z"
       fill="#287953"
       stroke="#287953"
-      stroke-width="1.3"
-      stroke-linecap="square"
-      stroke-linejoin="round"
+      strokeWidth="1.3"
+      strokeLinecap="square"
+      strokeLinejoin="round"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M16.531 10.816H8.38499V6.104C8.38499 4.942 9.32699 4 10.49 4H14.427C15.589 4 16.531 4.942 16.531 6.104V10.816Z"
       stroke="#287953"
-      stroke-width="1.3"
-      stroke-linecap="square"
-      stroke-linejoin="round"
+      strokeWidth="1.3"
+      strokeLinecap="square"
+      strokeLinejoin="round"
     />
   </svg>
 );

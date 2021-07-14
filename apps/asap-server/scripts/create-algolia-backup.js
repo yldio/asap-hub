c0b7b80fb540bd659d581ad90d533f79a26af33b
Piotr Szpak <piotr.szpak@news.co.uk>
2021-07-14 15:47:46 +0100

    remove unnecessary console log

--- apps/asap-server/scripts/create-algolia-backup.js
+++ apps/asap-server/scripts/create-algolia-backup.js
@@ -2,11 +2,6 @@ const algoliasearch = require('algoliasearch');
 const fs = require('fs/promises');
 
 const main = async () => {
-  console.log({
-    appId: process.env.ALGOLIA_APP_ID,
-    apiKey: process.env.ALGOLIA_API_KEY,
-  });
-
   const index = algoliasearch(
     process.env.ALGOLIA_APP_ID,
     process.env.ALGOLIA_API_KEY,

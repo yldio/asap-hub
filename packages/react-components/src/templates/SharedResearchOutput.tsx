f9a5a0605f62b988770f059ba44545c130d03423
Jhonny Moreira <jhonny.moreira@thetrainline.com>
2021-07-26 17:28:31 +0100

    adding new field to shared research output

--- packages/react-components/src/templates/SharedResearchOutput.tsx
+++ packages/react-components/src/templates/SharedResearchOutput.tsx
@@ -2,12 +2,14 @@ import { ComponentProps } from 'react';
 import { css } from '@emotion/react';
 import { ResearchOutputResponse } from '@asap-hub/model';
 
-import { Card, Headline2, Divider } from '../atoms';
-import { perRem } from '../pixels';
+import { Card, Headline2, Divider, Anchor } from '../atoms';
+import { mobileScreen, perRem } from '../pixels';
 import { contentSidePaddingWithNavigation } from '../layout';
 import { BackLink, CtaCard, TagList } from '../molecules';
 import { RichText, SharedResearchOutputHeaderCard } from '../organisms';
 import { createMailTo } from '../mail';
+import { externalLinkIcon } from '../icons';
+import { fern, pine } from '../colors';
 
 const containerStyles = css({
   padding: `${36 / perRem}em ${contentSidePaddingWithNavigation(8)}`,
@@ -27,6 +29,28 @@ const additionalInformationEntryStyles = css({
   display: 'flex',
   justifyContent: 'space-between',
   padding: `${6 / perRem}em 0`,
+  [`@media (max-width: ${mobileScreen.max}px)`]: {
+    flexDirection: 'column',
+  },
+});
+const additionalInformationValueStyles = css({
+  [`@media (max-width: ${mobileScreen.max}px)`]: {
+    marginTop: `${12 / perRem}em`,
+  },
+});
+const externalLinkStyle = css({
+  display: 'flex',
+  color: fern.rgb,
+  svg: {
+    stroke: fern.rgb,
+  },
+  ':hover, :focus': {
+    color: pine.rgb,
+    borderColor: pine.rgb,
+    svg: {
+      stroke: pine.rgb,
+    },
+  },
 });
 
 type SharedResearchOutputProps = Pick<
@@ -38,6 +62,10 @@ type SharedResearchOutputProps = Pick<
   | 'asapFunded'
   | 'usedInPublication'
   | 'pmsEmails'
+  | 'doi'
+  | 'rrid'
+  | 'accession'
+  | 'labCatalogNumber'
 > &
   ComponentProps<typeof SharedResearchOutputHeaderCard> & {
     backHref: string;
@@ -52,72 +80,150 @@ const SharedResearchOutput: React.FC<SharedResearchOutputProps> = ({
   asapFunded,
   usedInPublication,
   pmsEmails,
+  doi,
+  rrid,
+  accession,
+  labCatalogNumber,
   ...props
-}) => (
-  <div css={containerStyles}>
-    <BackLink href={backHref} />
-    <div css={cardsStyles}>
-      <SharedResearchOutputHeaderCard {...props} />
-      {(description || !!tags.length) && (
-        <Card>
-          {description && (
+}) => {
+  const isLabCatalogNumberALink =
+    labCatalogNumber?.startsWith('https://') ||
+    labCatalogNumber?.startsWith('http://');
+  return (
+    <div css={containerStyles}>
+      <BackLink href={backHref} />
+      <div css={cardsStyles}>
+        <SharedResearchOutputHeaderCard {...props} />
+        {(description || !!tags.length) && (
+          <Card>
+            {description && (
+              <div css={{ paddingBottom: `${12 / perRem}em` }}>
+                <Headline2 styleAsHeading={4}>Description</Headline2>
+                <RichText poorText text={description} />
+              </div>
+            )}
+            {description && !!tags.length && <Divider />}
+            {!!tags.length && (
+              <>
+                <Headline2 styleAsHeading={4}>Tags</Headline2>
+                <TagList tags={tags} />
+              </>
+            )}
+          </Card>
+        )}
+        {accessInstructions && (
+          <Card>
             <div css={{ paddingBottom: `${12 / perRem}em` }}>
-              <Headline2 styleAsHeading={4}>Description</Headline2>
-              <RichText poorText text={description} />
+              <Headline2 styleAsHeading={4}>Access Instructions</Headline2>
+              <RichText poorText text={accessInstructions} />
             </div>
-          )}
-          {description && !!tags.length && <Divider />}
-          {!!tags.length && (
-            <>
-              <Headline2 styleAsHeading={4}>Tags</Headline2>
-              <TagList tags={tags} />
-            </>
-          )}
-        </Card>
-      )}
-      {accessInstructions && (
+          </Card>
+        )}
         <Card>
-          <div css={{ paddingBottom: `${12 / perRem}em` }}>
-            <Headline2 styleAsHeading={4}>Access Instructions</Headline2>
-            <RichText poorText text={accessInstructions} />
-          </div>
+          <Headline2 styleAsHeading={4}>Additional Information</Headline2>
+          <ol css={additionalInformationListStyles}>
+            <li css={additionalInformationEntryStyles}>
+              <strong>Sharing Status</strong>
+              <span css={additionalInformationValueStyles}>
+                {sharingStatus}
+              </span>
+            </li>
+            {asapFunded === undefined || (
+              <>
+                <Divider />
+                <li css={additionalInformationEntryStyles}>
+                  <strong>ASAP Funded</strong>
+                  <span css={additionalInformationValueStyles}>
+                    {asapFunded ? 'Yes' : 'No'}
+                  </span>
+                </li>
+              </>
+            )}
+            {usedInPublication === undefined || (
+              <>
+                <Divider />
+                <li css={additionalInformationEntryStyles}>
+                  <strong>Used in a Publication</strong>
+                  <span css={additionalInformationValueStyles}>
+                    {usedInPublication ? 'Yes' : 'No'}
+                  </span>
+                </li>
+              </>
+            )}
+            {doi === undefined || (
+              <>
+                <Divider />
+                <li css={additionalInformationEntryStyles}>
+                  <strong>Identifier (DOI)</strong>
+                  <span css={additionalInformationValueStyles}>
+                    <Anchor href={`https://doi.org/${doi}`}>
+                      <div css={externalLinkStyle}>
+                        <span>{doi}</span>
+                        {externalLinkIcon}
+                      </div>
+                    </Anchor>
+                  </span>
+                </li>
+              </>
+            )}
+            {rrid === undefined || (
+              <>
+                <Divider />
+                <li css={additionalInformationEntryStyles}>
+                  <strong>Identifier (RRID)</strong>
+                  <span css={additionalInformationValueStyles}>
+                    <Anchor href={`https://scicrunch.org/resolver/${rrid}`}>
+                      <div css={externalLinkStyle}>
+                        <span>{rrid}</span>
+                        {externalLinkIcon}
+                      </div>
+                    </Anchor>
+                  </span>
+                </li>
+              </>
+            )}
+            {accession === undefined || (
+              <>
+                <Divider />
+                <li css={additionalInformationEntryStyles}>
+                  <strong>Identifier (Accession #)</strong>
+                  <span css={additionalInformationValueStyles}>
+                    {accession}
+                  </span>
+                </li>
+              </>
+            )}
+            {labCatalogNumber === undefined || (
+              <>
+                <Divider />
+                <li css={additionalInformationEntryStyles}>
+                  <strong>Lab Catalog Number</strong>
+                  <span css={additionalInformationValueStyles}>
+                    {isLabCatalogNumberALink ? (
+                      <Anchor href={labCatalogNumber}>
+                        <div css={externalLinkStyle}>
+                          <span>External Link</span>
+                          {externalLinkIcon}
+                        </div>
+                      </Anchor>
+                    ) : (
+                      <>{labCatalogNumber}</>
+                    )}
+                  </span>
+                </li>
+              </>
+            )}
+          </ol>
         </Card>
-      )}
-      <Card>
-        <Headline2 styleAsHeading={4}>Additional Information</Headline2>
-        <ol css={additionalInformationListStyles}>
-          <li css={additionalInformationEntryStyles}>
-            <strong>Sharing Status</strong>
-            <span>{sharingStatus}</span>
-          </li>
-          {asapFunded === undefined || (
-            <>
-              <Divider />
-              <li css={additionalInformationEntryStyles}>
-                <strong>ASAP Funded</strong>
-                <span>{asapFunded ? 'Yes' : 'No'}</span>
-              </li>
-            </>
-          )}
-          {usedInPublication === undefined || (
-            <>
-              <Divider />
-              <li css={additionalInformationEntryStyles}>
-                <strong>Used in a Publication</strong>
-                <span>{usedInPublication ? 'Yes' : 'No'}</span>
-              </li>
-            </>
-          )}
-        </ol>
-      </Card>
-      {!!pmsEmails.length && (
-        <CtaCard href={createMailTo(pmsEmails)} buttonText="Contact PM">
-          <strong>Interested in what you have seen?</strong>
-          <br /> Reach out to the PMs associated with this output
-        </CtaCard>
-      )}
+        {!!pmsEmails.length && (
+          <CtaCard href={createMailTo(pmsEmails)} buttonText="Contact PM">
+            <strong>Interested in what you have seen?</strong>
+            <br /> Reach out to the PMs associated with this output
+          </CtaCard>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SharedResearchOutput;

65a9da720d6d264d62b82f3c32f29cff81514338
Piotr Szpak <piotr.szpak@news.co.uk>
2021-07-28 11:30:49 +0100

    add algolia api key to fetch-by-code response

--- apps/asap-server/src/handlers/webhooks/fetch-by-code/fetch-by-code.ts
+++ apps/asap-server/src/handlers/webhooks/fetch-by-code/fetch-by-code.ts
@@ -1,12 +1,15 @@
+import { SearchClient } from 'algoliasearch';
 import Joi from '@hapi/joi';
 import { framework as lambda } from '@asap-hub/services-common';
 import { http } from '../../../utils/instrumented-framework';
 import { UserController } from '../../../controllers/users';
 import validateRequest from '../../../utils/validate-auth0-request';
 import { Handler } from '../../../utils/types';
+import { algoliaSearchApiKey } from '../../../config';
 
 export const fetchUserByCodeHandlerFactory = (
   userController: UserController,
+  algoliaClient: SearchClient,
 ): Handler =>
   http(async (request: lambda.Request): Promise<lambda.Response> => {
     await validateRequest(request);
@@ -24,8 +27,14 @@ export const fetchUserByCodeHandlerFactory = (
     };
 
     const user = await userController.fetchByCode(code);
+    const apiKey = algoliaClient.generateSecuredApiKey(algoliaSearchApiKey, {
+      validUntil: Date.now() + 36001, // which is one second over the TTL of the ID token
+    });
 
     return {
-      payload: user,
+      payload: {
+        ...user,
+        algoliaApiKey: apiKey,
+      },
     };
   });
--- apps/asap-server/src/handlers/webhooks/fetch-by-code/handler.ts
+++ apps/asap-server/src/handlers/webhooks/fetch-by-code/handler.ts
@@ -1,5 +1,10 @@
+import algoliasearch from 'algoliasearch';
+import { algoliaAppId, algoliaSearchApiKey } from '../../../config';
 import Users from '../../../controllers/users';
 import { Handler } from '../../../utils/types';
 import { fetchUserByCodeHandlerFactory } from './fetch-by-code';
 
-export const handler: Handler = fetchUserByCodeHandlerFactory(new Users());
+export const handler: Handler = fetchUserByCodeHandlerFactory(
+  new Users(),
+  algoliasearch(algoliaAppId, algoliaSearchApiKey),
+);

62cfa41455c7df8ac253c90318f0a3f3754f987d
anaescontrela <ana.escontrela@yld.io>
2021-07-30 11:34:13 +0100

    cleanup

--- packages/validation/test/teams.test.ts
+++ packages/validation/test/teams.test.ts
@@ -1,12 +1,10 @@
 import { TEAM_TOOL_URL } from '../src/teams';
 
-describe('ss', () => {
-  it('validates the url protocol', () => {
-    expect(TEAM_TOOL_URL.test('http://ww')).toBe(true);
-    expect(TEAM_TOOL_URL.test('https://ww')).toBe(true);
-    expect(TEAM_TOOL_URL.test('http:ww')).toBe(false);
-    expect(TEAM_TOOL_URL.test('https:ww')).toBe(false);
-    expect(TEAM_TOOL_URL.test('slack://tool')).toBe(false);
-    expect(TEAM_TOOL_URL.test('www')).toBe(false);
-  });
+it('validates the url protocol', () => {
+  expect(TEAM_TOOL_URL.test('http://ww')).toBe(true);
+  expect(TEAM_TOOL_URL.test('https://ww')).toBe(true);
+  expect(TEAM_TOOL_URL.test('http:ww')).toBe(false);
+  expect(TEAM_TOOL_URL.test('https:ww')).toBe(false);
+  expect(TEAM_TOOL_URL.test('slack://tool')).toBe(false);
+  expect(TEAM_TOOL_URL.test('www')).toBe(false);
 });

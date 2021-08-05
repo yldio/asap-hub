ae2d6390968a534cc91be6ff173c2291c15f27d6
Harlan Kohll <harlan.kohll@yld.io>
2021-08-05 11:09:49 +0100

    [DO NOT MERGE] [WIP] Test that pipeline fails with sq import errors

--- ci/import-squidex-data.sh
+++ ci/import-squidex-data.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env sh
 
 # create-app.py configures sq to new app
-if sq sync in backup | grep -q -E 'warn|error|fail'; then
+#Replace below line with sq sync in backup 2>&1 | tee sq-sync-log.txt
+echo "FAIL: example failure" | tee sq-sync-log.txt
+if grep -q -i -E 'warn|error|fail|exception' sq-sync-log.txt; then
     echo "Data import failure: sq sync in backup failed"
+    rm sq-sync-log.txt
     exit 1
 fi
 
 # apply schema changes on top of dev backup
-if sq sync in packages/squidex/schema -t schemas --delete | grep -q -E 'warn|error|fail'; then
+sq sync in packages/squidex/schema -t schemas --delete 2>&1 | tee sq-sync-schema-log.txt
+if grep -q -i -E 'warn|error|fail|exception' sq-sync-schema-log.txt; then
     echo "Data import failure: sq sync in packages/squidex/schema -t schemas --delete failed"
+    rm sq-sync-schema-log.txt
     exit 1
 fi 
+
+# Delete log files
+rm sq-sync-log.txt
+rm sq-sync-schema-log.txt

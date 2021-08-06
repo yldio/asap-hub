bed9eb6e0890551d6dadd325bd1c7ae685568575
Thomas Waterfall <thomas.waterfall@gmail.com>
2021-08-06 12:18:59 +0100

    Fix how script is called

--- apps/frontend/scripts/sentry-release.js
+++ apps/frontend/scripts/sentry-release.js
@@ -1,9 +1,10 @@
 const SentryCli = require('@sentry/cli');
 
 async function createReleaseAndUpload() {
-  const release = process.env.CI_PIPELINE_ID;
-  if (!release) {
-    console.warn('CI_PIPELINE_ID is not set');
+  if (!process.env.FRONTEND_SENTRY_RELEASE_AUTH_TOKEN) {
+    console.warn(
+      'This release will not be reported into sentry (hopefully because its a PR)',
+    );
     return;
   }
   const cli = new SentryCli(null, {
@@ -12,6 +13,7 @@ async function createReleaseAndUpload() {
     project: 'asap-hub-frontend',
   });
   try {
+    const release = process.env.CI_PIPELINE_ID;
     console.log('Creating sentry release ' + release);
     await cli.releases.new(release);
     console.log('Uploading source maps');

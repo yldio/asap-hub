716ee4079c987cbf3bf82c422469eb973de0121c
Piotr Szpak <piotr.szpak@news.co.uk>
2021-08-10 10:29:22 +0100

    add test email configuration

--- apps/asap-server/src/handlers/webhooks/user-created/invite.ts
+++ apps/asap-server/src/handlers/webhooks/user-created/invite.ts
@@ -1,4 +1,22 @@
-export const handler = (event: unknown): void => {
+import aws from 'aws-sdk';
+
+const ses = new aws.SES({ apiVersion: '2010-12-01', region: 'eu-west-1' });
+
+export const handler = async (event: unknown): Promise<void> => {
   // eslint-disable-next-line no-console
   console.log(JSON.stringify(event));
+
+  const params = {
+    Destination: {
+      ToAddresses: ['piotr.szpak@yld.io'],
+    },
+    Template: 'Welcome',
+    TemplateData: JSON.stringify({
+      firstName: 'Piotr',
+      link: 'http://google.com',
+    }),
+    Source: 'no-reply@yld.io',
+  };
+
+  await ses.sendTemplatedEmail(params).promise();
 };

edfedb3db1e818b17e0dab1a65f2935e96cd0ee2
Harlan Kohll <harlan.kohll@yld.io>
2021-08-10 13:49:03 +0100

    [DO NOT MERGE] [TEST] [WIP] Improved sq sync shell script

--- ci/import-squidex-data.sh
+++ ci/import-squidex-data.sh
@@ -1,21 +1,46 @@
 #!/usr/bin/env sh
 
+# Set to true if you want to print the sq sync logs on success for debugging
+PRINT_SYNC_LOGS_ON_SUCCESS=false
+
+# Render whitespace properly
+IFS=
+
+echo 'Importing data...'
+
 # create-app.py configures sq to new app
-sq sync in backup 2>&1 | tee sq-sync-log.txt
-if grep -q -i -E 'warn|error|fail|exception' sq-sync-log.txt; then
-    echo "Data import failure: sq sync in backup failed"
-    rm sq-sync-log.txt
+#export SQ_SYNC_OUTPUT="$(sq sync in backup)"
+export SQ_SYNC_OUTPUT="FAIL: example failure"
+if echo "$SQ_SYNC_OUTPUT" | grep -q -i -E 'warn|error|fail|exception'; then
+    echo "Data import failure - sq sync in backup failed:"
+    echo $SQ_SYNC_OUTPUT
+    unset SQ_SYNC_OUTPUT
+    unset IFS
     exit 1
 fi
 
 # apply schema changes on top of dev backup
-sq sync in packages/squidex/schema -t schemas --delete 2>&1 | tee sq-sync-schema-log.txt
-if grep -q -i -E 'warn|error|fail|exception' sq-sync-schema-log.txt; then
-    echo "Data import failure: sq sync in packages/squidex/schema -t schemas --delete failed"
-    rm sq-sync-schema-log.txt
+export SQ_SYNC_SCHEMA_OUTPUT="$(sq sync in packages/squidex/schema -t schemas --delete)"
+if echo "$SQ_SYNC_SCHEMA_OUTPUT" | grep -q -i -E 'warn|error|fail|exception'; then
+    echo "Data import failure - sq sync in packages/squidex/schema -t schemas --delete failed:"
+    echo $SQ_SYNC_SCHEMA_OUTPUT
+    unset SQ_SYNC_SCHEMA_OUTPUT
+    unset IFS
     exit 1
 fi 
 
-# Delete log files
-rm sq-sync-log.txt
-rm sq-sync-schema-log.txt
+# Print successful sq sync output if option enabled above
+if [ "$PRINT_SYNC_LOGS_ON_SUCCESS" = true ]; then
+    echo "sq sync in backup successful output:"
+    echo $SQ_SYNC_OUTPUT
+    echo "---------------------------------------------"
+    echo "sq sync in packages/squidex/schema -t schemas --delete successful output:"
+    echo $SQ_SYNC_SCHEMA_OUTPUT
+fi
+
+# Cleanup
+unset SQ_SYNC_OUTPUT
+unset SQ_SYNC_SCHEMA_OUTPUT
+unset IFS
+
+echo 'Import successful'

34ba92371bd4ba121f6f4b5ae7002de977a53c3d
Ana Escontrela <87698274+ana-yldio@users.noreply.github.com>
2021-08-11 11:29:01 +0100

    add new email template (#1007)

---
+++ apps/messages/src/templates/invite.tsx
@@ -0,0 +1,12 @@
+import { MessageLayout, WelcomeMessage } from '@asap-hub/react-components';
+import { welcome } from '@asap-hub/message-templates';
+
+import { APP_ORIGIN } from '../config';
+
+export default (
+  <MessageLayout appOrigin={APP_ORIGIN}>
+    <WelcomeMessage {...welcome} variant={'InviteWelcomeTemplate'} />
+  </MessageLayout>
+);
+
+export const subject = `Welcome ${welcome.firstName}!`;

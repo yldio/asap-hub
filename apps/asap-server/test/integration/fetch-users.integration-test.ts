e06935c56bf3fff0da425c26cead17695074c4e1
Harlan Kohll <harlan.kohll@yld.io>
2021-08-18 15:34:13 +0100

    Add duplicate orcid test

--- apps/asap-server/test/integration/fetch-users.integration-test.ts
+++ apps/asap-server/test/integration/fetch-users.integration-test.ts
@@ -76,10 +76,52 @@ describe('Users', () => {
       expect(e.output.statusCode).toBe(400);
 
       const parsedErrorData = JSON.parse(e.data);
-      expect(parsedErrorData.details[0]).toBe("orcid.iv: ORCID must have the following format: 0000-0000-0000-0000");
+      expect(parsedErrorData.details[0]).toBe(
+        'orcid.iv: ORCID must have the following format: 0000-0000-0000-0000',
+      );
     }
   });
 
-  // Duplicate orcid test?
+  test.only('Duplicate orcids should fail', async () => {
+    const randomEmail = chance.email();
+    const randomName = chance.guid();
+    const randomOrcid = createRandomOrcid();
 
+    // Refactor duplicate user like above?
+    const user: Partial<User> = {
+      firstName: 'John',
+      lastName: randomName,
+      jobTitle: 'Project Manager',
+      orcid: randomOrcid,
+      institution: 'Instituto Superior Tecnico',
+      email: randomEmail,
+      role: 'Grantee' as Role,
+      degree: 'MPH',
+    };
+
+    const duplicateUser: Partial<User> = {
+      firstName: 'John',
+      lastName: randomName,
+      jobTitle: 'Project Manager',
+      orcid: randomOrcid,
+      institution: 'Instituto Superior Tecnico',
+      email: chance.email(),
+      role: 'Grantee' as Role,
+      degree: 'MPH',
+    };
+
+    await createUser(user);
+
+    try {
+      await createUser(duplicateUser);
+    } catch (e) {
+      expect(e.name).toBe('HTTPError');
+      expect(e.output.statusCode).toBe(400);
+
+      const parsedErrorData = JSON.parse(e.data);
+      expect(parsedErrorData.details[0]).toBe(
+        'orcid.iv: Another content with the same value exists.',
+      );
+    }
+  });
 });

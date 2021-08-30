9c8a01ae638d3fd0e92e26a4ba4550ab13b76bea
Piotr Szpak <piotr.szpak@news.co.uk>
2021-08-30 09:28:22 +0100

    add fetch-calendar-by-id method

--- apps/asap-server/test/fixtures/calendars.fixtures.ts
+++ apps/asap-server/test/fixtures/calendars.fixtures.ts
@@ -1,22 +1,29 @@
-import { CalendarRaw } from '../../src/controllers/calendars';
+import {
+  CalendarRaw,
+  ResponseFetchCalendar,
+} from '../../src/controllers/calendars';
 import { CalendarResponse } from '@asap-hub/model';
-import { Calendar, WebhookPayload } from '@asap-hub/squidex';
-import { Rest } from '@asap-hub/squidex/src/entities/common';
+import {
+  Calendar,
+  RestCalendar,
+  Results,
+  WebhookPayload,
+} from '@asap-hub/squidex';
 
-export const calendarRaw: CalendarRaw = {
-  id: 'uuid',
+export const getCalendarRaw = (): CalendarRaw => ({
+  id: 'cc5f74e0-c611-4043-abde-cd3c0d5a3414',
   color: '#2952A3',
   name: 'Tech 4a - iPSCs - 3D & Co-cultures',
   syncToken: 'sync-token',
   resourceId: 'resource-id',
   expirationDate: 1617196357000,
   googleCalendarId: '3@group.calendar.google.com',
-};
+});
 
 export const getCalendarResponse = (): CalendarResponse => ({
-  id: 'calendar-id-1',
-  color: '#5C1158',
-  name: 'Kubernetes Meetups',
+  id: '3@group.calendar.google.com',
+  color: '#2952A3',
+  name: 'Tech 4a - iPSCs - 3D & Co-cultures',
 });
 
 export const getCalendarUpdatedWebhookEvent = (): WebhookPayload<Calendar> => ({
@@ -50,7 +57,8 @@ export const getCalendarPublishedWebhookEvent =
     },
   });
 
-const getRestCalendar = (): Rest<Calendar> => ({
+export const getRestCalendar = (): RestCalendar => ({
+  id: 'cms-calendar-id-1',
   data: {
     name: {
       iv: 'Tech 3 - Structural Biology',
@@ -69,6 +77,60 @@ const getRestCalendar = (): Rest<Calendar> => ({
     },
     expirationDate: {
       iv: 1631667628000,
+    },
+  },
+  created: '2021-01-07T16:44:09Z',
+  lastModified: '2021-01-07T16:44:09Z',
+});
+
+export const getCalendarsRestResponse = (): Results<RestCalendar> => ({
+  total: 2,
+  items: [
+    {
+      id: 'cms-calendar-id-1',
+      data: {
+        googleCalendarId: { iv: 'calendar-id-1' },
+        color: { iv: '#5C1158' },
+        name: { iv: 'Kubernetes Meetups' },
+        resourceId: { iv: 'resource-id' },
+        syncToken: { iv: 'sync-token' },
+        expirationDate: { iv: 1614697798681 },
+      },
+      created: '2021-01-07T16:44:09Z',
+      lastModified: '2021-01-07T16:44:09Z',
+    },
+    {
+      id: 'cms-calendar-id-2',
+      data: {
+        googleCalendarId: { iv: 'calendar-id-2' },
+        color: { iv: '#B1365F' },
+        name: { iv: 'Service Mesh Conferences' },
+        resourceId: { iv: 'resource-id-2' },
+        syncToken: { iv: 'sync-token-2' },
+        expirationDate: { iv: 1614697621081 },
+      },
+      created: '2021-01-07T16:44:09Z',
+      lastModified: '2021-01-07T16:44:09Z',
+    },
+  ],
+});
+
+export const getCalendarsGraphqlResponse = (): {
+  data: ResponseFetchCalendar;
+} => ({
+  data: {
+    findCalendarsContent: {
+      id: 'cc5f74e0-c611-4043-abde-cd3c0d5a3414',
+      created: '2021-01-07T16:44:09Z',
+      lastModified: '2021-01-07T16:44:09Z',
+      flatData: {
+        googleCalendarId: '3@group.calendar.google.com',
+        color: '#2952A3',
+        name: 'Tech 4a - iPSCs - 3D & Co-cultures',
+        resourceId: 'resource-id',
+        syncToken: 'sync-token',
+        expirationDate: 1617196357000,
+      },
     },
   },
 });

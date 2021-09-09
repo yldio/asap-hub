e7910424062334ebf28f11995416bbf618ff2ac8
Jhonny Moreira <jhonny.moreira@thetrainline.com>
2021-09-09 10:14:10 +0100

    adding TeamProfileOutputs regression test

--- packages/react-components/src/templates/__tests__/TeamProfileOutputs.test.tsx
+++ packages/react-components/src/templates/__tests__/TeamProfileOutputs.test.tsx
@@ -22,6 +22,47 @@ it('renders a coming soon text (REGRESSION)', () => {
   expect(getByText(/more\sto\scome/i)).toBeVisible();
   expect(getByText(/research\soutputs/i)).toBeVisible();
 });
+it('renders output cards (REGRESSION)', () => {
+  disable('ALGOLIA_RESEARCH_OUTPUTS');
+  const { getAllByRole } = render(
+    <TeamProfileOutputs
+      outputs={[
+        {
+          ...createResearchOutputResponse(),
+          id: 'uuid-output',
+          title: 'Title',
+          authors: [],
+          teams: [
+            {
+              id: 'uuid-team',
+              displayName: 'Unknown',
+            },
+          ],
+        },
+      ]}
+      numberOfItems={1}
+      numberOfPages={1}
+      currentPage={0}
+      renderPageHref={() => ''}
+      isListView={false}
+      cardViewHref={''}
+      listViewHref={''}
+    />,
+  );
+
+  const links = getAllByRole('link');
+  expect(links).toHaveLength(2);
+  const [titleLink, teamLink] = links;
+
+  expect(titleLink).toHaveTextContent('Title');
+  expect(teamLink).toHaveTextContent(/Unknown/);
+
+  expect(titleLink).toHaveAttribute(
+    'href',
+    expect.stringMatching(/uuid-output$/),
+  );
+  expect(teamLink).toHaveAttribute('href', expect.stringMatching(/uuid-team$/));
+});
 
 it('renders output cards', () => {
   const { getAllByRole, queryByText } = render(

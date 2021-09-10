b20ca1cb7882190703af7db3270ae18eae792a97
Piotr Szpak <piotr.szpak@news.co.uk>
2021-09-10 10:26:03 +0100

    add schema generate command

---
+++ apps/asap-server/scripts/schema-generate.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "Generating queries..."
+yarn workspace @asap-hub/asap-server run apollo codegen:generate --localSchemaFile=../../packages/squidex/graphql-schema/schema.json --target=typescript --tagName=gql --includes=./src/queries/* --passthroughCustomScalars
+echo "Generated. Cleaning up the output..."
+yarn run prettier --write ./apps/asap-server/src/queries/*
+echo "Done."

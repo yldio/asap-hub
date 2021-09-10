b6e02b2079832a0751b2a369606a6a83a79c05d0
Piotr Szpak <piotr.szpak@news.co.uk>
2021-09-10 10:04:15 +0100

    add schema download command

---
+++ apps/asap-server/scripts/schema-download.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+TOKEN=$(curl -s --request POST --url $SQUIDEX_BASE_URL/identity-server/connect/token --header 'Content-Type: application/x-www-form-urlencoded' --data grant_type=client_credentials --data client_id=$SQUIDEX_CLIENT_ID --data client_secret=$SQUIDEX_CLIENT_SECRET --data scope=squidex-api | jq .access_token -r)
+echo "Downloading schema..."
+yarn workspace @asap-hub/asap-server run apollo service:download --header "Authorization: Bearer $TOKEN" --endpoint "https://cloud.squidex.io/api/content/$SQUIDEX_APP_NAME/graphql" ../../packages/squidex/graphql-schema/schema.json
+echo "Downloaded. Cleaning up the output..."
+yarn run prettier --write ./packages/squidex/graphql-schema/schema.json
+echo "Done."

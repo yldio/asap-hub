f44b8148e0ba3480d2e394a32d7d332aaae142cf
Piotr Szpak <piotr.szpak@news.co.uk>
2021-09-10 16:00:51 +0100

    do not use jq

--- apps/asap-server/scripts/schema-download.sh
+++ apps/asap-server/scripts/schema-download.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-TOKEN=$(curl -s --request POST --url $SQUIDEX_BASE_URL/identity-server/connect/token --header 'Content-Type: application/x-www-form-urlencoded' --data grant_type=client_credentials --data client_id=$SQUIDEX_CLIENT_ID --data client_secret=$SQUIDEX_CLIENT_SECRET --data scope=squidex-api | jq .access_token -r)
+TOKEN=$(curl -s --request POST --url $SQUIDEX_BASE_URL/identity-server/connect/token --header 'Content-Type: application/x-www-form-urlencoded' --data grant_type=client_credentials --data client_id=$SQUIDEX_CLIENT_ID --data client_secret=$SQUIDEX_CLIENT_SECRET --data scope=squidex-api | grep 'access_token' | sed -r 's/^[^:]*:(.*)$/\1/' | tr -d '"')
 echo "Downloading schema..."
 yarn workspace @asap-hub/asap-server run apollo service:download --header "Authorization: Bearer $TOKEN" --endpoint "https://cloud.squidex.io/api/content/$SQUIDEX_APP_NAME/graphql" ../../packages/squidex/graphql-schema/schema.json
 echo "Downloaded. Cleaning up the output..."

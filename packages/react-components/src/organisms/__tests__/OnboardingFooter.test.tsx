670b5583f7b041b1a6def3c3d68730f727c4437c
anaescontrela <ana.escontrela@yld.io>
2021-09-13 12:25:39 +0100

    update tests

--- packages/react-components/src/organisms/__tests__/OnboardingFooter.test.tsx
+++ packages/react-components/src/organisms/__tests__/OnboardingFooter.test.tsx
@@ -5,17 +5,21 @@ import OnboardingFooter from '../OnboardingFooter';
 const props: ComponentProps<typeof OnboardingFooter> = {
   onboardable: { steps: [], isOnboardable: false },
 };
-it('renders the not onboardable footer', () => {
-  const { getByRole } = render(
+it('renders the not onboardable UI, when profile incomplete', () => {
+  const { getByRole, getByText } = render(
     <OnboardingFooter
       {...props}
-      onboardable={{ steps: [], isOnboardable: false }}
+      onboardable={{
+        steps: [{ label: 'Role', modalHref: '/' }],
+        isOnboardable: false,
+      }}
     />,
   );
-  expect(getByRole('heading')).toHaveTextContent(/ incomplete/i);
+  expect(getByRole('heading')).toHaveTextContent(/incomplete/i);
+  expect(getByText(/step/i).textContent).toEqual('Next Step: Role');
 });
 
-it('renders the onboardable footer', () => {
+it('renders the not onboardable UI, when profile complete', () => {
   const { getByTitle, getByRole } = render(
     <OnboardingFooter
       {...props}

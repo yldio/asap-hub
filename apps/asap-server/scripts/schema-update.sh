499416e827c86b545fb67858354c32d9213c2a5a
anaescontrela <ana.escontrela@yld.io>
2021-09-21 14:56:10 +0100

    update schema types

--- apps/asap-server/scripts/schema-update.sh
+++ apps/asap-server/scripts/schema-update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-export SQUIDEX_TOKEN=$(curl -s --request POST --url $SQUIDEX_BASE_URL/identity-server/connect/token --header 'Content-Type: application/x-www-form-urlencoded' --data grant_type=client_credentials --data client_id=$SQUIDEX_CLIENT_ID --data client_secret=$SQUIDEX_CLIENT_SECRET --data scope=squidex-api | grep 'access_token' | sed "s/.*\"access_token\":\s\"\([^\"]*\).*/\1/g")
+export SQUIDEX_TOKEN=$(curl -s --request POST --url $SQUIDEX_BASE_URL/identity-server/connect/token --header 'Content-Type: application/x-www-form-urlencoded' --data grant_type=client_credentials --data client_id=$SQUIDEX_CLIENT_ID --data client_secret=$SQUIDEX_CLIENT_SECRET --data scope=squidex-api | grep 'access_token' | sed "s/.*\"access_token\": \"\([^\"]*\).*/\1/g")
 echo "Downloading schema..."
 yarn workspace @asap-hub/asap-server run graphql-codegen --config codegen.yml
 echo "Downloaded. Cleaning up the output..."

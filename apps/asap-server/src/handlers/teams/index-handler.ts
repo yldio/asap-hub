f7e3359649e38cf41bd26f6701634e6af4da2632
anaescontrela <ana.escontrela@yld.io>
2021-10-12 17:38:33 +0100

    reindex all team's outputs

--- apps/asap-server/src/handlers/teams/index-handler.ts
+++ apps/asap-server/src/handlers/teams/index-handler.ts
@@ -22,26 +22,38 @@ export const indexResearchOutputByTeamHandler = (
   return async (
     event: EventBridgeEvent<TeamsEventType, SquidexWebhookTeamPayload>,
   ): Promise<void> => {
-    const outputsIds = event.detail.payload.data.outputs.iv;
+    const outputsIds = Array.from(
+      new Set(
+        (event.detail.payload.data.outputs.iv ?? []).concat(
+          event.detail.payload.dataOld?.outputs.iv ?? [],
+        ),
+      ),
+    );
+
+    logger.info(`Found ${outputsIds.length} research-output(s)`);
 
     if (outputsIds.length > 0) {
-      await Promise.all(
-        outputsIds.map(async (id) => {
-          logger.info(`Fetch research-output with id ${id}`);
-          const researchOutput = await researchOutputController.fetchById(id);
+      try {
+        await Promise.allSettled(
+          outputsIds.map(async (id) => {
+            logger.info(`Fetch research-output with id ${id}`);
+            const researchOutput = await researchOutputController.fetchById(id);
 
-          logger.info(
-            `Fetched research-output ${JSON.stringify(researchOutput)}`,
-          );
+            logger.info(
+              `Fetched research-output ${JSON.stringify(researchOutput)}`,
+            );
 
-          await algoliaIndex.saveObject({
-            ...researchOutput,
-            objectID: researchOutput.id,
-          });
+            await algoliaIndex.saveObject({
+              ...researchOutput,
+              objectID: researchOutput.id,
+            });
 
-          logger.info(`Saved research-output with id ${id}`);
-        }),
-      );
+            logger.info(`Saved research-output with id ${id}`);
+          }),
+        );
+      } catch (e) {
+        logger.info(`Failed to fetch research-outputs ${JSON.stringify(e)}`);
+      }
     }
   };
 };
@@ -53,6 +65,9 @@ export type SquidexWebhookTeamPayload = {
     type: 'Created';
     id: string;
     data: {
+      outputs: { iv: string[] };
+    };
+    dataOld?: {
       outputs: { iv: string[] };
     };
   };

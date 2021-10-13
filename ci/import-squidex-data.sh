932ec4a55bb45a26e29dbda1398a0bb2823a0264
Piotr Szpak <piotr.szpak@news.co.uk>
2021-10-13 14:18:28 +0100

    run squidex app creation conditionally

--- ci/import-squidex-data.sh
+++ ci/import-squidex-data.sh
@@ -2,20 +2,27 @@
 
 # Set this envvar to true in GitLab if you want to print the sq sync logs on success for debugging
 PRINT_SYNC_LOGS_ON_SUCCESS="${PRINT_SYNC_LOGS_ON_SUCCESS:-false}"
+# SEt this envvar to true in GitLab if you want the squidex app to be recreated on every commit
+RECREATE_APP_ON_EVERY_PR="${RECREATE_APP_ON_EVERY_PR:-false}"
+
 
 # Render whitespace properly
 IFS=
 
 echo 'Importing data...'
 
-# create-app.py configures sq to new app
-export SQ_SYNC_OUTPUT="$(sq sync in backup)"
-if echo "$SQ_SYNC_OUTPUT" | grep -q -i -E 'warn|error|fail|exception'; then
-    echo "Data import failure - sq sync in backup failed:"
-    echo $SQ_SYNC_OUTPUT
-    unset SQ_SYNC_OUTPUT
-    unset IFS
-    exit 1
+if [ "$RECREATE_APP_ON_EVERY_PR" = true ] || [ "$FIRST_RUN" = true ]; then
+    # create-app.py configures sq to new app
+    export SQ_SYNC_OUTPUT="$(sq sync in backup)"
+    if echo "$SQ_SYNC_OUTPUT" | grep -q -i -E 'warn|error|fail|exception'; then
+        echo "Data import failure - sq sync in backup failed:"
+        echo $SQ_SYNC_OUTPUT
+        unset SQ_SYNC_OUTPUT
+        unset IFS
+        exit 1
+    fi
+else
+    echo 'Skipping app creation and data import'
 fi
 
 # apply schema changes on top of dev backup
@@ -26,7 +33,7 @@ if echo "$SQ_SYNC_SCHEMA_OUTPUT" | grep -q -i -E 'warn|error|fail|exception'; th
     unset SQ_SYNC_SCHEMA_OUTPUT
     unset IFS
     exit 1
-fi 
+fi
 
 # Print successful sq sync output if option enabled above
 if [ "$PRINT_SYNC_LOGS_ON_SUCCESS" = true ]; then

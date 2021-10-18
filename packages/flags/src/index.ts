e8c49d1b668fb8113bbc3ea92bfacf435c1a31cb
Ana Escontrela <87698274+ana-yldio@users.noreply.github.com>
2021-10-18 16:46:16 +0100

    feat:enable search feature (#1096)

--- packages/flags/src/index.ts
+++ packages/flags/src/index.ts
@@ -8,6 +8,9 @@ export type Flags = Partial<Record<Flag, boolean>>;
 let overrides: Flags = {
   // flags already live in prod:
   // can also be used to manually disable a flag in development:
+  RESEARCH_OUTPUT_SHOW_AUTHORS_LIST: true,
+  ALGOLIA_RESEARCH_OUTPUTS: true,
+  RESEARCH_OUTPUTS_ON_AUTHOR_PROFILE: true,
 };
 
 const envDefaults: Record<string, boolean> = {

0cbfde070f1fa8c7264956435f3fd56631c02ed3
Mark Sims <mark.sims@acuris.com>
2021-11-03 09:48:47 +0000

    chore: fixes test error from gitlab
    PASS test-frontend apps/frontend/src/network/teams/__tests__/Outputs.test.tsx (5.677 s)
    /builds/yldio/asap-hub/.yarn/__virtual__/react-dom-virtual-640d0aa28e/0/cache/react-dom-npm-17.0.2-f551215af1-1c1eaa3bca.zip/node_modules/react-dom/cjs/react-dom.development.js:3905
          var evt = document.createEvent('Event');
                             ^
    TypeError: Cannot read property 'createEvent' of null

--- apps/frontend/src/network/teams/__tests__/Outputs.test.tsx
+++ apps/frontend/src/network/teams/__tests__/Outputs.test.tsx
@@ -23,194 +23,198 @@ import {
 
 jest.mock('../../../shared-research/api');
 jest.mock('../../../shared-research/export');
+describe('Outputs', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-afterEach(() => {
-  jest.clearAllMocks();
-});
+  const mockGetResearchOutputs = getResearchOutputs as jest.MockedFunction<
+    typeof getResearchOutputs
+  >;
 
-const mockGetResearchOutputs = getResearchOutputs as jest.MockedFunction<
-  typeof getResearchOutputs
->;
+  const mockCreateCsvFileStream = createCsvFileStream as jest.MockedFunction<
+    typeof createCsvFileStream
+  >;
 
-const mockCreateCsvFileStream = createCsvFileStream as jest.MockedFunction<
-  typeof createCsvFileStream
->;
-
-const renderOutputs = async (
-  teamOutputs: ResearchOutputResponse[],
-  searchQuery = '',
-  filters = new Set<string>(),
-  teamId = '42',
-) => {
-  const result = render(
-    <RecoilRoot
-      initializeState={({ reset }) => {
-        reset(
-          researchOutputsState({
-            searchQuery,
-            filters,
-            teamId,
-            currentPage: 0,
-            pageSize: CARD_VIEW_PAGE_SIZE,
-          }),
-        );
-        reset(
-          researchOutputsState({
-            searchQuery,
-            filters,
-            teamId,
-            currentPage: 0,
-            pageSize: MAX_ALGOLIA_RESULTS,
-          }),
-        );
-      }}
-    >
-      <Suspense fallback="loading">
-        <Auth0Provider user={{}}>
-          <WhenReady>
-            <MemoryRouter
-              initialEntries={[
-                {
-                  pathname: network({}).teams({}).team({ teamId }).outputs({})
-                    .$,
-                },
-              ]}
-            >
-              <Route
-                path={network({}).teams({}).team({ teamId }).outputs({}).$}
+  const renderOutputs = async (
+    teamOutputs: ResearchOutputResponse[],
+    searchQuery = '',
+    filters = new Set<string>(),
+    teamId = '42',
+  ) => {
+    const result = render(
+      <RecoilRoot
+        initializeState={({ reset }) => {
+          reset(
+            researchOutputsState({
+              searchQuery,
+              filters,
+              teamId,
+              currentPage: 0,
+              pageSize: CARD_VIEW_PAGE_SIZE,
+            }),
+          );
+          reset(
+            researchOutputsState({
+              searchQuery,
+              filters,
+              teamId,
+              currentPage: 0,
+              pageSize: MAX_ALGOLIA_RESULTS,
+            }),
+          );
+        }}
+      >
+        <Suspense fallback="loading">
+          <Auth0Provider user={{}}>
+            <WhenReady>
+              <MemoryRouter
+                initialEntries={[
+                  {
+                    pathname: network({}).teams({}).team({ teamId }).outputs({})
+                      .$,
+                  },
+                ]}
               >
-                <Outputs teamOutputs={teamOutputs} teamId={teamId} />
-              </Route>
-            </MemoryRouter>
-          </WhenReady>
-        </Auth0Provider>
-      </Suspense>
-    </RecoilRoot>,
-  );
+                <Route
+                  path={network({}).teams({}).team({ teamId }).outputs({}).$}
+                >
+                  <Outputs teamOutputs={teamOutputs} teamId={teamId} />
+                </Route>
+              </MemoryRouter>
+            </WhenReady>
+          </Auth0Provider>
+        </Suspense>
+      </RecoilRoot>,
+    );
 
-  await waitFor(() =>
-    expect(result.queryByText(/loading/i)).not.toBeInTheDocument(),
-  );
-  return result;
-};
+    await waitFor(() =>
+      expect(result.queryByText(/loading/i)).not.toBeInTheDocument(),
+    );
+    return result;
+  };
 
-it('generates a link to the output (REGRESSION)', async () => {
-  disable('ALGOLIA_RESEARCH_OUTPUTS');
-  const { getByText } = await renderOutputs([
-    { ...createResearchOutputResponse(), id: 'ro0', title: 'Some RO' },
-  ]);
-  expect(getByText(/Some RO/).closest('a')!.href).toMatch(/ro0$/);
-});
-
-it('generates a link back to the team (REGRESSION)', async () => {
-  disable('ALGOLIA_RESEARCH_OUTPUTS');
-
-  const { getByText } = await renderOutputs([
-    {
-      ...createResearchOutputResponse(),
-      teams: [{ id: '42', displayName: 'Some Team' }],
-    },
-  ]);
-  expect(getByText(/Some Team/).closest('a')).toHaveAttribute(
-    'href',
-    expect.stringMatching(/42$/),
-  );
-});
-
-it('renders search and filter', async () => {
-  const { getByRole } = await renderOutputs([
-    { ...createResearchOutputResponse(), id: 'ro0', title: 'Some RO' },
-  ]);
-  expect(
-    (getByRole('searchbox') as HTMLInputElement).placeholder,
-  ).toMatchInlineSnapshot(`"Enter a keyword, method, resource…"`);
-});
-
-it('renders a list of research outputs', async () => {
-  mockGetResearchOutputs.mockResolvedValue({
-    ...createAlgoliaResearchOutputResponse(2),
-    hits: createAlgoliaResearchOutputResponse(2).hits.map((hit, index) => ({
-      ...hit,
-      title: `Test Output ${index}`,
-    })),
+  it('generates a link to the output (REGRESSION)', async () => {
+    disable('ALGOLIA_RESEARCH_OUTPUTS');
+    const { getByText } = await renderOutputs([
+      { ...createResearchOutputResponse(), id: 'ro0', title: 'Some RO' },
+    ]);
+    expect(getByText(/Some RO/).closest('a')!.href).toMatch(/ro0$/);
   });
-  const { container } = await renderOutputs([], '');
-  expect(container.textContent).toContain('Test Output 0');
-  expect(container.textContent).toContain('Test Output 1');
-});
 
-it('calls getResearchOutputs with the right arguments', async () => {
-  const filters = new Set(['Proposal']);
-  const searchQuery = 'searchterm';
-  const teamId = '1234';
-  mockGetResearchOutputs.mockResolvedValue({
-    ...createAlgoliaResearchOutputResponse(2),
+  it('generates a link back to the team (REGRESSION)', async () => {
+    disable('ALGOLIA_RESEARCH_OUTPUTS');
+
+    const { getByText } = await renderOutputs([
+      {
+        ...createResearchOutputResponse(),
+        teams: [{ id: '42', displayName: 'Some Team' }],
+      },
+    ]);
+    expect(getByText(/Some Team/).closest('a')).toHaveAttribute(
+      'href',
+      expect.stringMatching(/42$/),
+    );
   });
-  const { getByRole, getByText, getByLabelText } = await renderOutputs(
-    [],
-    searchQuery,
-    filters,
-    teamId,
-  );
-  userEvent.type(getByRole('searchbox'), searchQuery);
 
-  userEvent.click(getByText('Filters'));
-  const checkbox = getByLabelText('Proposal');
-  expect(checkbox).not.toBeChecked();
+  it('renders search and filter', async () => {
+    const { getByRole } = await renderOutputs([
+      { ...createResearchOutputResponse(), id: 'ro0', title: 'Some RO' },
+    ]);
+    expect(
+      (getByRole('searchbox') as HTMLInputElement).placeholder,
+    ).toMatchInlineSnapshot(`"Enter a keyword, method, resource…"`);
+  });
 
-  userEvent.click(checkbox);
-  expect(checkbox).toBeChecked();
+  it('renders a list of research outputs', async () => {
+    mockGetResearchOutputs.mockResolvedValue({
+      ...createAlgoliaResearchOutputResponse(2),
+      hits: createAlgoliaResearchOutputResponse(2).hits.map((hit, index) => ({
+        ...hit,
+        title: `Test Output ${index}`,
+      })),
+    });
+    const { container } = await renderOutputs([], '');
+    expect(container.textContent).toContain('Test Output 0');
+    expect(container.textContent).toContain('Test Output 1');
+  });
 
-  await waitFor(() =>
-    expect(mockGetResearchOutputs).toHaveBeenLastCalledWith(
+  it('calls getResearchOutputs with the right arguments', async () => {
+    const filters = new Set(['Proposal']);
+    const searchQuery = 'searchterm';
+    const teamId = '1234';
+    mockGetResearchOutputs.mockResolvedValue({
+      ...createAlgoliaResearchOutputResponse(2),
+    });
+    const { getByRole, getByText, getByLabelText } = await renderOutputs(
+      [],
+      searchQuery,
+      filters,
+      teamId,
+    );
+    userEvent.type(getByRole('searchbox'), searchQuery);
+
+    userEvent.click(getByText('Filters'));
+    const checkbox = getByLabelText('Proposal');
+    expect(checkbox).not.toBeChecked();
+
+    userEvent.click(checkbox);
+    expect(checkbox).toBeChecked();
+
+    await waitFor(() =>
+      expect(mockGetResearchOutputs).toHaveBeenLastCalledWith(
+        expect.anything(),
+        expect.objectContaining({
+          teamId,
+          searchQuery,
+          filters,
+        }),
+      ),
+    );
+  });
+
+  it('triggers and export with the same parameters', async () => {
+    const filters = new Set(['Proposal']);
+    const searchQuery = 'Some Search';
+    const teamId = '12345';
+    mockGetResearchOutputs.mockResolvedValue({
+      ...createAlgoliaResearchOutputResponse(2),
+    });
+    const { getByRole, getByText, getByLabelText } = await renderOutputs(
+      [],
+      searchQuery,
+      filters,
+      teamId,
+    );
+    userEvent.type(getByRole('searchbox'), searchQuery);
+    userEvent.click(getByText('Filters'));
+    userEvent.click(getByLabelText('Proposal'));
+    await waitFor(() =>
+      expect(mockGetResearchOutputs).toHaveBeenLastCalledWith(
+        expect.anything(),
+        {
+          searchQuery,
+          filters,
+          teamId,
+          currentPage: 0,
+          pageSize: CARD_VIEW_PAGE_SIZE,
+        },
+      ),
+    );
+
+    userEvent.click(getByText(/export/i));
+    expect(mockCreateCsvFileStream).toHaveBeenLastCalledWith(
       expect.anything(),
-      expect.objectContaining({
-        teamId,
+      expect.stringMatching(/SharedOutputs_\d+\.csv/),
+    );
+    await waitFor(() =>
+      expect(mockGetResearchOutputs).toHaveBeenCalledWith(expect.anything(), {
         searchQuery,
         filters,
+        teamId,
+        currentPage: 0,
+        pageSize: MAX_ALGOLIA_RESULTS,
       }),
-    ),
-  );
-});
-
-it('triggers and export with the same parameters', async () => {
-  const filters = new Set(['Proposal']);
-  const searchQuery = 'Some Search';
-  const teamId = '12345';
-  mockGetResearchOutputs.mockResolvedValue({
-    ...createAlgoliaResearchOutputResponse(2),
+    );
   });
-  const { getByRole, getByText, getByLabelText } = await renderOutputs(
-    [],
-    searchQuery,
-    filters,
-    teamId,
-  );
-  userEvent.type(getByRole('searchbox'), searchQuery);
-  userEvent.click(getByText('Filters'));
-  userEvent.click(getByLabelText('Proposal'));
-  await waitFor(() =>
-    expect(mockGetResearchOutputs).toHaveBeenLastCalledWith(expect.anything(), {
-      searchQuery,
-      filters,
-      teamId,
-      currentPage: 0,
-      pageSize: CARD_VIEW_PAGE_SIZE,
-    }),
-  );
-
-  userEvent.click(getByText(/export/i));
-  expect(mockCreateCsvFileStream).toHaveBeenLastCalledWith(
-    expect.anything(),
-    expect.stringMatching(/SharedOutputs_\d+\.csv/),
-  );
-  await waitFor(() =>
-    expect(mockGetResearchOutputs).toHaveBeenCalledWith(expect.anything(), {
-      searchQuery,
-      filters,
-      teamId,
-      currentPage: 0,
-      pageSize: MAX_ALGOLIA_RESULTS,
-    }),
-  );
 });

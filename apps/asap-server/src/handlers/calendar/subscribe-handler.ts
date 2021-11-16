044ea8efb41c4716838073e19d8b0a8edc65a960
Mark Sims <mark.sims@acuris.com>
2021-11-16 12:16:56 +0000

    tries rebuilding

--- apps/asap-server/src/handlers/calendar/subscribe-handler.ts
+++ apps/asap-server/src/handlers/calendar/subscribe-handler.ts
@@ -5,7 +5,6 @@ import { Auth } from 'googleapis';
 import * as Sentry from '@sentry/serverless';
 import { framework as lambda } from '@asap-hub/services-common';
 import { WebhookPayload, Calendar } from '@asap-hub/squidex';
-
 import {
   googleApiUrl,
   asapApiUrl,

816cff36bc3b65dbfc12fd2543cadda18e8ac097
Piotr Szpak <piotr.szpak@news.co.uk>
2021-11-23 09:07:31 +0000

    tweak e2e test config

--- apps/e2e-tests/playwright.config.ts
+++ apps/e2e-tests/playwright.config.ts
@@ -1,6 +1,7 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-  testDir: './tests',
+  testDir: './tests/frontend',
+  testMatch: '**/*.e2e-test.{js,jsx,ts,tsx}',
 };
 export default config;

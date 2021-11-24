f97023716d68e97bfc585a7d2f92b4326a827b07
Mateusz Niezgoda <mateusz.niezgoda@yld.io>
2021-11-24 12:55:18 +0100

    Migration script proposal

---
+++ apps/asap-server/src/migrations/1637748102-move-type-proposal-to-grant-research.ts
@@ -0,0 +1,70 @@
+/* istanbul ignore file */
+import {
+  RestResearchOutput,
+  Results,
+  Squidex,
+} from '../../../../packages/squidex';
+import { Migration } from '../handlers/webhooks/webhook-run-migrations';
+
+interface Fetcher {
+  (pointer: number): Promise<Results<RestResearchOutput>>;
+}
+
+interface ItemProcessor {
+  (researchOutput: RestResearchOutput): any;
+}
+
+const fetchSquidex =
+  (
+    squidexClient: Squidex<RestResearchOutput>,
+    type: 'Proposal' | 'Grant Document',
+  ) =>
+  (pointer: number) =>
+    squidexClient.fetch({
+      $top: 10,
+      $skip: pointer,
+      $orderby: 'created asc',
+      $filter: `data/type/iv eq '${type}'`,
+    });
+
+const iterate = async (fetcher: Fetcher, process: ItemProcessor) => {
+  let pointer = 0;
+  let result: Results<RestResearchOutput>;
+
+  do {
+    result = await fetcher(pointer);
+
+    for (const researchOutput of result.items) {
+      await process(researchOutput);
+    }
+
+    pointer += 10;
+  } while (pointer < result.total);
+};
+
+export default class MoveResearchOutputTextToDescription extends Migration {
+  up = async (): Promise<void> => {
+    const squidexClient = new Squidex<RestResearchOutput>('research-outputs');
+
+    await iterate(fetchSquidex(squidexClient, 'Proposal'), (researchOutput) =>
+      squidexClient.patch(researchOutput.id, {
+        type: { iv: 'Grant Document' },
+        subtype: { iv: 'Proposal' },
+      }),
+    );
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  down = async (): Promise<void> => {
+    const squidexClient = new Squidex<RestResearchOutput>('research-outputs');
+
+    await iterate(
+      fetchSquidex(squidexClient, 'Grant Document'),
+      (researchOutput) =>
+        squidexClient.patch(researchOutput.id, {
+          type: { iv: 'Proposal' },
+          subtype: { iv: null },
+        }),
+    );
+  };
+}

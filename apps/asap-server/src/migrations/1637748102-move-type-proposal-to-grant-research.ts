03dd11a93b56c419032ebbee9670fa68491ee249
Piotr Szpak <piotr.szpak@yld.io>
2021-12-03 15:47:11 +0000

    update the migration and tweak helper test

--- apps/asap-server/src/migrations/1637748102-move-type-proposal-to-grant-research.ts
+++ apps/asap-server/src/migrations/1637748102-move-type-proposal-to-grant-research.ts
@@ -1,67 +1,36 @@
 /* istanbul ignore file */
-import { RestResearchOutput, Results, Squidex } from '@asap-hub/squidex';
+import { RestResearchOutput } from '@asap-hub/squidex';
 import { ResearchOutputSubtype } from '@asap-hub/model';
 
 import { Migration } from '../handlers/webhooks/webhook-run-migrations';
-
-interface Fetcher {
-  (pointer: number): Promise<Results<RestResearchOutput>>;
-}
-
-interface ItemProcessor {
-  (researchOutput: RestResearchOutput): unknown;
-}
-
-const fetchSquidex =
-  (
-    squidexClient: Squidex<RestResearchOutput>,
-    type: 'Proposal' | 'Grant Document',
-  ) =>
-  (pointer: number) =>
-    squidexClient.fetch({
-      $top: 10,
-      $skip: pointer,
-      $orderby: 'created asc',
-      $filter: `data/type/iv eq '${type}'`,
-    });
-
-const iterate = async (fetcher: Fetcher, process: ItemProcessor) => {
-  let pointer = 0;
-  let result: Results<RestResearchOutput>;
-
-  do {
-    result = await fetcher(pointer);
-
-    for (const researchOutput of result.items) {
-      await process(researchOutput);
-    }
-
-    pointer += 10;
-  } while (pointer < result.total);
-};
+import { applyToAllItemsInCollection } from '../utils/migrations';
 
 export default class MoveResearchOutputTextToDescription extends Migration {
   up = async (): Promise<void> => {
-    const squidexClient = new Squidex<RestResearchOutput>('research-outputs');
-
-    await iterate(fetchSquidex(squidexClient, 'Proposal'), (researchOutput) =>
-      squidexClient.patch(researchOutput.id, {
-        type: { iv: 'Grant Document' },
-        subtype: { iv: 'Proposal' },
-      }),
+    await applyToAllItemsInCollection<RestResearchOutput>(
+      'research-outputs',
+      async (researchOutput, squidexClient) => {
+        if (researchOutput.data.type.iv === 'Proposal') {
+          await squidexClient.patch(researchOutput.id, {
+            type: { iv: 'Grant Document' },
+            subtype: { iv: 'Proposal' },
+          });
+        }
+      },
     );
   };
 
   down = async (): Promise<void> => {
-    const squidexClient = new Squidex<RestResearchOutput>('research-outputs');
-
-    await iterate(
-      fetchSquidex(squidexClient, 'Grant Document'),
-      (researchOutput) =>
-        squidexClient.patch(researchOutput.id, {
-          type: { iv: 'Proposal' },
-          subtype: null as unknown as { iv: ResearchOutputSubtype },
-        }),
+    await applyToAllItemsInCollection<RestResearchOutput>(
+      'research-outputs',
+      async (researchOutput, squidexClient) => {
+        if (researchOutput.data.type.iv === 'Grant Document') {
+          await squidexClient.patch(researchOutput.id, {
+            type: { iv: 'Proposal' },
+            subtype: null as unknown as { iv: ResearchOutputSubtype },
+          });
+        }
+      },
     );
   };
 }

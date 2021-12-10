94aaca6cdbd642ec95d8c46c317eeefaea30a7ea
Mark Sims <mark.sims@acuris.com>
2021-12-10 10:44:37 +0000

    fix: squidex rules in PR environments (#1205)
    * tries applying rules to PR environments
    
    * renames rules files
    
    * adds comments to ci

--- ci/import-squidex-data.sh
+++ ci/import-squidex-data.sh
@@ -5,7 +5,6 @@ PRINT_SYNC_LOGS_ON_SUCCESS="${PRINT_SYNC_LOGS_ON_SUCCESS:-false}"
 # SEt this envvar to true in GitLab if you want the squidex app to be recreated on every commit
 RECREATE_APP_ON_EVERY_PR="${RECREATE_APP_ON_EVERY_PR:-false}"
 
-
 # Render whitespace properly
 IFS=
 
@@ -25,28 +24,16 @@ else
     echo 'Skipping app creation and data import'
 fi
 
-# apply schema changes on top of dev backup
-export SQ_SYNC_SCHEMA_OUTPUT="$(sq sync in packages/squidex/schema -t schemas --delete)"
-if echo "$SQ_SYNC_SCHEMA_OUTPUT" | grep -q -i -E 'warn|error|fail|exception'; then
-    echo "Data import failure - sq sync in packages/squidex/schema -t schemas --delete failed:"
-    echo $SQ_SYNC_SCHEMA_OUTPUT
-    unset SQ_SYNC_SCHEMA_OUTPUT
-    unset IFS
-    exit 1
-fi
-
 # Print successful sq sync output if option enabled above
 if [ "$PRINT_SYNC_LOGS_ON_SUCCESS" = true ]; then
+    echo "---------------------------------------------"
     echo "sq sync in backup successful output:"
     echo $SQ_SYNC_OUTPUT
     echo "---------------------------------------------"
-    echo "sq sync in packages/squidex/schema -t schemas --delete successful output:"
-    echo $SQ_SYNC_SCHEMA_OUTPUT
 fi
 
 # Cleanup
 unset SQ_SYNC_OUTPUT
-unset SQ_SYNC_SCHEMA_OUTPUT
 unset IFS
 
 echo 'Import successful'

3ed9a10b2f244e36f889328bc458f682129e6aef
Mateusz Niezgoda <mateusz.niezgoda@yld.io>
2021-12-20 10:21:20 +0100

    wip

--- packages/auth/src/__mocks__/index.ts
+++ packages/auth/src/__mocks__/index.ts
@@ -1,6 +1,8 @@
 const config: typeof import('../config') = {
   domain: 'auth.example.com',
   clientID: 'client_id',
+  apiToken: 'api_token',
+  currentTimestampHeader: 'timestamp-header'.toLocaleLowerCase()
 };
 
 const auth0PubKeys = jest.requireActual('../pubKeys').default;

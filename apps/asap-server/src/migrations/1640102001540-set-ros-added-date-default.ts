566616aa644a415b1d99e253f67ddeefade0aedf
anaescontrela <ana.escontrela@yld.io>
2021-12-21 15:55:34 +0000

    migration file

---
+++ apps/asap-server/src/migrations/1640102001540-set-ros-added-date-default.ts
@@ -0,0 +1,36 @@
+/* istanbul ignore file */
+import { RestResearchOutput, Results, SquidexRest } from '@asap-hub/squidex';
+import { Migration } from '../handlers/webhooks/webhook-run-migrations';
+
+export default class SetResearchOutputAddedDateDefault extends Migration {
+  up = async (): Promise<void> => {
+    const squidexClient = new SquidexRest<RestResearchOutput>(
+      'research-outputs',
+    );
+
+    let pointer = 0;
+    let result: Results<RestResearchOutput>;
+
+    do {
+      result = await squidexClient.fetch({
+        $top: 10,
+        $skip: pointer,
+        $filter: `data/addedDate/iv eq ''`,
+        $orderby: 'created asc',
+      });
+
+      for (const researchOutput of result.items) {
+        await squidexClient.patch(researchOutput.id, {
+          addedDate: {
+            iv: researchOutput.created,
+          },
+        });
+      }
+
+      pointer += 10;
+    } while (pointer < result.total);
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  down = async (): Promise<void> => {};
+}

5af2dafe1b6519615a496cca651f269b168b5cf1
Thomas Waterfall <thomas.waterfall@gmail.com>
2022-01-05 14:34:18 +0000

    Design Amends

--- packages/react-components/src/organisms/HelpSection.tsx
+++ packages/react-components/src/organisms/HelpSection.tsx
@@ -7,13 +7,13 @@ import { mailToGrants, mailToSupport } from '../mail';
 
 const bottomStyles = css({
   display: 'grid',
-  gridRowGap: `${36 / perRem}em`,
+  gridRowGap: `${12 / perRem}em`,
 });
 
 const HelpSection: React.FC = () => (
   <section css={bottomStyles}>
     <CtaCard href={mailToGrants()} buttonText="Request grant help">
-      <strong>Need help with grant-related matter?</strong>
+      <strong>Need help with a grant-related matter?</strong>
       <br /> ASAP and the Michael J Fox Foundation are here to help
     </CtaCard>
     <Paragraph accent="lead">

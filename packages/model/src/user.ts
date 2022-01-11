9d9a7b68ce0e6d2dd94515e3e07091e45e915aca
Piotr Szpak <piotr.szpak@yld.io>
2022-01-11 09:00:55 +0000

    revert FE changes

--- packages/model/src/user.ts
+++ packages/model/src/user.ts
@@ -158,7 +158,7 @@ export interface UserPatchRequest {
   reachOut?: string;
   researchInterests?: string;
   questions?: string[];
-  teams?: Pick<UserTeam, 'id'>[];
+  teams?: Pick<UserTeam, 'id' | 'mainResearchInterests' | 'responsibilities'>[];
   social?: Omit<UserSocialLinks, 'orcid'>;
   onboarded?: boolean;
 }

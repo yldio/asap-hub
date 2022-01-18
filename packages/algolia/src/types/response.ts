53f3ec8524ec3b13cb9792d5557fe4013b81d1ca
Mateusz Niezgoda <kontakt@mniezgoda.pl>
2022-01-18 10:12:53 +0100

    fix package version

--- packages/algolia/src/types/response.ts
+++ packages/algolia/src/types/response.ts
@@ -2,8 +2,10 @@ export type SearchResponseMetadata<TEntityName extends string> = {
   type: TEntityName;
 };
 
-export type SearchResponse<TEntity extends Record<string, unknown>, TEntityName extends string> =
-  TEntity & {
-    objectID: string;
-    __meta: SearchResponseMetadata<TEntityName>;
-  };
+export type SearchResponse<
+  TEntity extends Record<string, unknown>,
+  TEntityName extends string,
+> = TEntity & {
+  objectID: string;
+  __meta: SearchResponseMetadata<TEntityName>;
+};

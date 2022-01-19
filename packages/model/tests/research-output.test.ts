09490328e4dde4eaf5cb6d3cf3a0beb6823e9f4e
Mateusz Niezgoda <kontakt@mniezgoda.pl>
2022-01-19 15:36:20 +0100

    more tests

--- packages/model/tests/research-output.test.ts
+++ packages/model/tests/research-output.test.ts
@@ -1,7 +1,39 @@
-import { researchOutputMapSubtype } from '../src/research-output';
+import {
+  isResearchOutputType,
+  isResearchOutputSubtype,
+  researchOutputMapSubtype
+} from '../src/research-output';
 
 describe('Research Output Model', () => {
-  it('should map deprecated subtype', () => {
-    expect(researchOutputMapSubtype('Assays')).toEqual('Assay');
+  describe('Type', () => {
+    it('should recognize correct type', () => {
+      expect(isResearchOutputType('Dataset')).toEqual(true);
+    });
+
+    it('should not recognize correct type', () => {
+      expect(isResearchOutputType('NotADataset')).toEqual(false);
+    });
+  });
+
+  describe('SubType', () => {
+    it('should recognize correct subtype', () => {
+      expect(isResearchOutputSubtype('Report')).toEqual(true);
+    });
+
+    it('should not recognize correct subtype', () => {
+      expect(isResearchOutputSubtype('NotAReport')).toEqual(false);
+    });
+
+    it('should map deprecated subtype', () => {
+      expect(researchOutputMapSubtype('Assays')).toEqual('Assay');
+    });
+
+    it('should pass not deprecated subtype', () => {
+      expect(researchOutputMapSubtype('Cloning')).toEqual('Cloning');
+    });
+
+    it('should return null on not known subtype', () => {
+      expect(researchOutputMapSubtype('NotACloningSubtype')).toBeNull();
+    });
   });
 });

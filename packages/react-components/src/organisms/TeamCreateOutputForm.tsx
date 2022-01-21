a24582a7b7d2ecc1797625db812e44395cb6af36
anaescontrela <ana.escontrela@yld.io>
2022-01-21 12:54:14 +0000

    styling TeamCreateOutputForm component

--- packages/react-components/src/organisms/TeamCreateOutputForm.tsx
+++ packages/react-components/src/organisms/TeamCreateOutputForm.tsx
@@ -1,7 +1,27 @@
+import { css } from '@emotion/react';
 import { Button } from '../atoms';
+import { perRem } from '../pixels';
+import { contentSidePaddingWithNavigation } from '../layout';
+
+const controlsContainerStyles = css({
+  display: 'grid',
+  gridAutoFlow: 'column',
+  alignItems: 'end',
+  justifyContent: 'end',
+  columnGap: `${12 / perRem}em`,
+  padding: `${36 / perRem}em ${contentSidePaddingWithNavigation(8)} ${
+    48 / perRem
+  }em `,
+});
 
 const TeamCreateOutputForm: React.FC<{ onCreate: () => void }> = ({
   onCreate,
-}) => <Button onClick={onCreate}>Share</Button>;
+}) => (
+  <div css={controlsContainerStyles}>
+    <Button primary onClick={onCreate}>
+      Share
+    </Button>
+  </div>
+);
 
 export default TeamCreateOutputForm;

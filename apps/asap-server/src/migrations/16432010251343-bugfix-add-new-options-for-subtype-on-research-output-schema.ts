c44f18547c6fc8e152f13522c79711fc5ea86d90
Mateusz Niezgoda <kontakt@mniezgoda.pl>
2022-01-26 13:44:22 +0100

    Migration

---
+++ apps/asap-server/src/migrations/16432010251343-bugfix-add-new-options-for-subtype-on-research-output-schema.ts
@@ -0,0 +1,27 @@
+/* istanbul ignore file */
+import { researchOutputMapSubtype } from '@asap-hub/model';
+import { RestResearchOutput } from '@asap-hub/squidex';
+import { Migration } from '../handlers/webhooks/webhook-run-migrations';
+import { applyToAllItemsInCollection } from '../utils/migrations';
+
+export default class BugfixMapResearchOutputDeprecatedSubtype extends Migration {
+  up = async (): Promise<void> => {
+    await applyToAllItemsInCollection<RestResearchOutput>(
+      'research-outputs',
+      async (researchOutput, squidexClient) => {
+        const mappedSubtype = researchOutputMapSubtype(
+          researchOutput.data?.subtype?.iv,
+        );
+
+        if (mappedSubtype) {
+          await squidexClient.patch(researchOutput.id, {
+            subtype: { iv: mappedSubtype },
+          });
+        }
+      },
+    );
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  down = async (): Promise<void> => {};
+}

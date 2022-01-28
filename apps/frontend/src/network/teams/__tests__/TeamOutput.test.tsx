05078c2995b9a93d8cdaedf5ee06e0050f2397c8
Mark Sims <mark.sims@acuris.com>
2022-01-28 10:49:22 +0000

    remove root describe block from jest tests

--- apps/frontend/src/network/teams/__tests__/TeamOutput.test.tsx
+++ apps/frontend/src/network/teams/__tests__/TeamOutput.test.tsx
@@ -11,66 +11,64 @@ import { RecoilRoot } from 'recoil';
 import { refreshTeamState } from '../state';
 import TeamOutput from '../TeamOutput';
 
-describe('TeamOutput', () => {
-  it('Renders the research output', async () => {
-    const {
-      result: {
-        current: { enable },
-      },
-    } = renderHook(useFlags);
+it('Renders the research output', async () => {
+  const {
+    result: {
+      current: { enable },
+    },
+  } = renderHook(useFlags);
 
-    enable('ROMS_FORM');
+  enable('ROMS_FORM');
 
-    const teamId = 'team-id';
-    await renderPage(teamId);
+  const teamId = 'team-id';
+  await renderPage(teamId);
 
-    expect(
-      screen.getByRole('heading', { name: /Share bioinformatics/i }),
-    ).toBeInTheDocument();
-  });
-
-  it('Shows NotFoundPage when feature flag is off', async () => {
-    const {
-      result: {
-        current: { disable },
-      },
-    } = renderHook(useFlags);
-
-    disable('ROMS_FORM');
-
-    const teamId = 'team-id';
-    await renderPage(teamId);
-    expect(
-      screen.queryByRole('heading', { name: /Share bioinformatics/i }),
-    ).not.toBeInTheDocument();
-    expect(
-      screen.getByRole('heading', {
-        name: /Sorry! We can’t seem to find that page/i,
-      }),
-    ).toBeInTheDocument();
-  });
-
-  const renderPage = async (teamId: string) => {
-    const result = render(
-      <RecoilRoot
-        initializeState={({ set }) =>
-          set(refreshTeamState(teamId), Math.random())
-        }
-      >
-        <Suspense fallback="loading">
-          <Auth0Provider user={{}}>
-            <WhenReady>
-              <StaticRouter>
-                <TeamOutput teamId={teamId} />
-              </StaticRouter>
-            </WhenReady>
-          </Auth0Provider>
-        </Suspense>
-      </RecoilRoot>,
-    );
-    await waitFor(() =>
-      expect(result.queryByText(/loading/i)).not.toBeInTheDocument(),
-    );
-    return result;
-  };
+  expect(
+    screen.getByRole('heading', { name: /Share bioinformatics/i }),
+  ).toBeInTheDocument();
 });
+
+it('Shows NotFoundPage when feature flag is off', async () => {
+  const {
+    result: {
+      current: { disable },
+    },
+  } = renderHook(useFlags);
+
+  disable('ROMS_FORM');
+
+  const teamId = 'team-id';
+  await renderPage(teamId);
+  expect(
+    screen.queryByRole('heading', { name: /Share bioinformatics/i }),
+  ).not.toBeInTheDocument();
+  expect(
+    screen.getByRole('heading', {
+      name: /Sorry! We can’t seem to find that page/i,
+    }),
+  ).toBeInTheDocument();
+});
+
+const renderPage = async (teamId: string) => {
+  const result = render(
+    <RecoilRoot
+      initializeState={({ set }) =>
+        set(refreshTeamState(teamId), Math.random())
+      }
+    >
+      <Suspense fallback="loading">
+        <Auth0Provider user={{}}>
+          <WhenReady>
+            <StaticRouter>
+              <TeamOutput teamId={teamId} />
+            </StaticRouter>
+          </WhenReady>
+        </Auth0Provider>
+      </Suspense>
+    </RecoilRoot>,
+  );
+  await waitFor(() =>
+    expect(result.queryByText(/loading/i)).not.toBeInTheDocument(),
+  );
+  return result;
+};

0b7fb5323421f54a2e4af956984ec6b631c71770
Mark Sims <mark.sims@acuris.com>
2022-01-28 10:45:42 +0000

    remove root describe block from jest tests

--- packages/react-components/src/organisms/__tests__/TeamCreateOutputHeader.test.tsx
+++ packages/react-components/src/organisms/__tests__/TeamCreateOutputHeader.test.tsx
@@ -1,29 +1,26 @@
 import { createResearchOutput } from '@asap-hub/fixtures';
-import { render, screen } from '@testing-library/react';
 import { useFlags } from '@asap-hub/react-context';
+import { render, screen } from '@testing-library/react';
 import { renderHook } from '@testing-library/react-hooks';
-
 import TeamCreateOutputHeader from '../TeamCreateOutputHeader';
 
-describe('TeamCreateOutputHeader', () => {
-  beforeEach(() => {
-    const {
-      result: {
-        current: { disable },
-      },
-    } = renderHook(useFlags);
+beforeEach(() => {
+  const {
+    result: {
+      current: { disable },
+    },
+  } = renderHook(useFlags);
 
-    disable('ROMS_FORM');
-  });
-  it('renders the research output type in the header', () => {
-    render(<TeamCreateOutputHeader researchOutput={createResearchOutput()} />);
-    expect(
-      screen.getByRole('heading', { name: /Share bioinformatics/i }),
-    ).toBeInTheDocument();
-  });
+  disable('ROMS_FORM');
+});
+it('renders the research output type in the header', () => {
+  render(<TeamCreateOutputHeader researchOutput={createResearchOutput()} />);
+  expect(
+    screen.getByRole('heading', { name: /Share bioinformatics/i }),
+  ).toBeInTheDocument();
+});
 
-  it('renders the research output type in the description', () => {
-    render(<TeamCreateOutputHeader researchOutput={createResearchOutput()} />);
-    expect(screen.getByText(/Add your bioinformatics/)).toBeInTheDocument();
-  });
+it('renders the research output type in the description', () => {
+  render(<TeamCreateOutputHeader researchOutput={createResearchOutput()} />);
+  expect(screen.getByText(/Add your bioinformatics/)).toBeInTheDocument();
 });

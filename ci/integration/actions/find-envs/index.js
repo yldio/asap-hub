43891e7e64c0b5e88906f412cd3bf944856a8cfb
Jhonny Moreira <jhonny.moreira@thetrainline.com>
2022-02-02 14:40:25 +0000

    triggering workflow from action

--- ci/integration/actions/find-envs/index.js
+++ ci/integration/actions/find-envs/index.js
@@ -7,15 +7,18 @@ const { SQUIDEX_CLIENT_ID, SQUIDEX_CLIENT_SECRET } = process.env;
 const squidexUrl = 'https://cloud.squidex.io';
 
 async function main() {
-  const availableApps = await getAvailableApps();
-  console.log({ availableApps });
-  core.setOutput('availableApps', availableApps);
+  try {
+    const apps = await getAvailableApps();
+    core.setOutput('availableApps', apps);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 main();
 
 async function getAvailableApps() {
   const headers = await getClientHeaders();
-  const appsReqest = await fetch(squidexUrl + '/api/apps', {
+  const appsReqest = await fetch(`${squidexUrl}/api/apps`, {
     headers,
   });
   const apps = await appsReqest.json();
@@ -23,23 +26,20 @@ async function getAvailableApps() {
 }
 
 async function getClientHeaders() {
-  const tokenUrl = `${squidexUrl}/identity-server/connect/token`;
-  const body = {
+  const params = new URLSearchParams({
     grant_type: 'client_credentials',
     scope: 'squidex-api',
     client_id: SQUIDEX_CLIENT_ID,
     client_secret: SQUIDEX_CLIENT_SECRET,
-  };
-  const params = new URLSearchParams(body);
-  const response = await fetch(tokenUrl, {
+  });
+  const response = await fetch(`${squidexUrl}/identity-server/connect/token`, {
     method: 'POST',
     body: params,
   });
 
   const { access_token } = await response.json();
-  const headers = {
+  return {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${access_token}`,
   };
-  return headers;
 }

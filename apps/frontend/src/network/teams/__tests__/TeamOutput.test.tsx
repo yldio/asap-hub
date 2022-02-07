0a4ecb4174d05db81f16279d2b125bc9c00d77ea
Marco Rotili <marco.livio.rotili@gmail.com>
2022-02-07 10:50:45 +0100

    CRN-651 allow users to create outputs of different types

--- apps/frontend/src/network/teams/__tests__/TeamOutput.test.tsx
+++ apps/frontend/src/network/teams/__tests__/TeamOutput.test.tsx
@@ -8,37 +8,31 @@ import { renderHook } from '@testing-library/react-hooks';
 import { Suspense } from 'react';
 import { StaticRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import { network, OutputTypeParameter } from '@asap-hub/routing';
 import { refreshTeamState } from '../state';
-import TeamOutput from '../TeamOutput';
+import TeamOutput, { paramOutputTypeToResearchOutputType } from '../TeamOutput';
 
 it('Renders the research output', async () => {
-  const {
-    result: {
-      current: { enable },
-    },
-  } = renderHook(useFlags);
-
-  enable('ROMS_FORM');
-
   const teamId = 'team-id';
-  await renderPage(teamId);
+  await renderPage({ teamId });
 
   expect(
     screen.getByRole('heading', { name: /Share bioinformatics/i }),
   ).toBeInTheDocument();
 });
 
-it('Shows NotFoundPage when feature flag is off', async () => {
-  const {
-    result: {
-      current: { disable },
-    },
-  } = renderHook(useFlags);
-
-  disable('ROMS_FORM');
-
+it('switches research output type based on parameter', async () => {
   const teamId = 'team-id';
-  await renderPage(teamId);
+  await renderPage({ teamId, outputType: 'article' });
+
+  expect(
+    screen.getByRole('heading', { name: /Share an article/i }),
+  ).toBeInTheDocument();
+});
+
+it('Shows NotFoundPage when feature flag is off', async () => {
+  const teamId = 'team-id';
+  await renderPage({ teamId });
   expect(
     screen.queryByRole('heading', { name: /Share bioinformatics/i }),
   ).not.toBeInTheDocument();
@@ -49,7 +43,61 @@ it('Shows NotFoundPage when feature flag is off', async () => {
   ).toBeInTheDocument();
 });
 
-const renderPage = async (teamId: string) => {
+describe('paramOutputTypeToResearchOutputType', () => {
+  it('maps from article to Article', () => {
+    expect(paramOutputTypeToResearchOutputType('article')).toEqual('Article');
+  });
+
+  it('maps from bioinformatics to Bioinformatics', () => {
+    expect(paramOutputTypeToResearchOutputType('bioinformatics')).toEqual(
+      'Bioinformatics',
+    );
+  });
+
+  it('maps from dataset to Dataset', () => {
+    expect(paramOutputTypeToResearchOutputType('dataset')).toEqual('Dataset');
+  });
+
+  it('maps from lab-resource to Lab Resource', () => {
+    expect(paramOutputTypeToResearchOutputType('lab-resource')).toEqual(
+      'Lab Resource',
+    );
+  });
+
+  it('maps from protocol to Protocol', () => {
+    expect(paramOutputTypeToResearchOutputType('protocol')).toEqual('Protocol');
+  });
+
+  it('defaults to article on unknown input', () => {
+    expect(
+      paramOutputTypeToResearchOutputType('unknown' as OutputTypeParameter),
+    ).toEqual('Article');
+  });
+});
+
+interface RenderPageOptions {
+  teamId: string;
+  outputType?: OutputTypeParameter;
+  featureFlagEnabled?: boolean;
+}
+
+const renderPage = async ({
+  featureFlagEnabled = true,
+  teamId,
+  outputType = 'bioinformatics',
+}: RenderPageOptions) => {
+  const {
+    result: {
+      current: { disable, enable },
+    },
+  } = renderHook(useFlags);
+
+  if (featureFlagEnabled) {
+    enable('ROMS_FORM');
+  } else {
+    disable('ROMS_FORM');
+  }
+
   const result = render(
     <RecoilRoot
       initializeState={({ set }) =>
@@ -59,7 +107,12 @@ const renderPage = async (teamId: string) => {
       <Suspense fallback="loading">
         <Auth0Provider user={{}}>
           <WhenReady>
-            <StaticRouter>
+            <StaticRouter
+              location={network({})
+                .teams({})
+                .team({ teamId })
+                .createOutput({ outputType })}
+            >
               <TeamOutput teamId={teamId} />
             </StaticRouter>
           </WhenReady>

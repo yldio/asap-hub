bf241755a3300dc220298c9786aadb025c9d9936
Marco Rotili <marco.livio.rotili@gmail.com>
2022-02-10 21:10:38 +0100

    simplify tests using each

--- apps/frontend/src/network/teams/__tests__/TeamOutput.test.tsx
+++ apps/frontend/src/network/teams/__tests__/TeamOutput.test.tsx
@@ -43,36 +43,15 @@ it('Shows NotFoundPage when feature flag is off', async () => {
   ).toBeInTheDocument();
 });
 
-describe('paramOutputTypeToResearchOutputType', () => {
-  it('maps from article to Article', () => {
-    expect(paramOutputTypeToResearchOutputType('article')).toEqual('Article');
-  });
-
-  it('maps from bioinformatics to Bioinformatics', () => {
-    expect(paramOutputTypeToResearchOutputType('bioinformatics')).toEqual(
-      'Bioinformatics',
-    );
-  });
-
-  it('maps from dataset to Dataset', () => {
-    expect(paramOutputTypeToResearchOutputType('dataset')).toEqual('Dataset');
-  });
-
-  it('maps from lab-resource to Lab Resource', () => {
-    expect(paramOutputTypeToResearchOutputType('lab-resource')).toEqual(
-      'Lab Resource',
-    );
-  });
-
-  it('maps from protocol to Protocol', () => {
-    expect(paramOutputTypeToResearchOutputType('protocol')).toEqual('Protocol');
-  });
-
-  it('defaults to article on unknown input', () => {
-    expect(
-      paramOutputTypeToResearchOutputType('unknown' as OutputTypeParameter),
-    ).toEqual('Article');
-  });
+it.each([
+  ['article', 'Article'],
+  ['bioinformatics', 'Bioinformatics'],
+  ['dataset', 'Dataset'],
+  ['lab-resource', 'Lab Resource'],
+  ['protocol', 'Protocol'],
+  ['unknown' as OutputTypeParameter, 'Article'],
+])('maps from %s to %s', (param, outputType) => {
+  expect(paramOutputTypeToResearchOutputType(param)).toEqual(outputType);
 });
 
 interface RenderPageOptions {

33f143db821eca0a1fb84a81aa713548676db85e
Thomas Waterfall <thomas.waterfall@gmail.com>
2022-02-11 10:21:41 +0000

    Remove tests

--- apps/frontend/src/network/teams/__tests__/TeamProfile.test.tsx
+++ apps/frontend/src/network/teams/__tests__/TeamProfile.test.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 import { RecoilRoot } from 'recoil';
-import { render, waitFor, screen } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter, Route } from 'react-router-dom';
 import { createTeamResponse } from '@asap-hub/fixtures';
@@ -9,12 +9,10 @@ import {
   Auth0Provider,
   WhenReady,
 } from '@asap-hub/frontend/src/auth/test-utils';
-import { useFlags } from '@asap-hub/react-context';
-import { renderHook } from '@testing-library/react-hooks';
 
 import { createResearchOutputListAlgoliaResponse } from '../../../__fixtures__/algolia';
 import TeamProfile from '../TeamProfile';
-import { getTeam, createTeamResearchOutput } from '../api';
+import { getTeam } from '../api';
 import { refreshTeamState } from '../state';
 import { getResearchOutputs } from '../../../shared-research/api';
 
@@ -23,132 +21,72 @@ jest.mock('../groups/api');
 jest.mock('../../../shared-research/api');
 
 afterEach(() => jest.clearAllMocks());
-describe('TeamCreateOutputPage', () => {
-  beforeEach(() => {
-    const {
-      result: {
-        current: { enable },
-      },
-    } = renderHook(useFlags);
 
-    enable('ROMS_FORM');
+it('renders the header info', async () => {
+  const { getByText } = await renderPage({
+    ...createTeamResponse(),
+    displayName: 'Bla',
   });
-
-  it('renders the header info', async () => {
-    const teamResponse = createTeamResponse();
-    const teamId = teamResponse.id;
-
-    await renderPage(
-      teamResponse,
-      undefined,
-      network({})
-        .teams({})
-        .team({ teamId })
-        .createOutput({ outputType: 'bioinformatics' }).$,
-    );
-    expect(
-      screen.getByRole('heading', { name: /Share bioinformatics/i }),
-    ).toBeInTheDocument();
-  });
-  it('submits to api', async () => {
-    const teamResponse = createTeamResponse();
-    const teamId = teamResponse.id;
-
-    await renderPage(
-      teamResponse,
-      undefined,
-      network({})
-        .teams({})
-        .team({ teamId })
-        .createOutput({ outputType: 'bioinformatics' }).$,
-    );
-
-    userEvent.click(
-      screen.getByRole('heading', { name: /Share bioinformatics/i }),
-    );
-    waitFor(async () =>
-      expect(createTeamResearchOutput).toHaveBeenCalledWith(
-        {
-          addedDate: expect.anything(),
-          asapFunded: undefined,
-          link: 'https://hub.asap.science/',
-          sharingStatus: 'Network Only',
-          title: 'Output created through the ROMS form',
-          type: 'Bioinformatics',
-          usedInPublication: undefined,
-        },
-        expect.anything(),
-      ),
-    );
-  });
+  expect(getByText(/Team.+Bla/i)).toBeVisible();
 });
-describe('TeamProfilePage', () => {
-  it('renders the header info', async () => {
-    const { getByText } = await renderPage({
-      ...createTeamResponse(),
-      displayName: 'Bla',
-    });
-    expect(getByText(/Team.+Bla/i)).toBeVisible();
+
+it('renders the about info', async () => {
+  const { getByText } = await renderPage();
+  expect(getByText(/project overview/i)).toBeVisible();
+});
+
+it('navigates to the outputs tab', async () => {
+  const mockGetResearchOutputs = getResearchOutputs as jest.MockedFunction<
+    typeof getResearchOutputs
+  >;
+  mockGetResearchOutputs.mockResolvedValue({
+    ...createResearchOutputListAlgoliaResponse(1),
+  });
+  const { getByText, findByText } = await renderPage();
+
+  userEvent.click(getByText(/outputs/i, { selector: 'nav *' }));
+  expect(await findByText(/Output 1/i)).toBeVisible();
+});
+
+it('navigates to the workspace tab', async () => {
+  const { getByText, findByText } = await renderPage({
+    ...createTeamResponse(),
+    tools: [],
   });
 
-  it('renders the about info', async () => {
-    const { getByText } = await renderPage();
-    expect(getByText(/project overview/i)).toBeVisible();
+  userEvent.click(getByText(/workspace/i, { selector: 'nav *' }));
+  expect(await findByText(/tools/i)).toBeVisible();
+});
+it('does not allow navigating to the workspace tab when team tools are not available', async () => {
+  const { queryByText } = await renderPage({
+    ...createTeamResponse(),
+    tools: undefined,
   });
 
-  it('navigates to the outputs tab', async () => {
-    const mockGetResearchOutputs = getResearchOutputs as jest.MockedFunction<
-      typeof getResearchOutputs
-    >;
-    mockGetResearchOutputs.mockResolvedValue({
-      ...createResearchOutputListAlgoliaResponse(1),
-    });
-    const { getByText, findByText } = await renderPage();
+  expect(
+    queryByText(/workspace/i, { selector: 'nav *' }),
+  ).not.toBeInTheDocument();
+});
 
-    userEvent.click(getByText(/outputs/i, { selector: 'nav *' }));
-    expect(await findByText(/Output 1/i)).toBeVisible();
+it('renders the 404 page for a missing team', async () => {
+  const { getByText } = await renderPage(
+    { ...createTeamResponse(), id: '42' },
+    { teamId: '1337' },
+  );
+  expect(getByText(/sorry.+page/i)).toBeVisible();
+});
+
+it('deep links to the teams list', async () => {
+  const { container, getByLabelText } = await renderPage({
+    ...createTeamResponse({ teamMembers: 10 }),
+    id: '42',
   });
 
-  it('navigates to the workspace tab', async () => {
-    const { getByText, findByText } = await renderPage({
-      ...createTeamResponse(),
-      tools: [],
-    });
+  const anchor = getByLabelText(/\+\d/i).closest('a');
+  expect(anchor).toBeVisible();
+  const { hash } = new URL(anchor!.href, globalThis.location.href);
 
-    userEvent.click(getByText(/workspace/i, { selector: 'nav *' }));
-    expect(await findByText(/tools/i)).toBeVisible();
-  });
-  it('does not allow navigating to the workspace tab when team tools are not available', async () => {
-    const { queryByText } = await renderPage({
-      ...createTeamResponse(),
-      tools: undefined,
-    });
-
-    expect(
-      queryByText(/workspace/i, { selector: 'nav *' }),
-    ).not.toBeInTheDocument();
-  });
-
-  it('renders the 404 page for a missing team', async () => {
-    const { getByText } = await renderPage(
-      { ...createTeamResponse(), id: '42' },
-      { teamId: '1337' },
-    );
-    expect(getByText(/sorry.+page/i)).toBeVisible();
-  });
-
-  it('deep links to the teams list', async () => {
-    const { container, getByLabelText } = await renderPage({
-      ...createTeamResponse({ teamMembers: 10 }),
-      id: '42',
-    });
-
-    const anchor = getByLabelText(/\+\d/i).closest('a');
-    expect(anchor).toBeVisible();
-    const { hash } = new URL(anchor!.href, globalThis.location.href);
-
-    expect(container.querySelector(hash)).toHaveTextContent(/team members/i);
-  });
+  expect(container.querySelector(hash)).toHaveTextContent(/team members/i);
 });
 
 const renderPage = async (

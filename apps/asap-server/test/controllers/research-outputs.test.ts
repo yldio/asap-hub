cf30c1e79d56563648294d86e405ff3e5d1735f6
Jhonny Moreira <jhonny.moreira@thetrainline.com>
2022-02-16 13:15:52 +0000

    updating roms controller create for labs

--- apps/asap-server/test/controllers/research-outputs.test.ts
+++ apps/asap-server/test/controllers/research-outputs.test.ts
@@ -643,6 +643,7 @@ describe('ResearchOutputs controller', () => {
           }),
           asapFunded: { iv: 'Not Sure' },
           usedInAPublication: { iv: 'Not Sure' },
+          labs: { iv: ['1'] },
         })
         .reply(201, { id: researchOutputId })
         .get(`/api/content/${config.appName}/teams/${teamId}`)

0c5876f5e88de0fc528f4db4609bcf7e4f8ee8ea
Mark Sims <mark.sims@acuris.com>
2022-02-16 17:35:03 +0000

    tries webpack config

---
+++ apps/storybook/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        include: path.resolve(__dirname, '../'),
+      },
+    ],
+  },
+};

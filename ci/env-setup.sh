5da72fbcf5dbb65b848ec0bc0e7f66dd2cdb687c
Mark Sims <mark.sims@acuris.com>
2022-02-16 13:24:00 +0000

    tries export variables

--- ci/env-setup.sh
+++ ci/env-setup.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
 
 ASAP_HOSTNAME=${ASAP_HOSTNAME:-"hub.asap.science"}
-if [ ! -z $CI_EXTERNAL_PULL_REQUEST_IID ]; then
     export ASAP_API_URL="https://api-${CI_EXTERNAL_PULL_REQUEST_IID}.${ASAP_HOSTNAME}"
     export ASAP_APP_URL="https://${CI_EXTERNAL_PULL_REQUEST_IID}.${ASAP_HOSTNAME}"
     export ALGOLIA_INDEX="asap-hub_research_outputs_CI-${CI_EXTERNAL_PULL_REQUEST_IID}"
-fi

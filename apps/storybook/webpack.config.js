780ef45762bcf37f784d927d2eeff767f8a1a979
Mark Sims <mark.sims@acuris.com>
2022-02-17 09:26:52 +0000

    revert this commit

--- apps/storybook/webpack.config.js
+++ apps/storybook/webpack.config.js
@@ -1,20 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react'],
-        },
-      },
-      {
-        test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader'],
-        include: path.resolve(__dirname, '../'),
-      },
-    ],
-  },
+  resolve: {
+    modules: ['stories'],
+  }
 };

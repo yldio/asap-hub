59cfb99a8f75e76d3e375b892caaed50c88355f4
Mark Sims <mark.sims@acuris.com>
2022-02-17 10:02:24 +0000

    run outside of container

--- apps/storybook/config/main.js
+++ apps/storybook/config/main.js
@@ -7,4 +7,33 @@ module.exports = {
 
     require.resolve('@storybook/addon-viewport/register'),
   ],
+  webpackFinal: (config) => {
+    console.log(config);
+    // config.module.rules.push({
+    //   test: /\.tsx?$/,
+    //   exclude: /node_modules/,
+    //   use: [
+    //     {
+    //       loader: require.resolve('babel-loader'),
+    //       options: {
+    //         presets: [
+    //           require('babel-preset-react-app').default,
+    //         ],
+    //       },
+    //     },
+    //   ],
+    // });
+
+    // config.resolve.extensions.push('.ts', '.tsx');
+
+    // config.module.rules.push({
+    //   test: /\.mjs$/,
+    //   include: /node_modules/,
+    //   type: 'javascript/auto',
+    // });
+
+    // config.resolve.extensions.push('.mjs');
+
+    return config;
+  },
 };

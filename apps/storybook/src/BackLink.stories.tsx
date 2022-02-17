3d7f8bd5e12096cf5f6a32327b631875d89fcc08
Mark Sims <mark.sims@acuris.com>
2022-02-17 11:45:33 +0000

    imports React

--- apps/storybook/src/BackLink.stories.tsx
+++ apps/storybook/src/BackLink.stories.tsx
@@ -1,5 +1,6 @@
 import { BackLink } from '@asap-hub/react-components';
 import { text } from '@storybook/addon-knobs';
+import * as React from "react";
 
 export default {
   title: 'Molecules / Back Link',

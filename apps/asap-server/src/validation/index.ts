b1a1dce71745b0931f1d446120dea738b6fbb1c7
Piotr Szpak <piotr.szpak@yld.io>
2022-02-22 13:56:42 +0000

    add coercion and fetch-options validation

--- apps/asap-server/src/validation/index.ts
+++ apps/asap-server/src/validation/index.ts
@@ -1,8 +1,9 @@
 import Boom from '@hapi/boom';
 import Ajv, { JSONSchemaType, ValidateFunction } from 'ajv';
-import { NullableOptionalProperties } from '../utils/types';
+import { FetchOptions, NullableOptionalProperties } from '../utils/types';
 
 const ajv = new Ajv();
+const ajvCoerced = new Ajv({ coerceTypes: true });
 
 // The below has to be declared as a function due to:
 // https://github.com/ajv-validator/ajv/issues/1814
@@ -10,6 +11,7 @@ export function validateInput<T, B extends boolean>(
   schema: JSONSchemaType<T>,
   options?: {
     skipNull?: B;
+    coerce?: boolean;
   },
 ): (
   data: any,
@@ -18,9 +20,10 @@ export function validateInput<T>(
   schema: JSONSchemaType<T>,
   options?: {
     skipNull?: boolean;
+    coerce?: boolean;
   },
 ): (data: any) => NonNullable<T> | NullableOptionalProperties<T> {
-  const ajvValidation = ajv.compile(schema);
+  const ajvValidation = (options?.coerce ? ajvCoerced : ajv).compile(schema);
 
   return (data) => {
     if (validate(ajvValidation, data)) {
@@ -37,7 +40,9 @@ export function validateInput<T>(
     const errors = ajvValidation.errors;
 
     throw Boom.badRequest(
-      `Error "${errors?.[0]?.propertyName}": ${errors?.[0]?.message}`,
+      `Error "${errors?.[0]?.propertyName || errors?.[0]?.instancePath}": ${
+        errors?.[0]?.message
+      }`,
       {
         details: errors,
       },
@@ -53,3 +58,26 @@ const validate = <T>(
   ajvValidation: ValidateFunction<T>,
   data: any,
 ): data is NullableOptionalProperties<T> => ajvValidation(data);
+
+const fetchOptionsValidationSchema: JSONSchemaType<FetchOptions> = {
+  type: 'object',
+  properties: {
+    take: { type: 'number', nullable: true },
+    skip: { type: 'number', nullable: true },
+    search: { type: 'string', nullable: true },
+    filter: {
+      type: 'array',
+      items: { type: 'string' },
+      nullable: true,
+    },
+  },
+  additionalProperties: false,
+};
+
+export const validateFetchOptions = validateInput(
+  fetchOptionsValidationSchema,
+  {
+    skipNull: true,
+    coerce: true,
+  },
+);

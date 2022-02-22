f9186e80806f510072c0b439340433c0e07de020
Mark Sims <mark.sims@acuris.com>
2022-02-22 11:56:08 +0000

    remove debug code

--- apps/storybook/config/main.js
+++ apps/storybook/config/main.js
@@ -1,4 +1,3 @@
-const path = require('path');
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: [
@@ -8,42 +7,4 @@ module.exports = {
 
     require.resolve('@storybook/addon-viewport/register'),
   ],
-  webpackFinal: async (config, configType) => {
-    // config.module.rules.push({
-    //   test: /\.tsx?$/,
-    //   exclude: /node_modules/,
-    //   use: [
-    //     {
-    //       loader: require.resolve('babel-loader'),
-    //       options: {
-    //         presets: [
-    //           require('babel-preset-react-app').default,
-    //         ],
-    //       },
-    //     },
-    //   ],
-    // });
-
-    // config.resolve.extensions.push('.ts', '.tsx');
-
-    // config.module.rules.push({
-    //   test: /\.mjs$/,
-    //   include: /node_modules/,
-    //   type: 'javascript/auto',
-    // });
-
-    // config.resolve.extensions.push('.mjs');
-
-    config.resolve.modules = [
-      path.resolve(__dirname, '../src'),
-      'node_modules',
-    ];
-
-    console.log(
-      `The final webpack config`,
-      JSON.stringify(config, undefined, 2),
-      configType,
-    );
-    return config;
-  },
 };

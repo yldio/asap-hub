7a30c3083270a4a72be787ead8cdf7f9b0643e67
Jhonny Moreira <jhonny.moreira@thetrainline.com>
2022-02-23 10:32:38 +0000

    fix test rebase

--- packages/react-components/src/organisms/__tests__/TeamCreateOutputForm.test.tsx
+++ packages/react-components/src/organisms/__tests__/TeamCreateOutputForm.test.tsx
@@ -68,6 +68,7 @@ it('can submit a form when form data is valid', async () => {
       title: 'example title',
       description: 'example description',
       subTypes: ['Animal Model'],
+      labs: [],
     });
     expect(screen.getByRole('button', { name: /Share/i })).toBeEnabled();
   });

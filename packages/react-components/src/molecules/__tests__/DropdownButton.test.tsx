1e2f08073aa299156ee74e1729a9c4e957dfd5e1
Thomas Waterfall <thomas.waterfall@gmail.com>
2022-02-23 11:27:45 +0000

    Tests

--- packages/react-components/src/molecules/__tests__/DropdownButton.test.tsx
+++ packages/react-components/src/molecules/__tests__/DropdownButton.test.tsx
@@ -2,11 +2,38 @@ import { render, fireEvent } from '@testing-library/react';
 
 import DropdownButton from '../DropdownButton';
 
-it('renders a dropdownButton button', () => {
+it('renders a DropdownButton', () => {
   const { getByRole } = render(
     <DropdownButton buttonChildren={() => <>Example</>} />,
   );
   expect(getByRole('button').textContent).toContain('Example');
+});
+
+it('renders a DropdownButton link item', () => {
+  const { getByText } = render(
+    <DropdownButton buttonChildren={() => <>Example</>}>
+      {{ item: 'example', href: 'http://example.com' }}
+      {{ item: 'link2', href: '#' }}
+    </DropdownButton>,
+  );
+  expect(getByText('example').closest('a')).toHaveAttribute(
+    'href',
+    'http://example.com',
+  );
+});
+
+it('renders a dropdownButton button item', () => {
+  const onClick = jest.fn();
+  const { getByText, getByRole } = render(
+    <DropdownButton buttonChildren={() => <>Example</>}>
+      {{ item: 'Example Button', onClick }}
+      {{ item: 'link2', href: '#' }}
+    </DropdownButton>,
+  );
+  fireEvent.click(getByRole('button'));
+  fireEvent.click(getByText('Example Button'));
+
+  expect(onClick).toHaveBeenCalled();
 });
 
 it('renders a modal on click', () => {

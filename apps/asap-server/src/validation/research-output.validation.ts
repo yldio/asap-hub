355cd5b3ebf66de8c89f46566e567ec326a584f5
Piotr Szpak <piotr.szpak@yld.io>
2022-02-25 16:19:32 +0000

    update research outputs endpoints

---
+++ apps/asap-server/src/validation/research-output.validation.ts
@@ -0,0 +1,80 @@
+import {
+  ResearchOutputPostRequest,
+  researchOutputSubtypes,
+  researchOutputTypes,
+} from '@asap-hub/model';
+import { JSONSchemaType } from 'ajv';
+import { validateInput } from '.';
+
+type ResearchOutputParameters = {
+  researchOutputId: string;
+};
+
+const researchOutputParametersValidationSchema: JSONSchemaType<ResearchOutputParameters> =
+  {
+    type: 'object',
+    properties: {
+      researchOutputId: { type: 'string' },
+    },
+    required: ['researchOutputId'],
+    additionalProperties: false,
+  };
+
+export const validateResearchOutputParameters = validateInput(
+  researchOutputParametersValidationSchema,
+  {
+    skipNull: false,
+    coerce: false,
+  },
+);
+
+const researchOutputPostRequestValidationSchema: JSONSchemaType<ResearchOutputPostRequest> =
+  {
+    type: 'object',
+    properties: {
+      type: {
+        type: 'string',
+        enum: researchOutputTypes,
+      },
+      subTypes: {
+        type: 'array',
+        items: { type: 'string', enum: researchOutputSubtypes },
+        nullable: true,
+      },
+      description: { type: 'string', nullable: true },
+      tags: {
+        type: 'array',
+        items: { type: 'string' },
+      },
+      link: { type: 'string' },
+      title: { type: 'string' },
+      asapFunded: { type: 'boolean', nullable: true },
+      sharingStatus: { type: 'string' },
+      usedInPublication: { type: 'boolean', nullable: true },
+      addedDate: { type: 'string' },
+      teamId: { type: 'string' },
+      labs: {
+        type: 'array',
+        items: { type: 'string' },
+        nullable: true,
+      },
+    },
+    required: [
+      'type',
+      'tags',
+      'link',
+      'title',
+      'sharingStatus',
+      'addedDate',
+      'teamId',
+    ],
+    additionalProperties: false,
+  };
+
+export const validateResearchOutputPostRequestParameters = validateInput(
+  researchOutputPostRequestValidationSchema,
+  {
+    skipNull: true,
+    coerce: false,
+  },
+);

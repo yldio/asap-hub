dac9e998d668f48dd10a13b9759735ed99c8f65a
Piotr Szpak <piotr.szpak@yld.io>
2022-02-25 13:13:30 +0000

    update teams endpoints

---
+++ apps/asap-server/src/validation/team.validation.ts
@@ -0,0 +1,54 @@
+import { TeamPatchRequest } from '@asap-hub/model';
+import { JSONSchemaType } from 'ajv';
+import { validateInput } from '.';
+
+type TeamParameters = {
+  teamId: string;
+};
+
+const teamParametersValidationSchema: JSONSchemaType<TeamParameters> = {
+  type: 'object',
+  properties: {
+    teamId: { type: 'string' },
+  },
+  required: ['teamId'],
+  additionalProperties: false,
+};
+
+export const validateTeamParameters = validateInput(
+  teamParametersValidationSchema,
+  {
+    skipNull: false,
+    coerce: false,
+  },
+);
+
+const teamPatchRequestValidationSchema: JSONSchemaType<TeamPatchRequest> = {
+  type: 'object',
+  properties: {
+    tools: {
+      type: 'array',
+      items: {
+        type: 'object',
+        additionalProperties: false,
+        properties: {
+          name: { type: 'string' },
+          url: { type: 'string' },
+          description: { type: 'string', nullable: true },
+        },
+        required: ['name', 'url'],
+      },
+      nullable: true,
+    },
+  },
+  required: ['tools'],
+  additionalProperties: false,
+};
+
+export const validateTeamPatchRequest = validateInput(
+  teamPatchRequestValidationSchema,
+  {
+    skipNull: true,
+    coerce: false,
+  },
+);

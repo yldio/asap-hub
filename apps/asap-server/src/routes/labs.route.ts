ef664297f9d59c66e42490783d8fe84f64d3aad4
Piotr Szpak <piotr.szpak@yld.io>
2022-02-25 17:04:50 +0000

    update labs endpoint

--- apps/asap-server/src/routes/labs.route.ts
+++ apps/asap-server/src/routes/labs.route.ts
@@ -1,18 +1,12 @@
-import { framework } from '@asap-hub/services-common';
 import { Router } from 'express';
 import { LabsController } from '../controllers/labs';
-import { querySchema } from './teams.route';
-import { FetchOptions } from '../utils/types';
+import { validateFetchOptions } from '../validation';
 
 export const labsRouteFactory = (labsController: LabsController): Router => {
   const labsRoutes = Router();
 
   labsRoutes.get('/labs', async (req, res) => {
-    const options = framework.validate(
-      'query',
-      req.query,
-      querySchema,
-    ) as unknown as FetchOptions;
+    const options = validateFetchOptions(req.query);
 
     const result = await labsController.fetch(options);
 

7ec88a0369d9148680bca89051ddd7d41bb4735a
Mateusz Niezgoda <kontakt@mniezgoda.pl>
2022-02-27 19:52:48 +0100

    remove saveEntity from algolia's client mock

--- apps/asap-server/test/mocks/algolia-client.mock.ts
+++ apps/asap-server/test/mocks/algolia-client.mock.ts
@@ -2,7 +2,6 @@ import { AlgoliaSearchClient } from '@asap-hub/algolia';
 
 export const algoliaSearchClientMock = {
   save: jest.fn(),
-  saveEntity: jest.fn(),
   saveMany: jest.fn(),
   remove: jest.fn(),
   searchEntity: jest.fn(),

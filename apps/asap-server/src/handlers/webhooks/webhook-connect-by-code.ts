a2672c061d0455c52cb6cae8118fd09d2aa6b219
Marco Rotili <marco.livio.rotili@gmail.com>
2022-03-01 14:36:22 +0100

    Remove joi from webhook-connect-by-code.ts

--- apps/asap-server/src/handlers/webhooks/webhook-connect-by-code.ts
+++ apps/asap-server/src/handlers/webhooks/webhook-connect-by-code.ts
@@ -1,27 +1,36 @@
-import Joi from '@hapi/joi';
 import { SquidexGraphql } from '@asap-hub/squidex';
 import { framework as lambda } from '@asap-hub/services-common';
 
+import { JSONSchemaType } from 'ajv';
 import Users from '../../controllers/users';
 import { Handler } from '../../utils/types';
 import validateRequest from '../../utils/validate-auth0-request';
+import { validateInput } from '../../validation';
+
+type Body = {
+  code: string;
+  userId: string;
+};
+
+const bodySchema: JSONSchemaType<Body> = {
+  type: 'object',
+  properties: {
+    code: { type: 'string' },
+    userId: { type: 'string' },
+  },
+  required: ['code'],
+  additionalProperties: false,
+};
+
+const validateBody = validateInput(bodySchema, {
+  skipNull: false,
+  coerce: true,
+});
 
 export const handler: Handler = lambda.http(async (request) => {
   await validateRequest(request);
 
-  const bodySchema = Joi.object({
-    code: Joi.string().required(),
-    userId: Joi.string().required(),
-  }).required();
-
-  const { code, userId } = lambda.validate(
-    'body',
-    request.payload,
-    bodySchema,
-  ) as {
-    code: string;
-    userId: string;
-  };
+  const { code, userId } = validateBody(request.payload as never);
 
   const squidexGraphqlClient = new SquidexGraphql();
   const users = new Users(squidexGraphqlClient);

5662907707f0854d29a0f1ea625332596d41b5d7
marot <74212402+marot@users.noreply.github.com>
2022-03-02 10:01:27 +0100

    CRN-727 remove collapsing functionality in group description (#1375)

--- packages/react-components/src/organisms/GroupInformation.tsx
+++ packages/react-components/src/organisms/GroupInformation.tsx
@@ -1,41 +1,22 @@
-import { useState } from 'react';
 import { GroupResponse } from '@asap-hub/model';
 
-import { Button, Card, Headline3, Paragraph, Divider } from '../atoms';
+import { Card, Divider, Headline3, Paragraph } from '../atoms';
 import TagList from '../molecules/TagList';
-
-const TRIM_CONTENT = 160;
 
 type GroupInformationProps = Pick<GroupResponse, 'description' | 'tags'>;
 const GroupInformation: React.FC<GroupInformationProps> = ({
   description,
   tags,
-}) => {
-  const [showMore, setShowMore] = useState(false);
-  return (
-    <Card>
-      <Headline3>Group Description</Headline3>
-      <Paragraph accent="lead">
-        <span css={{ whiteSpace: 'pre-line' }}>
-          {showMore
-            ? description
-            : description.substring(0, TRIM_CONTENT).trim()}
-          {TRIM_CONTENT < description.length && (
-            <>
-              {!showMore && '…'}
-              <br />
-              <Button onClick={() => setShowMore(!showMore)} linkStyle>
-                {showMore ? 'Show less' : 'Show more'}
-              </Button>
-            </>
-          )}
-        </span>
-      </Paragraph>
-      <Divider />
-      <Headline3>Group Expertise</Headline3>
-      <TagList tags={tags} />
-    </Card>
-  );
-};
+}) => (
+  <Card>
+    <Headline3>Group Description</Headline3>
+    <Paragraph accent="lead">
+      <span css={{ whiteSpace: 'pre-line' }}>{description}</span>
+    </Paragraph>
+    <Divider />
+    <Headline3>Group Expertise</Headline3>
+    <TagList tags={tags} />
+  </Card>
+);
 
 export default GroupInformation;

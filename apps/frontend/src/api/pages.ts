319fa1be202c38db161c92a9213429dedfb00b32
anaescontrela <ana.escontrela@yld.io>
2022-03-07 15:16:22 +0000

    removes unused code (use-http)

--- apps/frontend/src/api/pages.ts
+++
@@ -1,7 +0,0 @@
-import { join } from 'path';
-import { PageResponse } from '@asap-hub/model';
-
-import { useGetOne } from './get-one';
-
-export const usePageByPath = (path: string) =>
-  useGetOne<PageResponse>(join('pages', path), { authenticated: false });

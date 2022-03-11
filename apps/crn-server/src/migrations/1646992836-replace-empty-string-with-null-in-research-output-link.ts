7f81753c483391fce73dc699de0fa9ac0ca62849
Marco Rotili <marco.livio.rotili@gmail.com>
2022-03-11 11:11:30 +0100

    add migration to convert empty strings into null

---
+++ apps/crn-server/src/migrations/1646992836-replace-empty-string-with-null-in-research-output-link.ts
@@ -0,0 +1,22 @@
+/* istanbul ignore file */
+import { RestResearchOutput } from '@asap-hub/squidex';
+import { Migration } from '../handlers/webhooks/webhook-run-migrations';
+import { applyToAllItemsInCollection } from '../utils/migrations';
+
+export default class MapResearchOutputDeprecatedSubtype extends Migration {
+  up = async (): Promise<void> => {
+    await applyToAllItemsInCollection<RestResearchOutput>(
+      'research-outputs',
+      async (researchOutput, squidexClient) => {
+        if (researchOutput?.data?.link?.iv === '') {
+          await squidexClient.patch(researchOutput.id, {
+            link: undefined
+          });
+        }
+      },
+    );
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  down = async (): Promise<void> => {};
+}

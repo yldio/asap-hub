8ed08822b9e0e94bd120a6616b3cfc24e192d938
Mark Sims <mark.sims@acuris.com>
2022-03-14 12:06:34 +0000

    tries more scripts

---
+++ .github/scripts/squidex-sync/config-rules.sh
@@ -0,0 +1,14 @@
+# Uses dir passed as argument or default one
+backupPath=${1:-"packages/squidex/schema/rules"}
+
+# Replace PLACEHOLDERS with variable available on the CI
+find $backupPath \
+  -type f \( -name "*.json" -not -name "__rule.json" \) \
+  -print0 | xargs -t -0 -I @file \
+  sed -i 's/SQUIDEX_SHARED_SECRET/'"$SQUIDEX_SHARED_SECRET"'/' @file
+
+# Replace PLACEHOLDERS with variable available on the CI
+find $backupPath \
+  -type f \( -name "*.json" -not -name "__rule.json" \) \
+  -print0 | xargs -t -0 -I @file \
+  sed -i 's#ASAP_API_URL#'"$ASAP_API_URL"'#' @file

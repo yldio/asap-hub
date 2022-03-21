2d545c18cf7403259fc526c430ef7d386df6f369
Piotr Szpak <piotr.szpak@yld.io>
2022-03-21 14:58:04 +0000

    add gp2 frontend s3

--- apps/gp2-server/serverless.ts
+++ apps/gp2-server/serverless.ts
@@ -1,19 +1,23 @@
-// import assert from 'assert';
+import assert from 'assert';
 import { AWS } from '@serverless/typescript';
 
-// ['AWS_ACM_CERTIFICATE_ARN', 'ASAP_HOSTNAME'].forEach((env) => {
-//   assert.ok(process.env[env], `${env} not defined`);
-// });
+['AWS_ACM_CERTIFICATE_ARN', 'ASAP_HOSTNAME'].forEach((env) => {
+  assert.ok(process.env[env], `${env} not defined`);
+});
 
 const region =
   (process.env.AWS_REGION as AWS['provider']['region']) || 'us-east-1';
 const service = 'gp2-hub';
 const stage = '12345-gp2';
-// const apiHostname = 'api-12345.gp2.asap.science';
-// const hostedZone = process.env.ASAP_HOSTNAME;
-// const acmCertificateArn = process.env.AWS_ACM_CERTIFICATE_ARN;
+const apiHostname = 'api-12345.gp2.asap.science';
+const appHostname = '12345.gp2.asap.science';
+const hostedZone = process.env.ASAP_HOSTNAME;
+const acmCertificateArn = process.env.AWS_ACM_CERTIFICATE_ARN;
 
-export const plugins = ['./serverless-plugins/serverless-webpack'];
+export const plugins = [
+  './serverless-plugins/serverless-webpack',
+  './serverless-plugins/serverless-s3-sync',
+];
 
 const serverlessConfig: AWS = {
   service,
@@ -48,8 +52,15 @@ const serverlessConfig: AWS = {
   custom: {
     webpack: {
       config: './webpack.config.js',
-      packager: 'yarn'
+      packager: 'yarn',
     },
+    s3Sync: [
+      {
+        bucketName: '${self:service}-${self:provider.stage}-gp2-frontend',
+        deleteRemoved: false,
+        localDir: '../gp2-frontend/build',
+      },
+    ],
   },
   functions: {
     apiHandler: {
@@ -64,52 +75,193 @@ const serverlessConfig: AWS = {
       ],
     },
   },
-  // resources: {
-  //   Resources: {
-  //     HttpApiDomain: {
-  //       Type: 'AWS::ApiGatewayV2::DomainName',
-  //       Properties: {
-  //         DomainName: apiHostname,
-  //         DomainNameConfigurations: [
-  //           {
-  //             CertificateArn: acmCertificateArn,
-  //             EndpointType: 'REGIONAL',
-  //           },
-  //         ],
-  //       },
-  //     },
-  //     HttpApiApiMapping: {
-  //       Type: 'AWS::ApiGatewayV2::ApiMapping',
-  //       DependsOn: ['HttpApiDomain'],
-  //       Properties: {
-  //         ApiId: { Ref: 'HttpApi' },
-  //         ApiMappingKey: '',
-  //         DomainName: apiHostname,
-  //         Stage: { Ref: 'HttpApiStage' },
-  //       },
-  //     },
-  //     HttpApiRecordSetGroup: {
-  //       Type: 'AWS::Route53::RecordSetGroup',
-  //       Properties: {
-  //         HostedZoneName: hostedZone,
-  //         RecordSets: [
-  //           {
-  //             Name: apiHostname,
-  //             Type: 'A',
-  //             AliasTarget: {
-  //               DNSName: {
-  //                 'Fn::GetAtt': ['HttpApiDomain', 'RegionalDomainName'],
-  //               },
-  //               HostedZoneId: {
-  //                 'Fn::GetAtt': ['HttpApiDomain', 'RegionalHostedZoneId'],
-  //               },
-  //             },
-  //           },
-  //         ],
-  //       },
-  //     },
-  //   },
-  // },
+  resources: {
+    Resources: {
+      HttpApiDomain: {
+        Type: 'AWS::ApiGatewayV2::DomainName',
+        Properties: {
+          DomainName: apiHostname,
+          DomainNameConfigurations: [
+            {
+              CertificateArn: acmCertificateArn,
+              EndpointType: 'REGIONAL',
+            },
+          ],
+        },
+      },
+      HttpApiApiMapping: {
+        Type: 'AWS::ApiGatewayV2::ApiMapping',
+        DependsOn: ['HttpApiDomain'],
+        Properties: {
+          ApiId: { Ref: 'HttpApi' },
+          ApiMappingKey: '',
+          DomainName: apiHostname,
+          Stage: { Ref: 'HttpApiStage' },
+        },
+      },
+      HttpApiRecordSetGroup: {
+        Type: 'AWS::Route53::RecordSetGroup',
+        Properties: {
+          HostedZoneName: hostedZone,
+          RecordSets: [
+            {
+              Name: apiHostname,
+              Type: 'A',
+              AliasTarget: {
+                DNSName: {
+                  'Fn::GetAtt': ['HttpApiDomain', 'RegionalDomainName'],
+                },
+                HostedZoneId: {
+                  'Fn::GetAtt': ['HttpApiDomain', 'RegionalHostedZoneId'],
+                },
+              },
+            },
+          ],
+        },
+      },
+      FrontendBucket: {
+        Type: 'AWS::S3::Bucket',
+        DeletionPolicy: 'Delete',
+        Properties: {
+          BucketName: '${self:service}-${self:provider.stage}-gp2-frontend',
+          AccessControl: 'PublicRead',
+          CorsConfiguration: {
+            CorsRules: [
+              {
+                AllowedMethods: ['GET', 'HEAD'],
+                AllowedHeaders: ['*'],
+                AllowedOrigins: ['*'],
+                MaxAge: 3000,
+              },
+            ],
+          },
+        },
+      },
+      BucketPolicyFrontend: {
+        Type: 'AWS::S3::BucketPolicy',
+        Properties: {
+          Bucket: '${self:service}-${self:provider.stage}-gp2-frontend',
+          PolicyDocument: {
+            Statement: [
+              {
+                Action: ['s3:GetObject'],
+                Effect: 'Allow',
+                Principal: '*',
+                Resource: {
+                  'Fn::Join': [
+                    '',
+                    [{ 'Fn::GetAtt': ['FrontendBucket', 'Arn'] }, '/*'],
+                  ],
+                },
+              },
+            ],
+          },
+        },
+      },
+      CloudFrontOriginAccessIdentityFrontend: {
+        Type: 'AWS::CloudFront::CloudFrontOriginAccessIdentity',
+        Properties: {
+          CloudFrontOriginAccessIdentityConfig: {
+            Comment: { Ref: 'FrontendBucket' },
+          },
+        },
+      },
+      CloudFrontDistribution: {
+        Type: 'AWS::CloudFront::Distribution',
+        DependsOn: ['FrontendBucket'],
+        Properties: {
+          DistributionConfig: {
+            Aliases: [appHostname],
+            CustomErrorResponses: [
+              {
+                ErrorCode: 404,
+                ResponseCode: 200,
+                ResponsePagePath: '/index.html',
+              },
+            ],
+            Origins: [
+              {
+                DomainName: {
+                  'Fn::GetAtt': ['FrontendBucket', 'RegionalDomainName'],
+                },
+                Id: 's3origin-frontend',
+                S3OriginConfig: {
+                  OriginAccessIdentity: {
+                    'Fn::Join': [
+                      '/',
+                      [
+                        'origin-access-identity/cloudfront',
+                        { Ref: 'CloudFrontOriginAccessIdentityFrontend' },
+                      ],
+                    ],
+                  },
+                },
+              },
+              {
+                CustomOriginConfig: {
+                  OriginProtocolPolicy: 'https-only',
+                },
+                DomainName: {
+                  'Fn::Join': [
+                    '.',
+                    [
+                      { Ref: 'HttpApi' },
+                      'execute-api',
+                      { Ref: 'AWS::Region' },
+                      { Ref: 'AWS::URLSuffix' },
+                    ],
+                  ],
+                },
+                Id: 'apigw',
+              },
+            ],
+            DefaultCacheBehavior: {
+              AllowedMethods: ['GET', 'HEAD', 'OPTIONS'],
+              CachedMethods: ['GET', 'HEAD', 'OPTIONS'],
+              Compress: true,
+              DefaultTTL: 3600,
+              ForwardedValues: {
+                Cookies: {
+                  Forward: 'none',
+                },
+                QueryString: false,
+              },
+              TargetOriginId: 's3origin-frontend',
+              ViewerProtocolPolicy: 'redirect-to-https',
+            },
+            CacheBehaviors: [],
+            DefaultRootObject: 'index.html',
+            Enabled: true,
+            PriceClass: 'PriceClass_100',
+            ViewerCertificate: {
+              AcmCertificateArn: acmCertificateArn,
+              MinimumProtocolVersion: 'TLSv1.2_2018',
+              SslSupportMethod: 'sni-only',
+            },
+          },
+        },
+      },
+      CloudFrontRecordSetGroup: {
+        Type: 'AWS::Route53::RecordSetGroup',
+        Properties: {
+          HostedZoneName: hostedZone,
+          RecordSets: [
+            {
+              Name: appHostname,
+              Type: 'A',
+              AliasTarget: {
+                DNSName: {
+                  'Fn::GetAtt': ['CloudFrontDistribution', 'DomainName'],
+                },
+                // https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-route53-aliastarget.html#cfn-route53-aliastarget-hostedzoneid
+                HostedZoneId: 'Z2FDTNDATAQYW2',
+              },
+            },
+          ],
+        },
+      },
+    },
+  },
 };
 
 module.exports = serverlessConfig;

90539a5a88babb9f4fd1f32366bab8b7232165ae
Jhonny Moreira <jhonny.moreira@thetrainline.com>
2022-04-01 12:08:57 +0100

    fix on api-util imports

--- apps/gp2-frontend/src/api-util.ts
+++ apps/gp2-frontend/src/api-util.ts
@@ -1,28 +1,10 @@
 import { configureScope } from '@sentry/react';
-import { API_BASE_URL } from './config';
 
 export type GetListOptions = {
   searchQuery: string;
   filters: Set<string>;
   currentPage: number | null;
   pageSize: number | null;
-};
-
-export const createListApiUrl = (
-  endpoint: string,
-  { searchQuery, filters, currentPage, pageSize }: GetListOptions,
-): URL => {
-  const url = new URL(endpoint, `${API_BASE_URL}/`);
-  if (searchQuery) url.searchParams.set('search', searchQuery);
-  if (pageSize !== null) {
-    url.searchParams.set('take', String(pageSize));
-    if (currentPage !== null) {
-      url.searchParams.set('skip', String(currentPage * pageSize));
-    }
-  }
-  filters.forEach((filter) => url.searchParams.append('filter', filter));
-
-  return url;
 };
 
 export const createSentryHeaders = () => {

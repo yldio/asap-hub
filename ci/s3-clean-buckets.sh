8529777d936c6f1a5712703c21cf1a0c3a6773c8
Piotr Szpak <piotr.szpak@yld.io>
2022-04-19 15:08:32 +0100

    remove CI dir

--- ci/s3-clean-buckets.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-# use this script if you need to clean up feature environments
-
-aws s3 ls | awk '{print $3}' | grep '^asap-hub-[[:digit:]]' | xargs -I % aws s3 rb s3://% --force
-

6c0519a06f1c092ac420f445ee79955210e3f289
Mark Sims <mark@bluepie.co.uk>
2022-04-21 11:33:12 +0100

    fixes broken tests

--- apps/crn-frontend/src/events/__tests__/Events.test.tsx
+++ apps/crn-frontend/src/events/__tests__/Events.test.tsx
@@ -1,7 +1,3 @@
-import { Suspense } from 'react';
-import { MemoryRouter, Route } from 'react-router-dom';
-import { render, waitFor } from '@testing-library/react';
-import { RecoilRoot } from 'recoil';
 import {
   Auth0Provider,
   WhenReady,
@@ -11,14 +7,17 @@ import {
   createListEventResponse,
 } from '@asap-hub/fixtures';
 import { events } from '@asap-hub/routing';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
-import Events from '../Events';
-import { refreshCalendarsState } from '../calendar/state';
-import { getCalendars } from '../calendar/api';
+import { Suspense } from 'react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 import { getEvents } from '../api';
-import { eventsState } from '../state';
+import { getCalendars } from '../calendar/api';
+import { refreshCalendarsState } from '../calendar/state';
+import Events from '../Events';
 import { getEventListOptions } from '../options';
+import { eventsState } from '../state';
 
 jest.useFakeTimers('modern');
 
@@ -52,30 +51,41 @@ const renderEventsPage = async (pathname = events({}).$) => {
     </RecoilRoot>,
   );
   await waitFor(() =>
-    expect(result.queryByText(/loading/i)).not.toBeInTheDocument(),
+    expect(screen.queryByText(/loading/i)).not.toBeInTheDocument(),
   );
   return result;
 };
 
 it('Renders the events page header', async () => {
   mockGetCalendars.mockResolvedValue(createListCalendarResponse(0));
-  const { getByRole } = await renderEventsPage();
-  expect(getByRole('heading', { level: 1 }).textContent).toEqual(
+  await renderEventsPage();
+  expect(screen.getByRole('heading', { level: 1 }).textContent).toEqual(
     'Calendar and Events',
   );
 });
 
-it('Defaults to the calendar page', async () => {
-  mockGetCalendars.mockResolvedValue(createListCalendarResponse(0));
-  const { getByTitle } = await renderEventsPage();
-  expect(getByTitle('Calendar').tagName).toBe('IFRAME');
+it('Defaults to the upcoming events page', async () => {
+  mockGetEvents.mockResolvedValue({
+    ...createListEventResponse(1),
+    items: createListEventResponse(1).items.map((item, index) => ({
+      ...item,
+      title: `Event title ${index}`,
+    })),
+  });
+  await renderEventsPage();
+  expect(
+    screen
+      .getAllByRole('heading', { level: 3 })
+      .map((heading) => heading.textContent),
+  ).toEqual(['Event title 0']);
 });
 
 describe('the events calendar page', () => {
   it('renders a google calendar iframe', async () => {
     mockGetCalendars.mockResolvedValue(createListCalendarResponse(0));
-    const { getByTitle } = await renderEventsPage(events({}).calendar({}).$);
-    expect(getByTitle('Calendar').tagName).toBe('IFRAME');
+    await renderEventsPage(events({}).calendar({}).$);
+    const calendars = screen.getByTitle('Calendar');
+    expect(calendars.tagName).toBe('IFRAME');
   });
 
   it('Displays a list of calendars', async () => {
@@ -86,9 +96,9 @@ describe('the events calendar page', () => {
         name: `Calendar title ${index}`,
       })),
     });
-    const { getByText } = await renderEventsPage(events({}).calendar({}).$);
-    expect(getByText(/calendar title 0/i)).toBeVisible();
-    expect(getByText(/calendar title 1/i)).toBeVisible();
+    await renderEventsPage(events({}).calendar({}).$);
+    expect(screen.getByText(/calendar title 0/i)).toBeVisible();
+    expect(screen.getByText(/calendar title 1/i)).toBeVisible();
   });
 });
 
@@ -101,17 +111,17 @@ describe('the events upcoming page', () => {
         title: `Event title ${index}`,
       })),
     });
-    const { getAllByRole } = await renderEventsPage(events({}).upcoming({}).$);
+    await renderEventsPage(events({}).upcoming({}).$);
     expect(
-      getAllByRole('heading', { level: 3 }).map(
-        (heading) => heading.textContent,
-      ),
+      screen
+        .getAllByRole('heading', { level: 3 })
+        .map((heading) => heading.textContent),
     ).toEqual(['Event title 0', 'Event title 1']);
   });
 
   it('can search for events', async () => {
-    const { getByRole } = await renderEventsPage(events({}).upcoming({}).$);
-    userEvent.type(getByRole('searchbox'), 'searchterm');
+    await renderEventsPage(events({}).upcoming({}).$);
+    userEvent.type(screen.getByRole('searchbox'), 'searchterm');
     await waitFor(() =>
       expect(mockGetEvents).toHaveBeenLastCalledWith(
         expect.objectContaining({ searchQuery: 'searchterm' }),

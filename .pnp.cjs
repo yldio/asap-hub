1612c9a27d9a6c3540aa8e728b4c86f00b179df1
Mark Sims <mark@bluepie.co.uk>
2022-05-04 12:23:49 +0100

    bumps babel/runtime-corejs3

--- .pnp.cjs
+++ .pnp.cjs
@@ -10851,7 +10851,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [
             ["@asap-hub/errors", "workspace:packages/errors"],
             ["@asap-hub/eslint-config-asap-hub", "workspace:packages/eslint-config-asap-hub"],
-            ["@babel/runtime-corejs3", "npm:7.14.0"]
+            ["@babel/runtime-corejs3", "npm:7.17.9"]
           ],
           "linkType": "SOFT",
         }]

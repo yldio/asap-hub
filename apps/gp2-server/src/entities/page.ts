4510b03a3de98ea5198474543c6fb6d25ca47863
Piotr Szpak <piotr.szpak@yld.io>
2022-05-05 07:49:28 +0100

    add tests and ignore some files from coverage

--- apps/gp2-server/src/entities/page.ts
+++ apps/gp2-server/src/entities/page.ts
@@ -1,3 +1,5 @@
+/* istanbul ignore file */
+// ignore this file for coverage since we don't have the requirements yet to test it
 import { PageResponse } from '@asap-hub/model';
 import { GraphqlPage, RestPage } from '@asap-hub/squidex';
 
--- packages/squidex/test/utils.test.ts
+++ packages/squidex/test/utils.test.ts
@@ -1,19 +1,48 @@
-import { parseDate, createURL } from '../src/utils';
-import { config } from '@asap-hub/squidex';
+import config from '../src/config';
+import {
+  parseDate,
+  createURL,
+  parseToSquidex,
+  sanitiseForSquidex,
+} from '../src/utils';
 
-describe('generate asset url from cms assets list', () => {
-  test('return a url of the assets', async () => {
-    expect(createURL(['1', '2'])).toEqual([
-      `${config.baseUrl}/api/assets/${config.appName}/1`,
-      `${config.baseUrl}/api/assets/${config.appName}/2`,
-    ]);
+describe('Squidex Utils', () => {
+  describe('createUrl - generate asset url from cms assets list', () => {
+    test('return a url of the assets', async () => {
+      expect(createURL(['1', '2'])).toEqual([
+        `${config.baseUrl}/api/assets/${config.appName}/1`,
+        `${config.baseUrl}/api/assets/${config.appName}/2`,
+      ]);
+    });
   });
-});
 
-describe('dates from squidex', () => {
-  test('parse return Date type', async () => {
-    expect(parseDate('2020-09-08T03:05:08Z')).toEqual(
-      new Date('2020-09-08T03:05:08.000Z'),
-    );
+  describe('parseDate - parse dates from squidex', () => {
+    test('parse return Date type', async () => {
+      expect(parseDate('2020-09-08T03:05:08Z')).toEqual(
+        new Date('2020-09-08T03:05:08.000Z'),
+      );
+    });
+  });
+
+  describe('sanitiseForSquidex - sanitise text for squidex', () => {
+    test('sanitise return string', async () => {
+      expect(sanitiseForSquidex(`"'+/?#&`)).toEqual('%22%27%27%2B%2F%3F%23%26');
+    });
+  });
+
+  describe('parseToSquidex - parse object to squidex', () => {
+    test('parse return object', async () => {
+      const object = {
+        a: 'a',
+        b: 'b',
+        c: 'c',
+      };
+
+      expect(parseToSquidex(object)).toEqual({
+        a: { iv: 'a' },
+        b: { iv: 'b' },
+        c: { iv: 'c' },
+      });
+    });
   });
 });

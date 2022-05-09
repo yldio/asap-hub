84a8ce254252ad063cda9c8e9b545feea065d39b
Mark Sims <mark@bluepie.co.uk>
2022-05-09 15:21:10 +0100

    removes AUTH0 defaults

--- packages/auth/src/config.ts
+++ packages/auth/src/config.ts
@@ -1,4 +1,5 @@
-export const domain: string =
-  process.env.AUTH0_DOMAIN || 'dev-asap-hub.us.auth0.com';
-export const clientID: string =
-  process.env.AUTH0_CLIENT_ID || 'xRDvgZe3Ql3LSZDs2dWQYzcohFnLyeL2';
+export const { AUTH0_DOMAIN: domain, AUTH0_CLIENT_ID: clientID } =
+  process.env as {
+    AUTH0_DOMAIN: string;
+    AUTH0_CLIENT_ID: string;
+  };

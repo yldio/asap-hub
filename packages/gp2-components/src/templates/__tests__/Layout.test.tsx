2149c1bbd6d359a78ce06e316c67b7875853ee8c
Jhonny Moreira <jhonny.moreira@thetrainline.com>
2022-05-26 13:45:32 +0100

    adding test for menu toggle

--- packages/gp2-components/src/templates/__tests__/Layout.test.tsx
+++ packages/gp2-components/src/templates/__tests__/Layout.test.tsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Layout } from '..';
 
 describe('Layout', () => {
@@ -9,5 +10,19 @@ describe('Layout', () => {
   it('renders the content', async () => {
     const { getByText } = render(<Layout>Content</Layout>);
     expect(getByText('Content')).toBeVisible();
+  });
+
+  it('renders and toggles the open and close menu button', async () => {
+    const { getByLabelText, queryByTitle } = render(<Layout>Content</Layout>);
+    expect(queryByTitle(/close/i)).not.toBeInTheDocument();
+    expect(queryByTitle(/menu/i)).toBeInTheDocument();
+
+    userEvent.click(getByLabelText(/toggle menu/i));
+    expect(queryByTitle(/close/i)).toBeInTheDocument();
+    expect(queryByTitle(/menu/i)).not.toBeInTheDocument();
+
+    userEvent.click(getByLabelText(/toggle menu/i));
+    expect(queryByTitle(/close/i)).not.toBeInTheDocument();
+    expect(queryByTitle(/menu/i)).toBeInTheDocument();
   });
 });

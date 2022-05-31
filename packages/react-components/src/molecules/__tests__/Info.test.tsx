d421c8c2a8db077c29f37e411350efb898c7af3c
Piotr S <piotr.szpak@yld.io>
2022-05-31 07:55:36 +0100

    feat: CRN-845 Update info icon (#1622)

--- packages/react-components/src/molecules/__tests__/Info.test.tsx
+++ packages/react-components/src/molecules/__tests__/Info.test.tsx
@@ -1,6 +1,5 @@
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { findParentWithStyle } from '@asap-hub/dom-test-utils';
 
 import Info from '../Info';
 
@@ -18,19 +17,4 @@ it('shows a the children when clicking the info icon', () => {
   const { getByTitle, getByText } = render(<Info>text</Info>);
   userEvent.click(getByTitle(/info/i));
   expect(getByText('text')).toBeVisible();
-});
-it('reflects the expanded state in the icon', () => {
-  const { getByTitle } = render(<Info>text</Info>);
-  const { stroke: initialStroke } = findParentWithStyle(
-    getByTitle(/info/i),
-    'stroke',
-  )!;
-
-  userEvent.click(getByTitle(/info/i));
-  const { stroke: expandedStroke } = findParentWithStyle(
-    getByTitle(/info/i),
-    'stroke',
-  )!;
-
-  expect(expandedStroke).not.toEqual(initialStroke);
 });

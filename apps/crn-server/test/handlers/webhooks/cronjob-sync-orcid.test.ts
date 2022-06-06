fac66e2ad04161114bb2be21b5598c1a30916d40
Mark Sims <mark@bluepie.co.uk>
2022-06-06 13:21:58 +0100

    use mock instead of nock

--- apps/crn-server/test/handlers/webhooks/cronjob-sync-orcid.test.ts
+++ apps/crn-server/test/handlers/webhooks/cronjob-sync-orcid.test.ts
@@ -1,28 +1,31 @@
-import { OrcidWork } from '@asap-hub/model';
-import { config, SquidexGraphql } from '@asap-hub/squidex';
+import { RestUser, SquidexGraphql, SquidexRestClient } from '@asap-hub/squidex';
 import nock from 'nock';
 import { handler } from '../../../src/handlers/webhooks/cronjob-sync-orcid';
 import {
   generateGraphqlFetchUsersResponse,
   getGraphQLUser,
   getSquidexUserGraphqlResponse,
+  patchResponse,
 } from '../../fixtures/users.fixtures';
-import { identity } from '../../helpers/squidex';
 import * as fixtures from './cronjob-sync-orcid.fixtures';
 
 const mockRequest: jest.MockedFunction<SquidexGraphql['request']> = jest.fn();
+const mockPatch: jest.MockedFunction<SquidexRestClient<RestUser>['patch']> =
+  jest.fn();
 
 jest.mock('@asap-hub/squidex', () => ({
   ...jest.requireActual('@asap-hub/squidex'),
   SquidexGraphql: class SquidexGraphql {
     request = mockRequest;
   },
+  SquidexRest: class SquidexRest {
+    patch = mockPatch;
+  },
 }));
 
 describe('Cronjob - Sync Users ORCID', () => {
   const orcid = '0000-0001-9884-1913';
 
-  beforeAll(identity);
   beforeEach(jest.resetAllMocks);
 
   test('should fetch ORCID works for users with orcid and lastSyncDate 1 month away', async () => {
@@ -30,20 +33,21 @@ describe('Cronjob - Sync Users ORCID', () => {
       .get(`/v2.1/${orcid}/works`)
       .reply(200, fixtures.orcidWorksResponse);
 
-    const userResponse = getGraphQLUser();
+    const userResponse = patchResponse();
+    const user = getGraphQLUser();
     const userToSync = {
-      ...userResponse,
+      ...user,
       flatData: {
-        ...userResponse.flatData,
+        ...user.flatData,
         orcidLastSyncDate: '2020-01-01T00:00:00.000Z',
         orcid,
         orcidWorks: fixtures.ORCIDWorksDeserialisedExpectation,
       },
     };
     const userNotToSync = {
-      ...userResponse,
+      ...user,
       flatData: {
-        ...userResponse.flatData,
+        ...user.flatData,
         orcidLastSyncDate: new Date().toISOString(),
         orcid,
         orcidWorks: fixtures.ORCIDWorksDeserialisedExpectation,
@@ -59,37 +63,18 @@ describe('Cronjob - Sync Users ORCID', () => {
       .mockResolvedValueOnce(outdatedUsersResponse)
       .mockResolvedValueOnce(singleUserResponse);
 
-    nock(config.baseUrl)
-      .patch(
-        `/api/content/${config.appName}/users/user-id-1`,
-        (body) =>
-          body.email.iv === userToSync.flatData.email &&
-          areOrcidWorksEqual(
-            body.orcidWorks.iv,
-            userToSync.flatData.orcidWorks,
-          ),
-      )
-      .reply(200, userResponse);
+    mockPatch.mockResolvedValueOnce(userResponse);
 
     const { statusCode } = await handler();
     expect(statusCode).toBe(200);
-    expect(nock.isDone()).toBe(true);
+    expect(mockPatch).toHaveBeenCalledWith(
+      singleUserResponse.findUsersContent?.id,
+      expect.objectContaining({
+        email: { iv: userToSync.flatData.email },
+        orcidWorks: {
+          iv: expect.arrayContaining(userToSync.flatData.orcidWorks),
+        },
+      }),
+    );
   });
 });
-
-const isOrcidWorkEqual = (orcidWork1: OrcidWork, orcidWork2: OrcidWork) =>
-  Object.keys(orcidWork1).length === Object.keys(orcidWork2).length &&
-  orcidWork1.doi === orcidWork2.doi &&
-  orcidWork1.title === orcidWork2.title &&
-  orcidWork1.type === orcidWork2.type &&
-  orcidWork1.lastModifiedDate === orcidWork2.lastModifiedDate &&
-  orcidWork1.id === orcidWork2.id;
-
-const areOrcidWorksEqual = (
-  orcidWorks1: OrcidWork[],
-  orcidWorks2: OrcidWork[],
-) =>
-  orcidWorks1.length === orcidWorks2.length &&
-  orcidWorks1.every((orcidWork, idx) =>
-    isOrcidWorkEqual(orcidWork, orcidWorks2[idx]!),
-  );

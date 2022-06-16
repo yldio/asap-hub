18cc7f3e2311c5d1f2641a19531ba7ec36058439
Piotr Szpak <piotr.szpak@yld.io>
2022-06-16 13:35:39 +0100

    import auth0 pubkeys

--- packages/auth/src/pubKeys/index.ts
+++ packages/auth/src/pubKeys/index.ts
@@ -1,5 +1,7 @@
-import { keys as prodPubKeys } from './auth0-pubkeys.prod.json';
-import { keys as devPubKeys } from './auth0-pubkeys.dev.json';
+import { keys as crnProdPubKeys } from './crn-auth0-pubkeys.prod.json';
+import { keys as crnDevPubKeys } from './crn-auth0-pubkeys.dev.json';
+import { keys as gp2ProdPubKeys } from './gp2-auth0-pubkeys.prod.json';
+import { keys as gp2DevPubKeys } from './gp2-auth0-pubkeys.dev.json';
 
 interface JWK {
   alg: string;
@@ -12,6 +14,11 @@ interface JWK {
   x5c: string[];
 }
 
-const keys: JWK[] = [...prodPubKeys, ...devPubKeys];
+const keys: JWK[] = [
+  ...crnProdPubKeys,
+  ...crnDevPubKeys,
+  ...gp2ProdPubKeys,
+  ...gp2DevPubKeys,
+];
 
 export default keys;

5d4da8a4a04164f09e5002bfdd3928dd47b19809
Mark Sims <mark@bluepie.co.uk>
2022-06-24 15:30:57 +0100

    move scripts out of algolia package
    Was getting:
    
    Module not found: Your application tried to access fs. While this module is usually interpreted as a Node builtin, your resolver is running inside a non-Node resolution context where such builtins are ignored. Since fs isn't otherwise declared in your dependencies, this makes the require call ambiguous and unsound.`

--- apps/asap-cli/src/scripts/algolia/get-settings.ts
+++ apps/asap-cli/src/scripts/algolia/get-settings.ts
@@ -1,6 +1,7 @@
 /* istanbul ignore file */
 import algoliasearch from 'algoliasearch';
 import fs from 'fs/promises';
+import { resolve } from 'path';
 
 export type GetSettings = {
   algoliaAppId: string;
@@ -13,7 +14,7 @@ export const getAlgoliaSettings = async ({
   algoliaCiApiKey,
   indexName,
 }: GetSettings): Promise<void> => {
-  const path = 'packages/algolia';
+  const path = resolve(__dirname, '../../../../../schema');
   const client = algoliasearch(algoliaAppId, algoliaCiApiKey);
   const index = client.initIndex(indexName);
   const { replicas: _, ...indexSettings } = await index.getSettings();

ecb793e4d59a7f81f3937d36f3021d12c1f5a87f
jhonny-m <jhonnyporto@hotmail.com>
2022-06-29 16:24:37 +0100

    adding nav and user menu to mobile menu toggle

--- packages/gp2-components/src/templates/Layout.tsx
+++ packages/gp2-components/src/templates/Layout.tsx
@@ -1,11 +1,54 @@
 import { useState, FC, ReactNode } from 'react';
 import { css } from '@emotion/react';
 
-import { ToastStack, drawerQuery, pixels } from '@asap-hub/react-components';
+import {
+  ToastStack,
+  drawerQuery,
+  pixels,
+  crossQuery,
+  Overlay,
+  paper,
+  navigationGrey,
+} from '@asap-hub/react-components';
 
 import NavigationHeader from '../organism/NavigationHeader';
+import UserMenu from '../molecules/UserMenu';
+import MainNavigation from '../organism/MainNavigation';
 
 const { mobileScreen, tabletScreen, vminLinearCalcClamped } = pixels;
+
+const styles = css({
+  height: '100%',
+  position: 'relative',
+  display: 'grid',
+  grid: `
+    "header     header"  max-content
+    "main-menu  content" max-content
+    "user-menu  content" 1fr / max-content 1fr
+`,
+  [crossQuery]: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+});
+
+const headerStyles = css({
+  gridArea: 'header',
+});
+
+const overlayStyles = css({
+  gridRow: 'main-menu / -1',
+  gridColumn: '1 / -1',
+
+  visibility: 'hidden',
+  [crossQuery]: {
+    display: 'none',
+  },
+});
+const overlayMenuShownStyles = css({
+  visibility: 'visible',
+});
 
 const contentStyles = css({
   width: '748px',
@@ -30,6 +73,32 @@ const contentStyles = css({
   },
 });
 
+const menuStyles = css({
+  backgroundColor: paper.rgb,
+  gridColumnStart: '1',
+  overflowY: 'auto',
+  display: 'flex',
+  flexDirection: 'column',
+  gridRow: `main-menu/-1`,
+  visibility: 'hidden',
+  transform: 'translateX(-100%)',
+  transition: `transform 250ms ease, visibility 0s 250ms`,
+});
+const menuMenuShownStyles = css({
+  visibility: 'visible',
+  transform: 'translateX(0)',
+  transition: `transform 250ms ease`,
+});
+
+const mainMenuStyles = css({
+  gridArea: 'main-menu',
+});
+const userMenuStyles = css({
+  backgroundColor: navigationGrey.rgb,
+  gridArea: 'user-menu',
+  flexGrow: 1,
+});
+
 type LayoutProps = {
   readonly children: ReactNode;
 };
@@ -38,17 +107,37 @@ const Layout: FC<LayoutProps> = ({ children }) => {
 
   return (
     <ToastStack>
-      <article css={css({ width: '100%' })}>
-        <NavigationHeader
-          menuOpen={menuShown}
-          onToggleMenu={() => {
-            setMenuShown(!menuShown);
-          }}
-        />
+      <article css={styles}>
+        <div css={headerStyles}>
+          <NavigationHeader
+            menuOpen={menuShown}
+            onToggleMenu={() => {
+              setMenuShown(!menuShown);
+            }}
+          />
+        </div>
+
+        <div
+          css={css({
+            gridRow: 'header-end / -1',
+            gridColumn: '1 / -1',
+            overflowY: 'auto',
+          })}
+        >
+          <main css={contentStyles}>{children}</main>
+        </div>
+        <div css={[overlayStyles, menuShown && overlayMenuShownStyles]}>
+          <Overlay shown={menuShown} onClick={() => setMenuShown(false)} />
+        </div>
+        <div css={[menuStyles, menuShown && menuMenuShownStyles]}>
+          <div css={[mainMenuStyles]}>
+            <MainNavigation />
+          </div>
+          <div css={[userMenuStyles]}>
+            <UserMenu />
+          </div>
+        </div>
       </article>
-      <div css={css({ width: '100%', overflowY: 'scroll' })}>
-        <article css={contentStyles}>{children}</article>
-      </div>
     </ToastStack>
   );
 };

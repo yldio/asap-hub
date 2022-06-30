4edee8d89355f5deab08d31e5ae656e362b1036c
jhonny-m <jhonnyporto@hotmail.com>
2022-06-30 16:23:46 +0100

    adding tests for layout and effect for location change

--- packages/gp2-components/src/templates/Layout.tsx
+++ packages/gp2-components/src/templates/Layout.tsx
@@ -1,4 +1,4 @@
-import { useState, FC, ReactNode } from 'react';
+import { useState, FC, ReactNode, useEffect, createRef } from 'react';
 import { css } from '@emotion/react';
 
 import {
@@ -9,11 +9,13 @@ import {
   Overlay,
   paper,
   navigationGrey,
+  usePrevious,
 } from '@asap-hub/react-components';
 
 import NavigationHeader from '../organism/NavigationHeader';
 import UserMenu from '../molecules/UserMenu';
 import MainNavigation from '../organism/MainNavigation';
+import { useLocation } from 'react-router-dom';
 
 const { mobileScreen, tabletScreen, vminLinearCalcClamped } = pixels;
 
@@ -105,6 +107,28 @@ type LayoutProps = {
 const Layout: FC<LayoutProps> = ({ children }) => {
   const [menuShown, setMenuShown] = useState(false);
 
+  let location: Location | undefined;
+  let prevLocation: Location | undefined;
+  const mainRef = createRef<HTMLDivElement>();
+
+  // This hook *is* called unconditionally despite what rules-of-hooks says
+  /* eslint-disable react-hooks/rules-of-hooks */
+  try {
+    location = useLocation();
+    prevLocation = usePrevious(location);
+  } catch {
+    // If there is no router, fine, never auto-close the menu
+  }
+  /* eslint-enable react-hooks/rules-of-hooks */
+  useEffect(() => {
+    setMenuShown(false);
+  }, [location]);
+  useEffect(() => {
+    if (location?.pathname !== prevLocation?.pathname && mainRef.current) {
+      mainRef.current.scrollTo(0, 0);
+    }
+  }, [location, prevLocation, mainRef]);
+
   return (
     <ToastStack>
       <article css={styles}>
@@ -124,7 +148,9 @@ const Layout: FC<LayoutProps> = ({ children }) => {
             overflowY: 'auto',
           })}
         >
-          <main css={contentStyles}>{children}</main>
+          <main ref={mainRef} css={contentStyles}>
+            {children}
+          </main>
         </div>
         <div css={[overlayStyles, menuShown && overlayMenuShownStyles]}>
           <Overlay shown={menuShown} onClick={() => setMenuShown(false)} />
--- packages/gp2-components/src/templates/__tests__/Layout.test.tsx
+++ packages/gp2-components/src/templates/__tests__/Layout.test.tsx
@@ -1,5 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
 import { Layout } from '..';
 
 describe('Layout', () => {
@@ -24,5 +25,45 @@ describe('Layout', () => {
     userEvent.click(screen.getByLabelText(/toggle menu/i));
     expect(screen.queryByTitle(/close/i)).not.toBeInTheDocument();
     expect(screen.getByTitle(/menu/i)).toBeInTheDocument();
+  });
+
+  it('closes the drawer when clicking the overlay', async () => {
+    const { getByLabelText } = render(<Layout>Content</Layout>);
+    userEvent.click(getByLabelText(/toggle menu/i));
+
+    userEvent.click(getByLabelText(/close/i));
+    expect(getByLabelText(/close/i)).not.toBeVisible();
+  });
+
+  it('closes the drawer on navigation', async () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Layout>Content</Layout>
+      </MemoryRouter>,
+    );
+    userEvent.click(screen.getByLabelText(/toggle menu/i));
+    await waitFor(() => {
+      expect(screen.getByLabelText(/close/i)).toBeVisible();
+    });
+
+    userEvent.click(
+      screen.getAllByText(/dashboard/i, { selector: 'nav *' })[0],
+    );
+    await waitFor(() => {
+      expect(screen.getByLabelText(/close/i)).not.toBeVisible();
+    });
+  });
+
+  it('scrolls to top between page navigations', async () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Layout>Content</Layout>
+      </MemoryRouter>,
+    );
+
+    userEvent.click(
+      screen.getAllByText(/dashboard/i, { selector: 'nav *' })[0],
+    );
+    expect(screen.getByRole('main').scrollTo).toHaveBeenCalled();
   });
 });

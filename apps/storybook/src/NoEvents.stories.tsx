567426ce29d4361c4ca9b34a83b326f0613165c9
Eve Montalvao <evemontalvao@gmail.com>
2022-06-30 10:03:08 -0300

    Create storybook component

---
+++ apps/storybook/src/NoEvents.stories.tsx
@@ -0,0 +1,21 @@
+import { ComponentProps } from 'react';
+import { boolean, select } from '@storybook/addon-knobs';
+import { NoEvents } from '@asap-hub/react-components';
+
+export default {
+  title: 'Organisms / No Events',
+};
+
+const props = (): ComponentProps<typeof NoEvents> => {
+  const past = boolean('Past Event', true);
+  const link = '';
+  const type = select('Type', ['team', 'group'], 'team');
+
+  return {
+    past,
+    link,
+    type,
+  };
+};
+
+export const Normal = () => <NoEvents {...props()} />;

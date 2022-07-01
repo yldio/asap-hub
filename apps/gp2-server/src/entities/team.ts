d18327168b256abfff13490506f53edfce9bf606
Piotr Szpak <piotr.szpak@yld.io>
2022-07-01 12:15:12 +0100

    remove more files from coverage

--- apps/gp2-server/src/entities/team.ts
+++ apps/gp2-server/src/entities/team.ts
@@ -1,3 +1,5 @@
+/* istanbul ignore file */
+// ignore this file for coverage since we don't have the requirements yet to test it
 import { teamRole, TeamRole } from '@asap-hub/model';
 
 export const isTeamRole = (data: string | null): data is TeamRole =>

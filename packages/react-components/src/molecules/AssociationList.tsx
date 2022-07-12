fa19ce463337c1dc33c07f9d9354ffb7c60a3df7
Eve Montalvao <evemontalvao@gmail.com>
2022-07-12 13:27:23 -0300

    Fix Lab icon spacing

--- packages/react-components/src/molecules/AssociationList.tsx
+++ packages/react-components/src/molecules/AssociationList.tsx
@@ -94,15 +94,16 @@ const AssociationList: FC<AssociationListProps> = ({
 
   if (limitExceeded) {
     return (
-      <>
+      <div>
+        {<div css={iconStyles}>{icon}</div>}
         {associations.length} {type}
         {associations.length === 1 ? '' : 's'}
-      </>
+      </div>
     );
   }
 
   return (
-    <>
+    <div>
       {inline && <div css={iconStyles}>{icon}</div>}
       <ul css={[containerStyles, inline && inlineContainerStyles]}>
         {associations.map(({ displayName, id }) => (
@@ -132,7 +133,7 @@ const AssociationList: FC<AssociationListProps> = ({
           </div>
         )}
       </ul>
-    </>
+    </div>
   );
 };
 

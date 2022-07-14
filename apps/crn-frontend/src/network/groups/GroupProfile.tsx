1c99fd8daf39cabd54a4479fafcbbc3b3ef55102
Ratiu Octavian <oratiu@gmail.com>
2022-07-14 15:39:02 +0300

    Cleanup

--- apps/crn-frontend/src/network/groups/GroupProfile.tsx
+++ apps/crn-frontend/src/network/groups/GroupProfile.tsx
@@ -4,15 +4,13 @@ import { v4 as uuid } from 'uuid';
 import {
   GroupProfilePage,
   NotFoundPage,
-  GroupNoEvents,
   NoEvents,
 } from '@asap-hub/react-components';
-import { useFlags } from '@asap-hub/react-context';
 import { events, network, useRouteParams } from '@asap-hub/routing';
 import { Frame } from '@asap-hub/frontend-utils';
 
 import { useGroupById } from './state';
-import { usePaginationParams, useSearch } from '../../hooks';
+import { usePaginationParams } from '../../hooks';
 import { useEvents } from '../../events/state';
 import { getEventListOptions } from '../../events/options';
 
@@ -20,16 +18,11 @@ const loadAbout = () =>
   import(/* webpackChunkName: "network-group-about" */ './About');
 const loadCalendar = () =>
   import(/* webpackChunkName: "network-group-calendar" */ './Calendar');
-const loadEventList = () =>
-  import(
-    /* webpackChunkName: "network-group-event-list" */ './events/EventList'
-  );
 const loadEvents = () =>
   import(/* webpackChunkName: "network-events" */ '../EventsEmbed');
 
 const About = lazy(loadAbout);
 const Calendar = lazy(loadCalendar);
-const EventList = lazy(loadEventList);
 const Events = lazy(loadEvents);
 loadAbout();
 
@@ -44,14 +37,11 @@ const GroupProfile: FC<GroupProfileProps> = ({ currentTime }) => {
 
   const [groupTeamsElementId] = useState(`group-teams-${uuid()}`);
 
-  const { searchQuery, setSearchQuery, debouncedSearchQuery } = useSearch();
-
   const route = network({}).groups({}).group;
   const { groupId } = useRouteParams(route);
   const { path } = useRouteMatch();
   const group = useGroupById(groupId);
   const { pageSize } = usePaginationParams();
-  const isAlgoliaEventsEnabled = useFlags().isEnabled('EVENTS_SEARCH');
 
   const upcomingEvents = useEvents(
     getEventListOptions(currentTime, {
@@ -105,74 +95,42 @@ const GroupProfile: FC<GroupProfileProps> = ({ currentTime }) => {
             </GroupProfilePage>
           </Route>
           <Route path={path + route({ groupId }).upcoming.template}>
-            <GroupProfilePage
-              {...props}
-              searchQuery={searchQuery}
-              onChangeSearchQuery={setSearchQuery}
-            >
+            <GroupProfilePage {...props}>
               <Frame title="Upcoming Events">
-                {isAlgoliaEventsEnabled ? (
-                  <Events
-                    constraint={{ groupId }}
-                    currentTime={currentTime}
-                    past={false}
-                    events={upcomingEvents}
-                    noEventsComponent={
-                      <NoEvents
-                        displayName={group.name}
-                        type="group"
-                        past={false}
-                        link={events({}).upcoming({}).$}
-                      />
-                    }
-                  />
-                ) : (
-                  <EventList
-                    currentTime={currentTime}
-                    searchQuery={debouncedSearchQuery}
-                    noEventsComponent={
-                      <GroupNoEvents
-                        past={false}
-                        link={events({}).upcoming({}).$}
-                      />
-                    }
-                  />
-                )}
+                <Events
+                  constraint={{ groupId }}
+                  currentTime={currentTime}
+                  past={false}
+                  events={upcomingEvents}
+                  noEventsComponent={
+                    <NoEvents
+                      displayName={group.name}
+                      type="group"
+                      past={false}
+                      link={events({}).upcoming({}).$}
+                    />
+                  }
+                />
               </Frame>
             </GroupProfilePage>
           </Route>
           <Route path={path + route({ groupId }).past.template}>
-            <GroupProfilePage
-              {...props}
-              searchQuery={searchQuery}
-              onChangeSearchQuery={setSearchQuery}
-            >
+            <GroupProfilePage {...props}>
               <Frame title="Past Events">
-                {isAlgoliaEventsEnabled ? (
-                  <Events
-                    events={pastEvents}
-                    constraint={{ groupId }}
-                    currentTime={currentTime}
-                    past={true}
-                    noEventsComponent={
-                      <NoEvents
-                        displayName={group.name}
-                        type="group"
-                        past={true}
-                        link={events({}).past({}).$}
-                      />
-                    }
-                  />
-                ) : (
-                  <EventList
-                    past
-                    currentTime={currentTime}
-                    searchQuery={debouncedSearchQuery}
-                    noEventsComponent={
-                      <GroupNoEvents past={true} link={events({}).past({}).$} />
-                    }
-                  />
-                )}
+                <Events
+                  events={pastEvents}
+                  constraint={{ groupId }}
+                  currentTime={currentTime}
+                  past={true}
+                  noEventsComponent={
+                    <NoEvents
+                      displayName={group.name}
+                      type="group"
+                      past={true}
+                      link={events({}).past({}).$}
+                    />
+                  }
+                />
               </Frame>
             </GroupProfilePage>
           </Route>

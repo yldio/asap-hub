b3895a5edd60af85465e196ad346dee5cfbb898f
Ratiu Octavian <oratiu@gmail.com>
2022-07-26 16:34:52 +0300

    review changes

--- packages/react-components/src/templates/__tests__/DiscoverNewsCardList.test.tsx
+++ packages/react-components/src/templates/__tests__/DiscoverNewsCardList.test.tsx
@@ -3,35 +3,33 @@ import { render, screen } from '@testing-library/react';
 
 import DiscoverNewsCardList from '../DiscoverNewsCardList';
 
-describe('Renders the DiscoverNewsCardList component', () => {
-  it('renders the provided title and subtitle', () => {
-    render(
-      <DiscoverNewsCardList
-        title="Tutorials"
-        subtitle="Explore our tutorials to understand how you can use the Hub and work with the tools."
-        news={[]}
-      />,
-    );
+it('renders the provided title and subtitle', () => {
+  render(
+    <DiscoverNewsCardList
+      title="Tutorials"
+      subtitle="Explore our tutorials to understand how you can use the Hub and work with the tools."
+      news={[]}
+    />,
+  );
 
-    expect(screen.getByText(/tutorials/i, { selector: 'h2' })).toBeVisible();
-    expect(screen.getByText(/Explore our tutorials/i)).toBeVisible();
-    expect(
-      screen.getByText(/Need help with a grant-related matter/i),
-    ).toBeVisible();
-  });
+  expect(screen.getByText(/tutorials/i, { selector: 'h2' })).toBeVisible();
+  expect(screen.getByText(/Explore our tutorials/i)).toBeVisible();
+  expect(
+    screen.getByText(/Need help with a grant-related matter/i),
+  ).toBeVisible();
+});
 
-  it('renders tutorial items', () => {
-    render(
-      <DiscoverNewsCardList
-        title="Tutorials"
-        subtitle="Explore our tutorials to understand how you can use the Hub and work with the tools."
-        news={[
-          createNewsResponse('First One', 'Tutorial'),
-          createNewsResponse('Second One', 'Tutorial'),
-        ]}
-      />,
-    );
-    expect(screen.getByText(/First One/, { selector: 'h4' })).toBeVisible();
-    expect(screen.getByText(/Second One/, { selector: 'h4' })).toBeVisible();
-  });
+it('renders news items', () => {
+  render(
+    <DiscoverNewsCardList
+      title="Tutorials"
+      subtitle="Explore our tutorials to understand how you can use the Hub and work with the tools."
+      news={[
+        createNewsResponse('First One', 'Tutorial'),
+        createNewsResponse('Second One', 'Tutorial'),
+      ]}
+    />,
+  );
+  expect(screen.getByText(/First One/, { selector: 'h4' })).toBeVisible();
+  expect(screen.getByText(/Second One/, { selector: 'h4' })).toBeVisible();
 });

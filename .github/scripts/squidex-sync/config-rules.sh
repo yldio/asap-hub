42188d140e28a81390ade2e8c85f06cb3cae529f
Mark Sims <mark@bluepie.co.uk>
2022-08-01 16:33:39 +0100

    updates squidex environment variables

--- .github/scripts/squidex-sync/config-rules.sh
+++ .github/scripts/squidex-sync/config-rules.sh
@@ -1,14 +1,18 @@
 # Uses dir passed as argument or default one
-backupPath=${1:-"packages/squidex/schema/crn/rules"}
+rulesPath=${1:-"packages/squidex/schema/crn/rules"}
 
 # Replace PLACEHOLDERS with variable available on the CI
-find $backupPath \
+find $rulesPath \
   -type f \( -name "*.json" -not -name "__rule.json" \) \
   -print0 | xargs -t -0 -I @file \
   sed -i 's/SQUIDEX_SHARED_SECRET/'"$SQUIDEX_SHARED_SECRET"'/' @file
 
-# Replace PLACEHOLDERS with variable available on the CI
-find $backupPath \
+find $rulesPath \
   -type f \( -name "*.json" -not -name "__rule.json" \) \
   -print0 | xargs -t -0 -I @file \
   sed -i 's#CRN_API_URL#'"$CRN_API_URL"'#' @file
+
+find $rulesPath \
+  -type f \( -name "*.json" -not -name "__rule.json" \) \
+  -print0 | xargs -t -0 -I @file \
+  sed -i 's#GP2_API_URL#'"$GP2_API_URL"'#' @file

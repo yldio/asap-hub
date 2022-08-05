78a8d7d6ad78ae6c8252f7e80a64197d0edc7f67
Matt Doughty <matt@apogee.guru>
2022-08-05 16:18:54 +0100

    Fix test.

--- apps/crn-server/test/utils/sentry-wrapper.test.ts
+++ apps/crn-server/test/utils/sentry-wrapper.test.ts
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/serverless';
 import { Handler } from 'aws-lambda';
 import { sentryWrapper } from '../../src/utils/sentry-wrapper';
 import {
@@ -8,34 +9,22 @@ import {
 } from '../../src/config';
 
 describe('Sentry wrapper correctly calls functions', () => {
-  const mockSentryInit = jest.fn((_) => true);
-  const mockSentryWrapHandler = jest.fn((handler: Handler) => handler);
-
-  jest.mock('@sentry/serverless', () => {
-    const sentryServerless = jest.requireActual('@sentry/serverless');
-
-    return {
-      ...sentryServerless,
-      AWSLambda: {
-        init: mockSentryInit,
-        wrapHandler: mockSentryWrapHandler,
-      },
-    };
-  });
-
   test('should call the init & wrapHandler functions', () => {
+    Sentry.AWSLambda.init = jest.fn((_) => true);
+    Sentry.AWSLambda.wrapHandler = jest.fn((handler: Handler) => handler);
+
     const handler = async (event: unknown, _context: unknown) => {
       return event;
     };
 
     sentryWrapper(handler);
 
-    expect(mockSentryInit).toHaveBeenCalledWith({
+    expect(Sentry.AWSLambda.init).toHaveBeenCalledWith({
       dsn: sentryDsn,
       tracesSampleRate: sentryTraceSampleRate,
       environment,
       release: currentRevision,
     });
-    expect(mockSentryWrapHandler).toHaveBeenCalledWith(handler);
+    expect(Sentry.AWSLambda.wrapHandler).toHaveBeenCalledWith(handler);
   });
 });

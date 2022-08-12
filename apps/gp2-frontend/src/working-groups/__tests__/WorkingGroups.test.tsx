c1bfde82e48ae9df1a037feae9382e1fc933cb60
Mark Sims <mark@bluepie.co.uk>
2022-08-12 15:27:09 +0100

    adds tests for working group page

---
+++ apps/gp2-frontend/src/working-groups/__tests__/WorkingGroups.test.tsx
@@ -0,0 +1,35 @@
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import { Suspense } from 'react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import { Auth0Provider, WhenReady } from '../../auth/test-utils';
+import WorkingGroups from '../WorkingGroups';
+
+const renderResearchOutputList = async () => {
+  render(
+    <RecoilRoot>
+      <Suspense fallback="loading">
+        <Auth0Provider user={{}}>
+          <WhenReady>
+            <MemoryRouter initialEntries={['/working-groups']}>
+              <Route path="/shared-research">
+                <WorkingGroups />
+              </Route>
+            </MemoryRouter>
+          </WhenReady>
+        </Auth0Provider>
+      </Suspense>
+    </RecoilRoot>,
+  );
+  await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
+};
+
+it('renders the Title', async () => {
+  await renderResearchOutputList();
+  const heading = screen.getByRole('heading', { name: 'Working Groups' });
+  expect(heading).toBeInTheDocument();
+});

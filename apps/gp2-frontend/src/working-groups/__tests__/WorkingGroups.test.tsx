88ff3637aab98a7163522e574df0076b00ca1f5d
Mark Sims <mark@bluepie.co.uk>
2022-08-17 11:12:30 +0100

    calls api for working groups

--- apps/gp2-frontend/src/working-groups/__tests__/WorkingGroups.test.tsx
+++ apps/gp2-frontend/src/working-groups/__tests__/WorkingGroups.test.tsx
@@ -1,3 +1,4 @@
+import { WorkingGroupResponse } from '@asap-hub/model/build/gp2';
 import {
   render,
   screen,
@@ -7,11 +8,32 @@ import { Suspense } from 'react';
 import { MemoryRouter, Route } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { Auth0Provider, WhenReady } from '../../auth/test-utils';
+import { getWorkingGroups } from '../api';
+import { refreshWorkingGroupsState } from '../state';
 import WorkingGroups from '../WorkingGroups';
+
+jest.mock('../api');
+
+const getWorkingGroup = (overrides: Partial<WorkingGroupResponse> = {}) => ({
+  id: '42',
+  title: 'Working Group Title',
+  members: [],
+  shortDescription: 'This is a short description',
+  leadingMembers: 'This is a list of leading members',
+  ...overrides,
+});
+const getWorkingGroupsFixture = (items = [getWorkingGroup()]) => ({
+  items,
+  total: items.length,
+});
 
 const renderWorkingGroupsList = async () => {
   render(
-    <RecoilRoot>
+    <RecoilRoot
+      initializeState={({ set }) => {
+        set(refreshWorkingGroupsState, Math.random());
+      }}
+    >
       <Suspense fallback="loading">
         <Auth0Provider user={{}}>
           <WhenReady>
@@ -27,9 +49,45 @@ const renderWorkingGroupsList = async () => {
   );
   await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
 };
+describe('WorkingGroups', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    jest.clearAllMocks();
+  });
 
-it('renders the Title', async () => {
-  await renderWorkingGroupsList();
-  const heading = screen.getByRole('heading', { name: 'Working Groups' });
-  expect(heading).toBeInTheDocument();
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('renders the Title', async () => {
+    const mockGetWorkingGroups = getWorkingGroups as jest.MockedFunction<
+      typeof getWorkingGroups
+    >;
+    mockGetWorkingGroups.mockResolvedValueOnce(getWorkingGroupsFixture());
+    await renderWorkingGroupsList();
+    expect(
+      screen.getByRole('heading', { name: 'Working Groups' }),
+    ).toBeInTheDocument();
+  });
+
+  it('renders a list of working groups', async () => {
+    const mockGetWorkingGroups = getWorkingGroups as jest.MockedFunction<
+      typeof getWorkingGroups
+    >;
+    const firstGroup = getWorkingGroup({ id: '42', title: 'Working Group 42' });
+    const secondGroup = getWorkingGroup({
+      id: '11',
+      title: 'Working Group 11',
+    });
+    mockGetWorkingGroups.mockResolvedValue(
+      getWorkingGroupsFixture([firstGroup, secondGroup]),
+    );
+    await renderWorkingGroupsList();
+    expect(
+      screen.getByRole('heading', { name: 'Working Group 42' }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: 'Working Group 11' }),
+    ).toBeInTheDocument();
+  });
 });

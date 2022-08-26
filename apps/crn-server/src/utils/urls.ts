d5785bab46399b4bcb21657d28d5ac0ea5fd2797
ricardocarvalho <ricardo.carvalho@yld.io>
2022-08-26 12:20:53 -0300

    [CRN-1027] use odata-query at RO provider

--- apps/crn-server/src/utils/urls.ts
+++ apps/crn-server/src/utils/urls.ts
@@ -1,4 +1,8 @@
+import buildQuery, { Filter } from 'odata-query';
 import { createUrlFactory } from '@asap-hub/squidex';
 import { appName, baseUrl } from '../config';
 
 export const createUrl = createUrlFactory({ appName, baseUrl });
+
+export const buildODataFilter = (filterObj: Filter): string =>
+  buildQuery({ filter: filterObj }).replace('?$filter=', '');

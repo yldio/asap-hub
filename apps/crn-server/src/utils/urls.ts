d1979f31ae9d3aca78b375b02665e5467b39440c
ricardocarvalho <ricardo.carvalho@yld.io>
2022-08-29 12:01:27 -0300

    [CRN-1027] update users.data-provider.ts

--- apps/crn-server/src/utils/urls.ts
+++ apps/crn-server/src/utils/urls.ts
@@ -6,3 +6,21 @@ export const createUrl = createUrlFactory({ appName, baseUrl });
 
 export const buildODataFilter = (filterObj: Filter): string =>
   buildQuery({ filter: filterObj }).replace('?$filter=', '');
+
+export const buildEqFilterForWords = (
+  field: string,
+  words?: string[],
+  subField?: string,
+): Filter => {
+  const filterKey = `data/${field}/iv${subField ? `/${subField}` : ''}`;
+  const filters = (words || []).reduce(
+    (acc: Filter[], word: string) => acc.concat({ [filterKey]: word }),
+    [],
+  );
+
+  if (!filters.length) {
+    return '';
+  }
+
+  return filters.length === 1 ? (filters[0] as Filter) : { or: filters };
+};

045824308999b6c74a35d326628820fee1373137
Matt Doughty <matt@apogee.guru>
2022-08-31 19:01:42 +0100

    Add dismissed getting started to GP2.

--- apps/gp2-server/src/data-providers/user.data-provider.ts
+++ apps/gp2-server/src/data-providers/user.data-provider.ts
@@ -362,6 +362,11 @@ export const parseGraphQLUserToDataObject = (
       item.flatData && typeof item.flatData.onboarded === 'boolean'
         ? item.flatData.onboarded
         : undefined,
+    dismissedGettingStarted:
+      item.flatData &&
+      typeof item.flatData.dismissedGettingStarted === 'boolean'
+        ? item.flatData.dismissedGettingStarted
+        : undefined,
     createdDate,
     orcid,
     firstName: item.flatData.firstName || '',

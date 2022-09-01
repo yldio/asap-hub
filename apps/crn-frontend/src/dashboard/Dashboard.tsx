cabab3abc1a7752b1c57ed70267c4e324364c2bd
Matt Doughty <matt@apogee.guru>
2022-09-01 10:31:37 -0400

    Update apps/crn-frontend/src/dashboard/Dashboard.tsx
    Co-authored-by: Thomas Waterfall <Thomas.Waterfall@gmail.com>

--- apps/crn-frontend/src/dashboard/Dashboard.tsx
+++ apps/crn-frontend/src/dashboard/Dashboard.tsx
@@ -41,10 +41,6 @@ const Dashboard: FC<Record<string, never>> = () => {
   usePrefetchCalendars();
   const patchUser = usePatchUserById(id);
   const user = useUserById(id);
-  const dismissedGettingStarted =
-    user && typeof user.dismissedGettingStarted === 'boolean'
-      ? user.dismissedGettingStarted
-      : false;
 
   return (
     <>

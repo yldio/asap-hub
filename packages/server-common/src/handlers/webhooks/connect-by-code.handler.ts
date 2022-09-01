2cd874a3a4ea8cddb70dd39ada8685483cd3b938
Piotr S <piotr.szpak@yld.io>
2022-09-01 12:15:52 +0100

    fix: Connect-by-code handler (#1993)
    * add debug logs to the webhook handler
    * wrap handler in lambda wrapper

--- packages/server-common/src/handlers/webhooks/connect-by-code.handler.ts
+++ packages/server-common/src/handlers/webhooks/connect-by-code.handler.ts
@@ -1,18 +1,21 @@
 import { framework as lambda } from '@asap-hub/services-common';
 import { UserController } from '../../controllers';
-import { validateAuth0Request } from '../../utils';
+import { Logger, validateAuth0Request } from '../../utils';
 import { validateWebhookConnectByCodeBody } from '../../validation';
 
 export const connectByCodeHandlerFactory =
   (
     users: UserController,
     auth0SharedSecret: string,
+    logger: Logger,
   ): ((request: lambda.Request) => Promise<{ statusCode: number }>) =>
   async (request) => {
     validateAuth0Request(request, auth0SharedSecret);
 
+    logger.debug(request.payload, 'Request payload');
+
     const { code, userId } = validateWebhookConnectByCodeBody(
-      request.payload as never,
+      request.payload as Record<string, unknown>,
     );
 
     await users.connectByCode(code, userId);

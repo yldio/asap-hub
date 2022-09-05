b648021bb9edc9f97b4dfff9e4e626cad24fe910
jhonny-m <jhonnyporto@hotmail.com>
2022-09-05 12:01:42 +0100

    adding test to toggle

--- packages/gp2-components/src/molecules/__tests__/ExpandableText.test.tsx
+++ packages/gp2-components/src/molecules/__tests__/ExpandableText.test.tsx
@@ -4,8 +4,7 @@ import React from 'react';
 import ExpandableText from '../ExpandableText';
 
 describe('ExpandableText', () => {
-  const text =
-    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt a itaque hic vel voluptatum commodi ab quisquam consectetur, explicabo reiciendis voluptatem iure dolorum sint praesentium atque. Sapiente laboriosam culpa consectetur?';
+  const text = 'this is a text';
   it('renders the children', () => {
     render(<ExpandableText>{text}</ExpandableText>);
     expect(screen.getByText(text)).toBeVisible();
@@ -27,9 +26,7 @@ describe('ExpandableText', () => {
     const button = screen.getByRole('button');
     expect(button).toBeVisible();
     expect(button.textContent).toMatchInlineSnapshot(`"Show moreChevron Down"`);
-
     userEvent.click(button);
-
     expect(button.textContent).toMatchInlineSnapshot(`"Show lessChevron Down"`);
   });
 });

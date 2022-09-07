cfbf5599fd956fb6b7cb2290ffe1b1d55658fac1
jhonny-m <jhonnyporto@hotmail.com>
2022-09-07 16:45:45 +0100

    removing mocks

--- packages/gp2-components/src/templates/ProjectsPage.tsx
+++ packages/gp2-components/src/templates/ProjectsPage.tsx
@@ -1,7 +1,5 @@
 import { projectsImage } from '../images';
-
 import PageBanner from '../organisms/PageBanner';
-import ProjectCard from '../organisms/ProjectCard';
 
 const props = {
   image: projectsImage,
@@ -11,22 +9,9 @@ const props = {
     'Explore past and present GP2 projects being carried out by teams within the network and discover open opportunities to join them.',
 };
 
-const projectProps = {
-  id: '42',
-  title: 'A Project',
-  startDate: '2020-07-06',
-  endDate: '2021-12-28',
-  status: 'Completed' as const,
-  projectProposalUrl: 'http://a-proposal',
-  members: [],
-};
-
 const ProjectsPage: React.FC = () => (
   <article>
     <PageBanner {...props} />
-    <main>
-      <ProjectCard {...projectProps}></ProjectCard>
-    </main>
   </article>
 );
 

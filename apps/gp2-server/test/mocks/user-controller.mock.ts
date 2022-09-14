e254f58d3155c17daa4d001f75f7645ba53b4be7
Mark Sims <mark@bluepie.co.uk>
2022-09-14 08:22:52 +0100

    adds fetch to gp2 user controller

--- apps/gp2-server/test/mocks/user-controller.mock.ts
+++ apps/gp2-server/test/mocks/user-controller.mock.ts
@@ -1,6 +1,7 @@
 import { UserController } from '../../src/controllers/user.controller';
 
 export const userControllerMock: jest.Mocked<UserController> = {
+  fetch: jest.fn(),
   fetchById: jest.fn(),
   fetchByCode: jest.fn(),
   connectByCode: jest.fn(),

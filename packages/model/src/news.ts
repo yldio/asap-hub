6518db04bfcc4c76c7b2fcb98cd1de4a09bdd5d2
Thomas Waterfall <thomas.waterfall@gmail.com>
2022-09-15 13:29:00 +0100

    Search and Filter frontend functionality

--- packages/model/src/news.ts
+++ packages/model/src/news.ts
@@ -2,13 +2,20 @@ import { ListResponse } from './common';
 
 /* istanbul ignore next */
 export const newsType = ['News', 'Tutorial', 'Working Groups'] as const;
+export const newsFrequency = [
+  'Biweekly Newsletter',
+  'CRN Quarterly',
+  'News Articles',
+] as const;
 
 export type NewsType = typeof newsType[number];
+export type NewsFrequency = typeof newsFrequency[number];
 
 export interface NewsResponse {
   id: string;
   title: string;
   type: NewsType;
+  frequency?: NewsFrequency;
   shortText?: string;
   text?: string;
   thumbnail?: string;
--- packages/react-components/src/templates/NewsPage.tsx
+++ packages/react-components/src/templates/NewsPage.tsx
@@ -1,16 +1,21 @@
 import { css } from '@emotion/react';
+import { ComponentProps } from 'react';
 
 import { perRem } from '../pixels';
 import NewsPageHeader from './NewsPageHeader';
 import { contentSidePaddingWithNavigation } from '../layout';
 
 const mainStyles = css({
-  padding: `${36 / perRem}em ${contentSidePaddingWithNavigation(8)}`,
+  padding: `${24 / perRem}em ${contentSidePaddingWithNavigation(8)} ${
+    36 / perRem
+  }em`,
 });
 
-const NewsPage: React.FC = ({ children }) => (
+type NewsPageProps = ComponentProps<typeof NewsPageHeader>;
+
+const NewsPage: React.FC<NewsPageProps> = ({ children, ...props }) => (
   <article>
-    <NewsPageHeader />
+    <NewsPageHeader {...props} />
     <main css={mainStyles}>{children}</main>
   </article>
 );

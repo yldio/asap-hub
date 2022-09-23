b16e7f5e303ad9cc001884d9d692d2908022e1e9
Mark Sims <mark@bluepie.co.uk>
2022-09-23 11:21:34 +0100

    creates UserList Component

---
+++ apps/gp2-frontend/src/users/UserList.tsx
@@ -0,0 +1,70 @@
+import { UsersPageBody } from '@asap-hub/gp2-components';
+import { useEffect } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import { useUsersState } from './state';
+
+export const usePaginationParams = () => {
+  const searchParams = new URLSearchParams(useLocation().search);
+  const currentPage = Number(searchParams.get('currentPage')) ?? 0;
+  const pageSize = 2;
+
+  return {
+    currentPage,
+    pageSize,
+  };
+};
+
+export const usePagination = (numberOfItems: number, pageSize: number) => {
+  const history = useHistory();
+  const searchParams = new URLSearchParams(useLocation().search);
+
+  const currentPage = Number(searchParams.get('currentPage')) ?? 0;
+  const lastAllowedPage = Math.max(0, Math.ceil(numberOfItems / pageSize) - 1);
+  const numberOfPages = Math.max(currentPage, lastAllowedPage) + 1;
+
+  const renderPageHref = (page: number) => {
+    const newSearchParams = new URLSearchParams(history.location.search);
+
+    if (page === currentPage) return '';
+    if (page === 0) newSearchParams.delete('currentPage');
+    else newSearchParams.set('currentPage', String(page));
+
+    const newParams = newSearchParams.toString();
+    return `${newParams.length ? '?' : history.location.pathname}${newParams}`;
+  };
+
+  useEffect(() => {
+    if (numberOfItems && currentPage > lastAllowedPage)
+      history.replace({
+        search: renderPageHref(lastAllowedPage),
+      });
+  });
+
+  return {
+    numberOfPages,
+    renderPageHref,
+  };
+};
+
+const UserList: React.FC<Record<string, never>> = () => {
+  const { currentPage, pageSize } = usePaginationParams();
+  const users = useUsersState({
+    currentPage,
+    pageSize,
+    searchQuery: '',
+    filters: new Set(),
+  });
+  const { numberOfPages, renderPageHref } = usePagination(
+    users.total,
+    pageSize,
+  );
+  return (
+    <UsersPageBody
+      users={users}
+      numberOfPages={numberOfPages}
+      currentPageIndex={currentPage}
+      renderPageHref={renderPageHref}
+    />
+  );
+};
+export default UserList;
--- apps/gp2-frontend/src/users/Users.tsx
+++ apps/gp2-frontend/src/users/Users.tsx
@@ -1,72 +1,12 @@
-import { UsersPage, UsersPageBody } from '@asap-hub/gp2-components';
-import { useEffect } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
-import { useUsersState } from './state';
+import { Frame } from '@asap-hub/frontend-utils';
+import { UsersPage } from '@asap-hub/gp2-components';
+import UserList from './UserList';
 
-export const usePaginationParams = () => {
-  const searchParams = new URLSearchParams(useLocation().search);
-  const currentPage = Number(searchParams.get('currentPage')) ?? 0;
-  const pageSize = 2;
-
-  return {
-    currentPage,
-    pageSize,
-  };
-};
-
-export const usePagination = (numberOfItems: number, pageSize: number) => {
-  const history = useHistory();
-  const searchParams = new URLSearchParams(useLocation().search);
-
-  const currentPage = Number(searchParams.get('currentPage')) ?? 0;
-  const lastAllowedPage = Math.max(0, Math.ceil(numberOfItems / pageSize) - 1);
-  const numberOfPages = Math.max(currentPage, lastAllowedPage) + 1;
-
-  const renderPageHref = (page: number) => {
-    const newSearchParams = new URLSearchParams(history.location.search);
-
-    if (page === currentPage) return '';
-    if (page === 0) newSearchParams.delete('currentPage');
-    else newSearchParams.set('currentPage', String(page));
-
-    const newParams = newSearchParams.toString();
-    return `${newParams.length ? '?' : history.location.pathname}${newParams}`;
-  };
-
-  useEffect(() => {
-    if (numberOfItems && currentPage > lastAllowedPage)
-      history.replace({
-        search: renderPageHref(lastAllowedPage),
-      });
-  });
-
-  return {
-    numberOfPages,
-    renderPageHref,
-  };
-};
-
-const Users: React.FC<Record<string, never>> = () => {
-  const { currentPage, pageSize } = usePaginationParams();
-  const users = useUsersState({
-    currentPage,
-    pageSize,
-    searchQuery: '',
-    filters: new Set(),
-  });
-  const { numberOfPages, renderPageHref } = usePagination(
-    users.total,
-    pageSize,
-  );
-  return (
-    <UsersPage>
-      <UsersPageBody
-        users={users}
-        numberOfPages={numberOfPages}
-        currentPageIndex={currentPage}
-        renderPageHref={renderPageHref}
-      />
-    </UsersPage>
-  );
-};
+const Users: React.FC<Record<string, never>> = () => (
+  <UsersPage>
+    <Frame title="Users">
+      <UserList />
+    </Frame>
+  </UsersPage>
+);
 export default Users;

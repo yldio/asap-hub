25dd938f73c76735a1d74769123155409dacd4f6
Carlos Barbosa <fbcarlos101@gmail.com>
2022-09-28 12:09:33 +0100

    feat: added Project Summary Footer to Project Card

--- packages/gp2-components/src/organisms/ProjectCard.tsx
+++ packages/gp2-components/src/organisms/ProjectCard.tsx
@@ -5,18 +5,13 @@ import {
   lead,
   LinkHeadline,
   pixels,
-  utils,
 } from '@asap-hub/react-components';
 import { gp2 as gp2Routing } from '@asap-hub/routing';
 import { css } from '@emotion/react';
-import { format, formatDistanceStrict as formatDistance } from 'date-fns';
-import dateIcon from '../icons/date-icon';
-import usersIcon from '../icons/users-icon';
-import IconWithLabel from '../molecules/IconWithLabel';
 import colors from '../templates/colors';
+import ProjectSummaryFooter from './ProjectSummaryFooter';
 import ProjectSummaryHeader from './ProjectSummaryHeader';
 
-const { getCounterString } = utils;
 const { rem } = pixels;
 
 type ProjectCardProps = Pick<
@@ -88,23 +83,11 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
         }),
       ]}
     >
-      <IconWithLabel icon={usersIcon}>
-        {getCounterString(members.length, 'Member')}
-      </IconWithLabel>
-      <IconWithLabel icon={dateIcon}>
-        <span>
-          {`${format(new Date(startDate), 'MMM yyyy')}${
-            endDate ? ` - ${format(new Date(endDate), 'MMM yyyy')} · ` : ''
-          }`}
-          {endDate && (
-            <span css={{ color: colors.neutral800.rgba }}>
-              {`(${formatDistance(new Date(startDate), new Date(endDate), {
-                unit: 'month',
-              })})`}
-            </span>
-          )}
-        </span>
-      </IconWithLabel>
+      <ProjectSummaryFooter
+        members={members}
+        startDate={startDate}
+        endDate={endDate}
+      />
     </div>
   </Card>
 );

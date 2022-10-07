214f66f7127fdaa50db9ad63c1948cee23588cb5
gabiayako <gabiayako@gmail.com>
2022-10-07 09:38:00 -0300

    adding test to check the click in the tutorial card

--- apps/crn-frontend/src/discover/__tests__/Tutorials.test.tsx
+++ apps/crn-frontend/src/discover/__tests__/Tutorials.test.tsx
@@ -1,21 +1,42 @@
 import { Suspense } from 'react';
 import { User } from '@asap-hub/auth';
+import { MemoryRouter, Route } from 'react-router-dom';
 import { render, waitFor, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { RecoilRoot } from 'recoil';
-import { createDiscoverResponse, createNewsResponse } from '@asap-hub/fixtures';
+import {
+  createDiscoverResponse,
+  createNewsResponse,
+  createTutorialsResponse,
+} from '@asap-hub/fixtures';
+import { discover, tutorials } from '@asap-hub/routing';
+import { TutorialsResponse } from '@asap-hub/model';
 
 import Tutorials from '../Tutorials';
+import Tutorial from '../tutorials/Tutorial';
 import { Auth0Provider, WhenReady } from '../../auth/test-utils';
 import { refreshDiscoverState } from '../state';
 import { getDiscover } from '../api';
+import { getTutorialById } from '../tutorials/api';
 
 jest.mock('../api');
+jest.mock('../tutorials/api');
 
 afterEach(() => {
   jest.clearAllMocks();
 });
 
 const mockGetDiscover = getDiscover as jest.MockedFunction<typeof getDiscover>;
+
+const mockGetTutorialById = getTutorialById as jest.MockedFunction<
+  typeof getTutorialById
+>;
+
+const tutorial: TutorialsResponse = {
+  id: '55724942-3408-4ad6-9a73-14b92226ffb6',
+  created: '2020-09-07T17:36:54Z',
+  title: 'First One title',
+};
 
 const renderDiscoverTutorials = async (user: Partial<User>) => {
   const result = render(
@@ -27,7 +48,14 @@ const renderDiscoverTutorials = async (user: Partial<User>) => {
       >
         <Auth0Provider user={user}>
           <WhenReady>
-            <Tutorials />
+            <MemoryRouter initialEntries={[{ pathname: discover({}).$ }]}>
+              <Route path={discover.template}>
+                <Tutorials />
+              </Route>
+              <Route path={tutorials.template + tutorials({}).article.template}>
+                <Tutorial />
+              </Route>
+            </MemoryRouter>
           </WhenReady>
         </Auth0Provider>
       </RecoilRoot>
@@ -61,4 +89,29 @@ it('renders the correct title and subtitle', async () => {
 
   expect(screen.getByText(/Tutorials/i, { selector: 'h2' })).toBeVisible();
   expect(screen.getByText(/Explore our tutorials/i)).toBeVisible();
+});
+
+it('renders tutorial page when user clicks tutorial card title', async () => {
+  mockGetDiscover.mockResolvedValue({
+    ...createDiscoverResponse(),
+    training: [createTutorialsResponse({ key: 'First One' })],
+  });
+
+  mockGetTutorialById.mockResolvedValue(tutorial);
+
+  await renderDiscoverTutorials({});
+
+  const tutorialAnchorTitle = screen.getByText(/First One title/i, {
+    selector: 'a',
+  }) as HTMLAnchorElement;
+
+  expect(tutorialAnchorTitle).toBeVisible();
+  expect(tutorialAnchorTitle.href).toContain('/tutorials/');
+
+  userEvent.click(tutorialAnchorTitle);
+  await waitFor(() => expect(mockGetTutorialById).toHaveBeenCalled());
+
+  expect(
+    await screen.findByText(/First One title/i, { selector: 'h1' }),
+  ).toBeVisible();
 });

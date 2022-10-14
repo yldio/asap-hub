770370f31ba01c4c29e65a94d3d259d509dcfa12
ricardocarvalho <ricardo.carvalho@yld.io>
2022-10-14 08:57:54 -0300

    [CRN-1115] fix alumni badge position

--- packages/react-components/src/templates/UserProfileHeader.tsx
+++ packages/react-components/src/templates/UserProfileHeader.tsx
@@ -66,9 +66,10 @@ const containerStyles = css({
 const nameHeaderStyles = css({
   display: 'flex',
   alignItems: 'center',
-  gap: `${15 / perRem}em`,
-  [`@media (max-width: ${tabletScreen.min}px)`]: {
-    flexWrap: 'wrap',
+  gap: `${6 / perRem}em`,
+  [`@media (max-width: ${tabletScreen.max}px)`]: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
     marginBottom: `${24 / perRem}em`,
   },
 });

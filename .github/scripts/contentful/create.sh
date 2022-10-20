79d6d8c06372aaf57cb2dbd0fc98b465c21a0dfb
Matt Doughty <matt@apogee.guru>
2022-10-20 16:53:51 +0100

    Fix data binary?

--- .github/scripts/contentful/create.sh
+++ .github/scripts/contentful/create.sh
@@ -22,7 +22,7 @@ STATUS_CODE=$(curl --silent \
                    --header "Authorization: Bearer ${CONTENTFUL_MANAGEMENT_ACCESS_TOKEN}" \
                    --header "Content-Type: application/vnd.contentful.management.v1+json" \
                    --header "X-Contentful-Source-Environment: ${CONTENTFUL_SOURCE_ENV}" \
-                   --data-binary '{"name": "'"$CONTENTFUL_ENV_ID"'"}' \
+                   --data-binary "{\"name\": \"${CONTENTFUL_ENV_ID}\"}" \
                    https://api.contentful.com/spaces/${CONTENTFUL_SPACE_ID}/environments)
 
 if [[ "$STATUS_CODE" == "201" ]]; then

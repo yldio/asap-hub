35aa1fbe05c4c9d082fc975f1f585f91eb14b9ca
Matt Doughty <matt@apogee.guru>
2022-10-20 16:24:01 +0100

    Create contentful environment on PR.

---
+++ .github/scripts/contentful/create.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+if [[ "ON_BRANCH_ENV" == "False" ]]; then
+  echo "No branch Environment required; not deleting environment."
+  exit 0
+fi
+
+if [[ "$CONTENTFUL_ENV_ID" == "Production" ]]; then
+  echo "Target is Production branch; nothing to do."
+  exit 0
+fi
+
+if [[ "$CONTENTFUL_ENV_ID" == "Development" ]]; then
+  echo "Target is Development branch; nothing to do."
+  exit 0
+fi
+
+STATUS_CODE=$(curl --silent \
+                   --request POST \
+                   --output /dev/null \
+                   --write-out "%{http_code}" \
+                   --header "Authorization: Bearer ${CONTENTFUL_MANAGEMENT_ACCESS_TOKEN}" \
+                   --header "X-Contentful-Source-Environment: ${CONTENTFUL_SOURCE_ENV}" \
+                   --data-binary "{\"name\": ${CONTENTFUL_ENV_ID}}" \
+                   https://api.contentful.com/spaces/${CONTENTFUL_SPACE_ID}/environments)
+
+if [[ "$STATUS_CODE" == "201" ]]; then
+  echo "Successfully created Environment ${CONTENTFUL_ENV_ID}."
+  exit 0
+else
+  echo "Failed to create Environment ${CONTENTFUL_ENV_ID}."
+  echo "Status code was ${STATUS_CODE}."
+  exit 1
+fi

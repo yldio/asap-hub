d050f2baea6ee089e376bc3cac28445c1344a70e
Mark Sims <mark@bluepie.co.uk>
2022-10-21 15:37:30 +0100

    apply fetch-by-code typings

--- apps/gp2-server/src/handlers/webhooks/fetch-by-code-handler.ts
+++ apps/gp2-server/src/handlers/webhooks/fetch-by-code-handler.ts
@@ -1,4 +1,5 @@
 import { ValidationError } from '@asap-hub/errors';
+import { gp2 } from '@asap-hub/model';
 import { validateAuth0Request } from '@asap-hub/server-common';
 import { framework as lambda } from '@asap-hub/services-common';
 import { RestUser, SquidexGraphql, SquidexRest } from '@asap-hub/squidex';
@@ -11,7 +12,7 @@ import { sentryWrapper } from '../../utils/sentry-wrapper';
 export const fetchUserByCodeHandlerFactory = (
   userController: UserController,
 ): lambda.Handler =>
-  lambda.http(async (request) => {
+  lambda.http<gp2.UserResponse>(async (request) => {
     validateAuth0Request(request, auth0SharedSecret);
 
     const { code } = validateParams(request.params);

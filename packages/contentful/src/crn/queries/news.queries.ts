ad75f91f2d2db3293f359434355b207fa888eda2
gabiayako <gabiayako@gmail.com>
2022-11-03 08:07:25 -0300

    fetch by id

--- packages/contentful/src/crn/queries/news.queries.ts
+++ packages/contentful/src/crn/queries/news.queries.ts
@@ -1,5 +1,36 @@
 import { gql } from 'graphql-tag';
 
+export const newsContentQueryFragment = gql`
+  fragment NewsContent on NewsCollection {
+    items {
+      sys {
+        firstPublishedAt
+      }
+      id
+      title
+      shortText
+      frequency
+      link
+      linkText
+      thumbnail {
+        url
+      }
+      text {
+        json
+      }
+    }
+  }
+`;
+
+export const FETCH_NEWS_BY_ID = gql`
+  query FetchNewsById($id: String!) {
+    newsCollection(where: { id: $id }) {
+      ...NewsContent
+    }
+  }
+  ${newsContentQueryFragment}
+`;
+
 export const FETCH_NEWS = gql`
   query FetchNews(
     $limit: Int
@@ -13,23 +44,43 @@ export const FETCH_NEWS = gql`
       where: { frequency_in: $frequency, title_contains: $title }
       order: sys_firstPublishedAt_DESC
     ) {
-      items {
-        sys {
-          firstPublishedAt
-        }
-        id
-        title
-        shortText
-        frequency
-        link
-        linkText
-        thumbnail {
-          url
-        }
-        text {
-          json
-        }
-      }
+      total
+      ...NewsContent
     }
   }
+  ${newsContentQueryFragment}
 `;
+
+// export const FETCH_NEWS = gql`
+//   query FetchNews(
+//     $limit: Int
+//     $skip: Int
+//     $frequency: [String]
+//     $title: String
+//   ) {
+// newsCollection(
+//   limit: $limit
+//   skip: $skip
+//   where: { frequency_in: $frequency, title_contains: $title }
+//   order: sys_firstPublishedAt_DESC
+// ) {
+//       items {
+//         sys {
+//           firstPublishedAt
+//         }
+//         id
+//         title
+//         shortText
+//         frequency
+//         link
+//         linkText
+//         thumbnail {
+//           url
+//         }
+//         text {
+//           json
+//         }
+//       }
+//     }
+//   }
+// `;

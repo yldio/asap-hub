577665a89cbf59011a4a153877595fd2abba4a46
Mark Sims <mark@bluepie.co.uk>
2022-11-18 09:06:26 +0000

    lazy onboarding

--- apps/gp2-frontend/src/onboarding/Routes.tsx
+++ apps/gp2-frontend/src/onboarding/Routes.tsx
@@ -1,12 +1,22 @@
 import { NotFoundPage } from '@asap-hub/react-components';
 import { gp2 } from '@asap-hub/routing';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
-import CoreDetails from './CoreDetails';
-import Welcome from './Welcome';
+import { lazy, useEffect } from 'react';
+
+const loadWelcome = () =>
+  import(/* webpackChunkName: "onboarding-welcome" */ './Welcome');
+const loadCoreDetail = () =>
+  import(/* webpackChunkName: "onboarding-core-details" */ './CoreDetails');
+
+const Welcome = lazy(loadWelcome);
+const CoreDetails = lazy(loadCoreDetail);
 
 const { onboarding } = gp2;
 
 const Routes: React.FC<Record<string, never>> = () => {
+  useEffect(() => {
+    loadWelcome().then(loadCoreDetail);
+  }, []);
   const { path } = useRouteMatch();
 
   return (

9e6c718b1b8ceaf64912a1bcc82e36fbb30997c7
Mark Sims <mark@bluepie.co.uk>
2022-11-21 15:52:06 +0000

    returns the telephone number if the user is the logged in user

--- apps/gp2-server/test/routes/user.route.test.ts
+++ apps/gp2-server/test/routes/user.route.test.ts
@@ -1,34 +1,24 @@
-import { FetchOptions } from '@asap-hub/model';
+import { FetchOptions, gp2 } from '@asap-hub/model';
+import { AuthHandler } from '@asap-hub/server-common';
 import Boom from '@hapi/boom';
 import supertest from 'supertest';
 import { appFactory } from '../../src/app';
 import { fetchExpectation, getUserResponse } from '../fixtures/user.fixtures';
-import { authHandlerMock } from '../mocks/auth-handler.mock';
 import { loggerMock } from '../mocks/logger.mock';
 import { userControllerMock } from '../mocks/user-controller.mock';
 
 describe('/users/ route', () => {
-  const appWithMockedAuth = appFactory({
-    userController: userControllerMock,
-    authHandler: authHandlerMock,
-    logger: loggerMock,
-  });
-
-  const app = appFactory({
-    userController: userControllerMock,
-    logger: loggerMock,
-  });
-
   afterEach(jest.resetAllMocks);
 
   describe('GET /users', () => {
     test('Should return 200 when no users exist', async () => {
+      const { app } = getApp();
       userControllerMock.fetch.mockResolvedValueOnce({
         items: [],
         total: 0,
       });
 
-      const response = await supertest(appWithMockedAuth).get('/users');
+      const response = await supertest(app).get('/users');
 
       expect(response.status).toBe(200);
       expect(response.body).toEqual({
@@ -38,21 +28,23 @@ describe('/users/ route', () => {
     });
 
     test('Should return the results correctly', async () => {
+      const { app } = getApp();
       userControllerMock.fetch.mockResolvedValueOnce(fetchExpectation);
 
-      const response = await supertest(appWithMockedAuth).get('/users');
+      const response = await supertest(app).get('/users');
 
       expect(response.status).toBe(200);
       expect(response.body).toEqual(fetchExpectation);
     });
 
     test('Should call the controller method with the correct parameters', async () => {
+      const { app } = getApp();
       userControllerMock.fetch.mockResolvedValueOnce({
         items: [],
         total: 0,
       });
 
-      await supertest(appWithMockedAuth).get('/users').query({
+      await supertest(app).get('/users').query({
         take: 15,
         skip: 5,
         search: 'something',
@@ -69,21 +61,19 @@ describe('/users/ route', () => {
 
     describe('Parameter validation', () => {
       test('Should return a 400 error when additional properties exist', async () => {
-        const response = await supertest(appWithMockedAuth)
-          .get('/users')
-          .query({
-            additionalField: 'some-data',
-          });
+        const { app } = getApp();
+        const response = await supertest(app).get('/users').query({
+          additionalField: 'some-data',
+        });
 
         expect(response.status).toBe(400);
       });
 
       test('Should return a validation error when the arguments are not valid', async () => {
-        const response = await supertest(appWithMockedAuth)
-          .get('/users')
-          .query({
-            take: 'invalid param',
-          });
+        const { app } = getApp();
+        const response = await supertest(app).get('/users').query({
+          take: 'invalid param',
+        });
 
         expect(response.status).toBe(400);
       });
@@ -91,7 +81,8 @@ describe('/users/ route', () => {
       test('Should return the results correctly when a filter is used', async () => {
         userControllerMock.fetch.mockResolvedValueOnce(fetchExpectation);
 
-        const response = await supertest(appWithMockedAuth)
+        const { app } = getApp();
+        const response = await supertest(app)
           .get('/users')
           .query({
             filter: { region: ['Europe'] },
@@ -103,7 +94,8 @@ describe('/users/ route', () => {
       test('Should return the results correctly when multiple filters are used', async () => {
         userControllerMock.fetch.mockResolvedValueOnce(fetchExpectation);
 
-        const response = await supertest(appWithMockedAuth)
+        const { app } = getApp();
+        const response = await supertest(app)
           .get('/users')
           .query({
             filter: { region: ['Europe', 'Asia'] },
@@ -118,7 +110,8 @@ describe('/users/ route', () => {
     test('Should return 404 when user doesnt exist', async () => {
       userControllerMock.fetchById.mockRejectedValueOnce(Boom.notFound());
 
-      const response = await supertest(appWithMockedAuth).get('/users/123');
+      const { app } = getApp();
+      const response = await supertest(app).get('/users/123');
 
       expect(response.status).toBe(404);
     });
@@ -126,7 +119,8 @@ describe('/users/ route', () => {
     test('Should return the results correctly', async () => {
       userControllerMock.fetchById.mockResolvedValueOnce(getUserResponse());
 
-      const response = await supertest(appWithMockedAuth).get('/users/123');
+      const { app } = getApp();
+      const response = await supertest(app).get('/users/123');
 
       expect(response.status).toBe(200);
       expect(response.body).toEqual(getUserResponse());
@@ -135,9 +129,13 @@ describe('/users/ route', () => {
     test('Should call the controller with the right parameter', async () => {
       const userId = 'abc123';
 
-      await supertest(appWithMockedAuth).get(`/users/${userId}`);
+      const { app, loggedInUserId } = getApp();
+      await supertest(app).get(`/users/${userId}`);
 
-      expect(userControllerMock.fetchById).toBeCalledWith(userId);
+      expect(userControllerMock.fetchById).toBeCalledWith(
+        userId,
+        loggedInUserId,
+      );
     });
   });
 
@@ -145,6 +143,7 @@ describe('/users/ route', () => {
     test('Should return 404 when user doesnt exist', async () => {
       userControllerMock.fetchByCode.mockRejectedValueOnce(Boom.forbidden());
 
+      const { app } = getApp();
       const response = await supertest(app).get('/users/invites/123');
 
       expect(response.status).toBe(404);
@@ -153,6 +152,7 @@ describe('/users/ route', () => {
     test('Should return the results correctly', async () => {
       userControllerMock.fetchByCode.mockResolvedValueOnce(getUserResponse());
 
+      const { app } = getApp();
       const response = await supertest(app).get('/users/invites/123');
 
       expect(response.status).toBe(200);
@@ -166,9 +166,28 @@ describe('/users/ route', () => {
     test('Should call the controller with the right parameter', async () => {
       const code = 'abc123';
 
+      const { app } = getApp();
       await supertest(app).get(`/users/invites/${code}`);
 
       expect(userControllerMock.fetchByCode).toBeCalledWith(code);
     });
   });
+  const getApp = () => {
+    const loggedInUserId = '11';
+    const loggedUser: gp2.UserResponse = {
+      ...getUserResponse(),
+      id: loggedInUserId,
+    };
+    const getLoggedUser = jest.fn().mockReturnValue(loggedUser);
+    const authHandlerMock: AuthHandler = (req, _res, next) => {
+      req.loggedInUser = getLoggedUser();
+      next();
+    };
+    const app = appFactory({
+      userController: userControllerMock,
+      authHandler: authHandlerMock,
+      logger: loggerMock,
+    });
+    return { app, loggedInUserId };
+  };
 });

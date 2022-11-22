bcbf261c05e1b53a7f591d3659855e0455d5d29d
gabiayako <gabiayako@gmail.com>
2022-11-22 09:24:42 -0300

    clear console log that was appearing in the test

--- packages/cms-data-sync/test/utils/entries.test.ts
+++ packages/cms-data-sync/test/utils/entries.test.ts
@@ -2,13 +2,14 @@ import { publishContentfulEntries } from '../../src/utils';
 import { entry } from '../fixtures';
 
 describe('publishContentfulEntries', () => {
+  console.log = jest.fn();
+
   it('calls entry publish function', async () => {
     await publishContentfulEntries([entry]);
     expect(entry.publish).toHaveBeenCalledTimes(1);
   });
 
   it('outputs a message when publish fails', async () => {
-    console.log = jest.fn();
     await publishContentfulEntries([entry]);
     expect(console.log).toHaveBeenCalledWith(
       `Entry with id ${entry.sys.id} could not be published.`,
@@ -19,7 +20,6 @@ describe('publishContentfulEntries', () => {
     jest
       .spyOn(entry, 'publish')
       .mockImplementationOnce(() => Promise.resolve(entry));
-    console.log = jest.fn();
     await publishContentfulEntries([entry]);
     expect(console.log).toHaveBeenCalledWith(
       `Published entry ${entry.sys.id}.`,

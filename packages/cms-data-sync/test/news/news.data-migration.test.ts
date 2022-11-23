21dc21d3bab8f78ab16ce84f702dd74178ae94f2
gabiayako <gabiayako@gmail.com>
2022-11-23 07:06:52 -0300

    wip text test in migration

--- packages/cms-data-sync/test/news/news.data-migration.test.ts
+++ packages/cms-data-sync/test/news/news.data-migration.test.ts
@@ -1,8 +1,10 @@
 import { SquidexGraphqlClient } from '@asap-hub/squidex';
 import { migrateNews } from '../../src/news/news.data-migration';
-import { entry, getContentfulEnvironmentMock } from '../fixtures';
+import { entry, getContentfulEnvironmentMock, squidexAsset } from '../fixtures';
 import {
   clearContentfulEntries,
+  createAssetLink,
+  convertHtmlToContentfulFormat,
   publishContentfulEntries,
   getSquidexAndContentfulClients,
 } from '../../src/utils';
@@ -10,6 +12,43 @@ import { Environment } from 'contentful-management';
 
 jest.mock('../../src/utils/setup');
 jest.mock('../../src/utils/entries');
+jest.mock('../../src/utils/assets');
+jest.mock('../../src/utils/rich-text');
+
+const squidexResponseWithTitleOnly = {
+  queryNewsAndEventsContents: [
+    {
+      id: 'news-1',
+      flatData: {
+        title: 'news',
+      },
+    },
+  ],
+};
+
+const squidexResponseWithThumb = {
+  queryNewsAndEventsContents: [
+    {
+      id: 'news-1',
+      flatData: {
+        title: 'news',
+        thumbnail: [squidexAsset],
+      },
+    },
+  ],
+};
+
+const squidexResponseWithText = {
+  queryNewsAndEventsContents: [
+    {
+      id: 'news-1',
+      flatData: {
+        title: 'news',
+        text: '<p>Hello world</p>',
+      },
+    },
+  ],
+};
 
 describe('migrateNews', () => {
   let contenfulEnv: Environment;
@@ -17,29 +56,15 @@ describe('migrateNews', () => {
 
   beforeEach(() => {
     jest.clearAllMocks();
+
+    contenfulEnv = getContentfulEnvironmentMock();
     squidexGraphqlClientMock = {
       request: jest.fn(),
     };
-    contenfulEnv = getContentfulEnvironmentMock();
-    const mock = getSquidexAndContentfulClients as jest.Mock; // spy on otherFn
-    mock.mockResolvedValueOnce({
+
+    (getSquidexAndContentfulClients as jest.Mock).mockResolvedValueOnce({
       contentfulEnvironment: contenfulEnv,
       squidexGraphqlClient: squidexGraphqlClientMock,
-    });
-
-    jest
-      .spyOn(entry, 'publish')
-      .mockImplementationOnce(() => Promise.resolve(entry));
-
-    squidexGraphqlClientMock.request.mockResolvedValueOnce({
-      queryNewsAndEventsContents: [
-        {
-          id: 'news-1',
-          flatData: {
-            title: 'news',
-          },
-        },
-      ],
     });
 
     jest.spyOn(contenfulEnv, 'getEntries').mockResolvedValueOnce({
@@ -52,36 +77,78 @@ describe('migrateNews', () => {
     });
 
     jest.spyOn(contenfulEnv, 'createEntryWithId').mockResolvedValueOnce(entry);
+
+    jest
+      .spyOn(entry, 'publish')
+      .mockImplementationOnce(() => Promise.resolve(entry));
   });
 
-  it('clear contentful entries', async () => {
+  it('clears contentful entries', async () => {
+    squidexGraphqlClientMock.request.mockResolvedValueOnce(
+      squidexResponseWithTitleOnly,
+    );
+
     await migrateNews();
     const clearContentfulEntriesMock = clearContentfulEntries as jest.Mock;
 
     expect(clearContentfulEntriesMock).toHaveBeenCalled();
   });
 
-  it('create news contentful entries', async () => {
-    await migrateNews();
+  describe('creates contentful entries', () => {
+    it('for a news that contains only title', async () => {
+      squidexGraphqlClientMock.request.mockResolvedValueOnce(
+        squidexResponseWithTitleOnly,
+      );
 
-    expect(contenfulEnv.createEntryWithId).toHaveBeenCalledWith(
-      'news',
-      'news-1',
-      {
-        fields: {
-          frequency: { 'en-US': 'News Articles' },
-          link: { 'en-US': undefined },
-          linkText: { 'en-US': undefined },
-          shortText: { 'en-US': undefined },
-          text: { 'en-US': null },
-          thumbnail: { 'en-US': null },
-          title: { 'en-US': 'news' },
+      await migrateNews();
+
+      expect(contenfulEnv.createEntryWithId).toHaveBeenCalledWith(
+        'news',
+        'news-1',
+        {
+          fields: {
+            frequency: { 'en-US': 'News Articles' },
+            link: { 'en-US': undefined },
+            linkText: { 'en-US': undefined },
+            shortText: { 'en-US': undefined },
+            text: { 'en-US': null },
+            thumbnail: { 'en-US': null },
+            title: { 'en-US': 'news' },
+          },
         },
-      },
-    );
+      );
+    });
+
+    it('for a news that contains thumbnail', async () => {
+      squidexGraphqlClientMock.request.mockResolvedValueOnce(
+        squidexResponseWithThumb,
+      );
+
+      await migrateNews();
+
+      const createAssetLinkMock = createAssetLink as jest.Mock;
+
+      expect(createAssetLinkMock).toHaveBeenCalled();
+    });
+
+    it('for a news that contains text', async () => {
+      squidexGraphqlClientMock.request.mockResolvedValueOnce(
+        squidexResponseWithText,
+      );
+
+      await migrateNews();
+
+      const convertHtmlToContentfulFormatMock =
+        convertHtmlToContentfulFormat as jest.Mock;
+
+      expect(convertHtmlToContentfulFormatMock).toHaveBeenCalled();
+    });
   });
 
   it('publish contentful entries', async () => {
+    squidexGraphqlClientMock.request.mockResolvedValueOnce(
+      squidexResponseWithTitleOnly,
+    );
     const publishContentfulEntriesMock = publishContentfulEntries as jest.Mock;
 
     await migrateNews();

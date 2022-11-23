ed427567dcd5e55b7c446675503641a692a065c2
Ratiu Octavian <oratiu@gmail.com>
2022-11-23 17:13:09 +0200

    reverted query

--- apps/crn-server/src/queries/working-groups.queries.ts
+++ apps/crn-server/src/queries/working-groups.queries.ts
@@ -1,40 +1,16 @@
 import gql from 'graphql-tag';
 
-export const workingGroupContentQueryFragment = gql`
-  fragment WorkingGroupContent on WorkingGroups {
-    id
-    lastModified
-    flatData {
-      title
-      description
-      externalLink
-      externalLinkText
-    }
-  }
-`;
-
 export const FETCH_WORKING_GROUP = gql`
   query FetchWorkingGroup($id: String!) {
     findWorkingGroupsContent(id: $id) {
-      ...WorkingGroupContent
-    }
-  }
-  ${workingGroupContentQueryFragment}
-`;
-
-export const FETCH_WORKING_GROUPS = gql`
-  query FetchWorkingGroups($top: Int, $skip: Int, $filter: String) {
-    queryWorkingGroupsContentsWithTotal(
-      top: $top
-      skip: $skip
-      filter: $filter
-      orderby: "data/name/iv"
-    ) {
-      total
-      items {
-        ...WorkingGroupContent
+      id
+      lastModified
+      flatData {
+        title
+        description
+        externalLink
+        externalLinkText
       }
     }
   }
-  ${workingGroupContentQueryFragment}
 `;

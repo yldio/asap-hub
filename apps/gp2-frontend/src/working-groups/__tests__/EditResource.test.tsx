12d6a073c7b835b0bd83c76e83669dec9932c555
Mark Sims <mark@bluepie.co.uk>
2022-11-23 15:26:03 +0000

    starts refactor of EditResource.test

--- apps/gp2-frontend/src/working-groups/__tests__/EditResource.test.tsx
+++ apps/gp2-frontend/src/working-groups/__tests__/EditResource.test.tsx
@@ -1,153 +1,46 @@
-import { gp2 as gp2Model } from '@asap-hub/model';
 import { gp2 as gp2Routing } from '@asap-hub/routing';
 import { gp2 as gp2Fixtures } from '@asap-hub/fixtures';
-import { workingGroups } from '@asap-hub/routing/src/gp2';
-import {
-  render,
-  screen,
-  waitFor,
-  waitForElementToBeRemoved,
-} from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { Suspense } from 'react';
 import { MemoryRouter, Route } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
-import { Auth0Provider, WhenReady } from '../../auth/test-utils';
 import EditResource from '../EditResource';
-import { refreshWorkingGroupState } from '../state';
-import { getWorkingGroup, putWorkingGroupResources } from '../api';
 
-jest.mock('../api');
-
-const renderWorkingGroupDetail = async ({
-  id,
-  userId = '11',
-  route,
-  role = 'Trainee',
-  children,
-}: {
-  id: string;
-  userId?: string;
-  route?: string;
-  role?: gp2Model.UserRole;
-  children: JSX.Element;
-}) => {
+const renderWorkingGroupDetail = async () => {
+  const workingGroup = gp2Fixtures.createWorkingGroupResponse();
+  const { id: workingGroupId } = workingGroup;
+  const props = {
+    workingGroupId,
+    workingGroup,
+    backHref: '/back',
+    updateWorkingGroupResources: jest.fn(),
+  };
   render(
-    <RecoilRoot
-      initializeState={({ set }) => {
-        set(refreshWorkingGroupState(id), Math.random());
-      }}
+    <MemoryRouter
+      initialEntries={[
+        gp2Routing.workingGroups({}).workingGroup({ workingGroupId }).$,
+      ]}
     >
-      <Suspense fallback="loading">
-        <Auth0Provider user={{ id: userId, role }}>
-          <WhenReady>
-            <MemoryRouter
-              initialEntries={[
-                route ||
-                  gp2Routing
-                    .workingGroups({})
-                    .workingGroup({ workingGroupId: id }).$,
-              ]}
-            >
-              <Route
-                path={
-                  gp2Routing.workingGroups.template +
-                  gp2Routing.workingGroups({}).workingGroup.template
-                }
-              >
-                {children}
-              </Route>
-            </MemoryRouter>
-          </WhenReady>
-        </Auth0Provider>
-      </Suspense>
-    </RecoilRoot>,
+      <Route
+        path={
+          gp2Routing.workingGroups.template +
+          gp2Routing.workingGroups({}).workingGroup.template
+        }
+      >
+        <EditResource {...props} />
+      </Route>
+    </MemoryRouter>,
   );
-
-  await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
 };
 
 describe('edit resources', () => {
-  const mockGetWorkingGroup = getWorkingGroup as jest.MockedFunction<
-    typeof getWorkingGroup
-  >;
-
-  const mockPutWorkingGroupResources =
-    putWorkingGroupResources as jest.MockedFunction<
-      typeof putWorkingGroupResources
-    >;
-
   it('see if modal appears', async () => {
-    const workingGroup = gp2Fixtures.createWorkingGroupResponse();
-    workingGroup.members = [
-      {
-        userId: '23',
-        firstName: 'Tony',
-        lastName: 'Stark',
-        role: 'Lead',
-      },
-    ];
-    mockGetWorkingGroup.mockResolvedValueOnce(workingGroup);
-
-    const resources = workingGroups({})
-      .workingGroup({ workingGroupId: workingGroup.id })
-      .resources({}).$;
-
-    const props = {
-      workingGroupId: workingGroup.id,
-      workingGroup,
-      backHref: resources,
-      updateWorkingGroupResources: jest.fn(),
-    };
-
-    await renderWorkingGroupDetail({
-      id: workingGroup.id,
-      userId: '23',
-      role: 'Administrator',
-      route: gp2Routing
-        .workingGroups({})
-        .workingGroup({ workingGroupId: workingGroup.id })
-        .resources({}).$,
-      children: <EditResource {...props} />,
-    });
+    await renderWorkingGroupDetail();
 
     expect(screen.getByRole('button', { name: /Save/i })).toBeInTheDocument();
   });
 
   it('see if content is empty', async () => {
-    const workingGroup = gp2Fixtures.createWorkingGroupResponse();
-    workingGroup.members = [
-      {
-        userId: '23',
-        firstName: 'Tony',
-        lastName: 'Stark',
-        role: 'Lead',
-      },
-    ];
-    delete workingGroup.resources;
-    mockGetWorkingGroup.mockResolvedValueOnce(workingGroup);
-
-    const resources = workingGroups({})
-      .workingGroup({ workingGroupId: workingGroup.id })
-      .resources({}).$;
-
-    const props = {
-      workingGroupId: workingGroup.id,
-      workingGroup,
-      backHref: resources,
-      updateWorkingGroupResources: jest.fn(),
-    };
-
-    await renderWorkingGroupDetail({
-      id: workingGroup.id,
-      userId: '23',
-      role: 'Administrator',
-      route: gp2Routing
-        .workingGroups({})
-        .workingGroup({ workingGroupId: workingGroup.id })
-        .resources({}).$,
-      children: <EditResource {...props} />,
-    });
+    await renderWorkingGroupDetail();
 
     expect(
       screen.getByRole('textbox', { name: 'Resource Type (required)' }),
@@ -155,42 +48,10 @@ describe('edit resources', () => {
   });
 
   it('can submit a form when form data is valid for editing', async () => {
-    const workingGroup = gp2Fixtures.createWorkingGroupResponse();
-    workingGroup.members = [
-      {
-        userId: '23',
-        firstName: 'Tony',
-        lastName: 'Stark',
-        role: 'Lead',
-      },
-    ];
-
     const title = 'example42 title';
     const type = 'Note';
 
-    const resources = workingGroups({})
-      .workingGroup({ workingGroupId: workingGroup.id })
-      .resources({}).$;
-
-    const props = {
-      workingGroupId: workingGroup.id,
-      workingGroup,
-      backHref: resources,
-      updateWorkingGroupResources: jest.fn(),
-    };
-
-    mockGetWorkingGroup.mockResolvedValueOnce(workingGroup);
-    mockPutWorkingGroupResources.mockResolvedValueOnce(workingGroup);
-    await renderWorkingGroupDetail({
-      id: workingGroup.id,
-      userId: '23',
-      role: 'Administrator',
-      route: gp2Routing
-        .workingGroups({})
-        .workingGroup({ workingGroupId: workingGroup.id })
-        .resources({}).$,
-      children: <EditResource {...props} />,
-    });
+    await renderWorkingGroupDetail();
 
     const typeBox = await screen.findByRole('textbox', { name: /type/i });
     userEvent.type(typeBox, `${type}{enter}`);

4dc7c3b05f2c89c550b86f97f5f23f6865de26bf
Thomas Waterfall <thomas.waterfall@gmail.com>
2022-11-23 16:52:37 +0000

    Update icon

--- packages/react-components/src/icons/interest-groups.tsx
+++ packages/react-components/src/icons/interest-groups.tsx
@@ -1,167 +1,69 @@
 /* istanbul ignore file */
 
 const interestGroups = (
-  <svg
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <title>Group</title>
-    <g id="General / Groups">
-      <rect width="24" height="24" fill="white" />
-      <g id="Frame">
-        <g id="Group" opacity="0.13">
-          <g id="Group_2" opacity="0.13">
-            <path
-              id="Vector"
-              opacity="0.13"
-              d="M21.9 2.1H2.1V21.9H21.9V2.1Z"
-              fill="white"
-            />
-            <path
-              id="Vector_2"
-              opacity="0.13"
-              d="M21.8 2.2V21.7H2.2V2.2H21.8ZM22 2H2V22H22V2Z"
-              fill="#C1272D"
-            />
-          </g>
-          <g id="Group_3" opacity="0.13">
-            <path
-              id="Vector_3"
-              opacity="0.13"
-              d="M23.8 0.2V23.7H0.2V0.2H23.8ZM24 0H0V24H24V0Z"
-              fill="#C1272D"
-            />
-          </g>
-          <g id="Group_4" opacity="0.13">
-            <path
-              id="Vector_4"
-              opacity="0.13"
-              d="M20.8 3.2V20.7H3.2V3.2H20.8ZM21 3H3V21H21V3Z"
-              fill="#00A99D"
-            />
-          </g>
-          <g id="Group_5" opacity="0.13">
-            <path
-              id="Vector_5"
-              opacity="0.13"
-              d="M12 2.2C17.4 2.2 21.8 6.6 21.8 12C21.8 17.4 17.4 21.8 12 21.8C6.6 21.8 2.2 17.4 2.2 12C2.2 6.6 6.6 2.2 12 2.2ZM12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2Z"
-              fill="#0071BC"
-            />
-          </g>
-          <g id="Group_6" opacity="0.13">
-            <path
-              id="Vector_6"
-              opacity="0.13"
-              d="M12 7.2C14.6 7.2 16.8 9.3 16.8 12C16.8 14.7 14.7 16.8 12 16.8C9.3 16.8 7.2 14.6 7.2 12C7.2 9.4 9.4 7.2 12 7.2ZM12 7C9.2 7 7 9.2 7 12C7 14.8 9.2 17 12 17C14.8 17 17 14.8 17 12C17 9.2 14.8 7 12 7Z"
-              fill="#C1272D"
-            />
-          </g>
-        </g>
-        <g id="Frame_2">
-          <g id="Group_7" opacity="0.13">
-            <g id="Group_8" opacity="0.13">
-              <path
-                id="Vector_7"
-                opacity="0.13"
-                d="M20.8 3.3V20.8H3.2V3.3H20.8ZM21 3H3V21H21V3Z"
-                fill="#00A99D"
-              />
-            </g>
-            <g id="Group_9" opacity="0.13">
-              <path
-                id="Vector_8"
-                opacity="0.13"
-                d="M12 2.3C17.4 2.3 21.8 6.7 21.8 12.1C21.8 17.5 17.4 21.9 12 21.9C6.6 21.9 2.2 17.4 2.2 12C2.2 6.6 6.6 2.3 12 2.3ZM12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2Z"
-                fill="#0071BC"
-              />
-            </g>
-          </g>
-          <g id="Group_10">
-            <g id="Group_11">
-              <path
-                id="Vector_9"
-                d="M15.3 8.4C16.9016 8.4 18.2 7.10162 18.2 5.5C18.2 3.89837 16.9016 2.6 15.3 2.6C13.6984 2.6 12.4 3.89837 12.4 5.5C12.4 7.10162 13.6984 8.4 15.3 8.4Z"
-                stroke="#4D646B"
-                strokeWidth="1.3"
-                strokeMiterlimit="10"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                id="Vector_10"
-                d="M10.3 15.1V12.3C10.3 11.4 11 10.7 11.9 10.7H18.7C19.6 10.7 20.3 11.4 20.3 12.3V15.1"
-                stroke="#4D646B"
-                strokeWidth="1.3"
-                strokeMiterlimit="10"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </g>
-            <g id="Group_12">
-              <path
-                id="Vector_11"
-                d="M7.9 9.1C9.22548 9.1 10.3 8.02548 10.3 6.7C10.3 5.37452 9.22548 4.3 7.9 4.3C6.57452 4.3 5.5 5.37452 5.5 6.7C5.5 8.02548 6.57452 9.1 7.9 9.1Z"
-                stroke="#4D646B"
-                strokeWidth="1.3"
-                strokeMiterlimit="10"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                id="Vector_12"
-                d="M3.6 15.1V12.6C3.6 11.8 4.2 11.2 5 11.2H7.9"
-                stroke="#4D646B"
-                strokeWidth="1.3"
-                strokeMiterlimit="10"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </g>
-          </g>
-          <g id="Group_13">
-            <path
-              id="Vector_13"
-              d="M5.2 21.5C6.13888 21.5 6.9 20.7389 6.9 19.8C6.9 18.8611 6.13888 18.1 5.2 18.1C4.26112 18.1 3.5 18.8611 3.5 19.8C3.5 20.7389 4.26112 21.5 5.2 21.5Z"
-              stroke="#4D646B"
-              strokeMiterlimit="10"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              id="Vector_14"
-              d="M18.8 21.5C19.7389 21.5 20.5 20.7389 20.5 19.8C20.5 18.8611 19.7389 18.1 18.8 18.1C17.8611 18.1 17.1 18.8611 17.1 19.8C17.1 20.7389 17.8611 21.5 18.8 21.5Z"
-              stroke="#4D646B"
-              strokeMiterlimit="10"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              id="Vector_15"
-              d="M11.9 21.5C12.8389 21.5 13.6 20.7389 13.6 19.8C13.6 18.8611 12.8389 18.1 11.9 18.1C10.9611 18.1 10.2 18.8611 10.2 19.8C10.2 20.7389 10.9611 21.5 11.9 21.5Z"
-              stroke="#4D646B"
-              strokeMiterlimit="10"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              id="Vector_16"
-              d="M6.9 19.8H10.2"
-              stroke="#4D646B"
-              strokeMiterlimit="10"
-            />
-            <path
-              id="Vector_17"
-              d="M13.7 19.8H17"
-              stroke="#4D646B"
-              strokeMiterlimit="10"
-            />
-          </g>
-        </g>
-      </g>
-    </g>
+  <svg width={24} height={24} fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M5.35 18.035a2.35 2.35 0 1 0 2.259 3h2.182a2.351 2.351 0 0 0 4.518 0h2.382a2.35 2.35 0 1 0 0-1.3H14.31a2.35 2.35 0 0 0-4.518 0H7.609a2.35 2.35 0 0 0-2.259-1.7Zm0 1.3a1.05 1.05 0 1 0 0 2.1 1.05 1.05 0 0 0 0-2.1Zm5.65 1.05a1.05 1.05 0 1 1 2.1 0 1.05 1.05 0 0 1-2.1 0Zm7.95-1.05a1.05 1.05 0 1 0 0 2.1 1.05 1.05 0 0 0 0-2.1Z"
+      fill="#4D646B"
+    />
+    <title>Interest Group</title>
+    <rect
+      width={1.6}
+      height={2.2}
+      rx={0.8}
+      transform="matrix(1 0 0 -1 11.2 2.2)"
+      fill="#4D646B"
+    />
+    <rect
+      x={3.2}
+      y={9.6}
+      width={1.6}
+      height={2.2}
+      rx={0.8}
+      transform="rotate(-90 3.2 9.6)"
+      fill="#4D646B"
+    />
+    <path
+      d="M14.857 12.971c.662-.453 1.202-1.245 1.576-1.95A4.869 4.869 0 0 0 17 8.74c0-1.31-.527-2.566-1.464-3.492A5.032 5.032 0 0 0 12 3.801c-1.326 0-2.598.52-3.536 1.446A4.907 4.907 0 0 0 7 8.74c0 .796.194 1.58.567 2.286.373.706.914 1.492 1.576 1.946m5.714 0H9.143m5.714 0-.279 1.842-.108.789a1.407 1.407 0 0 1-.481.858c-.26.22-.59.34-.931.34h-2.116a1.44 1.44 0 0 1-.931-.34c-.26-.22-.43-.525-.481-.858l-.109-.789-.278-1.842"
+      stroke="#4D646B"
+      strokeWidth={1.3}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M10 14.965h4"
+      stroke="#4D646B"
+      strokeWidth={0.65}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <rect
+      x={18.6}
+      y={9.6}
+      width={1.6}
+      height={2.2}
+      rx={0.8}
+      transform="rotate(-90 18.6 9.6)"
+      fill="#4D646B"
+    />
+    <rect
+      width={1.6}
+      height={2.2}
+      rx={0.8}
+      transform="scale(1 -1) rotate(45 8.457 5.373)"
+      fill="#4D646B"
+    />
+    <rect
+      x={17.725}
+      y={4.406}
+      width={1.6}
+      height={2.2}
+      rx={0.8}
+      transform="rotate(-135 17.725 4.406)"
+      fill="#4D646B"
+    />
   </svg>
 );
 

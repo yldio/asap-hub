66d28dd7fe318e1288a4f7b21ab172991dcdbf0e
gabiayako <gabiayako@gmail.com>
2022-11-30 10:24:52 -0300

    CRN-1139 - PR 1 | Ensure about tab is selected by default

--- apps/crn-frontend/src/network/working-groups/WorkingGroupProfile.tsx
+++ apps/crn-frontend/src/network/working-groups/WorkingGroupProfile.tsx
@@ -1,22 +1,34 @@
+import { FC } from 'react';
+import { useRouteMatch, Switch, Route, Redirect } from 'react-router-dom';
+import { Frame } from '@asap-hub/frontend-utils';
 import {
   NotFoundPage,
   WorkingGroupAbout,
   WorkingGroupPage,
 } from '@asap-hub/react-components';
 import { network, useRouteParams } from '@asap-hub/routing';
-import { FC } from 'react';
 import { useWorkingGroupById } from './state';
 
 const WorkingGroupProfile: FC = () => {
   const route = network({}).workingGroups({}).workingGroup;
   const { workingGroupId } = useRouteParams(route);
+  const { path } = useRouteMatch();
   const workingGroup = useWorkingGroupById(workingGroupId);
 
   if (workingGroup) {
     return (
-      <WorkingGroupPage {...workingGroup}>
-        <WorkingGroupAbout {...workingGroup} />
-      </WorkingGroupPage>
+      <Frame title={workingGroup.title}>
+        <Switch>
+          <Route path={path + route({ workingGroupId }).about.template}>
+            <WorkingGroupPage {...workingGroup}>
+              <Frame title="About">
+                <WorkingGroupAbout {...workingGroup} />
+              </Frame>
+            </WorkingGroupPage>
+          </Route>
+          <Redirect to={route({ workingGroupId }).about({}).$} />
+        </Switch>
+      </Frame>
     );
   }
 

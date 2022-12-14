ac9b99d95b7a171c57ee024fd2069a9ad26a604b
Mark Sims <mark@bluepie.co.uk>
2022-12-14 12:49:41 +0000

    adds render method to test

--- packages/gp2-components/src/organisms/__tests__/KeyInformationModal.test.tsx
+++ packages/gp2-components/src/organisms/__tests__/KeyInformationModal.test.tsx
@@ -1,29 +1,33 @@
 import { gp2 as gp2Fixtures } from '@asap-hub/fixtures';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { ComponentProps } from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import KeyInformationModal from '../KeyInformationModal';
 
 describe('KeyInformatiomModal', () => {
-  const defaultProps = {
+  type KeyInformationModalProps = ComponentProps<typeof KeyInformationModal>;
+  const defaultProps: KeyInformationModalProps = {
     ...gp2Fixtures.createUserResponse(),
     locationSuggestions: [],
     loadInstitutionOptions: () => Promise.resolve([]),
     backHref: '',
     onSave: jest.fn(),
   };
-  it('renders a dialog with the right title', () => {
-    render(<KeyInformationModal {...defaultProps} />, {
+  const renderKeyInformation = (
+    overrides: Partial<KeyInformationModalProps> = {},
+  ) =>
+    render(<KeyInformationModal {...defaultProps} {...overrides} />, {
       wrapper: MemoryRouter,
     });
+  it('renders a dialog with the right title', () => {
+    renderKeyInformation();
     expect(screen.getByRole('dialog')).toContainElement(
       screen.getByRole('heading', { name: 'Key Information' }),
     );
   });
   it('renders the firstName and lastName fields', () => {
-    render(<KeyInformationModal {...defaultProps} />, {
-      wrapper: MemoryRouter,
-    });
+    renderKeyInformation();
     expect(
       screen.getByRole('textbox', { name: 'First Name (required)' }),
     ).toBeVisible();
@@ -33,132 +37,115 @@ describe('KeyInformatiomModal', () => {
   });
   it('calls onSave with the right arguments', async () => {
     const onSave = jest.fn();
-    render(
-      <KeyInformationModal
-        {...defaultProps}
-        firstName="Gonçalo"
-        lastName="Ramos"
-        degrees={['PhD']}
-        positions={[
-          { institution: 'FPF', department: "Men's Team", role: 'Striker' },
-        ]}
-        country="Portugal"
-        city="Lisbon"
-        region="Europe"
-        onSave={onSave}
-      />,
-      {
-        wrapper: MemoryRouter,
-      },
-    );
+    const firstName = 'Gonçalo';
+    const lastName = 'Ramos';
+    const degrees: KeyInformationModalProps['degrees'] = ['PhD'];
+    const positions = [
+      { institution: 'FPF', department: "Men's Team", role: 'Striker' },
+    ];
+    const country = 'Portugal';
+    const city = 'Lisbon';
+    const region = 'Europe';
+    renderKeyInformation({
+      firstName,
+      lastName,
+      degrees,
+      positions,
+      country,
+      city,
+      region,
+      onSave,
+    });
     const saveButton = screen.getByRole('button', { name: 'Save' });
     userEvent.click(saveButton);
     expect(onSave).toHaveBeenCalledWith({
-      firstName: 'Gonçalo',
-      lastName: 'Ramos',
-      degrees: ['PhD'],
-      positions: [
-        { institution: 'FPF', department: "Men's Team", role: 'Striker' },
-      ],
-      country: 'Portugal',
-      city: 'Lisbon',
-      region: 'Europe',
+      firstName,
+      lastName,
+      degrees,
+      positions,
+      country,
+      city,
+      region,
     });
     await waitFor(() => expect(saveButton).toBeEnabled());
   });
   it('calls onSave with the updated fields', async () => {
+    const firstName = 'Gonçalo';
+    const lastName = 'Ramos';
+    const degrees: KeyInformationModalProps['degrees'] = ['PhD'];
+    const positions = [
+      { institution: 'FPF', department: "Men's Team", role: 'Striker' },
+    ];
+    const country = 'Portugal';
+    const city = 'Lisbon';
+    const region = 'Europe';
     const onSave = jest.fn();
-    render(
-      <KeyInformationModal
-        {...defaultProps}
-        firstName=""
-        lastName=""
-        degrees={[]}
-        positions={[]}
-        country=""
-        city=""
-        region="Asia"
-        locationSuggestions={['Portugal']}
-        loadInstitutionOptions={() => Promise.resolve(['FPF'])}
-        onSave={onSave}
-      />,
-      {
-        wrapper: MemoryRouter,
-      },
-    );
+    renderKeyInformation({
+      firstName: '',
+      lastName: '',
+      degrees: [],
+      positions: [],
+      country: '',
+      city: '',
+      region: 'Asia',
+      onSave,
+      locationSuggestions: ['Portugal'],
+      loadInstitutionOptions: () => Promise.resolve(['FPF']),
+    });
     userEvent.type(
       screen.getByRole('textbox', { name: 'First Name (required)' }),
-      'Gonçalo',
+      firstName,
     );
     userEvent.type(
       screen.getByRole('textbox', { name: 'Last Name (required)' }),
-      'Ramos',
+      lastName,
     );
     userEvent.click(screen.getByRole('textbox', { name: 'Degree (optional)' }));
-    userEvent.click(screen.getByText('PhD'));
+    userEvent.click(screen.getByText(degrees[0]));
     userEvent.click(
       screen.getByRole('textbox', {
         name: 'Region (required) Select the region you are based in.',
       }),
     );
-    userEvent.click(screen.getByText('Europe'));
+    userEvent.click(screen.getByText(region));
     userEvent.click(
       screen.getByRole('textbox', {
         name: 'Location (required) Select the location you are based in.',
       }),
     );
-    userEvent.click(screen.getByText('Portugal'));
+    userEvent.click(screen.getByText(country));
     userEvent.type(
       screen.getByRole('textbox', { name: 'City (optional)' }),
-      'Lisbon',
+      city,
     );
     userEvent.click(
       screen.getByRole('textbox', { name: 'Primary Institution (required)' }),
     );
-    const institution = await screen.findByText('FPF');
+    const institution = await screen.findByText(positions[0].institution);
     userEvent.click(institution);
     userEvent.type(
       screen.getByRole('textbox', { name: 'Primary Department (required)' }),
-      "Men's Team",
+      positions[0].department,
     );
     userEvent.type(
       screen.getByRole('textbox', { name: 'Primary Role (required)' }),
-      'Striker',
+      positions[0].role,
     );
     const saveButton = screen.getByRole('button', { name: 'Save' });
     userEvent.click(saveButton);
     expect(onSave).toHaveBeenCalledWith({
-      firstName: 'Gonçalo',
-      lastName: 'Ramos',
-      degrees: ['PhD'],
-      positions: [
-        { institution: 'FPF', department: "Men's Team", role: 'Striker' },
-      ],
-      country: 'Portugal',
-      city: 'Lisbon',
-      region: 'Europe',
+      firstName,
+      lastName,
+      degrees,
+      positions,
+      country,
+      city,
+      region,
     });
     await waitFor(() => expect(saveButton).toBeEnabled());
   }, 10000);
   test('can click add an extra position', () => {
-    render(
-      <KeyInformationModal
-        {...defaultProps}
-        firstName="Gonçalo"
-        lastName="Ramos"
-        degrees={['PhD']}
-        positions={[
-          { institution: 'FPF', department: "Men's Team", role: 'Striker' },
-        ]}
-        country="Portugal"
-        city="Lisbon"
-        region="Europe"
-        onSave={jest.fn()}
-      />,
-      {
-        wrapper: MemoryRouter,
-      },
-    );
+    renderKeyInformation();
     const addButton = screen.getByRole('button', {
       name: /add another institution/i,
     });
@@ -183,8 +170,9 @@ describe('KeyInformatiomModal', () => {
       screen.getByRole('textbox', { name: 'Other Role (required)' }),
     ).toBeVisible();
   });
+  test.todo('there can be only 3 positions');
   test.todo('can save an extra position');
   test.todo('can delete an extra position');
-  test.todo('there can be only 3 positions');
   test.todo('there is a minimum of one');
+  test.todo('is there an add icon');
 });

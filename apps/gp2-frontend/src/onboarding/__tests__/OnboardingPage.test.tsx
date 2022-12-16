7ae14dc42b7dab8d13423f17bea0fbcd35a32921
Carlos Barbosa <fbcarlos101@gmail.com>
2022-12-16 09:56:59 +0000

    feat: added coverage to Onboarding Page

---
+++ apps/gp2-frontend/src/onboarding/__tests__/OnboardingPage.test.tsx
@@ -0,0 +1,72 @@
+import { mockConsoleError } from '@asap-hub/dom-test-utils';
+import { gp2 as gp2Routing } from '@asap-hub/routing';
+import { gp2 as gp2Fixtures } from '@asap-hub/fixtures';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import { Suspense } from 'react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import { Auth0Provider, WhenReady } from '../../auth/test-utils';
+import { getUser } from '../../users/api';
+import { refreshUserState } from '../../users/state';
+import OnboardingPage from '../OnboardingPage';
+
+jest.mock('../../users/api');
+
+mockConsoleError();
+
+const renderOnboardingPage = async (id: string) => {
+  render(
+    <RecoilRoot
+      initializeState={({ set }) => {
+        set(refreshUserState(id), Math.random());
+      }}
+    >
+      <Suspense fallback="loading">
+        <Auth0Provider user={{ onboarded: false, id }}>
+          <WhenReady>
+            <MemoryRouter
+              initialEntries={[gp2Routing.onboarding({}).coreDetails({}).$]}
+            >
+              <Route path={gp2Routing.onboarding({}).coreDetails.template}>
+                <OnboardingPage />
+              </Route>
+            </MemoryRouter>
+          </WhenReady>
+        </Auth0Provider>
+      </Suspense>
+    </RecoilRoot>,
+  );
+
+  await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
+};
+
+describe('OnboardingPage', () => {
+  beforeEach(jest.resetAllMocks);
+  const mockGetUser = getUser as jest.MockedFunction<typeof getUser>;
+
+  it('renders header with title', async () => {
+    const user = gp2Fixtures.createUserResponse();
+    mockGetUser.mockResolvedValueOnce(user);
+    await renderOnboardingPage(user.id);
+    expect(
+      screen.getByRole('heading', { name: /registration/i }),
+    ).toBeVisible();
+  });
+
+  it('renders the progression levels', async () => {
+    const user = gp2Fixtures.createUserResponse();
+    mockGetUser.mockResolvedValueOnce(user);
+    await renderOnboardingPage(user.id);
+    expect(screen.getByRole('link', { name: /core details/i })).toBeVisible();
+    expect(screen.getByRole('link', { name: /background/i })).toBeVisible();
+    expect(screen.getByRole('link', { name: /gp2 groups/i })).toBeVisible();
+    expect(
+      screen.getByRole('link', { name: /additional details/i }),
+    ).toBeVisible();
+    expect(screen.getByRole('link', { name: /preview/i })).toBeVisible();
+  });
+});

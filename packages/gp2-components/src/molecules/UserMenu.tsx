991c070ee1b99cd0ad997d474a7363e0b84bca0e
Mark Sims <mark@bluepie.co.uk>
2022-12-20 16:25:24 +0000

    updates to have working groups and projects displayed in user menu

--- packages/gp2-components/src/molecules/UserMenu.tsx
+++ packages/gp2-components/src/molecules/UserMenu.tsx
@@ -1,9 +1,17 @@
+import { gp2 as gp2Model } from '@asap-hub/model';
 import { css } from '@emotion/react';
-import { logout } from '@asap-hub/routing';
+import { logout, gp2 as gp2Routing } from '@asap-hub/routing';
 
-import { NavigationLink, pixels, logoutIcon } from '@asap-hub/react-components';
+import {
+  NavigationLink,
+  pixels,
+  logoutIcon,
+  Divider,
+} from '@asap-hub/react-components';
 
 const { vminLinearCalc, mobileScreen, largeDesktopScreen, rem } = pixels;
+const { projects: projectsRoute, workingGroups: workingGroupRoute } =
+  gp2Routing;
 
 const containerStyles = css({
   minWidth: '312px',
@@ -26,8 +34,40 @@ const listStyles = css({
   padding: 0,
 });
 
-const UserMenu: React.FC = () => (
+type UserMenuProps = Pick<gp2Model.UserResponse, 'projects' | 'workingGroups'>;
+const UserMenu: React.FC<UserMenuProps> = ({ projects, workingGroups }) => (
   <nav css={containerStyles}>
+    {(workingGroups.length > 0 || projects.length > 0) && (
+      <>
+        <ul css={listStyles}>
+          {projects
+            .filter(({ status }) => status === 'Active')
+            .map(({ id, title }) => (
+              <li key={`user-menu-project-${id}`}>
+                <NavigationLink
+                  href={projectsRoute({}).project({ projectId: id }).$}
+                  icon={logoutIcon}
+                >
+                  My project: {title}
+                </NavigationLink>
+              </li>
+            ))}
+          {workingGroups.map(({ id, title }) => (
+            <li key={`user-menu-working-group-${id}`}>
+              <NavigationLink
+                href={
+                  workingGroupRoute({}).workingGroup({ workingGroupId: id }).$
+                }
+                icon={logoutIcon}
+              >
+                My working group: {title}
+              </NavigationLink>
+            </li>
+          ))}
+        </ul>
+        <Divider />
+      </>
+    )}
     <ul css={listStyles}>
       <li>
         <NavigationLink href={logout({}).$} icon={logoutIcon}>
--- packages/gp2-components/src/molecules/__tests__/UserMenu.test.tsx
+++ packages/gp2-components/src/molecules/__tests__/UserMenu.test.tsx
@@ -1,11 +1,137 @@
 import { render, screen } from '@testing-library/react';
+import { ComponentProps } from 'react';
+import { gp2 as gp2Routing, logout } from '@asap-hub/routing';
 import UserMenu from '../UserMenu';
 
+const { projects: projectsRoute, workingGroups: workingGroupsRoute } =
+  gp2Routing;
+
 describe('UserMenu', () => {
+  const props: ComponentProps<typeof UserMenu> = {
+    projects: [],
+    workingGroups: [],
+  };
+
   it('renders the navigation items', () => {
-    render(<UserMenu />);
+    render(<UserMenu {...props} />);
+    const link = screen.getByRole('link', { name: /log out/i });
+    expect(link).toBeVisible();
+    expect(link).toHaveAttribute('href', logout({}).$);
+  });
+
+  it('renders the projects', () => {
+    const projects: typeof props['projects'] = [
+      {
+        id: '11',
+        title: 'the first project title',
+        status: 'Active',
+        members: [],
+      },
+      {
+        id: '23',
+        title: 'the second project title',
+        status: 'Active',
+        members: [],
+      },
+    ];
+    render(<UserMenu {...props} projects={projects} />);
     expect(
-      screen.getAllByRole('listitem').map(({ textContent }) => textContent),
-    ).toEqual([expect.stringMatching(/log.*out/i)]);
+      screen.getByRole('link', { name: /the first project title/i }),
+    ).toBeVisible();
+    expect(
+      screen.getByRole('link', { name: /the second project title/i }),
+    ).toBeVisible();
+  });
+
+  it('links to the project details', () => {
+    const projects: typeof props['projects'] = [
+      {
+        id: '11',
+        title: 'the first project title',
+        status: 'Active',
+        members: [],
+      },
+    ];
+    render(<UserMenu {...props} projects={projects} />);
+    expect(
+      screen.getByRole('link', { name: /the first project title/i }),
+    ).toHaveAttribute(
+      'href',
+      projectsRoute({}).project({ projectId: projects[0].id }).$,
+    );
+  });
+
+  it('renders only active projects', () => {
+    const projects: typeof props['projects'] = [
+      {
+        id: '11',
+        title: 'the first project title',
+        status: 'Active',
+        members: [],
+      },
+      {
+        id: '23',
+        title: 'the second project title',
+        status: 'Inactive',
+        members: [],
+      },
+      {
+        id: '27',
+        title: 'the third project title',
+        status: 'Completed',
+        members: [],
+      },
+    ];
+    render(<UserMenu {...props} projects={projects} />);
+    expect(
+      screen.getByRole('link', { name: /the first project title/i }),
+    ).toBeVisible();
+    expect(
+      screen.queryByRole('link', { name: /the second project title/i }),
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole('link', { name: /the third project title/i }),
+    ).not.toBeInTheDocument();
+  });
+
+  it('renders the working groups', () => {
+    const workingGroups: typeof props['workingGroups'] = [
+      {
+        id: '11',
+        title: 'the first wg title',
+        members: [],
+      },
+      {
+        id: '23',
+        title: 'the second wg title',
+        members: [],
+      },
+    ];
+    render(<UserMenu {...props} workingGroups={workingGroups} />);
+    expect(
+      screen.getByRole('link', { name: /the first wg title/i }),
+    ).toBeVisible();
+    expect(
+      screen.getByRole('link', { name: /the second wg title/i }),
+    ).toBeVisible();
+  });
+
+  it('links to the working group detail page', () => {
+    const workingGroups: typeof props['workingGroups'] = [
+      {
+        id: '11',
+        title: 'the first wg title',
+        members: [],
+      },
+    ];
+    render(<UserMenu {...props} workingGroups={workingGroups} />);
+    expect(
+      screen.getByRole('link', { name: /the first wg title/i }),
+    ).toHaveAttribute(
+      'href',
+      workingGroupsRoute({}).workingGroup({
+        workingGroupId: workingGroups[0].id,
+      }).$,
+    );
   });
 });

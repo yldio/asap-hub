4a87a39bde414cd00f99f473197a2ce9710db207
Carlos Barbosa <fbcarlos101@gmail.com>
2022-12-23 12:24:04 +0000

    feat: test for the onboarded tab link

---
+++ packages/gp2-components/src/molecules/__tests__/OnboardedTabLink.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import { StaticRouter } from 'react-router-dom';
+import OnboardedTabLink from '../OnboardedTabLink';
+
+describe('OnboardedTabLink', () => {
+  it('should display the link when enabled', () => {
+    render(
+      <OnboardedTabLink disabled={false} href={'/test'}>
+        Link Test
+      </OnboardedTabLink>,
+      { wrapper: StaticRouter },
+    );
+    const link = screen.getByRole('link', { name: /link test/i });
+    expect(link).toBeVisible();
+    expect(link).toHaveAttribute('href', '/test');
+  });
+
+  it('should not display the link when disabled', () => {
+    render(
+      <OnboardedTabLink disabled={true} href={'/test'}>
+        Link Test
+      </OnboardedTabLink>,
+      { wrapper: StaticRouter },
+    );
+    expect(
+      screen.queryByRole('link', { name: /link test/i }),
+    ).not.toBeInTheDocument();
+    expect(screen.getByText('Link Test')).toBeVisible();
+  });
+});

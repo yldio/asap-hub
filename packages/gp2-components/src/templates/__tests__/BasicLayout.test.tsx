0df6f84bf07619a442a360485d8466b677eaefab
jhonny-m <jhonnyporto@hotmail.com>
2022-12-26 10:30:30 +0000

    removing test for refactored feature

--- packages/gp2-components/src/templates/__tests__/BasicLayout.test.tsx
+++ packages/gp2-components/src/templates/__tests__/BasicLayout.test.tsx
@@ -19,8 +19,3 @@ it('renders the content', async () => {
   render(<BasicLayout>Content</BasicLayout>);
   expect(screen.getByText('Content')).toBeVisible();
 });
-
-it('has padding of zero if noPadding is provided', async () => {
-  render(<BasicLayout noPadding>Content</BasicLayout>);
-  expect(screen.getByRole('main')).toHaveStyle('padding : 0');
-});

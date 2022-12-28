9ac3caea910ccbfbacf98f80b0e67b6826e01796
Ratiu Octavian <oratiu@gmail.com>
2022-12-28 12:32:37 +0200

    Fixed merge conflict

--- packages/react-components/src/organisms/ResearchOutputForm.tsx
+++ packages/react-components/src/organisms/ResearchOutputForm.tsx
@@ -16,13 +16,20 @@ import { ComponentProps, useState } from 'react';
 import { Button } from '../atoms';
 import { mobileScreen, perRem } from '../pixels';
 import { usePushFromHere } from '../routing';
-import { equals, noop } from '../utils';
+import { noop } from '../utils';
 import {
   Form,
   ResearchOutputExtraInformationCard,
   ResearchOutputFormSharingCard,
 } from './index';
 import ResearchOutputContributorsCard from './ResearchOutputContributorsCard';
+import {
+  createIdentifierField,
+  getDecision,
+  getIdentifierType,
+  getPublishDate,
+  isDirty,
+} from '../utils/researchOutputForm';
 
 const contentStyles = css({
   display: 'grid',
@@ -79,157 +86,6 @@ type ResearchOutputFormProps = Pick<
     researchOutputData?: ResearchOutputResponse;
     isEditMode: boolean;
   };
-
-const identifierTypeToFieldName: Record<
-  ResearchOutputIdentifierType,
-  'doi' | 'accession' | 'labCatalogNumber' | 'rrid' | undefined
-> = {
-  [ResearchOutputIdentifierType.Empty]: undefined,
-  [ResearchOutputIdentifierType.None]: undefined,
-  [ResearchOutputIdentifierType.DOI]: 'doi',
-  [ResearchOutputIdentifierType.AccessionNumber]: 'accession',
-  [ResearchOutputIdentifierType.RRID]: 'rrid',
-};
-
-export function createIdentifierField(
-  identifierType: ResearchOutputIdentifierType,
-  rawIdentifier: string,
-):
-  | { rrid: string }
-  | { doi: string }
-  | { accession: string }
-  | Record<never, never> {
-  const fieldName = identifierTypeToFieldName[identifierType];
-  if (fieldName) {
-    return { [fieldName]: rawIdentifier };
-  }
-
-  return {};
-}
-export type ResearchOutputState = {
-  title: string;
-  description: string;
-  link?: string;
-  type: string;
-  tags: readonly string[];
-  methods: string[];
-  organisms: string[];
-  environments: string[];
-  subtype?: string;
-  labCatalogNumber?: string;
-  teams: NonNullable<
-    ComponentProps<typeof ResearchOutputContributorsCard>['teams']
-  >;
-  labs: ComponentProps<typeof ResearchOutputContributorsCard>['labs'];
-  authors: ComponentProps<typeof ResearchOutputContributorsCard>['authors'];
-  identifierType: ResearchOutputIdentifierType;
-  identifier?: string;
-};
-
-export function isDirty(
-  {
-    title,
-    description,
-    link,
-    tags,
-    type,
-    methods,
-    organisms,
-    environments,
-    teams,
-    labs,
-    authors,
-    subtype,
-    labCatalogNumber,
-    identifierType,
-    identifier,
-  }: ResearchOutputState,
-  researchOutputData?: ResearchOutputResponse,
-): boolean {
-  if (researchOutputData) {
-    return (
-      title !== researchOutputData.title ||
-      description !== researchOutputData.description ||
-      link !== researchOutputData.link ||
-      type !== researchOutputData.type ||
-      !equals(methods, researchOutputData.methods) ||
-      !equals(organisms, researchOutputData.organisms) ||
-      !equals(environments, researchOutputData.environments) ||
-      !equals(
-        teams.map((team) => team.value),
-        researchOutputData.teams.map((team) => team.id),
-      ) ||
-      (labs &&
-        !equals(
-          labs.map((lab) => lab.value),
-          researchOutputData.labs.map((lab) => lab.id),
-        )) ||
-      (authors &&
-        !equals(
-          authors.map((author) => author.value),
-          researchOutputData.authors.map((author) => author?.id),
-        )) ||
-      subtype !== researchOutputData.subtype ||
-      identifierType !== getIdentifierType(true, researchOutputData) ||
-      isIdentifierModified(researchOutputData, identifier)
-    );
-  }
-  return (
-    tags?.length !== 0 ||
-    title !== '' ||
-    description !== '' ||
-    link !== '' ||
-    type !== '' ||
-    labs?.length !== 0 ||
-    authors?.length !== 0 ||
-    methods.length !== 0 ||
-    organisms.length !== 0 ||
-    environments.length !== 0 ||
-    labCatalogNumber !== '' ||
-    subtype !== undefined ||
-    teams?.length !== 1 ||
-    identifier !== '' ||
-    identifierType !== ResearchOutputIdentifierType.Empty
-  );
-}
-
-export function getIdentifierType(
-  isEditMode: boolean,
-  researchOutputData?: ResearchOutputResponse,
-): ResearchOutputIdentifierType {
-  if (researchOutputData?.doi) return ResearchOutputIdentifierType.DOI;
-
-  if (researchOutputData?.accession)
-    return ResearchOutputIdentifierType.AccessionNumber;
-
-  if (researchOutputData?.rrid) return ResearchOutputIdentifierType.RRID;
-
-  return isEditMode
-    ? ResearchOutputIdentifierType.None
-    : ResearchOutputIdentifierType.Empty;
-}
-
-export function isIdentifierModified(
-  researchOutputData: ResearchOutputResponse,
-  identifier?: string,
-): boolean {
-  return (
-    researchOutputData.doi !== identifier &&
-    researchOutputData.accession !== identifier &&
-    researchOutputData.rrid !== identifier
-  );
-}
-
-export function getPublishDate(publishDate?: string): Date | undefined {
-  if (publishDate) {
-    return new Date(publishDate);
-  }
-  return undefined;
-}
-
-export function getDecision(decision?: boolean): DecisionOption {
-  return decision === undefined ? 'Not Sure' : decision ? 'Yes' : 'No';
-}
 
 const ResearchOutputForm: React.FC<ResearchOutputFormProps> = ({
   onSave,
@@ -334,7 +190,7 @@ const ResearchOutputForm: React.FC<ResearchOutputFormProps> = ({
     researchOutputData?.environments || [],
   );
   const [subtype, setSubtype] = useState<string | undefined>(
-    researchOutputData?.subtype || '',
+    researchOutputData?.subtype,
   );
 
   const filteredResearchTags = researchTags.filter((d) =>

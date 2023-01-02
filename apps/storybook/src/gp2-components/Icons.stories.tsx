619d0c22a0d95ede8e8478de16d674104c976f5b
Carlos Barbosa <fbcarlos101@gmail.com>
2023-01-02 10:42:01 +0000

    feat: added icons to storybook

---
+++ apps/storybook/src/gp2-components/Icons.stories.tsx
@@ -0,0 +1,27 @@
+import * as components from '@asap-hub/gp2-components';
+
+export default { title: 'GP2 / Icons' };
+
+export const addIcon = () => components.addIcon;
+export const binIcon = () => components.binIcon;
+export const copyIcon = () => components.copyIcon;
+export const dashboardIcon = () => components.dashboardIcon;
+export const dateIcon = () => components.dateIcon;
+export const editIcon = () => components.editIcon;
+export const exportIcon = () => components.exportIcon;
+export const gp2LogoFull = () => components.gp2LogoFull;
+export const gp2LogoSmall = () => components.gp2LogoSmall;
+export const leftArrowIcon = () => components.leftArrowIcon;
+export const locationIcon = () => components.locationIcon;
+export const projectIcon = () => components.projectIcon;
+export const regionAfricaIcon = () => components.regionAfricaIcon;
+export const regionAsiaIcon = () => components.regionAsiaIcon;
+export const regionAustralasiaIcon = () => components.regionAustralasiaIcon;
+export const regionEuropeIcon = () => components.regionEuropeIcon;
+export const regionLatinAmericaIcon = () => components.regionLatinAmericaIcon;
+export const regionNorthAmericaIcon = () => components.regionNorthAmericaIcon;
+export const regionSouthAmericaIcon = () => components.regionSouthAmericaIcon;
+export const rightArrowIcon = () => components.rightArrowIcon;
+export const roleIcon = () => components.roleIcon;
+export const usersIcon = () => components.usersIcon;
+export const workingGroupIcon = () => components.workingGroupIcon;

ad4c0c33148551a588ad13cbe528c7e41982c7cc
Quentin Leonetti <q.leonetti@gmail.com>
2023-01-04 14:32:17 +0100

    CRN-1178 fixed tests

--- packages/react-components/src/organisms/__tests__/WorkingGroupsTabbedCard.test.tsx
+++ packages/react-components/src/organisms/__tests__/WorkingGroupsTabbedCard.test.tsx
@@ -1,15 +1,42 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 
-import WorkingGroupsTabbedCard from '../WorkingGroupsTabbedCard';
+import WorkingGroupsTabbedCard, {
+  GroupMemberShip,
+} from '../WorkingGroupsTabbedCard';
+
+const props = {
+  userName: 'Foo B',
+  groups: [],
+  isUserAlumni: false,
+};
 
 it('displays the title', () => {
-  const { getByText } = render(
-    <WorkingGroupsTabbedCard
-      userName="Brad B"
-      groups={[]}
-      isUserAlumni={false}
-    />,
-  );
+  render(<WorkingGroupsTabbedCard {...props} userName="Brad B" />);
 
-  expect(getByText("Brad B's Working Groups")).toBeInTheDocument();
+  expect(screen.getByText("Brad B's Working Groups")).toBeInTheDocument();
+});
+
+it('renders the no memberships message', () => {
+  const { rerender } = render(
+    <WorkingGroupsTabbedCard {...props} isUserAlumni={true} />,
+  );
+  expect(screen.getByText('There are no past memberships.')).toBeVisible();
+  rerender(<WorkingGroupsTabbedCard {...props} isUserAlumni={false} />);
+  expect(screen.getByText('There are no active memberships.')).toBeVisible();
+});
+
+it('splits the current and complete groups', () => {
+  const groups: GroupMemberShip[] = [
+    { id: 'group-1', name: 'Active WG', role: 'Leader', active: true },
+    { id: 'group-2', name: 'Complete WG', role: 'Ex-Leader', active: false },
+  ];
+  render(<WorkingGroupsTabbedCard {...props} groups={groups} />);
+
+  expect(screen.getByText('Active WG')).toBeInTheDocument();
+  expect(screen.queryByText('Complete WG')).not.toBeInTheDocument();
+
+  fireEvent.click(screen.getByText('Past Memberships (1)'));
+
+  expect(screen.getByText('Complete WG')).toBeInTheDocument();
+  expect(screen.queryByText('Active WG')).not.toBeInTheDocument();
 });

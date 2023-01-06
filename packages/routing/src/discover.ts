f6f59b01611b25f6a3c5cab2d1323548dc25fb73
gabiayako <gabiayako@gmail.com>
2023-01-06 13:51:15 -0300

    CRN-1244 Remove working groups from the Discover ASAP Page

--- packages/routing/src/discover.ts
+++ packages/routing/src/discover.ts
@@ -3,11 +3,6 @@ import { route, stringParser } from 'typesafe-routes';
 const tutorial = route('/:tutorialId', { tutorialId: stringParser }, {});
 const tutorials = route('/tutorials', {}, { tutorial });
 const guides = route('/guides', {}, {});
-const workingGroups = route('/working-groups', {}, {});
 const about = route('/about', {}, {});
 
-export default route(
-  '/discover',
-  {},
-  { tutorials, guides, workingGroups, about },
-);
+export default route('/discover', {}, { tutorials, guides, about });

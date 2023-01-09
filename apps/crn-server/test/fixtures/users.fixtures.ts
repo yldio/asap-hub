ebd6ad521897bb0e9a2a9d59b527136b104d6e77
Quentin Leonetti <q.leonetti@gmail.com>
2023-01-09 15:16:35 +0100

    CRN-1178 put back wg in fixtures

--- apps/crn-server/test/fixtures/users.fixtures.ts
+++ apps/crn-server/test/fixtures/users.fixtures.ts
@@ -281,7 +281,7 @@ export const getUserResponse = (): UserResponse => ({
   country: 'United Kingdom',
   city: 'London',
   lastModifiedDate: '2020-09-23T20:45:22.000Z',
-  // workingGroups: [],
+  workingGroups: [],
   orcidWorks: [
     {
       doi: 'test-doi',
@@ -340,7 +340,7 @@ export const fetchExpectation: ListUserResponse = {
       firstName: 'Arnold',
       lastName: 'Schwatzneger',
       lastModifiedDate: '2020-09-23T20:45:22.000Z',
-      // workingGroups: [],
+      workingGroups: [],
       orcidWorks: [
         {
           doi: 'test-doi',

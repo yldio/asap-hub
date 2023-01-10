93d1a6af25c3387f2b71539ff3f857701f0e0042
Gabriela Ueno <gabiayako@gmail.com>
2023-01-10 12:55:10 -0300

    Update apps/crn-server/test/integration/reminders.integration-test.ts
    Co-authored-by: Piotr S <piotr.szpak@yld.io>

--- apps/crn-server/test/integration/reminders.integration-test.ts
+++ apps/crn-server/test/integration/reminders.integration-test.ts
@@ -754,7 +754,7 @@ describe('Reminders', () => {
       eventIdsForDeletion = [];
     });
 
-    test('Should see the reminder when the event has finished and user is a PM from one of the speakers team', async () => {
+    test('Should see the reminder when the event has finished and user is a PM of one of the speaker teams', async () => {
       // setting system time to 10:05AM in UTC
       jest.setSystemTime(new Date('2022-08-10T11:05:00.0Z'));
 

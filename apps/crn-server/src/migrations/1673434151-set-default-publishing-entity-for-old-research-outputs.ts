89a7aa313ee00992ca8aac9c38ee9e7206d67eb0
Ratiu Octavian <oratiu@gmail.com>
2023-01-11 12:53:28 +0200

    Added migration for default value on researchOutput publishing entity

---
+++ apps/crn-server/src/migrations/1673434151-set-default-publishing-entity-for-old-research-outputs.ts
@@ -0,0 +1,21 @@
+/* istanbul ignore file */
+import { Migration } from '@asap-hub/server-common';
+import { RestResearchOutput } from '@asap-hub/squidex';
+import { applyToAllItemsInCollection } from '../utils/migrations';
+
+export default class SetResearchOutputPublishingEntityDefault extends Migration {
+  up = async (): Promise<void> => {
+    await applyToAllItemsInCollection<RestResearchOutput>(
+      'research-outputs',
+      async (researchOutput, squidexClient) => {
+        await squidexClient.patch(researchOutput.id, {
+          publishingEntity: {
+            iv: 'Team',
+          },
+        });
+      },
+    );
+  };
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  down = async (): Promise<void> => {};
+}

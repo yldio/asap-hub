a9028b15430da968b01a0c3847c4d9b5a28f6306
Piotr S <piotr.szpak@yld.io>
2023-01-13 10:05:24 +0000

    fix: Squidex sync error reporting and logs (#2435)

--- .github/scripts/prepare-squidex/import-data.sh
+++ .github/scripts/prepare-squidex/import-data.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 # Set this envvar to true in GitLab if you want to print the sq sync logs on success for debugging
 PRINT_SYNC_LOGS_ON_SUCCESS="${PRINT_SYNC_LOGS_ON_SUCCESS:-false}"
@@ -13,7 +13,7 @@ echo 'Importing data...'
 if [ "$RECREATE_APP_ON_EVERY_PR" = true ] || [ "$FIRST_RUN" = true ]; then
     # create-app.py configures sq to new app
     export SQ_SYNC_OUTPUT="$(sq sync in backup)"
-    if echo "$SQ_SYNC_OUTPUT" | grep -q -i -E 'warn|error|fail|exception'; then
+    if [[ "$SQ_SYNC_OUTPUT" =~ (warn|error|fail|exception) ]]; then
         echo "Data import failure - sq sync in backup failed:"
         echo $SQ_SYNC_OUTPUT
         unset SQ_SYNC_OUTPUT

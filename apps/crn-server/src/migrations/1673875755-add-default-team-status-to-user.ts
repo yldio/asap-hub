9176236736d1cbad9167f2fde3472f0191318124
ricardo <ricardo.carvalho@yld.io>
2023-01-16 10:31:11 -0300

    [CRN-153] add migration to set default team status to users

---
+++ apps/crn-server/src/migrations/1673875755-add-default-team-status-to-user.ts
@@ -0,0 +1,25 @@
+/* istanbul ignore file */
+import { Migration } from '@asap-hub/server-common';
+import { RestUser } from '@asap-hub/squidex';
+import { applyToAllItemsInCollection } from '../utils/migrations';
+
+export default class AddDefaultTeamStatusToUser extends Migration {
+  up = async (): Promise<void> => {
+    await applyToAllItemsInCollection<RestUser>(
+      'users',
+      async (user, squidexClient) => {
+        if (user.data.teams?.iv) {
+          await squidexClient.patch(user.id, {
+            teams: {
+              iv: user.data.teams.iv.map((v) => ({
+                ...v,
+                status: 'Active',
+              })),
+            },
+          });
+        }
+      },
+    );
+  };
+  down = async (): Promise<void> => {};
+}

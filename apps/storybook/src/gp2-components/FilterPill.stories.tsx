b1ab9d141b2c657f0d57a65690b82e3d421c57aa
Carlos Barbosa <fbcarlos101@gmail.com>
2023-01-23 14:34:05 +0000

    fix: fixed props' type from Filter Pill and Filter Pills stories

--- apps/storybook/src/gp2-components/FilterPill.stories.tsx
+++ apps/storybook/src/gp2-components/FilterPill.stories.tsx
@@ -1,17 +1,17 @@
 import { FilterPill } from '@asap-hub/gp2-components';
-import { ValueProps } from '@asap-hub/gp2-components/src/molecules/FilterPill';
+import { ComponentProps } from 'react';
 
 export default {
   title: 'GP2 / Molecules / Filter Pill',
   component: FilterPill,
 };
 
-const props = {
+const props: ComponentProps<typeof FilterPill> = {
   value: {
     id: 'region-Africa',
     label: 'Africa',
   },
-  onRemove: (value: ValueProps) => alert(`${value.label} erased!`),
+  onRemove: (value) => alert(`${value.label} erased!`),
 };
 
 export const Normal = () => <FilterPill {...props}></FilterPill>;
--- apps/storybook/src/gp2-components/FilterPills.stories.tsx
+++ apps/storybook/src/gp2-components/FilterPills.stories.tsx
@@ -1,12 +1,12 @@
 import { FilterPills } from '@asap-hub/gp2-components';
-import { ValueProps } from '@asap-hub/gp2-components/src/molecules/FilterPill';
+import { ComponentProps } from 'react';
 
 export default {
   title: 'GP2 / Organisms / Filter Pills',
   component: FilterPills,
 };
 
-const props = {
+const props: ComponentProps<typeof FilterPills> = {
   values: [
     {
       id: 'region-Africa',
@@ -17,7 +17,7 @@ const props = {
       label: 'Asia',
     },
   ],
-  onRemove: (value: ValueProps) => alert(`${value.label} erased!`),
+  onRemove: (value) => alert(`${value.label} erased!`),
 };
 
 export const Normal = () => <FilterPills {...props}></FilterPills>;

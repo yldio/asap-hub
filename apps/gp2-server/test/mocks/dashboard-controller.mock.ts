3d2ed2a6cb4268e65da240769c432fb00bc4ef69
Mark Sims <mark@bluepie.co.uk>
2023-01-27 09:42:31 +0000

    starts removing dashboard from server

--- apps/gp2-server/test/mocks/dashboard-controller.mock.ts
+++
@@ -1,5 +0,0 @@
-import { DashboardController } from '../../src/controllers/dashboard.controller';
-
-export const dashboardControllerMock: jest.Mocked<DashboardController> = {
-  fetch: jest.fn(),
-};

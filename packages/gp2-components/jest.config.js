14206835923d9767c17606cbbd48a5f858eb2dc1
Quentin Leonetti <q.leonetti@gmail.com>
2023-02-02 14:21:41 +0100

    CRN-1273 remove playwright from imports

---
+++ packages/gp2-components/jest.config.js
@@ -0,0 +1,22 @@
+const {
+  setupFilesAfterEnv,
+  ...baseConfig
+} = require('../../jest/jest-base.config.js');
+
+module.exports = {
+  ...baseConfig,
+
+  rootDir: __dirname,
+  testEnvironment: 'jsdom',
+
+  transformIgnorePatterns: [
+    ...baseConfig.transformIgnorePatterns,
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  setupFilesAfterEnv: [
+    ...(setupFilesAfterEnv || []),
+    require.resolve('../../jest/dom-extensions-setup-after-env.js'),
+  ],
+  displayName: 'test-gp2-components',
+};

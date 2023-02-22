6e2c297742aca2376063f477ddaec9187c2246e6
jhonny-m <jhonnyporto@hotmail.com>
2023-02-22 17:07:59 +0000

    storybook updates

--- apps/storybook/src/CalendarList.stories.tsx
+++ apps/storybook/src/CalendarList.stories.tsx
@@ -5,7 +5,6 @@ import { googleLegacyCalendarColor } from './colors';
 
 export default {
   title: 'Organisms / Calendar List',
-  component: CalendarList,
 };
 
 export const Normal = () => (
@@ -20,22 +19,16 @@ export const Normal = () => (
         color: googleLegacyCalendarColor('Calendar 1 Color'),
         id: '123',
         name: text('Calendar 1 Name', 'ASAP Calendar'),
-        groups: [],
-        workingGroups: [],
       },
       {
         color: googleLegacyCalendarColor('Calendar 2 Color'),
         id: '234',
         name: text('Calendar 2 Name', 'Sci 1 - GWAS Functional', '2'),
-        groups: [],
-        workingGroups: [],
       },
       {
         color: googleLegacyCalendarColor('Calendar 3 Color'),
         id: '456',
         name: text('Calendar 3 Name', 'Sci 2 - Aging and Progression', '3'),
-        groups: [],
-        workingGroups: [],
       },
     ]}
   />

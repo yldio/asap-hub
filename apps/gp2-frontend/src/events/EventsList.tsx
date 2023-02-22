adab41755734a993c28cdae87835732acd944ac4
jhonny-m <jhonnyporto@hotmail.com>
2023-02-22 16:29:53 +0000

    adding event mapper for gp2

--- apps/gp2-frontend/src/events/EventsList.tsx
+++ apps/gp2-frontend/src/events/EventsList.tsx
@@ -1,4 +1,5 @@
 import { getEventListOptions } from '@asap-hub/frontend-utils';
+import { gp2 } from '@asap-hub/model';
 import { EventsList } from '@asap-hub/react-components';
 import { usePagination, usePaginationParams } from '../hooks/pagination';
 import { useEvents } from './state';
@@ -7,6 +8,12 @@ type EventListProps = {
   readonly currentTime: Date;
   readonly past?: boolean;
 };
+
+export const eventMapper = ({ speakers, ...event }: gp2.EventResponse) => ({
+  ...event,
+  hasSpeakersToBeAnnounced: speakers.length === 0,
+  eventOwner: <div>GP2 Team</div>,
+});
 
 const EventList: React.FC<EventListProps> = ({ currentTime, past = false }) => {
   const { currentPage, pageSize } = usePaginationParams();
@@ -26,11 +33,7 @@ const EventList: React.FC<EventListProps> = ({ currentTime, past = false }) => {
       numberOfItems={total}
       renderPageHref={renderPageHref}
       numberOfPages={numberOfPages}
-      events={items.map(({ speakers, startDate, ...event }) => ({
-        ...event,
-        hasSpeakersToBeAnnounced: speakers.length === 0,
-        startDate,
-      }))}
+      events={items.map(eventMapper)}
     />
   );
 };

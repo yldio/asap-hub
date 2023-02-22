516b7c750b907264a7a6f0cbd4f178a027418367
ricardo <ricardo.carvalho@yld.io>
2023-02-22 13:01:16 -0300

    [CRN-1181] add UserTeamsTabbedCard.stories.tsx

--- apps/storybook/src/UserProfileRole.stories.tsx
+++ apps/storybook/src/UserProfileRole.stories.tsx
@@ -9,14 +9,12 @@ export default {
   decorators: [UserProfileDecorator],
 };
 
-const props = (): ComponentProps<typeof UserProfileRole> => {
-  return {
-    firstName: text('First Name', 'Daniel'),
-    researchInterests: text('Research Interests', 'My research Interests'),
-    responsibilities: text('Responsibilities', 'My responsibilities'),
-    reachOut: text('Reach Out', 'You need help setting up your profile'),
-    role: select('ASAP Hub Role', ['Staff', 'Grantee', 'Guest'], 'Grantee'),
-  };
-};
+const props = (): ComponentProps<typeof UserProfileRole> => ({
+  firstName: text('First Name', 'Daniel'),
+  researchInterests: text('Research Interests', 'My research Interests'),
+  responsibilities: text('Responsibilities', 'My responsibilities'),
+  reachOut: text('Reach Out', 'You need help setting up your profile'),
+  role: select('ASAP Hub Role', ['Staff', 'Grantee', 'Guest'], 'Grantee'),
+});
 
 export const Normal = () => <UserProfileRole {...props()} />;
---
+++ apps/storybook/src/UserTeamsTabbedCard.stories.tsx
@@ -0,0 +1,26 @@
+import { ComponentProps } from 'react';
+import { teamRole } from '@asap-hub/model';
+import { UserTeamsTabbedCard } from '@asap-hub/react-components';
+import { boolean, number } from '@storybook/addon-knobs';
+
+export default {
+  title: 'Organisms / User Teams Tabbed Card',
+};
+
+export const Normal = () => {
+  const props: ComponentProps<typeof UserTeamsTabbedCard> = {
+    userName: 'Phillip Mars',
+    isUserAlumni: boolean('Is user alumni?', false),
+    teams: Array.from({ length: number('Number of teams', 4) }).map(
+      (_, index) => ({
+        id: `${index + 1}`,
+        displayName: `Team ${index + 1}`,
+        role: teamRole[index] || teamRole[0],
+        teamInactiveSince: index === 0 ? new Date().toISOString() : '',
+        inactiveSinceDate: index === 1 ? new Date().toISOString() : '',
+      }),
+    ),
+  };
+
+  return <UserTeamsTabbedCard {...props} />;
+};

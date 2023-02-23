bf8b54e14208f3d783532db01ad1291c91cddd74
Quentin Leonetti <q.leonetti@gmail.com>
2023-02-23 14:51:09 +0100

    CRN-1284 set-output updated

--- .github/scripts/squidex/create-app.py
+++ .github/scripts/squidex/create-app.py
@@ -48,7 +48,7 @@ def main():
 
     appAvailable = isAppAvailable(appName, headers)
     if appAvailable:
-        print("::set-output name=app-created::false")
+        print >>$GITHUB_OUTPUT, "name=app-created::false"
         print("App", appName, "already exists")
     else:
         createAPP(appName)
@@ -56,6 +56,6 @@ def main():
         client = getAppClient(appName)
         promoteClient(appName, client)
         setApp(appName, client['id'], client['secret'], app)
-        print("::set-output name=app-created::true")
+        print >>$GITHUB_OUTPUT, "name=app-created::true"
 
 main()

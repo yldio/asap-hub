ffed5150a3d57a5fbe9afc2f17e27088024aafbf
dianaguimaraes <dguimaraes@candy.com>
2023-02-27 14:40:13 +0000

    fix DashboardPageBody storybook

--- apps/storybook/src/gp2-components/DashboardPageBody.stories.tsx
+++ apps/storybook/src/gp2-components/DashboardPageBody.stories.tsx
@@ -7,5 +7,15 @@ export default {
 };
 
 export const Normal = () => (
-  <DashboardPageBody news={gp2.createNewsResponse()} />
+  <DashboardPageBody
+    news={gp2.createNewsResponse()}
+    upcomingEvents={gp2
+      .createListEventResponse(3)
+      .items.map(({ speakers, ...event }) => ({
+        ...event,
+        hasSpeakersToBeAnnounced: !speakers.length,
+        eventOwner: <div>GP2 Team</div>,
+      }))}
+    totalOfUpcomingEvents={4}
+  />
 );

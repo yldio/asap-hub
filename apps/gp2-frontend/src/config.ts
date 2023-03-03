1cd7a83366550c39d5ff8b36b44fcb044f88f39e
jhonny-m <jhonnyporto@hotmail.com>
2023-03-03 09:31:55 +0000

    add sentry to gp2 frontend

--- apps/gp2-frontend/src/config.ts
+++ apps/gp2-frontend/src/config.ts
@@ -5,7 +5,7 @@ export const ALGOLIA_APP_ID =
   process.env.REACT_APP_ALGOLIA_APP_ID || 'LVYWOPQ0A9';
 export const ALGOLIA_INDEX =
   process.env.REACT_APP_ALGOLIA_INDEX || 'asap-hub_dev';
-export const SENTRY_DSN = process.env.REACT_APP_SENTRY_DSN;
+export const SENTRY_DSN = process.env.REACT_APP_GP2_SENTRY_DSN;
 export const ENVIRONMENT = process.env.REACT_APP_ENVIRONMENT || 'local';
 export const RELEASE = process.env.REACT_APP_RELEASE;
 export const AUTH0_AUDIENCE = process.env.REACT_APP_GP2_AUTH0_AUDIENCE || '';

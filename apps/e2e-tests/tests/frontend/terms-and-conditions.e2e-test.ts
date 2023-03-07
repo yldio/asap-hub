16decdc0f6773ed2014c7a8a177842f7dd4bc8ec
Leonard Martin <leonard.martin@gmail.com>
2023-03-07 15:32:39 +0000

    CRN-1320 - refactor smoke test for terms of service
    Locating the element by text content and asserting visibility will be potentially more resilient to rendering issues or content irregularities than trying to assert on the text content of the first h1 without compromising the intent of the test.

--- apps/e2e-tests/tests/frontend/terms-and-conditions.e2e-test.ts
+++ apps/e2e-tests/tests/frontend/terms-and-conditions.e2e-test.ts
@@ -6,7 +6,7 @@ test('Should be able to see the terms-and-conditions page', async ({
 }) => {
   await page.goto(`${config.appUrl}/terms-and-conditions`);
 
-  const h1 = page.locator('h1').first();
+  const h1 = page.locator('h1', { hasText: 'Terms of Use' });
 
-  await expect(h1).toHaveText('Terms of Use');
+  await expect(h1).toBeVisible();
 });

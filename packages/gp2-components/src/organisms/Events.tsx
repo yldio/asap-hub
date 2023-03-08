959dcf0306ebed1446b1c680719c8f807abcb7b9
Mark Sims <mark@bluepie.co.uk>
2023-03-08 14:36:01 +0000

    adds subscribe to events to the working groups and projects pages

--- packages/gp2-components/src/organisms/Events.tsx
+++ packages/gp2-components/src/organisms/Events.tsx
@@ -1,52 +1,24 @@
 import { css } from '@emotion/react';
-import {
-  CalendarLink,
-  Card,
-  Headline3,
-  Paragraph,
-  pixels,
-} from '@asap-hub/react-components';
-import { tabletScreen } from '@asap-hub/react-components/src/pixels';
-
-const { perRem } = pixels;
-
-const BUTTON_SPACING = 12 / perRem;
-const buttons = css({
-  display: 'flex',
-  flexFlow: 'wrap',
-  width: `calc(100% + ${BUTTON_SPACING}em)`,
-  listStyle: 'none',
-  margin: `${12 / perRem}em 0 0 0`,
-  padding: 0,
-});
-
-const button = css({
-  display: 'flex',
-  marginRight: `${BUTTON_SPACING}em`,
-  flexGrow: 1,
-
-  [`@media (min-width: ${tabletScreen.min}px)`]: {
-    flexGrow: 0,
-  },
-});
+import { CalendarLink, Paragraph } from '@asap-hub/react-components';
 
 type EventsProps = {
   readonly calendarId: string;
 };
 
+const buttonStyles = css({
+  width: 'fit-content',
+});
+
 const Events: React.FC<EventsProps> = ({ calendarId }) => (
-  <Card>
-    <Headline3>Events</Headline3>
+  <>
     <Paragraph accent="lead">
       Subscribe this project calendar to stay always updated with the latest
       events.
     </Paragraph>
-    <ul css={buttons}>
-      <li css={button}>
-        <CalendarLink id={calendarId}>Subscribe to Calendar</CalendarLink>
-      </li>
-    </ul>
-  </Card>
+    <div css={buttonStyles}>
+      <CalendarLink id={calendarId}>Subscribe to Calendar</CalendarLink>
+    </div>
+  </>
 );
 
 export default Events;
--- packages/gp2-components/src/organisms/__tests__/Events.test.tsx
+++ packages/gp2-components/src/organisms/__tests__/Events.test.tsx
@@ -1,19 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import { ComponentProps } from 'react';
 import userEvent from '@testing-library/user-event';
 import Events from '../Events';
 
-const props: ComponentProps<typeof Events> = {
-  calendarId: '42',
-};
-
-it('renders events header', () => {
-  render(<Events {...props} />);
-  expect(screen.getByRole('heading', { name: /Events/i })).toBeVisible();
-});
-
 it('renders the subscribe button', () => {
-  render(<Events {...props} calendarId="12w3" />);
+  render(<Events calendarId="12w3" />);
 
   const subscribe = screen.getByRole('button', {
     name: /subscribe to calendar/i,

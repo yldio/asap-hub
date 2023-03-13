97ae4e667426b57e5615f6b8e857ae63cf7da88e
Leonard Martin <leonard.martin@gmail.com>
2023-03-13 10:28:31 +0000

    CRN-1320 - extend expect timeout in end to end tests
    Requests for the first page load after spinning up an environment can be fairly slow, so give the tests a few more seconds to succeed.

--- apps/e2e-tests/playwright.config.ts
+++ apps/e2e-tests/playwright.config.ts
@@ -3,5 +3,8 @@ import { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   testDir: './tests/frontend',
   testMatch: '**/*.e2e-test.{js,jsx,ts,tsx}',
+  expect: {
+    timeout: 10000,
+  },
 };
 export default config;

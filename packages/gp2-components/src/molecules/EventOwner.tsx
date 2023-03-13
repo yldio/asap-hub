42ba25fe0c9d27accaac80ca652a210f821326dd
Mark Sims <mark@bluepie.co.uk>
2023-03-13 09:52:00 +0000

    addressed comments from review

--- packages/gp2-components/src/molecules/EventOwner.tsx
+++ packages/gp2-components/src/molecules/EventOwner.tsx
@@ -1,25 +1,20 @@
 import { gp2 as gp2Model } from '@asap-hub/model';
-import { Link, neutral900, pixels } from '@asap-hub/react-components';
+import { Link } from '@asap-hub/react-components';
 import { gp2 as gp2Routing } from '@asap-hub/routing';
-import { css } from '@emotion/react';
+import { ComponentProps } from 'react';
 import { projectIcon, workingGroupIcon } from '../icons';
 import IconWithLabel from './IconWithLabel';
 
-const { rem } = pixels;
-
 const { projects, workingGroups } = gp2Routing;
-const listItemStyles = css({
-  padding: `${rem(7.5)} 0`,
-  color: neutral900.rgb,
-  whiteSpace: 'break-spaces',
-  overflow: 'hidden',
-  textOverflow: 'ellipsis',
-  fontSize: rem(17),
-  display: 'flex',
-});
 
 type EventOwnerProps = Pick<gp2Model.EventResponse, 'project' | 'workingGroup'>;
-const getProperties = ({ project, workingGroup }: EventOwnerProps) => {
+const getProperties = ({
+  project,
+  workingGroup,
+}: EventOwnerProps): {
+  title: string;
+} & Partial<Pick<ComponentProps<typeof Link>, 'href'>> &
+  Pick<ComponentProps<typeof IconWithLabel>, 'icon'> => {
   if (workingGroup) {
     return {
       ...workingGroup,
@@ -29,27 +24,25 @@ const getProperties = ({ project, workingGroup }: EventOwnerProps) => {
       icon: workingGroupIcon,
     };
   }
-  return (
-    project && {
-      ...project,
-      href: projects({}).project({ projectId: project.id }).$,
-      icon: projectIcon,
-    }
-  );
+  return project
+    ? {
+        ...project,
+        href: projects({}).project({ projectId: project.id }).$,
+        icon: projectIcon,
+      }
+    : {
+        title: 'GP2 Hub',
+        icon: workingGroupIcon,
+      };
 };
 
 const EventOwner: React.FC<EventOwnerProps> = ({ project, workingGroup }) => {
-  const { title, href, icon } = getProperties({ project, workingGroup }) || {
-    title: 'GP2 Hub',
-    icon: workingGroupIcon,
-  };
+  const { title, href, icon } = getProperties({ project, workingGroup });
 
   return (
-    <div css={listItemStyles}>
-      <IconWithLabel noMargin icon={icon}>
-        {href ? <Link href={href}>{title}</Link> : title}
-      </IconWithLabel>
-    </div>
+    <IconWithLabel noMargin icon={icon}>
+      {href ? <Link href={href}>{title}</Link> : title}
+    </IconWithLabel>
   );
 };
 

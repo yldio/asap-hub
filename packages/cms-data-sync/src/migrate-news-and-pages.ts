9833ce3b3c3ec4a578fbd82a620c702e0f9e3597
gabiayako <gabiayako@gmail.com>
2023-03-20 13:44:22 -0300

    remove news-page migration

--- packages/cms-data-sync/src/migrate-news-and-pages.ts
+++
@@ -1,8 +0,0 @@
-/* istanbul ignore file */
-import { migrateNews } from './news/news.data-migration';
-import { migratePages } from './pages/pages.data-migration';
-
-(async () => {
-  await migrateNews();
-  await migratePages();
-})();

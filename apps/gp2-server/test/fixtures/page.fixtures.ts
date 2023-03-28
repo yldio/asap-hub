faef54f697491b7b0174a122851bb170ec0aaa2f
Carlos Barbosa <fbcarlos101@gmail.com>
2023-03-28 10:31:03 +0100

    remove contentful from page fixtures

--- apps/gp2-server/test/fixtures/page.fixtures.ts
+++ apps/gp2-server/test/fixtures/page.fixtures.ts
@@ -1,4 +1,3 @@
-import { FetchPagesQuery } from '@asap-hub/contentful';
 import { PageResponse } from '@asap-hub/model';
 
 export const pageResponse: PageResponse = {
@@ -10,51 +9,3 @@ export const pageResponse: PageResponse = {
   link: 'link',
   linkText: 'linkText',
 };
-
-export const getContentfulPagesGraphqlResponse = (): FetchPagesQuery => ({
-  pagesCollection: {
-    total: 1,
-    items: [getContentfulGraphqlPages()],
-  },
-});
-
-export const getContentfulGraphqlPages = (): NonNullable<
-  NonNullable<FetchPagesQuery['pagesCollection']>['items'][number]
-> => ({
-  sys: {
-    id: 'some-id',
-  },
-  title: 'Privacy Policy',
-  path: '/privacy-policy',
-  shortText: 'short text',
-  link: 'link',
-  linkText: 'linkText',
-  text: {
-    json: {
-      nodeType: 'document',
-      data: {},
-      content: [
-        {
-          nodeType: 'heading-1',
-          data: {},
-          content: [
-            {
-              nodeType: 'text',
-              value: 'Privacy Policy',
-              marks: [],
-              data: {},
-            },
-          ],
-        },
-      ],
-    },
-    links: {
-      entries: {
-        inline: [],
-      },
-      assets: {
-        block: [],
-      },
-    },
-  },
-});

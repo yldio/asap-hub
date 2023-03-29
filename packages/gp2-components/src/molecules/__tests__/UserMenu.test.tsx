784045343845a627ebbc5f865718e385c87da531
Carlos Barbosa <fbcarlos101@gmail.com>
2023-03-29 16:25:05 +0100

    add the ability to close when the user clicks outside of the user menu

--- packages/gp2-components/src/molecules/__tests__/UserMenu.test.tsx
+++ packages/gp2-components/src/molecules/__tests__/UserMenu.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { ComponentProps } from 'react';
 import { gp2 as gp2Routing, logout } from '@asap-hub/routing';
 import userEvent from '@testing-library/user-event';
@@ -172,6 +172,18 @@ describe('UserMenu', () => {
 
     userEvent.click(screen.getByRole('link', { name: /the first wg title/i }));
 
+    expect(closeUserMenu).toBeCalledWith(false);
+  });
+
+  it('closes when the user clicks outside the User Menu', () => {
+    const closeUserMenu = jest.fn();
+    render(
+      <>
+        <h1>Title</h1>
+        <UserMenu {...props} closeUserMenu={closeUserMenu} />{' '}
+      </>,
+    );
+    fireEvent.mouseDown(screen.getByRole('heading'));
     expect(closeUserMenu).toBeCalledWith(false);
   });
 });

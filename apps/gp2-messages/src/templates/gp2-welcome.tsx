72d3beafff0a36c4bd0788c796c1860b6929fb8b
dianaguimaraes <dguimaraes@candy.com>
2023-03-29 14:16:40 +0100

    add theming and update copy

--- apps/gp2-messages/src/templates/gp2-welcome.tsx
+++ apps/gp2-messages/src/templates/gp2-welcome.tsx
@@ -1,11 +1,13 @@
-import { MessageLayout, WelcomeMessage } from '@asap-hub/gp2-components';
+import { MessageLayout, Theme, WelcomeMessage } from '@asap-hub/gp2-components';
 import { welcome } from '@asap-hub/message-templates';
 import { GP2_APP_ORIGIN as APP_ORIGIN } from '../config';
 
 export default (
-  <MessageLayout appOrigin={APP_ORIGIN}>
-    <WelcomeMessage {...welcome} />
-  </MessageLayout>
+  <Theme>
+    <MessageLayout appOrigin={APP_ORIGIN}>
+      <WelcomeMessage {...welcome} />
+    </MessageLayout>
+  </Theme>
 );
 
 export const subject = `Welcome ${welcome.firstName}!`;

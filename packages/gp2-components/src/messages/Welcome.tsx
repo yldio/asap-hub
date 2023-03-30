bccb5de1d931bb835a59530030bc1c3b2679ef28
Mark Sims <mark@bluepie.co.uk>
2023-03-30 13:46:45 +0200

    fixes footer links in email for gp2 (#2900)
    * fixes footer links in email for gp2
    
    * fixes footer links in email for crn
    
    * add theming and update copy
    
    ---------
    
    Co-authored-by: dianaguimaraes <dguimaraes@candy.com>

--- packages/gp2-components/src/messages/Welcome.tsx
+++ packages/gp2-components/src/messages/Welcome.tsx
@@ -13,8 +13,9 @@ const Welcome: React.FC<WelcomeProps> = ({ firstName, link }) => (
     <Paragraph>Thank you for starting this journey with us!</Paragraph>
     <Paragraph>
       You’re one step closer to joining the GP2 Hub - A private, invite-only
-      network where the GP2 community collaborates. Here, you can connect with
-      your team, join events, browse through projects, and access research.{' '}
+      network where the GP2 community collaborates. Here, you can stay up to
+      date with GP2 activities, join events, browse through projects, and
+      connect with others.{' '}
     </Paragraph>
     <Paragraph>
       Click ‘Create Account’ below to get started - we look forward to seeing
@@ -24,14 +25,16 @@ const Welcome: React.FC<WelcomeProps> = ({ firstName, link }) => (
       Create account
     </Link>
     <Paragraph>
-      As with every new path, you may come across some questions and{' '}
-      <Link href={mailToSupport()}>our team</Link> would be delighted to answer
-      them.
+      As with every new experience, you may have some questions and{' '}
+      <Link href={mailToSupport({ subject: 'ASAP GP2: Tech Support' })}>
+        our team
+      </Link>{' '}
+      would be delighted to answer them.
     </Paragraph>
     <Paragraph>
       Reminder: GP2 Hub is a closed platform to foster trust, candor and
-      connection among grantees. You are expected to comply with confidentiality
-      guidelines.
+      connection among our members. You are expected to comply with
+      confidentiality guidelines.
     </Paragraph>
   </section>
 );
--- packages/react-components/src/messages/__tests__/Layout.test.tsx
+++ packages/react-components/src/messages/__tests__/Layout.test.tsx
@@ -1,19 +1,18 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import Layout from '../Layout';
 
 it('renders its children', () => {
-  const { getByText } = render(
-    <Layout appOrigin="https://hub.asap.science">text</Layout>,
-  );
-  expect(getByText('text')).toBeVisible();
+  render(<Layout appOrigin="https://hub.asap.science">text</Layout>);
+  expect(screen.getByText('text')).toBeVisible();
 });
 
 it('generates the terms link', () => {
-  const { getByText } = render(
-    <Layout appOrigin="https://hub.asap.science">text</Layout>,
-  );
-  expect(getByText(/terms/i).closest('a')!.href).toMatchInlineSnapshot(
-    `"https://hub.asap.science/privacy-policy"`,
-  );
+  render(<Layout appOrigin="https://hub.asap.science">text</Layout>);
+  expect(screen.getByRole('link', { name: /terms/i })).toBeVisible();
+});
+
+it('generates the privacy link', () => {
+  render(<Layout appOrigin="https://hub.asap.science">text</Layout>);
+  expect(screen.getByRole('link', { name: /privacy/i })).toBeVisible();
 });

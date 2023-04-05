74612c307189a16861f31015c058f14f74f3894e
Mark Sims <mark@bluepie.co.uk>
2023-04-05 10:38:31 +0200

    updates to node 18 (#2916)
    * updates to node 18
    
    * include aws-sdk
    
    * increases memorySize due to timeout
    
    * uses node 18 in Docker image
    
    * uses es2022 for server
    
    * fixes build messages
    
    * bumps @types/node to 18

--- apps/gp2-server/serverless.ts
+++ apps/gp2-server/serverless.ts
@@ -65,10 +65,10 @@ const serverlessConfig: AWS = {
   plugins,
   provider: {
     name: 'aws',
-    runtime: 'nodejs16.x',
+    runtime: 'nodejs18.x',
     architecture: 'arm64',
     timeout: 16,
-    memorySize: 512,
+    memorySize: 1024,
     region,
     stage,
     versionFunctions: false,

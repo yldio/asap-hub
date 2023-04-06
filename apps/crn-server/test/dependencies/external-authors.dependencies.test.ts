2fcf111258fab9eee945775caae9c95a459663ae
Piotr S <piotr.szpak@yld.io>
2023-04-06 08:34:09 +0100

    Update apps/crn-server/test/dependencies/external-authors.dependencies.test.ts
    Co-authored-by: Leonard Martin <leonard.martin@gmail.com>

--- apps/crn-server/test/dependencies/external-authors.dependencies.test.ts
+++ apps/crn-server/test/dependencies/external-authors.dependencies.test.ts
@@ -20,7 +20,7 @@ describe('External Authors Dependencies', () => {
     );
   });
 
-  it('Should resolve External-Author Squidex Data Provider when the Contentful feature flag is off', async () => {
+  it('Should resolve External-Author Contentful Data Provider when the Contentful feature flag is on', async () => {
     process.env.IS_CONTENTFUL_ENABLED_V2 = 'true';
     const { ExternalAuthorContentfulDataProvider } = await import(
       '../../src/data-providers/contentful/external-authors.data-provider'

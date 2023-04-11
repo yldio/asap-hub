ff1322f29814b33462815203ffe0e9833b888c6c
Ratiu Octavian <oratiu@gmail.com>
2023-04-11 12:50:04 +0300

    toast fix

--- packages/react-components/src/organisms/Toast.tsx
+++ packages/react-components/src/organisms/Toast.tsx
@@ -10,6 +10,7 @@ import {
   infoCircleYellowIcon,
   informationIcon,
   successIcon,
+  successLargeIcon,
 } from '../icons';
 import {
   perRem,
@@ -83,13 +84,19 @@ const wrapStyles = css({
   flexFlow: 'row',
 });
 
-export type ToastAccents = 'error' | 'info' | 'warning' | 'success';
+export type ToastAccents =
+  | 'error'
+  | 'info'
+  | 'warning'
+  | 'success'
+  | 'successLarge';
 
 const accentIcons: Record<ToastAccents, EmotionJSX.Element> = {
   error: errorIcon,
   info: informationIcon,
   warning: infoCircleYellowIcon,
   success: successIcon,
+  successLarge: successLargeIcon,
 };
 
 const accentStyles: Record<ToastAccents, CSSObject> = {
@@ -112,6 +119,11 @@ const accentStyles: Record<ToastAccents, CSSObject> = {
     backgroundColor: success100.rgb,
     color: success900.rgb,
     svg: { stroke: success500.rgb },
+  },
+  successLarge: {
+    backgroundColor: success100.rgb,
+    color: success900.rgb,
+    svg: { stroke: success500.rgb, width: 24, height: 24 },
   },
 };
 

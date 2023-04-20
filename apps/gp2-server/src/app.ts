ae6ca4cf3244d4d91fa2a087d680e2675fd12183
Mark Sims <mark@bluepie.co.uk>
2023-04-20 14:54:39 +0100

    enables contentful news

--- apps/gp2-server/src/app.ts
+++ apps/gp2-server/src/app.ts
@@ -61,6 +61,7 @@ import {
   AssetSquidexDataProvider,
 } from './data-providers/asset.data-provider';
 import { CalendarSquidexDataProvider } from './data-providers/calendar.data-provider';
+import { NewsContentfulDataProvider } from './data-providers/contentful/news.data-provider';
 import { PageContentfulDataProvider } from './data-providers/contentful/page.data-provider';
 import {
   ContributingCohortDataProvider,
@@ -209,8 +210,12 @@ export const appFactory = (libs: Libs = {}): Express => {
   const userDataProvider =
     libs.userDataProvider ||
     new UserSquidexDataProvider(squidexGraphqlClient, userRestClient);
-  const newsDataProvider =
-    libs.newsDataProvider || new NewsSquidexDataProvider(squidexGraphqlClient);
+  const newsSquidexDataProvider =
+    libs.newsSquidexDataProvider ||
+    new NewsSquidexDataProvider(squidexGraphqlClient);
+  const newsContentfulDataProvider =
+    libs.newsContentfulDataProvider ||
+    new NewsContentfulDataProvider(contentfulGraphQLClient);
   const workingGroupDataProvider =
     libs.workingGroupDataProvider ||
     new WorkingGroupSquidexDataProvider(
@@ -248,6 +253,9 @@ export const appFactory = (libs: Libs = {}): Express => {
     ? pageContentfulDataProvider
     : pageSquidexDataProvider;
 
+  const newsDataProvider = isContentfulEnabled
+    ? newsContentfulDataProvider
+    : newsSquidexDataProvider;
   // Controllers
 
   const workingGroupController =
@@ -373,28 +381,29 @@ export type Libs = {
   contributingCohortController?: ContributingCohortController;
   contributingCohortDataProvider?: ContributingCohortDataProvider;
   eventController?: gp2.EventController;
-  externalUsersController?: ExternalUsersController;
   eventDataProvider?: gp2.EventDataProvider;
+  externalUsersController?: ExternalUsersController;
   externalUsersDataProvider?: ExternalUserDataProvider;
   logger?: Logger;
+  newsContentfulDataProvider?: NewsDataProvider;
   newsController?: NewsController;
-  newsDataProvider?: NewsDataProvider;
+  newsSquidexDataProvider?: NewsDataProvider;
   outputController?: OutputController;
   outputDataProvider?: OutputDataProvider;
+  pageContentfulDataProvider?: PageDataProvider;
   pageController?: PageController;
   pageSquidexDataProvider?: PageDataProvider;
-  pageContentfulDataProvider?: PageDataProvider;
   projectController?: ProjectController;
   projectDataProvider?: ProjectDataProvider;
+  sentryErrorHandler?: typeof Sentry.Handlers.errorHandler;
+  sentryRequestHandler?: typeof Sentry.Handlers.requestHandler;
+  sentryTransactionIdHandler?: RequestHandler;
   userController?: UserController;
   userDataProvider?: UserDataProvider;
   workingGroupController?: WorkingGroupController;
   workingGroupDataProvider?: WorkingGroupDataProvider;
   workingGroupNetworkController?: WorkingGroupNetworkController;
   workingGroupNetworkDataProvider?: WorkingGroupNetworkDataProvider;
-  sentryErrorHandler?: typeof Sentry.Handlers.errorHandler;
-  sentryRequestHandler?: typeof Sentry.Handlers.requestHandler;
-  sentryTransactionIdHandler?: RequestHandler;
   // sentryTransactionIdHandler?: RequestHandler;
   // extra handlers only for tests and local development
   mockRequestHandlers?: RequestHandler[];
--- apps/gp2-server/test/app.test.ts
+++ apps/gp2-server/test/app.test.ts
@@ -1,7 +1,10 @@
 import supertest from 'supertest';
 import { authHandlerMock } from './mocks/auth-handler.mock';
 
-import { PageDataProvider } from '../src/data-providers/types';
+import {
+  NewsDataProvider,
+  PageDataProvider,
+} from '../src/data-providers/types';
 
 describe('Contentful feature flag', () => {
   const OLD_ENV = process.env;
@@ -16,11 +19,54 @@ describe('Contentful feature flag', () => {
     process.env = OLD_ENV;
   });
 
-  describe('Page Data Provider', () => {
-    beforeEach(() => {
-      jest.clearAllMocks();
-    });
+  describe('News Data Provider', () => {
+    const newsSquidexDataProviderMock = {
+      fetch: jest.fn(),
+    } as unknown as jest.Mocked<NewsDataProvider>;
 
+    const newsContentfulDataProviderMock = {
+      fetch: jest.fn(),
+    } as unknown as jest.Mocked<NewsDataProvider>;
+
+    test.each(['false', undefined])(
+      'news controller uses squidex data provider when GP2_CONTENTFUL_ENABLED is %s',
+      async (enabled) => {
+        process.env.GP2_CONTENTFUL_ENABLED = enabled;
+
+        const { appFactory } = require('../src/app');
+
+        const app = appFactory({
+          newsSquidexDataProvider: newsSquidexDataProviderMock,
+          newsContentfulDataProvider: newsContentfulDataProviderMock,
+          authHandler: authHandlerMock,
+        });
+        await supertest(app).get('/news');
+
+        expect(newsSquidexDataProviderMock.fetch).toHaveBeenCalledTimes(1);
+
+        expect(newsContentfulDataProviderMock.fetch).not.toHaveBeenCalled();
+      },
+      10_000,
+    );
+
+    test('news controller uses contentful data provider when GP2_CONTENTFUL_ENABLED is true', async () => {
+      process.env.GP2_CONTENTFUL_ENABLED = 'true';
+
+      const { appFactory } = require('../src/app');
+
+      const app = appFactory({
+        authHandler: authHandlerMock,
+        newsSquidexDataProvider: newsSquidexDataProviderMock,
+        newsContentfulDataProvider: newsContentfulDataProviderMock,
+      });
+      await supertest(app).get('/news');
+
+      expect(newsSquidexDataProviderMock.fetch).not.toHaveBeenCalled();
+
+      expect(newsContentfulDataProviderMock.fetch).toHaveBeenCalledTimes(1);
+    });
+  });
+  describe('Page Data Provider', () => {
     const pageSquidexDataProviderMock = {
       fetch: jest.fn(),
     } as unknown as jest.Mocked<PageDataProvider>;
@@ -29,37 +75,24 @@ describe('Contentful feature flag', () => {
       fetch: jest.fn(),
     } as unknown as jest.Mocked<PageDataProvider>;
 
-    test('page controller uses squidex data provider when GP2_CONTENTFUL_ENABLED is false', async () => {
-      process.env.GP2_CONTENTFUL_ENABLED = 'false';
+    test.each(['false', undefined])(
+      'page controller uses squidex data provider when GP2_CONTENTFUL_ENABLED is %s',
+      async (enabled) => {
+        process.env.GP2_CONTENTFUL_ENABLED = enabled;
 
-      const { appFactory } = require('../src/app');
+        const { appFactory } = require('../src/app');
 
-      const app = appFactory({
-        pageSquidexDataProvider: pageSquidexDataProviderMock,
-        pageContentfulDataProvider: pageContentfulDataProviderMock,
-        authHandler: authHandlerMock,
-      });
-      await supertest(app).get('/pages/privacy-policy');
+        const app = appFactory({
+          pageSquidexDataProvider: pageSquidexDataProviderMock,
+          pageContentfulDataProvider: pageContentfulDataProviderMock,
+          authHandler: authHandlerMock,
+        });
+        await supertest(app).get('/pages/privacy-policy');
 
-      expect(pageSquidexDataProviderMock.fetch).toHaveBeenCalledTimes(1);
-      expect(pageContentfulDataProviderMock.fetch).not.toHaveBeenCalled();
-    });
-
-    test('page controller uses squidex data provider when GP2_CONTENTFUL_ENABLED undefined', async () => {
-      process.env.GP2_CONTENTFUL_ENABLED = undefined;
-
-      const { appFactory } = require('../src/app');
-
-      const app = appFactory({
-        pageSquidexDataProvider: pageSquidexDataProviderMock,
-        pageContentfulDataProvider: pageContentfulDataProviderMock,
-        authHandler: authHandlerMock,
-      });
-      await supertest(app).get('/pages/privacy-policy');
-
-      expect(pageSquidexDataProviderMock.fetch).toHaveBeenCalledTimes(1);
-      expect(pageContentfulDataProviderMock.fetch).not.toHaveBeenCalled();
-    });
+        expect(pageSquidexDataProviderMock.fetch).toHaveBeenCalledTimes(1);
+        expect(pageContentfulDataProviderMock.fetch).not.toHaveBeenCalled();
+      },
+    );
 
     test('page controller uses contentful data provider when GP2_CONTENTFUL_ENABLED is true', async () => {
       process.env.GP2_CONTENTFUL_ENABLED = 'true';

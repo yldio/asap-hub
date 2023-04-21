d47b0be165838306c38aefdb031ef3d171635542
Piotr S <piotr.szpak@yld.io>
2023-04-21 09:41:50 +0200

    feat: CT-4 Split Reseach-Tags Controller (#2982)

--- apps/crn-server/test/controllers/research-tags.test.ts
+++ apps/crn-server/test/controllers/research-tags.test.ts
@@ -1,176 +1,61 @@
 import ResearchTags from '../../src/controllers/research-tags';
+import { ResearchTagDataProvider } from '../../src/data-providers/research-tags.data-provider';
 import {
-  getListResearchTagResponse,
-  getSquidexResearchTagsGraphqlResponse,
+  getResearchTagDataObject,
+  getResearchTagResponse,
 } from '../fixtures/research-tag.fixtures';
-import { getSquidexGraphqlClientMockServer } from '../mocks/squidex-graphql-client-with-server.mock';
-import { getSquidexGraphqlClientMock } from '../mocks/squidex-graphql-client.mock';
+import { getDataProviderMock } from '../mocks/data-provider.mock';
 
 describe('ResearchTags controller', () => {
-  const squidexGraphqlClientMock = getSquidexGraphqlClientMock();
-  const researchTags = new ResearchTags(squidexGraphqlClientMock);
-
-  const squidexGraphqlClientMockServer = getSquidexGraphqlClientMockServer();
-  const researchTagsMockGraphql = new ResearchTags(
-    squidexGraphqlClientMockServer,
-  );
+  const researchTagDataProvider = getDataProviderMock();
+  const researchTags = new ResearchTags(researchTagDataProvider);
 
   afterEach(() => {
     jest.resetAllMocks();
   });
 
   describe('Fetch method', () => {
-    test('Should fetch the research tags from squidex graphql', async () => {
-      const result = await researchTagsMockGraphql.fetch({
-        take: 8,
-        skip: 0,
+    test('Should return an empty list when there are no research tags', async () => {
+      researchTagDataProvider.fetch.mockResolvedValueOnce({
+        total: 0,
+        items: [],
       });
+      const result = await researchTags.fetch({});
 
-      expect(result).toMatchObject(getListResearchTagResponse());
+      expect(result).toEqual({ items: [], total: 0 });
     });
 
-    test('Should return an empty result when the client returns an empty array of data', async () => {
-      const squidexGraphqlResponse = getSquidexResearchTagsGraphqlResponse();
-      squidexGraphqlResponse.queryResearchTagsContentsWithTotal!.total = 0;
-      squidexGraphqlResponse.queryResearchTagsContentsWithTotal!.items = [];
-      squidexGraphqlClientMock.request.mockResolvedValueOnce(
-        squidexGraphqlResponse,
-      );
-
-      const result = await researchTags.fetch({ take: 10, skip: 5 });
-
-      expect(result).toEqual({ total: 0, items: [] });
-    });
-
-    test('Should return an empty result when the client returns a response with query property set to null', async () => {
-      const squidexGraphqlResponse = getSquidexResearchTagsGraphqlResponse();
-      squidexGraphqlResponse.queryResearchTagsContentsWithTotal = null;
-      squidexGraphqlClientMock.request.mockResolvedValueOnce(
-        squidexGraphqlResponse,
-      );
-
-      const result = await researchTags.fetch({ take: 10, skip: 5 });
-
-      expect(result).toEqual({ total: 0, items: [] });
-    });
-
-    test('Should return an empty result when the client returns a response with items property set to null', async () => {
-      const squidexGraphqlResponse = getSquidexResearchTagsGraphqlResponse();
-      squidexGraphqlResponse.queryResearchTagsContentsWithTotal!.items = null;
-      squidexGraphqlClientMock.request.mockResolvedValueOnce(
-        squidexGraphqlResponse,
-      );
-
-      const result = await researchTags.fetch({ take: 10, skip: 5 });
-
-      expect(result).toEqual({ total: 0, items: [] });
-    });
-
-    test('Should throw an error when squidex returns an invalid category', async () => {
-      const squidexGraphqlResponse = getSquidexResearchTagsGraphqlResponse();
-      squidexGraphqlResponse.queryResearchTagsContentsWithTotal!.items![0]!.flatData.category =
-        'invalid category';
-      squidexGraphqlClientMock.request.mockResolvedValueOnce(
-        squidexGraphqlResponse,
-      );
-
-      await expect(
-        researchTags.fetch({ take: 10, skip: 5 }),
-      ).rejects.toThrowError(
-        TypeError('Invalid category received from Squidex'),
-      );
-    });
-
-    test('Should throw an error when squidex returns an invalid entity', async () => {
-      const squidexGraphqlResponse = getSquidexResearchTagsGraphqlResponse();
-      squidexGraphqlResponse.queryResearchTagsContentsWithTotal!.items![0]!.flatData.entities =
-        ['Research Output', 'invalid entity'];
-      squidexGraphqlClientMock.request.mockResolvedValueOnce(
-        squidexGraphqlResponse,
-      );
-
-      await expect(
-        researchTags.fetch({ take: 10, skip: 5 }),
-      ).rejects.toThrowError(TypeError('Invalid entity received from Squidex'));
-    });
-
-    describe('Parameters', () => {
-      beforeEach(() => {
-        squidexGraphqlClientMock.request.mockResolvedValueOnce(
-          getSquidexResearchTagsGraphqlResponse(),
-        );
+    test('Should return the research tags', async () => {
+      researchTagDataProvider.fetch.mockResolvedValueOnce({
+        total: 1,
+        items: [getResearchTagDataObject()],
       });
 
-      test('Should pass the pagination parameters as expected', async () => {
-        await researchTags.fetch({ take: 13, skip: 7 });
+      const result = await researchTags.fetch({});
 
-        expect(squidexGraphqlClientMock.request).toHaveBeenCalledWith(
-          expect.anything(),
-          {
-            top: 13,
-            skip: 7,
-            filter: '',
-          },
-        );
+      expect(result).toEqual({
+        items: [getResearchTagResponse()],
+        total: 1,
+      });
+    });
+
+    test('Should call the data provider with correct parameters', async () => {
+      await researchTags.fetch({
+        take: 10,
+        skip: 5,
+        filter: { entity: 'Research Output', type: 'some-type' },
       });
 
-      test('Should pass the entity filter parameters as expected', async () => {
-        await researchTags.fetch({
-          take: 11,
-          skip: 4,
-          filter: {
-            entity: 'Research Output',
-          },
-        });
-
-        expect(squidexGraphqlClientMock.request).toHaveBeenCalledWith(
-          expect.anything(),
-          {
-            top: 11,
-            skip: 4,
-            filter: `(data/entities/iv eq 'Research Output')`,
-          },
-        );
-      });
-
-      test('Should pass the type filter parameters as expected', async () => {
-        await researchTags.fetch({
-          take: 15,
-          skip: 3,
-          filter: {
-            type: 'Software',
-          },
-        });
-
-        expect(squidexGraphqlClientMock.request).toHaveBeenCalledWith(
-          expect.anything(),
-          {
-            top: 15,
-            skip: 3,
-            filter: `(data/types/iv eq 'Software')`,
-          },
-        );
-      });
-
-      test('Should pass the both filters parameters as expected', async () => {
-        await researchTags.fetch({
-          take: 11,
-          skip: 4,
-          filter: {
-            entity: 'Research Output',
-            type: 'Software',
-          },
-        });
-
-        expect(squidexGraphqlClientMock.request).toHaveBeenCalledWith(
-          expect.anything(),
-          {
-            top: 11,
-            skip: 4,
-            filter: `(data/types/iv eq 'Software') and (data/entities/iv eq 'Research Output')`,
-          },
-        );
-      });
+      const expectedParameters: Parameters<
+        ResearchTagDataProvider['fetch']
+      >[0] = {
+        filter: { entity: 'Research Output', type: 'some-type' },
+        skip: 5,
+        take: 10,
+      };
+      expect(researchTagDataProvider.fetch).toHaveBeenCalledWith(
+        expectedParameters,
+      );
     });
   });
 });

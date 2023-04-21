ede33c5aa4d3dbbc258b32b81ad247da57c9615c
dianaguimaraes <dguimaraes@candy.com>
2023-04-21 12:08:35 +0100

    delete duplicated migration code

--- packages/contentful/migrations/gp2/contributingCohortsMembership/6ekgyp1432o9-create-contributing-cohorts-membership-1682073967694.js
+++ packages/contentful/migrations/gp2/contributingCohortsMembership/6ekgyp1432o9-create-contributing-cohorts-membership-1682073967694.js
@@ -76,27 +76,6 @@ module.exports.up = function (migration) {
     'urlEditor',
     {},
   );
-  const contributingCohorts = migration
-    .createContentType('contributingCohorts')
-    .name('Contributing Cohorts')
-    .description('')
-    .displayField('name');
-
-  contributingCohorts
-    .createField('name')
-    .name('Name')
-    .type('Symbol')
-    .localized(false)
-    .required(true)
-    .validations([
-      {
-        unique: true,
-      },
-    ])
-    .disabled(false)
-    .omitted(false);
-
-  contributingCohorts.changeFieldControl('name', 'builtin', 'singleLine', {});
 };
 
 module.exports.down = (migration) => {

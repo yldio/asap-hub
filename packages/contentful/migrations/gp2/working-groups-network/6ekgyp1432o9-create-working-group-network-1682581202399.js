8a3e9a9b3cad9293a302b8f73f9c04b132c46986
Mark Sims <mark@bluepie.co.uk>
2023-04-27 11:39:35 +0100

    adds working group network content model

---
+++ packages/contentful/migrations/gp2/working-groups-network/6ekgyp1432o9-create-working-group-network-1682581202399.js
@@ -0,0 +1,103 @@
+module.exports.description = 'Create working group network content models';
+
+module.exports.up = (migration) => {
+  const workingGroupNetwork = migration
+    .createContentType('workingGroupNetwork')
+    .name('Working Group Network')
+    .description('');
+
+  workingGroupNetwork
+    .createField('operational')
+    .name('Operational Working Groups')
+    .type('Array')
+    .localized(false)
+    .required(true)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Link',
+      validations: [
+        {
+          linkContentType: ['workingGroups'],
+          size: {
+            min: 1,
+          },
+        },
+      ],
+
+      linkType: 'Entry',
+    });
+
+  workingGroupNetwork
+    .createField('monogenic')
+    .name('Monogenic Working Groups')
+    .type('Array')
+    .localized(false)
+    .required(true)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Link',
+      validations: [
+        {
+          linkContentType: ['workingGroups'],
+          size: {
+            min: 1,
+          },
+        },
+      ],
+
+      linkType: 'Entry',
+    });
+
+  workingGroupNetwork
+    .createField('complexDisease')
+    .name('Complex Disease Working Groups')
+    .type('Array')
+    .localized(false)
+    .required(true)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Link',
+      validations: [
+        {
+          linkContentType: ['workingGroups'],
+          size: {
+            min: 1,
+          },
+        },
+      ],
+
+      linkType: 'Entry',
+    });
+  workingGroupNetwork
+    .createField('support')
+    .name('Support Working Groups')
+    .type('Array')
+    .localized(false)
+    .required(true)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Link',
+      validations: [
+        {
+          linkContentType: ['workingGroups'],
+          size: {
+            min: 1,
+          },
+        },
+      ],
+
+      linkType: 'Entry',
+    });
+};
+
+module.exports.down = (migration) => {
+  migration.deleteContentType('workingGroupNetwork');
+};

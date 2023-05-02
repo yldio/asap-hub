806474fc3511e9734f8099f879750907a18b3087
Mark Sims <mark@bluepie.co.uk>
2023-05-02 17:02:59 +0100

    updates more outputs fields

--- packages/contentful/migrations/gp2/outputs/20230502124309-enable-related-entity.js
+++ packages/contentful/migrations/gp2/outputs/20230502124309-enable-related-entity.js
@@ -4,11 +4,24 @@ module.exports.up = (migration) => {
   const outputs = migration.editContentType('outputs');
 
   outputs.editField('relatedEntity').disabled(false);
+  outputs.editField('lastUpdatedPartial').disabled(false);
+  outputs.editField('createdBy').disabled(false);
+  outputs.editField('updatedBy').disabled(false);
 
   outputs.changeFieldControl('relatedEntity', 'builtin', 'entryLinkEditor', {
     showLinkEntityAction: true,
     showCreateEntityAction: false,
   });
+  outputs.changeFieldControl(
+    'lastUpdatedPartial',
+    'app',
+    'mqvX9KU5AthRTlnIRhNNh',
+    {
+      observedField: 'lastUpdatedPartial',
+    },
+  );
+  outputs.changeFieldControl('createdBy', 'app', '2finDNk15g5UtOq4DaLNxv', {});
+  outputs.changeFieldControl('updatedBy', 'app', '2finDNk15g5UtOq4DaLNxv', {});
 };
 
 module.exports.down = (migration) => {

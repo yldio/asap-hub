15cb49e6b9a34e323f9b5a2f0f762f0a3971e3f4
Leonard Martin <leonard.martin@gmail.com>
2023-05-10 11:42:09 +0100

    CT-47 - rewrite event speakers contentful app to not require title field
    Extends the multiple reference input with a custom card to load data from the connected team/user/external author.

--- packages/contentful-app-extensions/event-speakers-title/src/locations/Field.tsx
+++ packages/contentful-app-extensions/event-speakers-title/src/locations/Field.tsx
@@ -1,48 +1,124 @@
-import React, { useEffect, useState } from 'react';
-import { Paragraph } from '@contentful/f36-components';
-import { FieldExtensionSDK } from '@contentful/app-sdk';
-import { useSDK } from '@contentful/react-apps-toolkit';
+import React from 'react';
+import {
+  MultipleEntryReferenceEditor,
+  CustomEntityCardProps,
+  useEntity,
+} from '@contentful/field-editor-reference';
+import { entityHelpers } from '@contentful/field-editor-shared';
+import { EntryCard, MenuItem, Text, Badge } from '@contentful/f36-components';
+import { FieldExtensionSDK, Entry } from '@contentful/app-sdk';
+import { useSDK, useAutoResizer } from '@contentful/react-apps-toolkit';
 
-const getTitle = (teamName?: string, userName?: string) => {
-  if (teamName && userName) return `${teamName} - ${userName}`;
-  if (userName) return userName;
-  if (teamName) return teamName;
-  return undefined;
+type CustomEntryCardProps = CustomEntityCardProps & { entity: Entry };
+
+const TeamTitle = ({ id }: { id: string }) => {
+  const { data: team } = useEntity<Entry>('Entry', id);
+  if (!team) {
+    return null;
+  }
+  return (
+    <>
+      {team.fields?.displayName?.['en-US']} <Badge>Team</Badge>
+    </>
+  );
 };
+const UserTitle = ({ id }: { id: string }) => {
+  const { data: user } = useEntity<Entry>('Entry', id);
+  const contentType = user?.sys?.contentType?.sys?.id;
+
+  if (contentType === 'users') {
+    const name = `${user.fields?.firstName?.['en-US']} ${user.fields?.lastName?.['en-US']}`;
+    return (
+      <>
+        {name} <Badge>User</Badge>
+      </>
+    );
+  }
+  if (contentType === 'externalAuthors') {
+    return (
+      <>
+        {user.fields?.name?.['en-US']} <Badge>External Author</Badge>
+      </>
+    );
+  }
+  return null;
+};
+
+const Card = ({ entity, onEdit, onRemove }: CustomEntryCardProps) => {
+  const sdk = useSDK<FieldExtensionSDK>();
+  const { fields, sys } = entity;
+  const teamId = fields.team?.['en-US'].sys.id;
+  const userId = fields.user?.['en-US'].sys.id;
+
+  const removeSpeaker = async () => {
+    if (sys.publishedVersion) {
+      await sdk.space.unpublishEntry(entity);
+    }
+    await sdk.space.deleteEntry(entity);
+    if (onRemove) {
+      onRemove();
+    }
+  };
+
+  const status = entityHelpers.getEntryStatus(sys);
+
+  const defaultProps = {
+    contentType: 'Speaker',
+    actions: [
+      <MenuItem key="remove" onClick={removeSpeaker}>
+        Remove
+      </MenuItem>,
+    ],
+    status,
+    onClick: onEdit,
+  };
+
+  return (
+    <EntryCard {...defaultProps}>
+      {!teamId && !userId && (
+        <Text fontColor="colorNegative">No speakers selected</Text>
+      )}
+      {teamId && (
+        <Text>
+          <TeamTitle id={teamId} />
+        </Text>
+      )}
+      {userId && (
+        <Text>
+          <UserTitle id={userId} />
+        </Text>
+      )}
+    </EntryCard>
+  );
+};
+
+export const CustomCard = ({ entity, ...props }: CustomEntityCardProps) => (
+  <Card {...props} entity={entity as Entry} />
+);
 
 const Field = () => {
   const sdk = useSDK<FieldExtensionSDK>();
+  useAutoResizer();
 
-  const [title, setTitle] = useState(sdk.field.getValue());
-
-  const [teamName, setTeamName] = useState('');
-  const [userName, setUserName] = useState('');
-
-  useEffect(() => {
-    sdk.window.startAutoResizer();
-  }, [sdk.window]);
-
-  sdk.entry.onSysChanged(async () => {
-    const teamId = sdk.entry.fields.team.getValue()?.sys?.id;
-    if (teamId) {
-      const team = await sdk.space.getEntry(teamId);
-      setTeamName(team.fields.displayName['en-US']);
-    }
-
-    const userId = sdk.entry.fields.user.getValue()?.sys?.id;
-    if (userId) {
-      const user = await sdk.space.getEntry(userId);
-      setUserName(user.fields.name['en-US']);
-    }
-  });
-
-  useEffect(() => {
-    const titleValue = getTitle(teamName, userName);
-    sdk.field.setValue(titleValue);
-    setTitle(titleValue);
-  }, [teamName, userName, sdk.field]);
-
-  return <Paragraph>{title ?? ''}</Paragraph>;
+  return (
+    <MultipleEntryReferenceEditor
+      isInitiallyDisabled={false}
+      hasCardEditActions={true}
+      renderCustomCard={CustomCard}
+      parameters={{
+        instance: {
+          showLinkEntityAction: false,
+          showCreateEntityAction: true,
+          bulkEditing: false,
+        },
+      }}
+      viewType="link"
+      sdk={sdk}
+      actionLabels={{
+        createNew: () => 'Add',
+      }}
+    />
+  );
 };
 
 export default Field;
--- packages/contentful-app-extensions/event-speakers-title/src/test/locations/Field.test.tsx
+++ packages/contentful-app-extensions/event-speakers-title/src/test/locations/Field.test.tsx
@@ -1,78 +1,347 @@
 import '@testing-library/jest-dom';
-import React from 'react';
-import Field from '../../locations/Field';
-import { render, screen, waitFor } from '@testing-library/react';
-
-import { useSDK } from '@contentful/react-apps-toolkit';
+import React, { useState } from 'react';
+import Field, { CustomCard } from '../../locations/Field';
+import {
+  render,
+  screen,
+  waitFor,
+  fireEvent,
+  queryByTestId,
+} from '@testing-library/react';
+import { FieldExtensionSDK, Entry } from '@contentful/app-sdk';
+import {
+  MultipleEntryReferenceEditor,
+  CustomEntityCardProps,
+  useEntity,
+} from '@contentful/field-editor-reference';
+import { useSDK, useAutoResizer } from '@contentful/react-apps-toolkit';
 
 jest.mock('@contentful/react-apps-toolkit', () => ({
   useSDK: jest.fn(),
+  useAutoResizer: jest.fn(),
 }));
 
+jest.mock('@contentful/field-editor-reference', () => ({
+  MultipleEntryReferenceEditor: jest.fn(),
+  useEntity: jest.fn(),
+}));
+
+const mockBaseSdk = () => ({
+  window: {
+    startAutoResizer: jest.fn(),
+  },
+  space: {
+    unpublishEntry: jest.fn(),
+    deleteEntry: jest.fn(),
+  },
+});
+
 describe('Field component', () => {
-  it('sets title based on team name and user name', async () => {
-    const mockBaseSdk = {
-      window: {
-        startAutoResizer: jest.fn(),
-      },
-      field: {
-        getValue: jest.fn(),
-        setValue: jest.fn(),
-      },
-      space: {
-        getEntry: jest.fn().mockImplementation((entryId) => {
-          if (entryId === 'team-id') {
-            return Promise.resolve({
+  let sdk: jest.Mocked<FieldExtensionSDK>;
+
+  beforeEach(() => {
+    sdk = mockBaseSdk() as unknown as jest.Mocked<FieldExtensionSDK>;
+    (useSDK as jest.Mock).mockReturnValue(sdk);
+    (MultipleEntryReferenceEditor as jest.Mock).mockImplementation(() => (
+      <p>MultipleEntryReferenceEditor</p>
+    ));
+  });
+
+  it('enables automatic resizing', async () => {
+    render(<Field />);
+    expect(useAutoResizer).toHaveBeenCalled();
+  });
+
+  it('passes a custom card renderer to <MultipleEntryReferenceEditor />', async () => {
+    render(<Field />);
+
+    expect(MultipleEntryReferenceEditor).toHaveBeenCalled();
+    expect(
+      (MultipleEntryReferenceEditor as jest.Mock).mock.lastCall[0]
+        .renderCustomCard,
+    ).toEqual(CustomCard);
+  });
+
+  describe('CustomCard component', () => {
+    beforeEach(() => {
+      (useEntity as jest.Mock).mockImplementation((type, id) => {
+        if (id === 'team-1') {
+          return {
+            data: {
+              sys: {
+                contentType: {
+                  sys: {
+                    id: 'teams',
+                  },
+                },
+              },
               fields: {
                 displayName: {
-                  'en-US': 'Team A',
+                  'en-US': 'My Team',
                 },
               },
-            });
-          }
-
-          if (entryId === 'user-id') {
-            return Promise.resolve({
+            },
+          };
+        }
+        if (id === 'user-1') {
+          return {
+            data: {
+              sys: {
+                contentType: {
+                  sys: {
+                    id: 'users',
+                  },
+                },
+              },
+              fields: {
+                firstName: {
+                  'en-US': 'First',
+                },
+                lastName: {
+                  'en-US': 'Last',
+                },
+              },
+            },
+          };
+        }
+        if (id === 'external-author-1') {
+          return {
+            data: {
+              sys: {
+                contentType: {
+                  sys: {
+                    id: 'externalAuthors',
+                  },
+                },
+              },
               fields: {
                 name: {
-                  'en-US': 'User B',
+                  'en-US': 'External Speaker',
                 },
               },
-            });
-          }
-        }),
-      },
-      entry: {
-        onSysChanged: jest.fn((cb) => {
-          cb(jest.fn());
-          return jest.fn();
-        }),
-        fields: {
-          team: {
-            getValue: jest.fn(() => ({
-              sys: {
-                id: 'team-id',
-              },
-            })),
-          },
-          user: {
-            getValue: jest.fn(() => ({
-              sys: {
-                id: 'user-id',
-              },
-            })),
-          },
-        },
-      },
-    };
-    (useSDK as jest.Mock).mockReturnValue(mockBaseSdk);
-    render(<Field />);
-    await waitFor(() => {
-      expect(mockBaseSdk.field.setValue).toHaveBeenCalledWith(
-        'Team A - User B',
-      );
+            },
+          };
+        }
+      });
     });
 
-    expect(screen.getByText('Team A - User B')).toBeInTheDocument();
+    it('loads team name from related entity and renders team name', async () => {
+      const props = {
+        entity: {
+          fields: {
+            team: {
+              'en-US': {
+                sys: {
+                  id: 'team-1',
+                },
+              },
+            },
+            user: null,
+          },
+          sys: {
+            type: 'Entry',
+            publishedVersion: 1,
+            version: 1,
+          },
+        },
+        onEdit: jest.fn(),
+        onRemove: jest.fn(),
+      } as unknown as CustomEntityCardProps;
+
+      render(<CustomCard {...props} />);
+
+      await waitFor(() => {
+        expect(useEntity).toHaveBeenCalledWith('Entry', 'team-1');
+        expect(screen.queryByText('My Team')).toBeInTheDocument();
+        expect(screen.queryByText('Team')).toBeInTheDocument();
+        expect(screen.queryByText('User')).not.toBeInTheDocument();
+        expect(screen.queryByText('External Author')).not.toBeInTheDocument();
+      });
+    });
+
+    it('loads user name from related entity and renders user name', async () => {
+      const props = {
+        entity: {
+          fields: {
+            team: null,
+            user: {
+              'en-US': {
+                sys: {
+                  id: 'user-1',
+                },
+              },
+            },
+          },
+          sys: {
+            type: 'Entry',
+            publishedVersion: 1,
+            version: 1,
+          },
+        },
+        onEdit: jest.fn(),
+        onRemove: jest.fn(),
+      } as unknown as CustomEntityCardProps;
+
+      render(<CustomCard {...props} />);
+
+      await waitFor(() => {
+        expect(useEntity).toHaveBeenCalledWith('Entry', 'user-1');
+        expect(screen.queryByText('First Last')).toBeInTheDocument();
+        expect(screen.queryByText('Team')).not.toBeInTheDocument();
+        expect(screen.queryByText('User')).toBeInTheDocument();
+        expect(screen.queryByText('External Author')).not.toBeInTheDocument();
+      });
+    });
+
+    it('loads external author name from related entity and renders external author name', async () => {
+      const props = {
+        entity: {
+          fields: {
+            team: null,
+            user: {
+              'en-US': {
+                sys: {
+                  id: 'external-author-1',
+                },
+              },
+            },
+          },
+          sys: {
+            type: 'Entry',
+            publishedVersion: 1,
+            version: 1,
+          },
+        },
+        onEdit: jest.fn(),
+        onRemove: jest.fn(),
+      } as unknown as CustomEntityCardProps;
+
+      render(<CustomCard {...props} />);
+
+      await waitFor(() => {
+        expect(useEntity).toHaveBeenCalledWith('Entry', 'external-author-1');
+        expect(screen.queryByText('External Speaker')).toBeInTheDocument();
+        expect(screen.queryByText('Team')).not.toBeInTheDocument();
+        expect(screen.queryByText('User')).not.toBeInTheDocument();
+        expect(screen.queryByText('External Author')).toBeInTheDocument();
+      });
+    });
+
+    it('calls the `onEdit` handler in contentful to open the related entity when clicked', async () => {
+      const props = {
+        entity: {
+          fields: {
+            team: null,
+            user: null,
+          },
+          sys: {
+            type: 'Entry',
+            publishedVersion: 1,
+            version: 1,
+          },
+        },
+        onEdit: jest.fn(),
+        onRemove: jest.fn(),
+      } as unknown as CustomEntityCardProps;
+
+      render(<CustomCard {...props} />);
+
+      await waitFor(() => {
+        expect(screen.getByText('No speakers selected')).toBeInTheDocument();
+      });
+      fireEvent.click(screen.getByText('No speakers selected'));
+      await waitFor(() => {
+        expect(props.onEdit).toHaveBeenCalled();
+      });
+    });
+
+    it('renders a remove button in the card menu', async () => {
+      const props = {
+        entity: {
+          fields: {
+            team: null,
+            user: null,
+          },
+          sys: {
+            type: 'Entry',
+            publishedVersion: 1,
+            version: 1,
+          },
+        },
+        onEdit: jest.fn(),
+        onRemove: jest.fn(),
+      } as unknown as CustomEntityCardProps;
+
+      render(<CustomCard {...props} />);
+
+      await waitFor(() => {
+        expect(screen.getByText('No speakers selected')).toBeInTheDocument();
+      });
+      fireEvent.click(screen.getByLabelText('Actions'));
+      await waitFor(() => {
+        expect(screen.getByText('Remove')).toBeVisible();
+      });
+    });
+
+    it('unpublishes and deletes the entry when the remove button is clicked', async () => {
+      const props = {
+        entity: {
+          fields: {
+            team: null,
+            user: null,
+          },
+          sys: {
+            type: 'Entry',
+            publishedVersion: 1,
+            version: 1,
+          },
+        },
+        onEdit: jest.fn(),
+        onRemove: jest.fn(),
+      } as unknown as CustomEntityCardProps;
+
+      render(<CustomCard {...props} />);
+
+      await waitFor(() => {
+        expect(screen.getByText('No speakers selected')).toBeInTheDocument();
+      });
+      fireEvent.click(screen.getByLabelText('Actions'));
+      fireEvent.click(screen.getByText('Remove'));
+      await waitFor(() => {
+        expect(sdk.space.unpublishEntry).toHaveBeenCalledWith(props.entity);
+        expect(sdk.space.deleteEntry).toHaveBeenCalledWith(props.entity);
+        expect(props.onRemove).toHaveBeenCalled();
+      });
+    });
+
+    it('does not unpublish the entry when the remove button is clicked if the entry is unpublished', async () => {
+      const props = {
+        entity: {
+          fields: {
+            team: null,
+            user: null,
+          },
+          sys: {
+            type: 'Entry',
+            version: 1,
+          },
+        },
+        onEdit: jest.fn(),
+        onRemove: jest.fn(),
+      } as unknown as CustomEntityCardProps;
+
+      render(<CustomCard {...props} />);
+
+      await waitFor(() => {
+        expect(screen.getByText('No speakers selected')).toBeInTheDocument();
+      });
+      fireEvent.click(screen.getByLabelText('Actions'));
+      fireEvent.click(screen.getByText('Remove'));
+      await waitFor(() => {
+        expect(sdk.space.deleteEntry).toHaveBeenCalledWith(props.entity);
+        expect(props.onRemove).toHaveBeenCalled();
+        expect(sdk.space.unpublishEntry).not.toHaveBeenCalled();
+      });
+    });
   });
 });

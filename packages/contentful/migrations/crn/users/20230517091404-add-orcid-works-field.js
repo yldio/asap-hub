8402c512a5c4ef8b1c5742cc1f7eb97620f0d7ad
Piotr Szpak <piotr.lukasz.szpak@gmail.com>
2023-05-17 10:19:55 +0100

    create migration file

---
+++ packages/contentful/migrations/crn/users/20230517091404-add-orcid-works-field.js
@@ -0,0 +1,22 @@
+module.exports.description = 'Add orcid works field to users content type';
+
+module.exports.up = (migration) => {
+  const users = migration.editContentType('users');
+  users
+    .createField('orcidWorks')
+    .name('Orcid Works')
+    .type('Object')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  users.moveField('orcidWorks').afterField('orcidLastModifiedDate');
+  users.changeFieldControl('orcidWorks', 'app', '2finDNk15g5UtOq4DaLNxv', {});
+};
+
+module.exports.down = (migration) => {
+  const users = migration.editContentType('users');
+  users.deleteField('positions');
+};

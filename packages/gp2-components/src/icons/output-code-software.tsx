356803923a8938f254692a8c664ed61902a81948
jhonny-m <jhonnyporto@hotmail.com>
2023-06-12 11:21:29 +0100

    adding new icons to share output and reorder nav

---
+++ packages/gp2-components/src/icons/output-code-software.tsx
@@ -0,0 +1,25 @@
+/* istanbul ignore file */
+
+const outputCodeSoftwareIcon = (
+  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none">
+    <title>Code/Software</title>
+    <path
+      fill="#4D646B"
+      fillRule="evenodd"
+      d="M7.736 8.744a.65.65 0 0 1-.341.853l-4.856 2.081a.35.35 0 0 0 0 .644l4.856 2.08a.65.65 0 1 1-.512 1.195l-4.856-2.08c-1.333-.572-1.333-2.462 0-3.034l4.856-2.08a.65.65 0 0 1 .853.34Z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="#4D646B"
+      d="M10.44 18a.65.65 0 0 1-.613-.869l3.822-10.7a.65.65 0 1 1 1.224.438l-3.821 10.7a.65.65 0 0 1-.612.431Z"
+    />
+    <path
+      fill="#4D646B"
+      fillRule="evenodd"
+      d="M16.98 8.71a.65.65 0 0 0 .29.871l4.212 2.106a.35.35 0 0 1 0 .626l-4.211 2.106a.65.65 0 0 0 .581 1.162l4.211-2.105c1.217-.608 1.217-2.344 0-2.952l-4.21-2.105a.65.65 0 0 0-.873.29Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+export default outputCodeSoftwareIcon;

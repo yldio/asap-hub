d21617b6e931db5d21e0ea368c2886c8dfa20562
Jhonny Moreira <jhonnyporto@hotmail.com>
2023-06-13 13:08:35 +0100

    tootip to copy emails (#3173)
    * tootip to copy emails
    
    * adding tests

--- packages/gp2-components/src/organisms/EmailSection.tsx
+++ packages/gp2-components/src/organisms/EmailSection.tsx
@@ -1,11 +1,13 @@
 import {
-  Button,
   Link,
   mail,
   pixels,
   Subtitle,
+  Tooltip,
+  secondaryStyles,
 } from '@asap-hub/react-components';
 import { css } from '@emotion/react';
+import { useEffect, useState } from 'react';
 import copyIcon from '../icons/copy-icon';
 
 type EmailSectionProps = {
@@ -27,46 +29,77 @@ const containerStyles = css({
 const itemStyles = css({
   display: 'grid',
   columnGap: rem(16),
-  gridTemplateColumns: `${rem(40)} auto`,
-});
-
-const buttonStyles = css({
-  padding: rem(8),
-  margin: 0,
+  gridTemplateColumns: `1fr ${rem(32)} 1fr`,
   alignItems: 'center',
-  boxSizing: 'border-box',
-  height: rem(40),
-  width: rem(40),
 });
 
-const EmailSection: React.FC<EmailSectionProps> = ({ contactEmails }) => (
-  <div css={containerStyles}>
-    {contactEmails.map(
-      ({ email, contact }) =>
-        email && (
-          <div css={itemStyles} key={contact}>
-            <Button
-              overrideStyles={buttonStyles}
-              onClick={() => navigator.clipboard.writeText(email)}
-            >
-              {copyIcon}
-            </Button>
+const EmailSection: React.FC<EmailSectionProps> = ({ contactEmails }) => {
+  const [tooltipShown, setTooltipShown] = useState<boolean>(false);
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (tooltipShown) {
+      timer = setTimeout(() => {
+        setTooltipShown(false);
+      }, 1000);
+    }
+    return () => timer && clearTimeout(timer);
+  }, [tooltipShown, setTooltipShown]);
+  return (
+    <div css={containerStyles}>
+      {contactEmails.map(
+        ({ email, contact }) =>
+          email && (
             <div
               css={css({
                 display: 'flex',
                 gap: rem(8),
                 flexDirection: 'column',
               })}
+              key={email}
             >
               <Subtitle noMargin accent="lead">
                 {contact}
               </Subtitle>
-              <Link href={createMailTo(email)}>{email}</Link>
+              <div css={itemStyles} key={contact}>
+                <Link href={createMailTo(email)}>{email}</Link>
+                <button
+                  css={[
+                    secondaryStyles,
+
+                    css({
+                      padding: rem(8),
+                      margin: 0,
+                      outline: 'none',
+                      boxSizing: 'border-box',
+                      borderStyle: 'solid',
+                      borderWidth: rem(1),
+                      borderRadius: rem(4),
+                      cursor: 'pointer',
+                    }),
+                  ]}
+                  onClick={() => {
+                    navigator.clipboard.writeText(email);
+                    setTooltipShown(true);
+                  }}
+                >
+                  <Tooltip maxContent bottom={rem(24)} shown={tooltipShown}>
+                    Email copied
+                  </Tooltip>
+                  <div
+                    css={css({
+                      width: rem(16),
+                      height: rem(16),
+                    })}
+                  >
+                    {copyIcon}
+                  </div>
+                </button>
+              </div>
             </div>
-          </div>
-        ),
-    )}
-  </div>
-);
+          ),
+      )}
+    </div>
+  );
+};
 
 export default EmailSection;

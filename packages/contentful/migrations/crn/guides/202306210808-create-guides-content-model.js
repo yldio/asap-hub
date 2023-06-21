7bdc08727f08b7760d7c55146ef4a0cb8c5c9700
Quentin Leonetti <q.leonetti@gmail.com>
2023-06-21 14:40:29 +0200

    CRN-1078 initial commit

---
+++ packages/contentful/migrations/crn/guides/202306210808-create-guides-content-model.js
@@ -0,0 +1,22 @@
+module.exports.description = 'Create external authors content model';
+
+module.exports.up = (migration) => {
+  const guides = migration
+    .createContentType('guides')
+    .name('Guides')
+    .description('')
+    .displayField('name');
+  guides.createField('name')
+    .name('Name')
+    .type('Symbol')
+    .localized(false)
+    .required(true)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+}
+
+module.exports.down = (migration) => {
+    migration.deleteContentType('guides');
+}

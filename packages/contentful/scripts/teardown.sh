d6d92d0443cb8bac4dd15453ea6af164f04a5958
Leonard Martin <leonard.martin@gmail.com>
2023-06-28 13:41:01 +0100

    Add contentful backed integration tests (#3211)
    * Add contentful backed integration tests
    
    * split the integration tests into ./squidex and ./contentful directories and add yarn test:integration:<cms> commands to run one or the other
    * add contentful helper to automatically teardown data that was added as part of a test run
    * add option to retry with a timeout when creating new contentful environments
    * add basic integration tests for user endpoints
    * add contentful integration test runner to test pipeline
    
    Use async-retry library for retrying
    
    * Share integration tests between Contentful and Squidex

---
+++ packages/contentful/scripts/teardown.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+if [[ "$CONTENTFUL_ENV_ID" == "Production" ]]; then
+  echo "Target is Production branch; not deleting."
+  exit 0
+fi
+
+if [[ "$CONTENTFUL_ENV_ID" == "Development" ]]; then
+  echo "Target is Development branch; not deleting."
+  exit 0
+fi
+
+echo "Contentful env to be deleted is ${CONTENTFUL_ENV_ID}."
+
+STATUS_CODE_CHECK=$(curl --silent \
+                         --request GET \
+                         --output /dev/null \
+                         --write-out "%{http_code}" \
+                         --header "Authorization: Bearer ${CONTENTFUL_MANAGEMENT_ACCESS_TOKEN}" \
+                         https://api.contentful.com/spaces/${CONTENTFUL_SPACE_ID}/environments/${CONTENTFUL_ENV_ID})
+
+if [[ "$STATUS_CODE_CHECK" == "404" ]]; then
+  echo "Environment for this branch not found -- nothing to delete."
+  exit 0
+fi
+
+STATUS_CODE=$(curl --silent \
+                   --request DELETE \
+                   --output /dev/null \
+                   --write-out "%{http_code}" \
+                   --header "Authorization: Bearer ${CONTENTFUL_MANAGEMENT_ACCESS_TOKEN}" \
+                   https://api.contentful.com/spaces/${CONTENTFUL_SPACE_ID}/environments/${CONTENTFUL_ENV_ID})
+
+if [[ "$STATUS_CODE" == "204" ]]; then
+  echo "Successfully requested deletion of Environment ${CONTENTFUL_ENV_ID}."
+  exit 0
+else
+  echo "Failed to request deletion of Environment ${CONTENTFUL_ENV_ID}."
+  echo "Status code was ${STATUS_CODE}."
+  exit 1
+fi

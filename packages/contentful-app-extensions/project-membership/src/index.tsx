c41bca57a2b164887fd8acbe09d7b79a2e28b550
Mariana <marianafrazaosaraiva@gmail.com>
2023-07-06 15:12:59 +0100

    add contentful app for project membership card

---
+++ packages/contentful-app-extensions/project-membership/src/index.tsx
@@ -0,0 +1,15 @@
+import { GlobalStyles } from '@contentful/f36-components';
+import { SDKProvider } from '@contentful/react-apps-toolkit';
+
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
+root.render(
+  <SDKProvider>
+    <GlobalStyles />
+    <App />
+  </SDKProvider>,
+);
---
+++ packages/contentful-app-extensions/project-membership/src/locations/Field.tsx
@@ -0,0 +1,136 @@
+import React from 'react';
+import {
+  MultipleEntryReferenceEditor,
+  CustomEntityCardProps,
+  useEntity,
+} from '@contentful/field-editor-reference';
+import { entityHelpers } from '@contentful/field-editor-shared';
+import {
+  EntryCard,
+  MenuItem,
+  Heading,
+  Paragraph,
+  Text,
+  EntityStatus,
+  EntryCardProps,
+} from '@contentful/f36-components';
+import { FieldExtensionSDK, Entry } from '@contentful/app-sdk';
+import { useSDK, useAutoResizer } from '@contentful/react-apps-toolkit';
+
+type CustomEntryCardProps = CustomEntityCardProps & { entity: Entry };
+type CardProps = {
+  actions: EntryCardProps['actions'];
+  onClick: CustomEntityCardProps['onEdit'];
+  status: EntityStatus;
+};
+type TeamCardProps = {
+  id: string;
+  role: string;
+  email: string;
+} & CardProps;
+
+const MemberCard = ({
+  id,
+  actions,
+  email,
+  role,
+  onClick,
+  status,
+}: TeamCardProps) => {
+  const { data: user } = useEntity<Entry>('Entry', id);
+  if (!user) {
+    return <EntryCard isLoading />;
+  }
+  return (
+    <EntryCard
+      contentType="Member"
+      actions={actions}
+      onClick={onClick}
+      status={status}
+    >
+      <Heading marginBottom="none">
+        {user.fields.firstName?.['en-US']} {user.fields.lastName?.['en-US']}
+      </Heading>
+      <Paragraph marginBottom="none">{email}</Paragraph>
+      <Paragraph marginBottom="none">{role}</Paragraph>
+    </EntryCard>
+  );
+};
+
+const MissingMemberCard = ({ actions, onClick, status }: CardProps) => (
+  <EntryCard
+    contentType="Member"
+    actions={actions}
+    onClick={onClick}
+    status={status}
+  >
+    <Text fontColor="colorNegative">No member selected</Text>
+  </EntryCard>
+);
+
+const Card = ({ entity, onEdit, onRemove }: CustomEntryCardProps) => {
+  const sdk = useSDK<FieldExtensionSDK>();
+  const { fields, sys } = entity;
+  const userId = fields.user?.['en-US'].sys.id;
+  const role = fields.role?.['en-US'];
+  const email = fields.user?.['en-US'].fields.email;
+  const removeMembership = async () => {
+    if (sys.publishedVersion) {
+      await sdk.space.unpublishEntry(entity);
+    }
+    await sdk.space.deleteEntry(entity);
+    if (onRemove) {
+      onRemove();
+    }
+  };
+
+  const status = entityHelpers.getEntryStatus(sys);
+
+  const defaultProps = {
+    contentType: 'user',
+    actions: [
+      <MenuItem key="remove" onClick={removeMembership}>
+        Remove
+      </MenuItem>,
+    ],
+    status,
+    onClick: onEdit,
+  };
+
+  return userId ? (
+    <MemberCard {...defaultProps} id={userId} role={role} email={email} />
+  ) : (
+    <MissingMemberCard {...defaultProps} />
+  );
+};
+
+export const CustomCard = ({ entity, ...props }: CustomEntityCardProps) => (
+  <Card {...props} entity={entity as Entry} />
+);
+
+const Field = () => {
+  const sdk = useSDK<FieldExtensionSDK>();
+  useAutoResizer();
+
+  return (
+    <MultipleEntryReferenceEditor
+      isInitiallyDisabled={false}
+      hasCardEditActions={true}
+      renderCustomCard={CustomCard}
+      parameters={{
+        instance: {
+          showLinkEntityAction: false,
+          showCreateEntityAction: true,
+          bulkEditing: false,
+        },
+      }}
+      viewType="link"
+      sdk={sdk}
+      actionLabels={{
+        createNew: () => 'Add',
+      }}
+    />
+  );
+};
+
+export default Field;

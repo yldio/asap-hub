dc721240d0a440ba3cca482f8e974f80a28749ee
AkosuaA <nattuah@gmail.com>
2023-07-06 13:44:44 +0100

    GP2-518 - add request new project button

--- packages/gp2-components/src/organisms/ProjectsHeader.tsx
+++ packages/gp2-components/src/organisms/ProjectsHeader.tsx
@@ -1,14 +1,28 @@
 import { projectsImage } from '../images';
 import PageBanner from './PageBanner';
+import { Link, externalLinkIcon } from '@asap-hub/react-components';
 
 const props = {
   image: projectsImage,
   position: 'center',
-  title: 'Project Directory',
+  title: 'Projects',
   description:
-    'Explore past and present GP2 projects and discover open opportunities to join them.',
+    'Explore GP2 projects being carried out by the network. If you are interested in starting a new project, please request a new project below.',
 };
 
-const ProjectsHeader: React.FC = () => <PageBanner {...props} />;
+const ProjectsHeader: React.FC = () => (
+  <PageBanner {...props}>
+    <Link
+      href="https://docs.google.com/forms/d/e/1FAIpQLScYnKgzk-gxFW6a8CgEkwowjCnWGdWqLxwF9YWacYHMnSaPzg/viewform"
+      label="Request New Project"
+      noMargin
+      primary
+      buttonStyle
+    >
+      Request New Project
+      <span>{externalLinkIcon}</span>
+    </Link>
+  </PageBanner>
+);
 
 export default ProjectsHeader;

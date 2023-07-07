5e8297bf6fc7d8c19b33942e1245b84c76892c22
jhonny-m <jhonnyporto@hotmail.com>
2023-07-07 10:49:54 +0100

    adding coverage

--- packages/fixtures/src/gp2/news.ts
+++ packages/fixtures/src/gp2/news.ts
@@ -1,6 +1,6 @@
 import { gp2 } from '@asap-hub/model';
 
-const mockedNews: gp2.NewsResponse = {
+export const mockedNews: gp2.NewsResponse = {
   created: '2023-02-02T14:38:42.000Z',
   id: '9bce95a3-b5d7-436d-81ed-297089a1b7a3',
   link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',

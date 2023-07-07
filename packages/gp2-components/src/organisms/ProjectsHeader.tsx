091a1e0cf5506655ee100baa994972443a1506c3
AkosuaA <nattuah@gmail.com>
2023-07-07 15:25:25 +0100

    modify css

--- packages/gp2-components/src/organisms/ProjectsHeader.tsx
+++ packages/gp2-components/src/organisms/ProjectsHeader.tsx
@@ -1,7 +1,9 @@
-import { Link, externalLinkIcon } from '@asap-hub/react-components';
+import { Link, externalLinkIcon, pixels } from '@asap-hub/react-components';
+import { css } from '@emotion/react';
 import { projectsImage } from '../images';
 import PageBanner from './PageBanner';
 
+const { perRem, mobileScreen } = pixels;
 const props = {
   image: projectsImage,
   position: 'center',
@@ -10,18 +12,32 @@ const props = {
     'Explore GP2 projects being carried out by the network. If you are interested in starting a new project, please request a new project below.',
 };
 
+const buttonStyle = css({
+  [`@media (max-width: ${mobileScreen.max}px)`]: {
+    width: '72%',
+  },
+});
+
+const externalIconStyle = css({
+  display: 'flex',
+  alignSelf: 'center',
+  marginLeft: `${8 / perRem}em`,
+});
+
 const ProjectsHeader: React.FC = () => (
   <PageBanner {...props}>
-    <Link
-      href="https://docs.google.com/forms/d/e/1FAIpQLScYnKgzk-gxFW6a8CgEkwowjCnWGdWqLxwF9YWacYHMnSaPzg/viewform"
-      label="Request New Project"
-      primary
-      buttonStyle
-      small
-    >
-      Request New Project
-      <span>{externalLinkIcon}</span>
-    </Link>
+    <div css={buttonStyle}>
+      <Link
+        href="https://docs.google.com/forms/d/e/1FAIpQLScYnKgzk-gxFW6a8CgEkwowjCnWGdWqLxwF9YWacYHMnSaPzg/viewform"
+        label="Request New Project"
+        primary
+        buttonStyle
+        small
+      >
+        Request New Project
+        <span css={externalIconStyle}>{externalLinkIcon}</span>
+      </Link>
+    </div>
   </PageBanner>
 );
 

c50a325ddbe06ee75524a28f7ea7f67485d6011d
AkosuaA <nattuah@gmail.com>
2023-07-10 11:20:06 +0100

    GP2-518 - add request new project button (#3275)
    * GP2-518 - add request new project button
    
    * add margin
    
    * fix import order
    
    * reduce button padding
    
    * modify css

--- packages/gp2-components/src/organisms/ProjectsHeader.tsx
+++ packages/gp2-components/src/organisms/ProjectsHeader.tsx
@@ -1,14 +1,44 @@
+import { Link, externalLinkIcon, pixels } from '@asap-hub/react-components';
+import { css } from '@emotion/react';
 import { projectsImage } from '../images';
 import PageBanner from './PageBanner';
 
+const { perRem, mobileScreen } = pixels;
 const props = {
   image: projectsImage,
   position: 'center',
-  title: 'Project Directory',
+  title: 'Projects',
   description:
-    'Explore past and present GP2 projects and discover open opportunities to join them.',
+    'Explore GP2 projects being carried out by the network. If you are interested in starting a new project, please request a new project below.',
 };
 
-const ProjectsHeader: React.FC = () => <PageBanner {...props} />;
+const buttonStyle = css({
+  [`@media (max-width: ${mobileScreen.max}px)`]: {
+    width: '72%',
+  },
+});
+
+const externalIconStyle = css({
+  display: 'flex',
+  alignSelf: 'center',
+  marginLeft: `${8 / perRem}em`,
+});
+
+const ProjectsHeader: React.FC = () => (
+  <PageBanner {...props}>
+    <div css={buttonStyle}>
+      <Link
+        href="https://docs.google.com/forms/d/e/1FAIpQLScYnKgzk-gxFW6a8CgEkwowjCnWGdWqLxwF9YWacYHMnSaPzg/viewform"
+        label="Request New Project"
+        primary
+        buttonStyle
+        small
+      >
+        Request New Project
+        <span css={externalIconStyle}>{externalLinkIcon}</span>
+      </Link>
+    </div>
+  </PageBanner>
+);
 
 export default ProjectsHeader;

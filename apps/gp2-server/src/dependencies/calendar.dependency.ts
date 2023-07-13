e51bbf236a6427f3f921ff1929a992ed474590d4
Mark Sims <mark@bluepie.co.uk>
2023-07-13 15:43:46 +0100

    updates dependancies

--- apps/gp2-server/src/dependencies/calendar.dependency.ts
+++ apps/gp2-server/src/dependencies/calendar.dependency.ts
@@ -1,22 +1,15 @@
-import { getGraphQLClient } from '@asap-hub/contentful';
+import { GraphQLClient } from '@asap-hub/contentful';
 import { gp2 as gp2Model } from '@asap-hub/model';
-import {
-  contentfulAccessToken,
-  contentfulEnvId,
-  contentfulSpaceId,
-} from '../config';
 import { CalendarContentfulDataProvider } from '../data-providers/calendar.data-provider';
-import { getContentfulRestClientFactory } from './clients.dependency';
+import {
+  getContentfulGraphQLClientFactory,
+  getContentfulRestClientFactory,
+} from './clients.dependency';
 
-export const getCalendarDataProvider = (): gp2Model.CalendarDataProvider => {
-  const contentfulGraphQLClient = getGraphQLClient({
-    space: contentfulSpaceId,
-    accessToken: contentfulAccessToken,
-    environment: contentfulEnvId,
-  });
-
-  return new CalendarContentfulDataProvider(
+export const getCalendarDataProvider = (
+  contentfulGraphQLClient: GraphQLClient = getContentfulGraphQLClientFactory(),
+): gp2Model.CalendarDataProvider =>
+  new CalendarContentfulDataProvider(
     contentfulGraphQLClient,
     getContentfulRestClientFactory,
   );
-};

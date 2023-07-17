73bcd78d995b26605d40d9d13e232909092e3fc6
AkosuaA <nattuah@gmail.com>
2023-07-17 17:51:58 +0100

    modify route

--- apps/gp2-server/src/controllers/news.controller.ts
+++ apps/gp2-server/src/controllers/news.controller.ts
@@ -1,10 +1,10 @@
-import { gp2 } from '@asap-hub/model';
+import { gp2, FetchNewsOptions } from '@asap-hub/model';
 import { NewsDataProvider } from '../data-providers/types';
 
 export default class NewsController {
   constructor(private newsDataProvider: NewsDataProvider) {}
 
-  async fetch(options: gp2.FetchNewsOptions): Promise<gp2.ListNewsResponse> {
+  async fetch(options?: FetchNewsOptions): Promise<gp2.ListNewsResponse> {
     const { search, filter, ...paginationOptions } = options || {};
 
     const { total, items } = await this.newsDataProvider.fetch({
--- apps/gp2-server/src/routes/news.route.ts
+++ apps/gp2-server/src/routes/news.route.ts
@@ -1,11 +1,14 @@
 import { gp2 } from '@asap-hub/model';
 import { Router } from 'express';
 import NewsController from '../controllers/news.controller';
+import {
+  validateNewsFetchParameters,
+} from '../validation/news.validation';
 
 export const newsRouteFactory = (newsController: NewsController): Router => {
   const newsRoutes = Router();
 
-  newsRoutes.get<unknown, gp2.ListNewsResponse>('/news', async (_req, res) => {
+  newsRoutes.get<unknown, gp2.ListNewsResponse>('/news', async (req, res) => {
     const { query } = req;
 
     const options = validateNewsFetchParameters(query);

e95bf92e0f9921cc5355002b034a67a1f1b0f3ae
Quentin Leonetti <q.leonetti@gmail.com>
2023-07-18 16:45:29 +0200

    CRN-1441 add missing test

--- packages/react-components/src/atoms/__tests__/Icon.test.tsx
+++ packages/react-components/src/atoms/__tests__/Icon.test.tsx
@@ -1,8 +1,13 @@
-import { imageStyle } from '../Icon';
+import { render } from '@testing-library/react';
+import Icon, { imageStyle } from '../Icon';
 
 describe('Icon', () => {
+  it('has the proper css', () => {
+    const { container } = render(<Icon url="test" />);
+    expect(container.firstChild).toHaveAttribute('class');
+  });
   describe('imageStyle', () => {
-    it('returns the proper css', () => {
+    it('generate the proper css', () => {
       const style = imageStyle('test');
       expect(style.styles).toContain('background-image:url("test")');
     });

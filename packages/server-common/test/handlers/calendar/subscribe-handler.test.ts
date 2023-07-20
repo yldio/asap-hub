32d246e308cc7bd8193af40ad8f3dd88b1c3fea5
Mark Sims <mark@bluepie.co.uk>
2023-07-20 14:00:38 +0100

    GP2 530 invalidate calendar on change calendar id limit (#3352)
    * sets reserved concurrency for calendar subscriber to 1
    
    * allows resourceId to be set to null
    
    * tries to subscribe if there is no resourceId
    
    * stops retries if calendar is not found
    
    * refactors type check

--- packages/server-common/test/handlers/calendar/subscribe-handler.test.ts
+++ packages/server-common/test/handlers/calendar/subscribe-handler.test.ts
@@ -19,6 +19,87 @@ describe('Subscription', () => {
     cms             | address
     ${'squidex'}    | ${'webhook/events'}
     ${'contentful'} | ${'webhook/events/contentful'}
+  `('404', async ({ address, cms }) => {
+    getJWTCredentials.mockResolvedValueOnce(googleApiAuthJWTCredentials);
+
+    nock(googleApiUrl)
+      .post('/oauth2/v4/token')
+      .reply(404, {
+        access_token: '1/8xbJqaOZXSUZbHLl5EOtu1pxz3fmmetKx9W8CV4t79M',
+        scope: `${googleApiUrl}auth/prediction`,
+        token_type: 'Bearer',
+        expires_in: 3600,
+      })
+      .post(`/calendar/v3/calendars/${calendarId}/events/watch`, {
+        id: getCalendarCreateEvent().payload.id,
+        token: googleApiToken,
+        type: 'web_hook',
+        address: `${asapApiUrl}/${address}`,
+        params: {
+          // 30 days, which is a maximum TTL
+          ttl: 2592000,
+        },
+      });
+
+    const subscribeToEventChanges = subscribeToEventChangesFactory(
+      getJWTCredentials,
+      logger,
+      { googleApiUrl, googleApiToken, asapApiUrl, cms },
+    );
+
+    const result = await subscribeToEventChanges(
+      calendarId,
+      getCalendarCreateEvent().payload.id,
+    );
+
+    expect(result).toEqual({
+      resourceId: null,
+      expiration: null,
+    });
+    expect(nock.isDone()).toBe(true);
+  });
+  test.each`
+    cms             | address
+    ${'squidex'}    | ${'webhook/events'}
+    ${'contentful'} | ${'webhook/events/contentful'}
+  `('500', async ({ address, cms }) => {
+    getJWTCredentials.mockResolvedValueOnce(googleApiAuthJWTCredentials);
+
+    nock(googleApiUrl)
+      .post('/oauth2/v4/token')
+      .reply(500, {
+        access_token: '1/8xbJqaOZXSUZbHLl5EOtu1pxz3fmmetKx9W8CV4t79M',
+        scope: `${googleApiUrl}auth/prediction`,
+        token_type: 'Bearer',
+        expires_in: 3600,
+      })
+      .post(`/calendar/v3/calendars/${calendarId}/events/watch`, {
+        id: getCalendarCreateEvent().payload.id,
+        token: googleApiToken,
+        type: 'web_hook',
+        address: `${asapApiUrl}/${address}`,
+        params: {
+          // 30 days, which is a maximum TTL
+          ttl: 2592000,
+        },
+      });
+
+    const subscribeToEventChanges = subscribeToEventChangesFactory(
+      getJWTCredentials,
+      logger,
+      { googleApiUrl, googleApiToken, asapApiUrl, cms },
+    );
+
+    await expect(
+      subscribeToEventChanges(calendarId, getCalendarCreateEvent().payload.id),
+    ).rejects.toThrow();
+
+    expect(nock.isDone()).toBe(true);
+  });
+  test.each`
+    cms             | address
+    ${'squidex'}    | ${'webhook/events'}
+    ${'contentful'} | ${'webhook/events/contentful'}
   `(
     'Should subscribe to the calendar events notifications and return the resourceId when cms is $cms',
     async ({ address, cms }) => {
@@ -99,10 +180,6 @@ describe('Unsubscribing', () => {
       .reply(200, {});
 
     await unsubscribeFromEventChanges(resourceId, channelId);
-
-    if (!nock.isDone()) {
-      console.error('pending mocks: %j', nock.pendingMocks());
-    }
 
     expect(nock.isDone()).toBe(true);
   });

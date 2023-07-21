e0279f467ff606c714e442351163bd1dd80ee8a9
Mariana <marianafrazaosaraiva@gmail.com>
2023-07-21 10:40:32 +0100

    comment unfinshed work

--- apps/gp2-frontend/src/outputs/OutputDirectory.tsx
+++ apps/gp2-frontend/src/outputs/OutputDirectory.tsx
@@ -1,24 +1,24 @@
 import { OutputsPageList } from '@asap-hub/gp2-components';
 import Frame from '../Frame';
-import { useSearch } from '../hooks/search';
+// import { useSearch } from '../hooks/search';
 // import OutputList from './OutputList';
 
 const OutputDirectory = () => {
   // use general search that is being built for news filter
-  const {
-    filters,
-    searchQuery,
-    setSearchQuery,
-    // debouncedSearchQuery,
-    updateFilters,
-  } = useSearch();
+  // const {
+  //   filters,
+  //   searchQuery,
+  //   setSearchQuery,
+  //   debouncedSearchQuery,
+  //   updateFilters,
+  // } = useSearch();
 
   return (
     <OutputsPageList
-      searchQuery={searchQuery}
-      onSearchQueryChange={setSearchQuery}
-      updateFilters={updateFilters}
-      filters={filters}
+    // searchQuery={searchQuery}
+    // onSearchQueryChange={setSearchQuery}
+    // updateFilters={updateFilters}
+    // filters={filters}
     >
       <Frame title="Output List">
         {/* <OutputList searchQuery={debouncedSearchQuery} filters={filters} /> */}

43f19138f39891236e293a95c9e4728de346eda3
Leonard Martin <leonard.martin@gmail.com>
2023-08-15 12:30:08 +0100

    Allow `--upsert` flag to be passed into migration script
    This enables upsert mode when running locally, but is prevented by the flg validation. Allow it.

--- packages/cms-data-sync/src/index.ts
+++ packages/cms-data-sync/src/index.ts
@@ -3,8 +3,13 @@ import { runMigrations, models, Flag } from './run-migrations';
 
 const flags = process.argv.slice(2);
 
+const allowedFlags = [
+  '--upsert',
+  ...models.map((model: string) => `--${model}`),
+];
+
 flags.forEach((flag: string) => {
-  if (!models.some((model: string) => flag === `--${model}`)) {
+  if (!allowedFlags.includes(flag)) {
     // eslint-disable-next-line no-console
     console.error(`Unrecognised flag: ${flag}`);
     process.exit(1);

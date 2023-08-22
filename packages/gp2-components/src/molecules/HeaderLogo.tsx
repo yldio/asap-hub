a5f4eea684c70fb78d36ed32719e515f483f4cba
Mariana <marianafrazaosaraiva@gmail.com>
2023-08-22 10:42:35 +0100

    update logo

--- packages/gp2-components/src/molecules/HeaderLogo.tsx
+++ packages/gp2-components/src/molecules/HeaderLogo.tsx
@@ -1,6 +1,7 @@
 import { Anchor, pixels } from '@asap-hub/react-components';
 import { css } from '@emotion/react';
-import gp2LogoSmall from '../icons/gp2-logo-small';
+import { gp2LogoFull } from '../icons';
+// import gp2LogoSmall from '../icons/gp2-logo-small';
 
 const { rem } = pixels;
 
@@ -15,7 +16,7 @@ type HeaderLogoProps = {
 
 const HeaderLogo: React.FC<HeaderLogoProps> = ({ logoHref = '/' }) => (
   <div css={containerStyles}>
-    <Anchor href={logoHref}>{gp2LogoSmall}</Anchor>
+    <Anchor href={logoHref}>{gp2LogoFull}</Anchor>
   </div>
 );
 

1363470243ace8b4223ddd1a5544a4ade9f650a6
AkosuaA <nattuah@gmail.com>
2023-08-22 11:44:13 +0100

    GP2HUB-74 gp2 google tag manager (#3570)
    * GP2HUB-74 - add gp2 google tag manager
    
    * GP2HUB-74 - update crn gtm config
    
    * GP2HUB-74 - update crn gtm id name

--- apps/gp2-frontend/src/config.ts
+++ apps/gp2-frontend/src/config.ts
@@ -1,4 +1,4 @@
-export const GTM_CONTAINER_ID = process.env.REACT_APP_GTM_CONTAINER_ID;
+export const GTM_CONTAINER_ID = process.env.REACT_APP_GP2_GTM_CONTAINER_ID;
 export const API_BASE_URL =
   process.env.REACT_APP_GP2_API_BASE_URL || 'http://localhost:4444';
 export const ALGOLIA_APP_ID =

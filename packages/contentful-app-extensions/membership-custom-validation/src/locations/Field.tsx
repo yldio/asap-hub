75415c46595406b816947acf56c1a2c0165bd0e9
AkosuaA <nattuah@gmail.com>
2023-08-25 15:39:08 +0100

    GP2HUB-51-add working groups membership validation

---
+++ packages/contentful-app-extensions/membership-custom-validation/src/locations/Field.tsx
@@ -0,0 +1,107 @@
+import { Entry, FieldAppSDK } from '@contentful/app-sdk';
+import { Note, Stack } from '@contentful/f36-components';
+import { useSDK, useAutoResizer } from '@contentful/react-apps-toolkit';
+import React, { useEffect, useState } from 'react';
+import { getEntry, onEntryChanged } from '../utils';
+
+export const DUPLICATE_MEMBERS_MESSAGE =
+  'The same member relation has been added multiple times';
+const hasDuplicates = (array: string[]) => {
+  const uniqueItems = new Set(array);
+  return uniqueItems.size !== array.length;
+};
+
+const Field = () => {
+  useAutoResizer();
+
+  const sdk = useSDK<FieldAppSDK>();
+
+  const [warnings, setWarnings] = useState<string[]>([]);
+
+  const getValidations = async () => {
+    const entry = getEntry(sdk);
+
+    if (!entry.fields.members || !entry.fields.members.length) {
+      return null;
+    }
+
+    const memberIds = entry.fields.members.map((e: Entry) => e.sys.id);
+
+    const entries = await sdk.cma.entry.getMany({
+      query: {
+        'sys.id[in]': memberIds,
+      },
+    });
+
+    const groupedUsers = entries.items.reduce(
+      (groups: { [key: string]: string[] }, membership) => {
+        const userId = membership.fields?.user?.['en-US']?.sys?.id;
+        const role = membership.fields?.role?.['en-US'];
+        if (!userId) return groups;
+        if (!groups[userId]) {
+          // eslint-disable-next-line no-param-reassign
+          groups[userId] = [];
+        }
+        groups[userId].push(role);
+        return groups;
+      },
+      {},
+    );
+
+    const validationMessages = await Promise.all(
+      Object.entries(groupedUsers).map(async ([userId, duplicateRoles]) => {
+        if (duplicateRoles?.length > 1) {
+          const userEntry = await sdk.cma.entry.get({
+            entryId: userId,
+          });
+          const roles = duplicateRoles.join(', ');
+          return `User ${userEntry.fields.firstName['en-US']} ${userEntry.fields.lastName['en-US']} has been added in multiple roles [${roles}].`;
+        }
+        return null;
+      }),
+    );
+
+    return {
+      duplicateMembers: hasDuplicates(memberIds),
+      duplicateUserMessages: validationMessages.filter(
+        (x) => x !== null,
+      ) as string[],
+    };
+  };
+
+  useEffect(
+    () =>
+      onEntryChanged(sdk, async () => {
+        const newWarnings: string[] = [];
+
+        const validationMessages = await getValidations();
+        if (validationMessages?.duplicateMembers) {
+          newWarnings.push(DUPLICATE_MEMBERS_MESSAGE);
+        }
+
+        if (validationMessages?.duplicateUserMessages?.length) {
+          validationMessages.duplicateUserMessages.forEach((message) => {
+            newWarnings.push(message);
+          });
+        }
+
+        setWarnings(newWarnings);
+        sdk.field.setValue(newWarnings.length === 0 ? 'true' : 'false');
+      }),
+    [sdk],
+  );
+
+  return (
+    <Stack flexDirection="column" alignItems="flex-start">
+      {warnings.length > 0
+        ? warnings.map((warning, index) => (
+            <Note key={index} variant="warning">
+              {warning}
+            </Note>
+          ))
+        : null}
+    </Stack>
+  );
+};
+
+export default Field;
---
+++ packages/contentful-app-extensions/membership-custom-validation/src/test/locations/Field.test.tsx
@@ -0,0 +1,159 @@
+import { FieldExtensionSDK } from '@contentful/app-sdk';
+import { useSDK, useAutoResizer } from '@contentful/react-apps-toolkit';
+import '@testing-library/jest-dom';
+import { render, screen, waitFor } from '@testing-library/react';
+import React from 'react';
+
+import Field from '../../locations/Field';
+
+jest.mock('@contentful/react-apps-toolkit', () => ({
+  useSDK: jest.fn(),
+  useAutoResizer: jest.fn(),
+}));
+
+const memberFields = {
+  role: {
+    'en-US': 'Project Manager',
+  },
+  user: {
+    'en-US': {
+      sys: {
+        id: 'user-1',
+      },
+    },
+  },
+};
+
+const unsubscribeFn = () => {};
+const mockBaseSdk = () => ({
+  field: {
+    setValue: jest.fn(),
+  },
+  entry: {
+    getSys: jest.fn(),
+    getMetadata: jest.fn(),
+    fields: {
+      members: {
+        id: 'members',
+        onValueChanged: jest.fn(() => unsubscribeFn),
+        getValue: jest.fn(() => [
+          {
+            sys: {
+              type: 'Link',
+              linkType: 'Entry',
+              id: 'member-link-1',
+            },
+          },
+        ]),
+      },
+    },
+    onMetadataChanged: jest.fn(() => unsubscribeFn),
+    onSysChanged: jest.fn((cb) => {
+      cb(() => ({
+        id: '456',
+        space: {
+          sys: {
+            id: '123',
+          },
+        },
+        firstPublishedAt: '2023-04-14T18:00:00.000Z',
+      }));
+      return jest.fn(() => unsubscribeFn);
+    }),
+  },
+  cma: {
+    entry: {
+      getMany: jest.fn(() =>
+        Promise.resolve({
+          items: [
+            {
+              fields: memberFields,
+            },
+          ],
+        }),
+      ),
+      get: jest.fn(() =>
+        Promise.resolve({
+          sys: {
+            contentType: {
+              sys: {
+                id: 'users',
+              },
+            },
+          },
+        }),
+      ),
+    },
+  },
+});
+
+describe('Field component', () => {
+  let sdk: jest.Mocked<FieldExtensionSDK>;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    sdk = mockBaseSdk() as unknown as jest.Mocked<FieldExtensionSDK>;
+    (useSDK as jest.Mock).mockReturnValue(sdk);
+  });
+
+  it('enables automatic resizing', async () => {
+    render(<Field />);
+    await waitFor(() => {
+      expect(useAutoResizer).toHaveBeenCalled();
+    });
+  });
+
+  it('displays warning message when a user is added multiple types', async () => {
+    const baseSdk = mockBaseSdk();
+    const testSdk = {
+      ...baseSdk,
+      cma: {
+        entry: {
+          getMany: jest.fn((param) =>
+            Promise.resolve({
+              items: [
+                {
+                  fields: memberFields,
+                },
+                {
+                  fields: memberFields,
+                },
+              ],
+            }),
+          ),
+          get: jest.fn((param) => {
+            return Promise.resolve({
+              sys: {
+                contentType: {
+                  sys: {
+                    id: 'users',
+                  },
+                },
+              },
+              fields: {
+                firstName: {
+                  'en-US': 'John',
+                },
+                lastName: {
+                  'en-US': 'Doe',
+                },
+              },
+            });
+          }),
+        },
+      },
+    } as unknown as jest.Mocked<FieldExtensionSDK>;
+    (useSDK as jest.Mock).mockReturnValue(testSdk);
+
+    render(<Field />);
+    await waitFor(() => {
+      expect(testSdk.field.setValue).toHaveBeenCalledWith('false');
+    });
+    expect(
+      await screen.findByText(
+        'User John Doe has been added in multiple roles [Project Manager, Project Manager].',
+      ),
+    ).toBeInTheDocument();
+  });
+});

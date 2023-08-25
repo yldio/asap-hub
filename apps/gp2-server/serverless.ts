6712ab34f2f7ce2eacc81ae28b374452cb8d8335
Mark Sims <mark@bluepie.co.uk>
2023-08-25 15:05:59 +0100

    adds handler for indexing users into algolia

--- apps/gp2-server/serverless.ts
+++ apps/gp2-server/serverless.ts
@@ -384,6 +384,30 @@ const serverlessConfig: AWS = {
         SENTRY_DSN: sentryDsnHandlers,
       },
     },
+    algoliaIndexUser: {
+      handler: './src/handlers/event/algolia-index-user-handler.handler',
+      events: [
+        {
+          eventBridge: {
+            eventBus,
+            pattern: {
+              source: [eventBusSource],
+              'detail-type': [
+                'UsersPublished',
+                'UsersUpdated',
+                'UsersUnpublished',
+                'UsersDeleted',
+              ] satisfies gp2.WebhookDetailType[],
+            },
+          },
+        },
+      ],
+      environment: {
+        ALGOLIA_API_KEY: `\${ssm:gp2-algolia-index-api-key-${envAlias}}`,
+        ALGOLIA_INDEX: `${algoliaIndex}`,
+        SENTRY_DSN: sentryDsnHandlers,
+      },
+    },
     contentfulWebhook: {
       handler: './src/handlers/webhooks/contentful.handler',
       events: [

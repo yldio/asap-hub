e1f14a25367f7c27dd45dd2d98ff743585b85d18
Mark Sims <mark@bluepie.co.uk>
2023-08-31 07:12:45 +0100

    sync news in github actions

--- apps/gp2-server/scripts/cli.ts
+++ apps/gp2-server/scripts/cli.ts
@@ -13,7 +13,7 @@ yargs(hideBin(process.argv))
         .positional('entity', {
           describe: 'specific an entity to import',
           type: 'string',
-          choices: ['output', 'project', 'event', 'user'],
+          choices: ['output', 'project', 'event', 'user', 'news'],
           demandOption: true,
         })
         .option('filename', {
@@ -22,7 +22,10 @@ yargs(hideBin(process.argv))
           description: 'The output file name',
         }),
     handler: async ({ entity, filename }) =>
-      exportEntity(entity as 'output' | 'project' | 'event' | 'user', filename),
+      exportEntity(
+        entity as 'output' | 'project' | 'event' | 'user' | 'news',
+        filename,
+      ),
   })
   .demandCommand(1)
   .help('h')

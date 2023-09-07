ee8d5a3bdded396a689d918876105fb1af71f52b
Thomas Waterfall <thomas.waterfall@gmail.com>
2023-09-07 14:56:02 +0100

    Tests

--- packages/react-components/src/atoms/ScrollToTop.tsx
+++ packages/react-components/src/atoms/ScrollToTop.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-export const ScrollToTop = () => {
+const ScrollToTop: React.FC = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {

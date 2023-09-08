257fa76367476368b055e6260b816eac1ed99b1d
Leonard Martin <leonard.martin@gmail.com>
2023-09-08 10:52:55 +0100

    Remove Squidex options from dependency resolvers
    It might be desired to remove these completly at some point, but in the interests of making small, self-contained changes, leave them and only resolve to Contentful.

--- apps/crn-server/src/dependencies/external-authors.dependencies.ts
+++ apps/crn-server/src/dependencies/external-authors.dependencies.ts
@@ -1,54 +1,22 @@
 import { getGraphQLClient as getContentfulGraphQLClient } from '@asap-hub/contentful';
 import {
-  RestExternalAuthor,
-  SquidexGraphql,
-  SquidexRest,
-} from '@asap-hub/squidex';
-import {
-  appName,
-  baseUrl,
   contentfulAccessToken,
   contentfulEnvId,
   contentfulSpaceId,
-  isContentfulEnabled,
 } from '../config';
 import { ExternalAuthorContentfulDataProvider } from '../data-providers/contentful/external-author.data-provider';
-import {
-  ExternalAuthorDataProvider,
-  ExternalAuthorSquidexDataProvider,
-} from '../data-providers/external-author.data-provider';
-import { getAuthToken } from '../utils/auth';
+import { ExternalAuthorDataProvider } from '../data-providers/external-author.data-provider';
 import { getContentfulRestClientFactory } from './clients.dependencies';
 
 export const getExternalAuthorDataProvider = (): ExternalAuthorDataProvider => {
-  if (isContentfulEnabled) {
-    const contentfulGraphQLClient = getContentfulGraphQLClient({
-      space: contentfulSpaceId,
-      accessToken: contentfulAccessToken,
-      environment: contentfulEnvId,
-    });
-
-    return new ExternalAuthorContentfulDataProvider(
-      contentfulGraphQLClient,
-      getContentfulRestClientFactory,
-    );
-  }
-
-  const externalAuthorRestClient = new SquidexRest<RestExternalAuthor>(
-    getAuthToken,
-    'external-authors',
-    {
-      appName,
-      baseUrl,
-    },
-  );
-  const squidexGraphqlClient = new SquidexGraphql(getAuthToken, {
-    appName,
-    baseUrl,
+  const contentfulGraphQLClient = getContentfulGraphQLClient({
+    space: contentfulSpaceId,
+    accessToken: contentfulAccessToken,
+    environment: contentfulEnvId,
   });
 
-  return new ExternalAuthorSquidexDataProvider(
-    externalAuthorRestClient,
-    squidexGraphqlClient,
+  return new ExternalAuthorContentfulDataProvider(
+    contentfulGraphQLClient,
+    getContentfulRestClientFactory,
   );
 };
--- apps/crn-server/src/dependencies/working-groups.dependencies.ts
+++ apps/crn-server/src/dependencies/working-groups.dependencies.ts
@@ -1,54 +1,22 @@
 import { getGraphQLClient as getContentfulGraphQLClient } from '@asap-hub/contentful';
 import {
-  RestWorkingGroup,
-  SquidexGraphql,
-  SquidexRest,
-} from '@asap-hub/squidex';
-import {
-  appName,
-  baseUrl,
   contentfulAccessToken,
   contentfulEnvId,
   contentfulSpaceId,
-  isContentfulEnabled,
 } from '../config';
 import { WorkingGroupContentfulDataProvider } from '../data-providers/contentful/working-group.data-provider';
-import { WorkingGroupSquidexDataProvider } from '../data-providers/working-group.data-provider';
-
 import { WorkingGroupDataProvider } from '../data-providers/types';
-import { getAuthToken } from '../utils/auth';
 import { getContentfulRestClientFactory } from './clients.dependencies';
 
 export const getWorkingGroupDataProvider = (): WorkingGroupDataProvider => {
-  if (isContentfulEnabled) {
-    const contentfulGraphQLClient = getContentfulGraphQLClient({
-      space: contentfulSpaceId,
-      accessToken: contentfulAccessToken,
-      environment: contentfulEnvId,
-    });
-
-    return new WorkingGroupContentfulDataProvider(
-      contentfulGraphQLClient,
-      getContentfulRestClientFactory,
-    );
-  }
-
-  const squidexGraphqlClient = new SquidexGraphql(getAuthToken, {
-    appName,
-    baseUrl,
+  const contentfulGraphQLClient = getContentfulGraphQLClient({
+    space: contentfulSpaceId,
+    accessToken: contentfulAccessToken,
+    environment: contentfulEnvId,
   });
 
-  const squidexRestClient = new SquidexRest<RestWorkingGroup>(
-    getAuthToken,
-    'working-groups',
-    {
-      appName,
-      baseUrl,
-    },
-  );
-
-  return new WorkingGroupSquidexDataProvider(
-    squidexGraphqlClient,
-    squidexRestClient,
+  return new WorkingGroupContentfulDataProvider(
+    contentfulGraphQLClient,
+    getContentfulRestClientFactory,
   );
 };

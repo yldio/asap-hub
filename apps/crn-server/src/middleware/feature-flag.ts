8036657352a62d978f5056829d5684af16bc3162
Leonard Martin <leonard.martin@gmail.com>
2023-09-08 16:03:30 +0100

    Remove IS_CONTENTFUL_ENABLED feature flag
    Removes code related to contentful data provider switching and replaces with a generic example for tests, using the same approach as `@asap-hub/flags`.

--- apps/crn-server/src/middleware/feature-flag.ts
+++ apps/crn-server/src/middleware/feature-flag.ts
@@ -1,13 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
-import { isContentfulEnabled } from '../config';
 import { FeatureFlagDependencySwitch } from '../utils/feature-flag';
 
 export const featureFlagMiddlewareFactory =
-  (featureFlagDependencySwitch: FeatureFlagDependencySwitch) =>
+  (_featureFlagDependencySwitch: FeatureFlagDependencySwitch) =>
   (req: Request, res: Response, next: NextFunction): void => {
-    featureFlagDependencySwitch.setFeatureFlag(
-      'IS_CONTENTFUL_ENABLED',
-      isContentfulEnabled,
-    );
-    return next();
+    // example feature flag
+    // _featureFlagDependencySwitch.setFeatureFlag('PERSISTENT_EXAMPLE', true);
+    next();
   };

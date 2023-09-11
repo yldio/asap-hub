e5e4661209fbde412fc48835f3a02e94adb0d237
Piotr Szpak <piotr.lukasz.szpak@gmail.com>
2023-09-11 16:07:49 +0100

    remove some parsers

--- apps/crn-server/src/data-providers/contentful/team.data-provider.ts
+++ apps/crn-server/src/data-providers/contentful/team.data-provider.ts
@@ -23,8 +23,10 @@ import {
 } from '@asap-hub/contentful';
 
 import { isTeamRole, sortMembers } from '../transformers';
-import { FetchTeamsOptions, TeamDataProvider } from '../types/teams.data-provider.types';
-
+import {
+  FetchTeamsOptions,
+  TeamDataProvider,
+} from '../types/teams.data-provider.types';
 
 export type TeamItem = NonNullable<
   NonNullable<FetchTeamsQuery['teamsCollection']>['items'][number]
@@ -35,7 +37,6 @@ export type Membership = NonNullable<
     NonNullable<TeamItem['linkedFrom']>['teamMembershipCollection']
   >['items'][number]
 >;
-
 
 export class TeamContentfulDataProvider implements TeamDataProvider {
   constructor(

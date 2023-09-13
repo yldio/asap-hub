9ee6d23f9cf682f74289a5915d58a785d3329449
AkosuaA <nattuah@gmail.com>
2023-09-13 16:07:34 +0100

    CRN-1473 - add tutorials field update migration

---
+++ packages/contentful/migrations/crn/tutorials/20230913101121-update-fields.js
@@ -0,0 +1,183 @@
+module.exports.description = 'Updates fields on tutorials model';
+
+module.exports.up = (migration) => {
+  const tutorials = migration.editContentType('tutorials');
+  tutorials
+    .createField('authors')
+    .name('Authors')
+    .type('Array')
+    .localized(false)
+    .required(false)
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Link',
+
+      validations: [
+        {
+          linkContentType: ['users', 'externalAuthors'],
+        },
+      ],
+
+      linkType: 'Entry',
+    });
+
+  tutorials
+    .createField('teams')
+    .name('Teams')
+    .type('Array')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Link',
+
+      validations: [
+        {
+          linkContentType: ['teams'],
+        },
+      ],
+
+      linkType: 'Entry',
+    });
+
+  tutorials
+    .createField('addedDate')
+    .name('Added Date')
+    .type('Date')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  tutorials
+    .createField('lastUpdated')
+    .name('Last updated')
+    .type('Date')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  tutorials
+    .createField('tags')
+    .type('Array')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Link',
+
+      validations: [
+        {
+          linkContentType: ['researchTags'],
+        },
+      ],
+
+      linkType: 'Entry',
+    });
+
+  tutorials
+    .createField('relatedTutorials')
+    .name('Related Tutorials')
+    .type('Array')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Link',
+
+      validations: [
+        {
+          linkContentType: ['tutorials'],
+        },
+      ],
+
+      linkType: 'Entry',
+    });
+
+  tutorials
+    .createField('relatedEvents')
+    .name('Related Events')
+    .type('Array')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Link',
+
+      validations: [
+        {
+          linkContentType: ['events'],
+        },
+      ],
+
+      linkType: 'Entry',
+    });
+
+  tutorials
+    .createField('asapFunded')
+    .name('ASAP Funded')
+    .type('Symbol')
+    .localized(false)
+    .required(true)
+    .validations([
+      {
+        in: ['Yes', 'No', 'Not Sure'],
+      },
+    ])
+    .disabled(false)
+    .omitted(false);
+
+  tutorials
+    .createField('usedInAPublication')
+    .name('Used in a Publication')
+    .type('Symbol')
+    .localized(false)
+    .required(true)
+    .validations([
+      {
+        in: ['Yes', 'No', 'Not Sure'],
+      },
+    ])
+    .disabled(false)
+    .omitted(false);
+
+  tutorials
+    .createField('sharingStatus')
+    .name('Sharing Status')
+    .type('Symbol')
+    .localized(false)
+    .required(true)
+    .validations([
+      {
+        in: ['Public', 'Network Only'],
+      },
+    ])
+    .disabled(false)
+    .omitted(false);
+};
+
+module.exports.down = (migration) => {
+  const tutorials = migration.editContentType('tutorials');
+  tutorials.deleteField('authors');
+  tutorials.deleteField('teams');
+  tutorials.deleteField('addedDate');
+  tutorials.deleteField('lastUpdated');
+  tutorials.deleteField('tags');
+  tutorials.deleteField('relatedTutorials');
+  tutorials.deleteField('relatedEvents');
+  tutorials.deleteField('asapFunded');
+  tutorials.deleteField('usedInAPublication');
+  tutorials.deleteField('sharingStatus');
+};

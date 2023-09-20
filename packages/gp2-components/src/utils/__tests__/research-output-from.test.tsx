22176912bec09b8a7f0bdf7c62689f8535325f47
Gabriela Ueno <gabiayako@gmail.com>
2023-09-20 14:09:25 -0300

    more tests

---
+++ packages/gp2-components/src/utils/__tests__/research-output-from.test.tsx
@@ -0,0 +1,11 @@
+import { getPublishDate } from '../research-output-form';
+
+describe('getPublishDate', () => {
+  const dateString = new Date().toString();
+  it('returns a new date if date string exists', () => {
+    expect(getPublishDate(dateString)).toBeInstanceOf(Date);
+  });
+  it('returns undefined if no date string is present', () => {
+    expect(getPublishDate()).toBeUndefined();
+  });
+});

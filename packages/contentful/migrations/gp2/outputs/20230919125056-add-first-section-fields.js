50e99a6fa2e607af9dd8e19e77745114127509ff
Gabriela Ueno <gabiayako@gmail.com>
2023-09-26 07:09:37 -0300

    migration

--- packages/contentful/migrations/gp2/outputs/20230919125056-add-first-section-fields.js
+++ packages/contentful/migrations/gp2/outputs/20230919125056-add-first-section-fields.js
@@ -1,5 +1,5 @@
 module.exports.description =
-  'Add description, gp2funded and sharing status fields';
+  'Add description, gp2funded and sharing status fields, re-order fields and add help text to publishDate';
 
 module.exports.up = (migration) => {
   const outputs = migration.editContentType('outputs');
@@ -48,9 +48,29 @@ module.exports.up = (migration) => {
   });
   outputs.changeFieldControl('sharingStatus', 'builtin', 'dropdown', {});
 
+  outputs.moveField('createdBy').afterField('addedDate');
+  outputs.moveField('lastUpdatedPartial').afterField('createdBy');
+  outputs.moveField('updatedBy').afterField('lastUpdatedPartial');
+  outputs.moveField('title').afterField('updatedBy');
+  outputs.moveField('link').afterField('title');
+  outputs.moveField('documentType').afterField('link');
+  outputs.moveField('type').afterField('documentType');
+  outputs.moveField('subtype').afterField('type');
   outputs.moveField('description').afterField('subtype');
   outputs.moveField('gp2Supported').afterField('description');
   outputs.moveField('sharingStatus').afterField('gp2Supported');
+  outputs.moveField('publishDate').afterField('sharingStatus');
+  outputs.moveField('tags').afterField('publishDate');
+  outputs.moveField('authors').afterField('tags');
+  outputs.moveField('relatedEntity').afterField('authors');
+  outputs.moveField('adminNotes').afterField('relatedEntity');
+
+  outputs.changeFieldControl('publishDate', 'builtin', 'datePicker', {
+    ampm: '24',
+    format: 'timeZ',
+    helpText:
+      'This should be the date your output was shared publicly on its repository',
+  });
 };
 
 module.exports.down = (migration) => {

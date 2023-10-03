20e0a7a804479e9d3a41b6c9a7a3916c05e74147
Gabriela Ueno <gabiayako@gmail.com>
2023-10-03 09:20:11 -0300

    GP2HUB-76 Update keywords to tags - Part 1

---
+++ packages/contentful/migrations/gp2/events/20231003121846-create-tags-and-copy-data.js
@@ -0,0 +1,32 @@
+module.exports.description = 'Create tags field and copy keywords to tags';
+
+module.exports.up = (migration) => {
+  const events = migration.editContentType('events');
+
+  events
+    .createField('tags')
+    .name('Tags')
+    .type('Array')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Link',
+
+      validations: [
+        {
+          linkContentType: ['tags'],
+        },
+      ],
+
+      linkType: 'Entry',
+    });
+};
+
+module.exports.down = (migration) => {
+  const events = migration.editContentType('events');
+
+  events.deleteField('tags');
+};
---
+++ packages/contentful/migrations/gp2/projects/20231003121701-change-tags-valid-linked-types.js
@@ -0,0 +1,31 @@
+module.exports.description = 'Change tags linked type';
+
+module.exports.up = (migration) => {
+  const projects = migration.editContentType('projects');
+  projects.editField('tags').items({
+    type: 'Link',
+
+    validations: [
+      {
+        linkContentType: ['tags'],
+      },
+    ],
+
+    linkType: 'Entry',
+  });
+};
+
+module.exports.down = (migration) => {
+  const projects = migration.editContentType('projects');
+  projects.editField('tags').items({
+    type: 'Link',
+
+    validations: [
+      {
+        linkContentType: ['keywords'],
+      },
+    ],
+
+    linkType: 'Entry',
+  });
+};

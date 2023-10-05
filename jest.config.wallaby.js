64c3cf31f39888f01b1de2937a92a77dbc2894a4
Piotr Szpak <piotr.lukasz.szpak@gmail.com>
2023-10-05 09:32:31 +0100

    update wallaby config

--- jest.config.wallaby.js
+++ jest.config.wallaby.js
@@ -1,4 +1,14 @@
 const baseConfig = require('./jest.config.js');
+baseConfig.projects.forEach((p) => {
+  if (p.collectCoverageFrom) {
+    p.collectCoverageFrom = ['**/src/**/*.{js,jsx,ts,tsx}'];
+  }
+  delete p.watchPlugins;
+});
+
+baseConfig.projects = baseConfig.projects.filter(
+  (p) => !p.rootDir?.endsWith('packages/contentful-app-extensions'),
+);
 
 module.exports = {
   ...baseConfig,

d14129ec63b5e0f22af79cfa6f2130d2bd71b907
Piotr Szpak <piotr.lukasz.szpak@gmail.com>
2023-10-05 15:30:13 +0100

    add unit test coverage for fetch-orcid

--- packages/server-common/test/fixtures/users.fixtures.ts
+++ packages/server-common/test/fixtures/users.fixtures.ts
@@ -1,4 +1,5 @@
-import { UserDataObject } from '@asap-hub/model';
+import { OrcidWork, UserDataObject } from '@asap-hub/model';
+import { ORCIDWorksResponse } from '../../src/utils/fetch-orcid';
 
 export const getUserDataObject = (): UserDataObject => ({
   id: 'userId',
@@ -29,3 +30,89 @@ export const getUserDataObject = (): UserDataObject => ({
   workingGroups: [],
   interestGroups: [],
 });
+
+export const orcidWorksResponse: ORCIDWorksResponse = {
+  'last-modified-date': { value: 1594690575911 },
+  group: [
+    {
+      'last-modified-date': { value: 1593270649404 },
+      'external-ids': {
+        'external-id': [
+          {
+            'external-id-type': 'doi',
+            'external-id-value': '10.1101/2020.06.24.169219',
+            'external-id-url': null,
+            'external-id-relationship': 'SELF',
+          },
+          {
+            'external-id-type': 'doi',
+            'external-id-value': '10.1101/2020.06.24.169219',
+            'external-id-url': {
+              value: 'https://doi.org/10.1101/2020.06.24.169219',
+            },
+            'external-id-relationship': 'SELF',
+          },
+        ],
+      },
+      'work-summary': [
+        {
+          'put-code': 76241838,
+          'created-date': { value: 1593086686737 },
+          'last-modified-date': { value: 1593270649404 },
+          source: {
+            'source-orcid': null,
+            'source-client-id': {
+              uri: 'https://orcid.org/client/0000-0001-9884-1913',
+              path: '0000-0001-9884-1913',
+              host: 'orcid.org',
+            },
+            'source-name': { value: 'Crossref' },
+          },
+          title: {
+            title: {
+              value:
+                'JIP3 links lysosome transport to regulation of multiple components of the axonal cytoskeleton',
+            },
+            subtitle: null,
+            'translated-title': null,
+          },
+          'external-ids': {
+            'external-id': [
+              {
+                'external-id-type': 'doi',
+                'external-id-value': '10.1101/2020.06.24.169219',
+                'external-id-url': {
+                  value: 'https://doi.org/10.1101/2020.06.24.169219',
+                },
+                'external-id-relationship': 'SELF',
+              },
+            ],
+          },
+          type: 'OTHER',
+          'publication-date': {
+            year: { value: '2020' },
+            month: { value: '06' },
+            day: { value: '24' },
+            'media-type': null,
+          },
+          visibility: 'PUBLIC',
+          path: '/0000-0001-9045-0723/work/76241838',
+          'display-index': '0',
+        },
+      ],
+    },
+  ],
+  path: '/0000-0001-9045-0723/works',
+};
+
+export const orcidWorksDeserialisedExpectation: OrcidWork[] = [
+  {
+    doi: 'https://doi.org/10.1101/2020.06.24.169219',
+    id: '76241838',
+    title:
+      'JIP3 links lysosome transport to regulation of multiple components of the axonal cytoskeleton',
+    type: 'OTHER',
+    publicationDate: { year: '2020', month: '06', day: '24' },
+    lastModifiedDate: '1593270649404',
+  },
+];

d77c0c4a5df02f9ea3015df72f75749326c60ab5
Octavian Ratiu <oratiu@gmail.com>
2023-10-11 14:46:16 +0300

    Added contentful migration on users for link validation

---
+++ packages/contentful/migrations/crn/users/20231011113618-add-validation-to-links.js
@@ -0,0 +1,98 @@
+module.exports.description = 'Adds validation to social media urls on the users';
+
+module.exports.up = (migration) => {
+  const users = migration.editContentType('users');
+
+  users.editField('linkedIn').validations([
+    {
+      regexp: {
+        pattern:
+          '^(ftp|http|https):\\/\\/(\\w+:{0,1}\\w*@)?(\\S+)(:[0-9]+)?(\\/|\\/([\\w#!:.?+=&%@!\\-/]))?$',
+        flags: null,
+      },
+    },
+  ]);
+
+  users.editField('twitter').validations([
+    {
+      regexp: {
+        pattern:
+          '^(ftp|http|https):\\/\\/(\\w+:{0,1}\\w*@)?(\\S+)(:[0-9]+)?(\\/|\\/([\\w#!:.?+=&%@!\\-/]))?$',
+        flags: null,
+      },
+    },
+  ]);
+
+  users.editField('github').validations([
+    {
+      regexp: {
+        pattern:
+          '^(ftp|http|https):\\/\\/(\\w+:{0,1}\\w*@)?(\\S+)(:[0-9]+)?(\\/|\\/([\\w#!:.?+=&%@!\\-/]))?$',
+        flags: null,
+      },
+    },
+  ]);
+
+  users.editField('googleScholar').validations([
+    {
+      regexp: {
+        pattern:
+          '^(ftp|http|https):\\/\\/(\\w+:{0,1}\\w*@)?(\\S+)(:[0-9]+)?(\\/|\\/([\\w#!:.?+=&%@!\\-/]))?$',
+        flags: null,
+      },
+    },
+  ]);
+
+  users.editField('orcid').validations([
+    {
+      unique: true,
+    },
+    {
+      regexp: {
+        pattern:
+          '^(ftp|http|https):\\/\\/(\\w+:{0,1}\\w*@)?(\\S+)(:[0-9]+)?(\\/|\\/([\\w#!:.?+=&%@!\\-/]))?$',
+        flags: null,
+      },
+    },
+  ]);
+
+  users.editField('researchGate').validations([
+    {
+      regexp: {
+        pattern:
+          '^(ftp|http|https):\\/\\/(\\w+:{0,1}\\w*@)?(\\S+)(:[0-9]+)?(\\/|\\/([\\w#!:.?+=&%@!\\-/]))?$',
+        flags: null,
+      },
+    },
+  ]);
+
+  users.editField('researcherId').validations([
+    {
+      regexp: {
+        pattern:
+          '^(ftp|http|https):\\/\\/(\\w+:{0,1}\\w*@)?(\\S+)(:[0-9]+)?(\\/|\\/([\\w#!:.?+=&%@!\\-/]))?$',
+        flags: null,
+      },
+    },
+  ]);
+};
+
+module.exports.down = (migration) => {
+  const users = migration.editContentType('users');
+
+  users.editField('linkedin').validations([]);
+  users.editField('twitter').validations([]);
+  users.editField('github').validations([]);
+  users.editField('googleScholar').validations([]);
+  users.editField('orcid').validations([
+    {
+      unique: true,
+    },
+  ]);
+  users.editField('researchGate').validations([]);
+  users.editField('researcherId').validations([]);
+
+  users.changeFieldControl('orcid', 'builtin', 'singleLine', {});
+  users.changeFieldControl('researchGate', 'builtin', 'singleLine', {});
+  users.changeFieldControl('researcherId', 'builtin', 'singleLine', {});
+};

b014044ef3c31f82763f238dc95f05c3570fcd3c
Thomas Waterfall <thomas.waterfall@gmail.com>
2023-10-16 16:21:05 +0100

    add JSON Web Key Sets

--- packages/auth/src/pubKeys/index.ts
+++ packages/auth/src/pubKeys/index.ts
@@ -1,5 +1,6 @@
 import { keys as crnProdPubKeys } from './crn-auth0-pubkeys.prod.json';
 import { keys as crnDevPubKeys } from './crn-auth0-pubkeys.dev.json';
+import { keys as crnPrPubKeys } from './crn-auth0-pubkeys.pr.json';
 import { keys as gp2ProdPubKeys } from './gp2-auth0-pubkeys.prod.json';
 import { keys as gp2DevPubKeys } from './gp2-auth0-pubkeys.dev.json';
 
@@ -17,6 +18,7 @@ interface JWK {
 const keys: JWK[] = [
   ...crnProdPubKeys,
   ...crnDevPubKeys,
+  ...crnPrPubKeys,
   ...gp2ProdPubKeys,
   ...gp2DevPubKeys,
 ];

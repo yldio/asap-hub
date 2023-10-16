8b32bc9d1d6491652595083343e7dbdcf4a1d3ef
Mark Sims <mark@bluepie.co.uk>
2023-10-16 14:25:51 +0100

    removes study link from cohort membership

---
+++ packages/contentful/migrations/gp2/contributingCohortsMembership/20231016142409-remove-study-link.js
@@ -0,0 +1,39 @@
+module.exports.description = 'removes study link';
+
+module.exports.up = (migration) => {
+  const contributingCohorts = migration.editContentType(
+    'contributingCohortsMembership',
+  );
+  contributingCohorts.deleteContentType('studyLink');
+};
+
+module.exports.down = (migration) => {
+  const contributingCohorts = migration.editContentType(
+    'contributingCohortsMembership',
+  );
+
+  contributingCohorts
+    .createField('studyLink')
+    .name('Study Link')
+    .type('Symbol')
+    .localized(false)
+    .required(false)
+    .validations([
+      {
+        regexp: {
+          pattern:
+            '^(ftp|http|https):\\/\\/(\\w+:{0,1}\\w*@)?(\\S+)(:[0-9]+)?(\\/|\\/([\\w#!:.?+=&%@!\\-/]))?$',
+          flags: null,
+        },
+      },
+    ])
+    .disabled(false)
+    .omitted(false);
+
+  contributingCohorts.changeFieldControl(
+    'studyLink',
+    'builtin',
+    'urlEditor',
+    {},
+  );
+};

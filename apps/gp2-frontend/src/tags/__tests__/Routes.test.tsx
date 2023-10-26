a4b5f1a8a965cafdfdb9b3d1416f5299075302e4
Quentin Leonetti <q.leonetti@gmail.com>
2023-10-26 12:53:19 +0200

    more tests

---
+++ apps/gp2-frontend/src/tags/__tests__/Routes.test.tsx
@@ -0,0 +1,67 @@
+import { User } from '@asap-hub/auth';
+import { render, screen, waitFor } from '@testing-library/react';
+import { Suspense } from 'react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import { Auth0Provider, WhenReady } from '../../auth/test-utils';
+import {
+  createAlgoliaResponse,
+  createEventAlgoliaRecord,
+  createOutputAlgoliaRecord,
+} from '../../__fixtures__/algolia';
+
+import { getTagSearchResults } from '../api';
+import Routes from '../Routes';
+
+jest.mock('../api');
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
+const mockGetTagSearchResults = getTagSearchResults as jest.MockedFunction<
+  typeof getTagSearchResults
+>;
+const renderPage = async ({ user = {} }: { user?: Partial<User> }) => {
+  render(
+    <Suspense fallback="loading">
+      <RecoilRoot>
+        <Auth0Provider user={{ ...user, role: 'Network Collaborator' }}>
+          <WhenReady>
+            <MemoryRouter initialEntries={['/tags']}>
+              <Route path="/tags">
+                <Routes />
+              </Route>
+            </MemoryRouter>
+          </WhenReady>
+        </Auth0Provider>
+      </RecoilRoot>
+    </Suspense>,
+  );
+  await waitFor(() =>
+    expect(screen.queryByText(/loading/i)).not.toBeInTheDocument(),
+  );
+};
+
+describe('Routes', () => {
+  it('renders the title', async () => {
+    mockGetTagSearchResults.mockResolvedValue(
+      createAlgoliaResponse<'output'>([]),
+    );
+    await renderPage({});
+    expect(screen.getByRole('heading', { name: 'Tags Search' })).toBeVisible();
+  });
+
+  it('renders a list of search items', async () => {
+    const items = createAlgoliaResponse<'event' | 'output'>([
+      createEventAlgoliaRecord({ customTitle: 'Event' }),
+      createOutputAlgoliaRecord(),
+    ]);
+
+    mockGetTagSearchResults.mockResolvedValue(items);
+    await renderPage({});
+
+    expect(screen.getByText('Event 0')).toBeVisible();
+    expect(screen.getByText('Output 1')).toBeVisible();
+  });
+});

8167dfd53c5fbcfb6c84fd17ee52eca5681b4aa9
Gabriela Ueno <gabiayako@gmail.com>
2023-10-30 07:25:42 -0300

    refactor type

--- apps/crn-frontend/src/tags/TagsList.tsx
+++ apps/crn-frontend/src/tags/TagsList.tsx
@@ -1,10 +1,10 @@
-import { EntityResponses } from '@asap-hub/algolia';
+import { CRNTagEntitiesList } from '@asap-hub/algolia';
 import { TagsPageBody } from '@asap-hub/react-components';
 import { usePagination, usePaginationParams, useSearch } from '../hooks';
 import { useTagSearch } from './state';
 
 type TagsProps = {
-  entities: Array<keyof EntityResponses['crn']>;
+  entities: CRNTagEntitiesList;
 };
 const Tags: React.FC<TagsProps> = ({ entities }) => {
   const { tags, searchQuery, filters } = useSearch();

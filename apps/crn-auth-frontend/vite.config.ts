f96048d72f28c46fefc5382c8e2ac3362ea3b534
Piotr Szpak <piotr.lukasz.szpak@gmail.com>
2023-11-23 10:45:35 +0000

    temp changes for auth0 WIP

--- apps/crn-auth-frontend/vite.config.ts
+++ apps/crn-auth-frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '',
+  base: 'https://dev.hub.asap.science',
   plugins: [react()],
   server: {
     open: true,

8eeb253dc093dbaa2be09c75b6f4e91ef8fbc78f
Mark Sims <mark@bluepie.co.uk>
2023-11-24 10:39:04 +0000

    disables no-floating-promise for CheckAuth

--- apps/crn-frontend/src/auth/CheckAuth.tsx
+++ apps/crn-frontend/src/auth/CheckAuth.tsx
@@ -1,6 +1,6 @@
-import { ReactNode, FC, useEffect } from 'react';
-import { useAuth0CRN } from '@asap-hub/react-context';
 import { Loading } from '@asap-hub/react-components';
+import { useAuth0CRN } from '@asap-hub/react-context';
+import { FC, ReactNode, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 interface CheckAuthProps {
@@ -10,7 +10,10 @@ const CheckAuth: FC<CheckAuthProps> = ({ children }) => {
   const { isAuthenticated, loading, checkSession } = useAuth0CRN();
   const location = useLocation();
   useEffect(() => {
-    if (checkSession) checkSession();
+    if (checkSession) {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      checkSession();
+    }
   }, [location, checkSession]);
 
   if (loading) {

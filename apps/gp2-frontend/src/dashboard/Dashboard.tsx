91a1abc4d72a9e372d422db702f01ddcf4df308e
Mark Sims <mark@bluepie.co.uk>
2023-11-24 12:36:43 +0000

    fixes promise on Dashboard

--- apps/gp2-frontend/src/dashboard/Dashboard.tsx
+++ apps/gp2-frontend/src/dashboard/Dashboard.tsx
@@ -1,14 +1,17 @@
 import { DashboardPage } from '@asap-hub/gp2-components';
 
-import { FC, lazy, useState } from 'react';
+import { FC, lazy, useEffect, useState } from 'react';
 import Frame from '../Frame';
 
 const loadBody = () =>
   import(/* webpackChunkName: "dashboard-body" */ './Body');
 const Body = lazy(loadBody);
-loadBody();
 
 const Dashboard: FC<Record<string, never>> = () => {
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    loadBody();
+  }, []);
   const [currentTime] = useState(new Date());
 
   return (

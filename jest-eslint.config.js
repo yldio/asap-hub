1f322a4dcb8144548be2bd5fc6ee9f7b2f84765d
Mark Sims <mark@bluepie.co.uk>
2023-11-24 09:34:36 +0000

    fixes OOM errors

--- jest-eslint.config.js
+++ jest-eslint.config.js
@@ -3,8 +3,9 @@ const baseConfig = require('./jest/jest-base.config.js');
 const lintConfigs = require('./jest/jest-lint.config');
 
 module.exports = {
-  ...baseConfig,
   projects: [...lintConfigs],
   testTimeout: 10000,
   testRegex: '^$', // root project does not have tests itself
+  maxWorkers: 1,
+  // workerIdleMemoryLimit: '1024MB'
 };

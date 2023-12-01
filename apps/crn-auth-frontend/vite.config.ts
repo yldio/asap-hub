bd1226781a1ab9b59cd29316c2ec3814fd2c7d4d
Piotr Szpak <piotr.lukasz.szpak@gmail.com>
2023-12-01 15:07:44 +0000

    convert to vite

--- apps/crn-auth-frontend/vite.config.ts
+++ apps/crn-auth-frontend/vite.config.ts
@@ -2,21 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base:
-    process.env.CRN_AUTH_FRONTEND_BASE_URL ||
-    'https://dev.hub.asap.science/.auth/',
   plugins: [react()],
   server: {
     open: true,
     port: 3000,
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`,
-      },
-    },
   },
 });
---
+++ apps/crn-frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+    port: 3000,
+  },
+  define: {
+    // here is the main update
+    global: 'globalThis',
+  },
+});

41df812ec155db49753779fc08b02b5fc2193b5a
Piotr Szpak <piotr.lukasz.szpak@gmail.com>
2023-12-04 16:25:51 +0000

    fix the config to use import.meta instead of process

--- apps/gp2-frontend/src/config.ts
+++ apps/gp2-frontend/src/config.ts
@@ -1,13 +1,14 @@
-export const GTM_CONTAINER_ID = process.env.VITE_APP_GP2_GTM_CONTAINER_ID;
+export const GTM_CONTAINER_ID = import.meta.env.VITE_APP_GP2_GTM_CONTAINER_ID;
 export const API_BASE_URL =
-  process.env.VITE_APP_GP2_API_BASE_URL || 'http://localhost:4444';
+  import.meta.env.VITE_APP_GP2_API_BASE_URL || 'http://localhost:4444';
 export const ALGOLIA_APP_ID =
-  process.env.VITE_APP_GP2_ALGOLIA_APP_ID || 'R44097HEU2';
+  import.meta.env.VITE_APP_GP2_ALGOLIA_APP_ID || 'R44097HEU2';
 export const ALGOLIA_INDEX =
-  process.env.VITE_APP_GP2_ALGOLIA_INDEX || 'gp2-hub_dev';
-export const SENTRY_DSN = process.env.VITE_APP_GP2_SENTRY_DSN;
-export const ENVIRONMENT = process.env.VITE_APP_ENVIRONMENT || 'local';
-export const RELEASE = process.env.VITE_APP_RELEASE;
-export const AUTH0_AUDIENCE = process.env.VITE_APP_GP2_AUTH0_AUDIENCE || '';
-export const AUTH0_CLIENT_ID = process.env.VITE_APP_GP2_AUTH0_CLIENT_ID || '';
-export const AUTH0_DOMAIN = process.env.VITE_APP_GP2_AUTH0_DOMAIN || '';
+  import.meta.env.VITE_APP_GP2_ALGOLIA_INDEX || 'gp2-hub_dev';
+export const SENTRY_DSN = import.meta.env.VITE_APP_GP2_SENTRY_DSN;
+export const ENVIRONMENT = import.meta.env.VITE_APP_ENVIRONMENT || 'local';
+export const RELEASE = import.meta.env.VITE_APP_RELEASE;
+export const AUTH0_AUDIENCE = import.meta.env.VITE_APP_GP2_AUTH0_AUDIENCE || '';
+export const AUTH0_CLIENT_ID =
+  import.meta.env.VITE_APP_GP2_AUTH0_CLIENT_ID || '';
+export const AUTH0_DOMAIN = import.meta.env.VITE_APP_GP2_AUTH0_DOMAIN || '';

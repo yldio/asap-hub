5982f32ca7e3123e5c62318edf3aef766eaa3c0a
Piotr Szpak <piotr.lukasz.szpak@gmail.com>
2023-12-05 12:37:42 +0000

    support browserslist

--- apps/gp2-frontend/vite.config.ts
+++ apps/gp2-frontend/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import browserslistToEsbuild from 'browserslist-to-esbuild';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
+    target: browserslistToEsbuild(),
   },
   server: {
     open: true,

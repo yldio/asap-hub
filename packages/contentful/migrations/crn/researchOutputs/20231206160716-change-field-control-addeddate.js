972b0a8e3fddddc056713e82e869fbc4d13caca2
AkosuaA <nattuah@gmail.com>
2023-12-07 10:11:06 +0000

    fix format

--- packages/contentful/migrations/crn/researchOutputs/20231206160716-change-field-control-addeddate.js
+++ packages/contentful/migrations/crn/researchOutputs/20231206160716-change-field-control-addeddate.js
@@ -3,7 +3,11 @@ module.exports.description = 'Make addedDate not required';
 module.exports.up = (migration) => {
   const researchOutputs = migration.editContentType('researchOutputs');
 
-  researchOutputs.changeFieldControl('addedDate', 'app', '1TW8dnAmczxAAQ46SQpQUi');
+  researchOutputs.changeFieldControl(
+    'addedDate',
+    'app',
+    '1TW8dnAmczxAAQ46SQpQUi',
+  );
 };
 
 module.exports.down = (migration) => {

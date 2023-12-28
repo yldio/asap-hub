be654c1b9478b79ee17f3bb008cc85814a158757
Quentin Leonetti <q.leonetti@gmail.com>
2023-12-28 12:41:44 +0100

    format

--- apps/gp2-server/src/data-providers/output.data-provider.ts
+++ apps/gp2-server/src/data-providers/output.data-provider.ts
@@ -491,44 +491,49 @@ const cleanOutput = (
     'mainEntityId'
   >,
 ) =>
-  Object.entries(outputToUpdate).reduce((acc, [key, value]) => {
-    switch (key) {
-      case 'authors':
-        return {
-          ...acc,
-          authors: (value as gp2Model.OutputUpdateDataObject['authors']).map(
-            (author) =>
-              getLinkEntity(author.userId || (author.externalUserId as string)),
-          ),
-        };
-      case 'createdBy':
-        return {
-          ...acc,
-          createdBy: linkEntityValue(value as string),
-          updatedBy: linkEntityValue(value as string),
-        };
-      case 'updatedBy':
-        return { ...acc, updatedBy: linkEntityValue(value as string) };
-      case 'tagIds':
-        return { ...acc, tags: linkEntityValue(value as string[]) };
-      case 'contributingCohortIds':
-        return {
-          ...acc,
-          contributingCohorts: linkEntityValue(value as string[]),
-        };
-      case 'relatedOutputIds':
-        return { ...acc, relatedOutputs: linkEntityValue(value as string[]) };
-      case 'relatedEventIds':
-        return { ...acc, relatedEvents: linkEntityValue(value as string[]) };
-      case 'versions':
-        return {
-          ...acc,
-          versions: value ? getLinkEntities(value as string[]) : undefined,
-        };
-      default:
-        return { ...acc, [key]: value };
-    }
-  }, {} as { [key: string]: unknown });
+  Object.entries(outputToUpdate).reduce(
+    (acc, [key, value]) => {
+      switch (key) {
+        case 'authors':
+          return {
+            ...acc,
+            authors: (value as gp2Model.OutputUpdateDataObject['authors']).map(
+              (author) =>
+                getLinkEntity(
+                  author.userId || (author.externalUserId as string),
+                ),
+            ),
+          };
+        case 'createdBy':
+          return {
+            ...acc,
+            createdBy: linkEntityValue(value as string),
+            updatedBy: linkEntityValue(value as string),
+          };
+        case 'updatedBy':
+          return { ...acc, updatedBy: linkEntityValue(value as string) };
+        case 'tagIds':
+          return { ...acc, tags: linkEntityValue(value as string[]) };
+        case 'contributingCohortIds':
+          return {
+            ...acc,
+            contributingCohorts: linkEntityValue(value as string[]),
+          };
+        case 'relatedOutputIds':
+          return { ...acc, relatedOutputs: linkEntityValue(value as string[]) };
+        case 'relatedEventIds':
+          return { ...acc, relatedEvents: linkEntityValue(value as string[]) };
+        case 'versions':
+          return {
+            ...acc,
+            versions: value ? getLinkEntities(value as string[]) : undefined,
+          };
+        default:
+          return { ...acc, [key]: value };
+      }
+    },
+    {} as { [key: string]: unknown },
+  );
 
 type OutputsCollection = gp2Contentful.FetchOutputsQuery['outputsCollection'];
 const parseOutputsCollection = (outputsCollection: OutputsCollection) => {

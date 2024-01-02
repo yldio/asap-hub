ca7234f01697f96ebfea76fba6f20f56bb6768fc
Gabriela Ueno <gabiayako@gmail.com>
2024-01-02 09:51:00 -0300

    fix lint

--- apps/e2e-tests/tests/crn-server/pages.e2e-test.ts
+++ apps/e2e-tests/tests/crn-server/pages.e2e-test.ts
@@ -3,9 +3,7 @@ import { config } from '../../config';
 describe('Pages endpoints', () => {
   test('Should get the privacy-policy page', async () => {
     console.log({ origin: config.apiOrigin });
-    const response = await fetch(
-      `${config.apiOrigin}/pages/privacy-policy`,
-    );
+    const response = await fetch(`${config.apiOrigin}/pages/privacy-policy`);
     const body = await response.json();
 
     expect(response.ok).toBe(true);

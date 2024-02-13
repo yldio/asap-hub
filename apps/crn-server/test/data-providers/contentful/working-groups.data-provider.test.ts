f9225073f71c6f36b621369a66f33c686cb1ab74
AkosuaA <nattuah@gmail.com>
2024-02-13 13:51:26 +0000

    fix format

--- apps/crn-server/test/data-providers/contentful/working-groups.data-provider.test.ts
+++ apps/crn-server/test/data-providers/contentful/working-groups.data-provider.test.ts
@@ -508,19 +508,19 @@ describe('Working Groups data provider', () => {
                     firstName: 'John',
                     lastName: 'Doe',
                     alumniSinceDate: null,
-                    teamsCollection:{
+                    teamsCollection: {
                       items: [
                         {
                           role: 'Project Manager',
                           team: {
                             sys: {
-                              id: 'team-1'
+                              id: 'team-1',
                             },
-                            displayName: 'Team 1'
-                          }
-                        }
-                      ]
-                    }
+                            displayName: 'Team 1',
+                          },
+                        },
+                      ],
+                    },
                   },
                 },
                 {
@@ -537,19 +537,19 @@ describe('Working Groups data provider', () => {
                     firstName: 'Jane',
                     lastName: 'Doe',
                     alumniSinceDate: null,
-                    teamsCollection:{
+                    teamsCollection: {
                       items: [
                         {
                           role: 'Trainee',
                           team: {
                             sys: {
-                              id: 'team-2'
+                              id: 'team-2',
                             },
-                            displayName: 'Team 2'
-                          }
-                        }
-                      ]
-                    }
+                            displayName: 'Team 2',
+                          },
+                        },
+                      ],
+                    },
                   },
                 },
               ],
@@ -572,13 +572,15 @@ describe('Working Groups data provider', () => {
           firstName: 'John',
           id: 'leader-1',
           lastName: 'Doe',
-          teams: [{
-            displayName: 'Team 1',
-            id: 'team-1',
-            inactiveSinceDate: undefined,
-            role: 'Project Manager',
-            teamInactiveSince: "",
-          }],
+          teams: [
+            {
+              displayName: 'Team 1',
+              id: 'team-1',
+              inactiveSinceDate: undefined,
+              role: 'Project Manager',
+              teamInactiveSince: '',
+            },
+          ],
         });
         expect(result!.members).toHaveLength(1);
         expect(result!.members[0]?.user).toEqual({
@@ -589,13 +591,15 @@ describe('Working Groups data provider', () => {
           firstName: 'Jane',
           id: 'member-1',
           lastName: 'Doe',
-          teams: [{
-            displayName: 'Team 2',
-            id: 'team-2',
-            inactiveSinceDate: undefined,
-            role: 'Trainee',
-            teamInactiveSince: "",
-          }],
+          teams: [
+            {
+              displayName: 'Team 2',
+              id: 'team-2',
+              inactiveSinceDate: undefined,
+              role: 'Trainee',
+              teamInactiveSince: '',
+            },
+          ],
         });
       });
 

daad8f8d8a2a30d741a7e970b56494fa07d5b98b
AkosuaA <nattuah@gmail.com>
2024-02-13 13:30:05 +0000

    modify interest group list and detail pages

--- packages/react-components/src/molecules/__tests__/CtaCard.test.tsx
+++ packages/react-components/src/molecules/__tests__/CtaCard.test.tsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import CtaCard from '../CtaCard';
 
@@ -11,6 +12,27 @@ it('renders a cta card with contact button', () => {
   const link = getByRole('link');
   expect(link).toHaveAttribute('href', 'test123');
   expect(link.textContent).toEqual('Button');
+});
+
+it('renders a cta card with copy email button', () => {
+  Object.assign(navigator, {
+    clipboard: {
+      writeText: jest.fn(),
+    },
+  });
+  jest.spyOn(navigator.clipboard, 'writeText');
+  const { getByRole } = render(
+    <CtaCard href="mailto:test123@gmail.com" buttonText="Button" displayCopy>
+      content
+    </CtaCard>,
+  );
+
+  const copyButton = getByRole('button', { name: 'Copy' });
+  expect(copyButton).toBeVisible();
+  userEvent.click(copyButton);
+  expect(navigator.clipboard.writeText).toHaveBeenCalledWith(
+    'test123@gmail.com',
+  );
 });
 
 it('renders a cta card with children', () => {

f93eeff6c65408f439fcc4a602925cec92a6c1c2
Gabriela Ueno <gabiayako@gmail.com>
2024-02-27 07:40:20 -0300

    add logs

--- packages/server-common/src/handlers/invalidate-cache/invalidate-handler.ts
+++ packages/server-common/src/handlers/invalidate-cache/invalidate-handler.ts
@@ -8,6 +8,7 @@ import { Logger } from '../../utils';
 export const createInvalidateCacheHandler =
   (distributionId: string, logger: Logger) =>
   async (event: S3Event): Promise<void> => {
+    logger.info('distributionId', distributionId);
     logger.debug(`event: ${JSON.stringify(event)}`);
     const client = new CloudFrontClient({ apiVersion: '2020-05-31' });
     const command = new CreateInvalidationCommand({
@@ -17,5 +18,7 @@ export const createInvalidateCacheHandler =
         Paths: { Quantity: 1, Items: ['/index.html'] },
       },
     });
+    logger.info('command', command);
+
     await client.send(command);
   };

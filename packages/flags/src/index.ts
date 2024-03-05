8d35b815abe0cc0c9c71a340bad96f3980eff3f2
Quentin Leonetti <q.leonetti@gmail.com>
2024-03-05 13:39:17 +0100

    turn analytics flag off

--- packages/flags/src/index.ts
+++ packages/flags/src/index.ts
@@ -8,6 +8,7 @@ export type Flags = Partial<Record<Flag, boolean | undefined>>;
 let overrides: Flags = {
   // flags already live in prod:
   // can also be used to manually disable a flag in development:
+  ANALYTICS: false,
   DISPLAY_EVENTS: false,
 };
 

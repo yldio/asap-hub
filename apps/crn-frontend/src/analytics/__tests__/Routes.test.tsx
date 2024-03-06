0b0e500e920aaf994af1e46f07fd9fcb31359741
Piotr Szpak <piotr.lukasz.szpak@gmail.com>
2024-03-06 14:20:50 +0000

    test API failure for coverage

--- apps/crn-frontend/src/analytics/__tests__/Routes.test.tsx
+++ apps/crn-frontend/src/analytics/__tests__/Routes.test.tsx
@@ -1,10 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import { mockConsoleError } from '@asap-hub/dom-test-utils';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import { MemoryRouter, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import { Suspense } from 'react';
+import { mockConsoleError } from '@asap-hub/dom-test-utils';
 import { analytics } from '@asap-hub/routing';
 
 import About from '../Routes';
 import { getAnalyticsLeadership } from '../api';
+import { Auth0Provider, WhenReady } from '../../auth/test-utils';
 
 jest.mock('../api');
 mockConsoleError();
@@ -18,12 +25,21 @@ const mockGetMemberships = getAnalyticsLeadership as jest.MockedFunction<
 
 const renderPage = async () => {
   render(
-    <MemoryRouter initialEntries={['/analytics']}>
-      <Route path={analytics.template}>
-        <About />
-      </Route>
-    </MemoryRouter>,
+    <RecoilRoot>
+      <Suspense fallback="loading">
+        <Auth0Provider user={{}}>
+          <WhenReady>
+            <MemoryRouter initialEntries={['/analytics']}>
+              <Route path={analytics.template}>
+                <About />
+              </Route>
+            </MemoryRouter>
+          </WhenReady>
+        </Auth0Provider>
+      </Suspense>
+    </RecoilRoot>,
   );
+  await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
 };
 
 describe('Analytics page', () => {
@@ -36,5 +52,14 @@ describe('Analytics page', () => {
         selector: 'h1',
       }),
     ).toBeVisible();
+  });
+
+  it('renders error message when the response is not a 2XX', async () => {
+    mockGetMemberships.mockRejectedValue(new Error('Failed to fetch'));
+
+    await renderPage();
+
+    expect(mockGetMemberships).toHaveBeenCalled();
+    expect(screen.getByText(/Something went wrong/i)).toBeVisible();
   });
 });

02eac9899f66c2525c138c69989f2cdca3a9adea
Gabriela Ueno <gabiayako@gmail.com>
2024-03-06 09:36:49 -0300

    ASAP-299 Update ASAP footer

--- packages/react-components/src/organisms/HelpSection.tsx
+++ packages/react-components/src/organisms/HelpSection.tsx
@@ -1,20 +1,20 @@
 import { css } from '@emotion/react';
 
-import { perRem } from '../pixels';
+import { rem } from '../pixels';
 import { Paragraph, Link } from '../atoms';
 import { CtaCard } from '../molecules';
 import { mailToGrants, mailToSupport } from '../mail';
 
 const bottomStyles = css({
   display: 'grid',
-  gridRowGap: `${12 / perRem}em`,
+  gridRowGap: rem(12),
 });
 
 const HelpSection: React.FC = () => (
   <section css={bottomStyles}>
-    <CtaCard href={mailToGrants()} buttonText="Request grant help">
+    <CtaCard href={mailToGrants()} buttonText="Contact grants team" displayCopy>
       <strong>Need help with a grant-related matter?</strong>
-      <br /> ASAP and the Michael J Fox Foundation are here to help
+      <br /> The grants team is here to help.
     </CtaCard>
     <Paragraph accent="lead">
       If you're facing a technical issue with the Hub, our team is happy to

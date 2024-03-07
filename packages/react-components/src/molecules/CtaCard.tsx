f589f849b33e58dbab97e3b0e8faae53c627a491
Gabriela Ueno <gabiayako@gmail.com>
2024-03-07 06:51:48 -0300

    ASAP-291 Update user profile footer (#4187)
    * ASAP-291 Update user profile footer

--- packages/react-components/src/molecules/CtaCard.tsx
+++ packages/react-components/src/molecules/CtaCard.tsx
@@ -2,33 +2,18 @@ import { ReactNode } from 'react';
 import { css } from '@emotion/react';
 
 import { Card, Paragraph } from '../atoms';
-import {
-  mobileScreen,
-  perRem,
-  vminLinearCalcClamped,
-  tabletScreen,
-  smallDesktopScreen,
-} from '../pixels';
+import { smallDesktopScreen, rem } from '../pixels';
 import CtaContactSection from './CtaContactSection';
 
 const getInTouchStyles = css({
   display: 'grid',
   gridTemplateColumns: 'auto',
   alignItems: 'center',
-  padding: `${12 / perRem}em ${vminLinearCalcClamped(
-    mobileScreen,
-    24,
-    tabletScreen,
-    36,
-    'px',
-  )}`,
+  gap: rem(24),
+  padding: `${rem(32)} ${rem(24)}`,
   [`@media (min-width: ${smallDesktopScreen.min}px)`]: {
     gridTemplateColumns: 'auto max-content',
   },
-  [`@media (min-width: ${tabletScreen.min}px) and (max-width: ${smallDesktopScreen.min}px)`]:
-    {
-      paddingBottom: `${14 / perRem}em`,
-    },
 });
 
 type CtaCardProps = {
@@ -46,7 +31,7 @@ const CtaCard: React.FC<CtaCardProps> = ({
 }) => (
   <Card padding={false} accent="green">
     <div css={getInTouchStyles}>
-      <Paragraph>{children}</Paragraph>
+      <Paragraph noMargin>{children}</Paragraph>
       <CtaContactSection
         href={href}
         displayCopy={displayCopy}

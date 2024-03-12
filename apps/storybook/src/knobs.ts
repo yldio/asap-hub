5db875bed5ac116f299340860d7e91815d4c6057
Quentin Leonetti <q.leonetti@gmail.com>
2024-03-12 13:00:29 +0100

    fix build

--- apps/storybook/src/knobs.ts
+++ apps/storybook/src/knobs.ts
@@ -6,3 +6,4 @@ export const boolean = (label, value) => value;
 export const select = (label, value) => value;
 export const array = (label, value) => value;
 export const date = (label, value) => value;
+export const optionsKnob= (label, value) => value;

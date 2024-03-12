37ce0d08c002532e27fc7daf168e94270d83dda6
Quentin Leonetti <q.leonetti@gmail.com>
2024-03-12 15:59:55 +0100

    linting 2

--- apps/storybook/src/theme.tsx
+++ apps/storybook/src/theme.tsx
@@ -2,6 +2,6 @@ import { css } from 'emotion';
 import { Decorator } from '@storybook/react';
 import { themes } from '@asap-hub/react-components';
 
-export const ThemeDecorator: Decorator = (storyFn, context) => {
-  return <div className={css(themes['light'])}>{storyFn(context)}</div>;
-};
+export const ThemeDecorator: Decorator = (storyFn, context) => (
+  <div className={css(themes.light)}>{storyFn(context)}</div>
+);

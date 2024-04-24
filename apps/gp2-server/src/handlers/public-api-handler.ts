9fa3331efb73dc60532f19d9e80e0521a329f79e
Piotr Szpak <piotr.lukasz.szpak@gmail.com>
2024-04-24 16:14:47 +0100

    attach handler

---
+++ apps/gp2-server/src/handlers/public-api-handler.ts
@@ -0,0 +1,32 @@
+/* istanbul ignore file */
+import { withRequest } from '@asap-hub/server-common';
+import * as Sentry from '@sentry/serverless';
+import { APIGatewayProxyEventV2 } from 'aws-lambda';
+import { Request as RequestExpress } from 'express';
+import serverlessHttp from 'serverless-http';
+import 'source-map-support/register';
+import { publicAppFactory } from '../publicApp';
+import { sentryWrapper } from '../utils/sentry-wrapper';
+
+const app = publicAppFactory({
+  sentryErrorHandler: Sentry.Handlers.errorHandler,
+  sentryRequestHandler: Sentry.Handlers.requestHandler,
+});
+
+interface RequestWithContext extends RequestExpress {
+  context: APIGatewayProxyEventV2['requestContext'];
+}
+
+const httpHandler = serverlessHttp(app, {
+  request(
+    request: RequestWithContext,
+    event: APIGatewayProxyEventV2,
+    context: { awsRequestId: string },
+  ) {
+    request.context = event.requestContext;
+    withRequest(event, context);
+  },
+});
+
+/* istanbul ignore next */
+export const apiHandler = sentryWrapper(httpHandler);

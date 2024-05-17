670e05edebe932ceb9aaff97483998b475501e43
Piotr Szpak <piotr.lukasz.szpak@gmail.com>
2024-05-17 09:29:58 +0100

    add apicache

--- apps/gp2-server/test/routes/public/public-app.route.test.ts
+++ apps/gp2-server/test/routes/public/public-app.route.test.ts
@@ -1,5 +1,6 @@
 import supertest from 'supertest';
 import { publicAppFactory } from '../../../src/publicApp';
+import { outputControllerMock } from '../../mocks/output.controller.mock';
 
 describe('Public App default routes', () => {
   test('Should return a 404 when the path is not found', async () => {
@@ -24,5 +25,32 @@ describe('Public App default routes', () => {
 
     expect(status).toBe(200);
     expect(headers['access-control-allow-origin']).toEqual('*');
+  });
+
+  test('Should cache the response and not call the controller again for the same parameters', async () => {
+    const app = publicAppFactory({
+      outputController: outputControllerMock,
+    });
+
+    const { headers: headers1 } = await supertest(app).get(
+      '/public/outputs/output-id',
+    );
+    await supertest(app).get('/public/outputs/output-id');
+
+    expect(outputControllerMock.fetchById).toHaveBeenCalledTimes(1);
+    expect(headers1['cache-control']).toEqual('max-age=3600');
+  });
+
+  test('Should call the controller each time when a different request parameter is used', async () => {
+    const app = publicAppFactory({
+      outputController: outputControllerMock,
+    });
+
+    outputControllerMock.fetch.mockReset();
+
+    await supertest(app).get('/public/outputs');
+    await supertest(app).get('/public/outputs?take=5');
+
+    expect(outputControllerMock.fetch).toHaveBeenCalledTimes(2);
   });
 });

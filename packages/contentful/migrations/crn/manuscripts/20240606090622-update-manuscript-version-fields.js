d930600d0fa537c0df7ae925326b7afa61403769
AkosuaA <nattuah@gmail.com>
2024-06-06 13:51:45 +0100

    rename fields

--- packages/contentful/migrations/crn/manuscripts/20240606090622-update-manuscript-version-fields.js
+++ packages/contentful/migrations/crn/manuscripts/20240606090622-update-manuscript-version-fields.js
@@ -1,66 +1,67 @@
-module.exports.description = '<Put your description here>';
+module.exports.description =
+  'Additional fields added to Manuscript Version model';
 
 module.exports.up = (migration) => {
   const manuscriptVersions = migration.editContentType('manuscriptVersions');
   manuscriptVersions
-  .createField('preprintDOI')
-  .name('Preprint DOI')
-  .type('Symbol')
-  .localized(false)
-  .required(false)
-  .validations([
-    {
-      regexp: {
-        pattern: '^10\\.\\d{4}.*$',
-        flags: null,
+    .createField('preprintDoi')
+    .name('Preprint DOI')
+    .type('Symbol')
+    .localized(false)
+    .required(false)
+    .validations([
+      {
+        regexp: {
+          pattern: '^10\\.\\d{4}.*$',
+          flags: null,
+        },
+        message: 'Preprint DOI must start with 10',
       },
-      message: 'Preprint DOI must start with 10',
-    },
-  ])
-  .disabled(false)
-  .omitted(false);
+    ])
+    .disabled(false)
+    .omitted(false);
 
   manuscriptVersions
-  .createField('publicationDOI')
-  .name('Publication DOI')
-  .type('Symbol')
-  .localized(false)
-  .required(false)
-  .validations([
-    {
-      regexp: {
-        pattern: '^10\\.\\d{4}.*$',
-        flags: null,
+    .createField('publicationDoi')
+    .name('Publication DOI')
+    .type('Symbol')
+    .localized(false)
+    .required(false)
+    .validations([
+      {
+        regexp: {
+          pattern: '^10\\.\\d{4}.*$',
+          flags: null,
+        },
+        message: 'Publication DOI must start with 10',
       },
-      message: 'Publication DOI must start with 10',
-    },
-  ])
-  .disabled(false)
-  .omitted(false);
+    ])
+    .disabled(false)
+    .omitted(false);
 
   manuscriptVersions
-  .createField('requestingAPCCoverage')
-  .name('Requesting APC Coverage')
-  .type('Symbol')
-  .localized(false)
-  .required(false)
-  .validations([
-    {
-      in: ['Yes', 'No', 'Already submitted'],
-    },
-  ])
-  .disabled(false)
-  .omitted(false);
+    .createField('requestingApcCoverage')
+    .name('Requesting APC Coverage')
+    .type('Symbol')
+    .localized(false)
+    .required(false)
+    .validations([
+      {
+        in: ['Yes', 'No', 'Already submitted'],
+      },
+    ])
+    .disabled(false)
+    .omitted(false);
 
   manuscriptVersions
-  .createField('details')
-  .name('Details')
-  .type('Symbol')
-  .localized(false)
-  .required(false)
-  .validations([])
-  .disabled(false)
-  .omitted(false);
+    .createField('details')
+    .name('Details')
+    .type('Symbol')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
 };
 
 module.exports.down = (migration) => {

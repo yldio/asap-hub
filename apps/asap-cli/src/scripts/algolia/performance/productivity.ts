22ffcafc817202506b83d45ea1c6fc6809d2a51f
Quentin Leonetti <q.leonetti@gmail.com>
2024-06-20 17:28:13 +0200

    refactor analytics performance processing for extension

--- apps/asap-cli/src/scripts/algolia/performance/productivity.ts
+++ apps/asap-cli/src/scripts/algolia/performance/productivity.ts
@@ -1,4 +1,3 @@
-import { SearchResponse } from '@algolia/client-search';
 import {
   PerformanceMetrics,
   TeamOutputDocumentType,
@@ -6,70 +5,12 @@ import {
   timeRanges,
 } from '@asap-hub/model';
 import type { SearchIndex } from 'algoliasearch';
-import algoliasearch from 'algoliasearch';
-
-type RoundingType = 'ceil' | 'floor';
-
-const roundToTwoDecimals = (number: number, type?: RoundingType): number => {
-  const factor = 100;
-
-  if (!type) {
-    return parseFloat(number.toFixed(2));
-  }
-
-  return type === 'ceil'
-    ? Math.ceil(number * factor) / factor
-    : Math.floor(number * factor) / factor;
-};
-
-export const getStandardDeviation = (
-  numbers: number[],
-  mean: number,
-): number => {
-  const n = numbers.length;
-  if (n === 0) return 0;
-
-  const variance =
-    numbers.reduce((sum, value) => sum + (value - mean) ** 2, 0) / n;
-
-  return Math.sqrt(variance);
-};
-
-export const getBellCurveMetrics = (
-  data: number[],
-  isInteger: boolean = true,
-): PerformanceMetrics => {
-  const mean = data.reduce((sum, value) => sum + value, 0) / data.length;
-  const stdDev = getStandardDeviation(data, mean);
-
-  const inferiorLimit = mean - stdDev;
-  const superiorLimit = mean + stdDev;
-
-  return {
-    belowAverageMin: isInteger
-      ? Math.min(...data)
-      : roundToTwoDecimals(Math.min(...data)),
-    belowAverageMax: isInteger
-      ? Math.floor(inferiorLimit)
-      : roundToTwoDecimals(inferiorLimit, 'floor'),
-    averageMin: isInteger
-      ? Math.ceil(inferiorLimit)
-      : roundToTwoDecimals(inferiorLimit, 'ceil'),
-    averageMax: isInteger
-      ? Math.floor(superiorLimit)
-      : roundToTwoDecimals(superiorLimit, 'floor'),
-    aboveAverageMin: isInteger
-      ? Math.ceil(superiorLimit)
-      : roundToTwoDecimals(superiorLimit, 'ceil'),
-    aboveAverageMax: isInteger
-      ? Math.max(...data)
-      : roundToTwoDecimals(Math.max(...data)),
-  };
-};
-
-type Hit = {
-  objectID: string;
-};
+import {
+  deletePreviousObjects,
+  getAllHits,
+  getBellCurveMetrics,
+  Hit,
+} from '../process-performance';
 
 type UserProductivityHit = Hit & {
   asapOutput: number;
@@ -79,38 +20,6 @@ type UserProductivityHit = Hit & {
 
 type TeamProductivityHit = Hit & {
   [documentType in TeamOutputDocumentType]: number;
-};
-
-export const getAllHits = async <T>(
-  getPaginatedHits: (page: number) => Promise<SearchResponse<T>>,
-): Promise<T[]> => {
-  let page = 0;
-  let totalPages = 0;
-  let allHits: T[] = [];
-
-  /* eslint-disable no-await-in-loop */
-  do {
-    const result = await getPaginatedHits(page);
-    allHits = allHits.concat(result.hits);
-    if (totalPages === 0) {
-      totalPages = result.nbPages;
-    }
-    page += 1;
-  } while (page < totalPages);
-  /* eslint-enable no-await-in-loop */
-
-  return allHits;
-};
-
-export const deletePreviousObjects = async (
-  index: SearchIndex,
-  type: 'user-productivity' | 'team-productivity',
-) => {
-  const previous = await index.search('', {
-    filters: `__meta.type:"${type}-performance"`,
-  });
-  const objectIDs = previous.hits.map(({ objectID }) => objectID);
-  await index.deleteObjects(objectIDs);
 };
 
 export const processUserProductivityPerformance = async (
@@ -220,30 +129,4 @@ export const processTeamProductivityPerformance = async (
       { autoGenerateObjectIDIfNotExist: true },
     );
   });
-};
-
-export type ProcessProductivityPerformance = {
-  algoliaAppId: string;
-  algoliaCiApiKey: string;
-  indexName: string;
-  metric: 'all' | 'user-productivity' | 'team-productivity';
-};
-
-/* istanbul ignore next */
-export const processProductivityPerformance = async ({
-  algoliaAppId,
-  algoliaCiApiKey,
-  indexName,
-  metric,
-}: ProcessProductivityPerformance): Promise<void> => {
-  const client = algoliasearch(algoliaAppId, algoliaCiApiKey);
-  const index = client.initIndex(indexName);
-
-  if (metric === 'all' || metric === 'user-productivity') {
-    await processUserProductivityPerformance(index);
-  }
-
-  if (metric === 'all' || metric === 'team-productivity') {
-    await processTeamProductivityPerformance(index);
-  }
 };
--- packages/algolia/src/analytics/types.ts
+++ packages/algolia/src/analytics/types.ts
@@ -1,7 +1,17 @@
 export const TEAM_LEADERSHIP = 'team-leadership';
 export const TEAM_PRODUCTIVITY = 'team-productivity';
 export const USER_PRODUCTIVITY = 'user-productivity';
-export const USER_PRODUCTIVITY_PERFORMANCE = 'user-productivity-performance';
-export const TEAM_PRODUCTIVITY_PERFORMANCE = 'team-productivity-performance';
 export const TEAM_COLLABORATION = 'team-collaboration';
 export const USER_COLLABORATION = 'user-collaboration';
+
+export const USER_PRODUCTIVITY_PERFORMANCE = 'user-productivity-performance';
+export const TEAM_PRODUCTIVITY_PERFORMANCE = 'team-productivity-performance';
+
+const types = [
+  TEAM_LEADERSHIP,
+  TEAM_PRODUCTIVITY,
+  USER_PRODUCTIVITY,
+  TEAM_COLLABORATION,
+  USER_COLLABORATION,
+];
+export type AnalyticType = (typeof types)[number];

1574d9580b9caa89f19349e22dbb8de7db91066c
Piotr Szpak <piotr.lukasz.szpak@gmail.com>
2024-06-26 16:10:02 +0100

    linting stuff

--- apps/crn-frontend/src/analytics/productivity/__tests__/Productivity.test.tsx
+++ apps/crn-frontend/src/analytics/productivity/__tests__/Productivity.test.tsx
@@ -11,7 +11,6 @@ import {
 import {
   SortTeamProductivity,
   DocumentCategoryOption,
-  OutputTypeOption,
   SortUserProductivity,
   TeamProductivityAlgoliaResponse,
   UserProductivityAlgoliaResponse,

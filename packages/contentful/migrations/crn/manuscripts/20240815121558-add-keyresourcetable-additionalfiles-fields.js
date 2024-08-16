e0369be356b2db4e3eaa73d6c748d93f44a4c652
AkosuaA <nattuah@gmail.com>
2024-08-16 11:36:56 +0100

    fix format

--- packages/contentful/migrations/crn/manuscripts/20240815121558-add-keyresourcetable-additionalfiles-fields.js
+++ packages/contentful/migrations/crn/manuscripts/20240815121558-add-keyresourcetable-additionalfiles-fields.js
@@ -1,42 +1,43 @@
-module.exports.description = 'Add Key Resource Table & Additional Files fields to Manuscript Version';
+module.exports.description =
+  'Add Key Resource Table & Additional Files fields to Manuscript Version';
 
 module.exports.up = (migration) => {
   const manuscriptVersions = migration.editContentType('manuscriptVersions');
 
   manuscriptVersions
-  .createField('keyResourceTable')
-  .name('Key Resource Table')
-  .type('Link')
-  .localized(false)
-  .required(true)
-  .validations([
-    {
-      linkMimetypeGroup: ['spreadsheet'],
-    },
-  ])
-  .disabled(false)
-  .omitted(false)
-  .linkType('Asset');
+    .createField('keyResourceTable')
+    .name('Key Resource Table')
+    .type('Link')
+    .localized(false)
+    .required(true)
+    .validations([
+      {
+        linkMimetypeGroup: ['spreadsheet'],
+      },
+    ])
+    .disabled(false)
+    .omitted(false)
+    .linkType('Asset');
 
   manuscriptVersions
-  .createField('additionalFiles')
-  .name('Additional Files')
-  .type('Array')
-  .localized(false)
-  .required(false)
-  .disabled(false)
-  .omitted(false)
-  .items({
-    type: 'Link',
+    .createField('additionalFiles')
+    .name('Additional Files')
+    .type('Array')
+    .localized(false)
+    .required(false)
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Link',
 
-    validations: [
-      {
-        linkMimetypeGroup: ['pdfdocument', 'spreadsheet'],
-      },
-    ],
+      validations: [
+        {
+          linkMimetypeGroup: ['pdfdocument', 'spreadsheet'],
+        },
+      ],
 
-    linkType: 'Asset',    
-  })
+      linkType: 'Asset',
+    });
 };
 
 module.exports.down = (migration) => {

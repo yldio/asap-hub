ae1b260ce786ff23a58b4bb71114cfc00caa799c
AkosuaA <nattuah@gmail.com>
2024-09-02 15:58:52 +0100

    update

--- packages/react-components/src/organisms/CaptionCard.tsx
+++ packages/react-components/src/organisms/CaptionCard.tsx
@@ -2,17 +2,18 @@ import { css } from '@emotion/react';
 
 import { rem, smallDesktopScreen } from '../pixels';
 import { Card } from '../atoms';
+import { contentSidePaddingWithNavigation } from '../layout';
 
 const cardStyles = css({ marginBottom: rem(32) });
-const containerPadding = rem(24);
 
 const containerStyles = css({
-  padding: containerPadding,
+  padding: rem(24),
   display: 'grid',
-  [`@media (max-width: calc(${smallDesktopScreen.width}px + ${containerPadding}))`]:
-    {
-      gridTemplateColumns: '1fr',
-    },
+  [`@media (max-width: calc(${
+    smallDesktopScreen.width
+  }px + ${contentSidePaddingWithNavigation(4)}))`]: {
+    gridTemplateColumns: '1fr',
+  },
   gridTemplateColumns: '1fr 1fr',
   rowGap: rem(16),
   columnGap: rem(16),

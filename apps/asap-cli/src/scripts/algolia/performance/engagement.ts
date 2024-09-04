3ff1b4050cbaf0b2bdba0b6337e061b7bb77ea1b
Quentin Leonetti <q.leonetti@gmail.com>
2024-09-04 13:56:26 +0200

    update engagement performance to use filtering (#4366)
    * update engagement performance to use filtering
    
    * remove useless await

--- apps/asap-cli/src/scripts/algolia/performance/engagement.ts
+++ apps/asap-cli/src/scripts/algolia/performance/engagement.ts
@@ -1,5 +1,7 @@
 import { SearchIndex } from 'algoliasearch';
 
+import { timeRanges } from '@asap-hub/model';
+
 import {
   deletePreviousObjects,
   getAllHits,
@@ -19,37 +21,40 @@ export const processEngagementPerformance = async (index: SearchIndex) => {
 
   await deletePreviousObjects(index, type);
 
-  const getPaginatedHits = (page: number) =>
-    index.search<EngagementHit>('', {
-      filters: `__meta.type:"${type}"`,
-      attributesToRetrieve: [
-        'eventCount',
-        'totalSpeakerCount',
-        'uniqueAllRolesCount',
-        'uniqueKeyPersonnelCount',
-      ],
-      page,
-      hitsPerPage: 50,
-    });
+  timeRanges.forEach(async (range) => {
+    const getPaginatedHits = (page: number) =>
+      index.search<EngagementHit>('', {
+        filters: `__meta.range:"${range}" AND __meta.type:"${type}"`,
+        attributesToRetrieve: [
+          'eventCount',
+          'totalSpeakerCount',
+          'uniqueAllRolesCount',
+          'uniqueKeyPersonnelCount',
+        ],
+        page,
+        hitsPerPage: 50,
+      });
 
-  const hits = await getAllHits<EngagementHit>(getPaginatedHits);
+    const hits = await getAllHits<EngagementHit>(getPaginatedHits);
 
-  await index.saveObject(
-    {
-      events: getPerformanceMetrics(hits.map((hit) => hit.eventCount)),
-      totalSpeakers: getPerformanceMetrics(
-        hits.map((hit) => hit.totalSpeakerCount),
-      ),
-      uniqueAllRoles: getPerformanceMetrics(
-        hits.map((hit) => hit.uniqueAllRolesCount),
-      ),
-      uniqueKeyPersonnel: getPerformanceMetrics(
-        hits.map((hit) => hit.uniqueKeyPersonnelCount),
-      ),
-      __meta: {
-        type: `${type}-performance`,
+    await index.saveObject(
+      {
+        events: getPerformanceMetrics(hits.map((hit) => hit.eventCount)),
+        totalSpeakers: getPerformanceMetrics(
+          hits.map((hit) => hit.totalSpeakerCount),
+        ),
+        uniqueAllRoles: getPerformanceMetrics(
+          hits.map((hit) => hit.uniqueAllRolesCount),
+        ),
+        uniqueKeyPersonnel: getPerformanceMetrics(
+          hits.map((hit) => hit.uniqueKeyPersonnelCount),
+        ),
+        __meta: {
+          range,
+          type: `${type}-performance`,
+        },
       },
-    },
-    { autoGenerateObjectIDIfNotExist: true },
-  );
+      { autoGenerateObjectIDIfNotExist: true },
+    );
+  });
 };

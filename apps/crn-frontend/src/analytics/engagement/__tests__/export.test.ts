264ad4ffd0eab5ebcdba5252cfa298e942310d63
Quentin Leonetti <q.leonetti@gmail.com>
2024-09-06 12:09:49 +0200

    update csv export

--- apps/crn-frontend/src/analytics/engagement/__tests__/export.test.ts
+++ apps/crn-frontend/src/analytics/engagement/__tests__/export.test.ts
@@ -1,4 +1,4 @@
-import { EngagementDataObject } from '@asap-hub/model';
+import { EngagementDataObject, EngagementPerformance } from '@asap-hub/model';
 import { engagementToCSV } from '../export';
 
 describe('engagementToCSV', () => {
@@ -14,19 +14,38 @@ describe('engagementToCSV', () => {
     uniqueKeyPersonnelCount: 2,
     uniqueKeyPersonnelCountPercentage: 67,
   };
+  const metric = {
+    belowAverageMin: 1,
+    belowAverageMax: 1,
+    averageMin: 1,
+    averageMax: 1,
+    aboveAverageMin: 1,
+    aboveAverageMax: 1,
+  };
+
+  const performance: EngagementPerformance = {
+    events: metric,
+    totalSpeakers: metric,
+    uniqueAllRoles: metric,
+    uniqueKeyPersonnel: metric,
+  };
 
   it('exports engagement data', () => {
-    expect(engagementToCSV(data)).toEqual({
+    expect(engagementToCSV(data, performance)).toEqual({
       Team: 'Test Team',
       'Team Status': 'Active',
       'Inactive Since': '',
       Members: 1,
-      Events: 4,
-      'Total Speakers': 3,
+      'Presentations: Average': 'Above',
+      'Presentations: Value': 4,
+      'Total Speakers: Average': 'Above',
+      'Total Speakers: Value': 3,
       'Unique Speakers (All Roles): Value': 3,
       'Unique Speakers (All Roles): Percentage': '100%',
+      'Unique Speakers (All Roles): Average': 'Above',
       'Unique Speakers (Key Personnel): Value': 2,
       'Unique Speakers (Key Personnel): Percentage': '67%',
+      'Unique Speakers (Key Personnel): Average': 'Above',
     });
   });
 });
--- apps/crn-frontend/src/analytics/engagement/export.ts
+++ apps/crn-frontend/src/analytics/engagement/export.ts
@@ -1,14 +1,34 @@
-import { EngagementDataObject } from '@asap-hub/model';
+import { EngagementDataObject, EngagementPerformance } from '@asap-hub/model';
+import { utils } from '@asap-hub/react-components';
 
-export const engagementToCSV = (data: EngagementDataObject) => ({
+export const engagementToCSV = (
+  data: EngagementDataObject,
+  performance: EngagementPerformance,
+) => ({
   Team: data.name,
   'Team Status': data.inactiveSince ? 'Inactive' : 'Active',
   'Inactive Since': data.inactiveSince ? data.inactiveSince.split('T')[0] : '',
   Members: data.memberCount,
-  Events: data.eventCount,
-  'Total Speakers': data.totalSpeakerCount,
+  'Presentations: Value': data.eventCount,
+  'Presentations: Average': utils.getPerformanceText(
+    data.eventCount,
+    performance.events,
+  ),
+  'Total Speakers: Value': data.totalSpeakerCount,
+  'Total Speakers: Average': utils.getPerformanceText(
+    data.totalSpeakerCount,
+    performance.totalSpeakers,
+  ),
   'Unique Speakers (All Roles): Value': data.uniqueAllRolesCount,
   'Unique Speakers (All Roles): Percentage': `${data.uniqueAllRolesCountPercentage}%`,
+  'Unique Speakers (All Roles): Average': utils.getPerformanceText(
+    data.uniqueAllRolesCount,
+    performance.uniqueAllRoles,
+  ),
   'Unique Speakers (Key Personnel): Value': data.uniqueKeyPersonnelCount,
   'Unique Speakers (Key Personnel): Percentage': `${data.uniqueKeyPersonnelCountPercentage}%`,
+  'Unique Speakers (Key Personnel): Average': utils.getPerformanceText(
+    data.uniqueKeyPersonnelCount,
+    performance.uniqueKeyPersonnel,
+  ),
 });

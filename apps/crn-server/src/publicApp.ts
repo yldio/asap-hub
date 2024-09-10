5871eaa875a15cbc17e3b555e2bd861fa35aa5ff
Gabriela Ueno <gabiayako@gmail.com>
2024-09-10 10:58:45 -0300

    Revert "crn test"
    This reverts commit 683fed83bbc360cd379e9bb4e89d3f9770fbf9b2.

--- apps/crn-server/src/publicApp.ts
+++ apps/crn-server/src/publicApp.ts
@@ -101,12 +101,6 @@ export const publicAppFactory = (
     res.status(200).json({ status: 'ok' });
   });
 
-  /* istanbul ignore next */
-  basicRoutes.get('/test', (req, res) => {
-    Sentry.captureMessage('Test message for Sentry');
-    res.send('Check Sentry for the test message!');
-  });
-
   const researchOutputRoutes = researchOutputRouteFactory(
     researchOutputController,
   );

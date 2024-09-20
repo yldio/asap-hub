5f49aaecd6c76f3f5605c8672d7c5fc64c7c3e45
AkosuaA <nattuah@gmail.com>
2024-09-20 13:54:59 +0100

    separate tests

--- packages/react-components/src/organisms/__tests__/ManuscriptCard.test.tsx
+++ packages/react-components/src/organisms/__tests__/ManuscriptCard.test.tsx
@@ -8,16 +8,16 @@ const props: ComponentProps<typeof ManuscriptCard> = {
   ...createManuscriptResponse(),
 };
 
-it('displays quick checks when present', () => {
-  const { getByText, queryByText, getByRole, rerender, getAllByText } = render(
+it('displays manuscript version card when expanded', () => {
+  const { getByText, queryByText, getByRole, rerender } = render(
     <ManuscriptCard {...props} />,
   );
+
+  expect(queryByText(/Original Research/i)).not.toBeInTheDocument();
+
+  expect(queryByText(/Preprint, version 1/i)).not.toBeInTheDocument();
+
   userEvent.click(getByRole('button'));
-  expect(
-    queryByText(
-      /Included ASAP as an affiliation within the author list for all ASAP-affiliated authors/i,
-    ),
-  ).not.toBeInTheDocument();
 
   rerender(
     <ManuscriptCard
@@ -25,244 +25,13 @@ it('displays quick checks when present', () => {
       versions={[
         {
           ...props.versions[0]!,
-          asapAffiliationIncludedDetails:
-            "Including ASAP as an affiliation hasn't been done due to compliance with journal guidelines, needing agreement from authors and institutions, administrative complexities, and balancing recognition with primary affiliations.",
-          codeDepositedDetails:
-            "This hasn't been done due to time constraints, pending review, and ensuring proper documentation.",
-          createdBy: {
-            id: 'user-1',
-            firstName: 'Joe',
-            lastName: 'Doe',
-            displayName: 'Joe Doe',
-            teams: [
-              {
-                id: 'team-a',
-                name: 'Team A',
-              },
-            ],
-          },
-          publishedAt: '2024-06-21T11:06:58.899Z',
+          type: 'Original Research',
+          lifecycle: 'Preprint, version 1',
         },
       ]}
     />,
   );
 
-  expect(
-    getByText(
-      /Included ASAP as an affiliation within the author list for all ASAP-affiliated authors/i,
-    ),
-  ).toBeVisible();
-  expect(
-    getByText(
-      /Including ASAP as an affiliation hasn't been done due to compliance with journal guidelines, needing agreement from authors and institutions, administrative complexities, and balancing recognition with primary affiliations./i,
-    ),
-  ).toBeVisible();
-
-  expect(
-    getByText(/Deposited all newly generated code and analysis scripts/i),
-  ).toBeVisible();
-  expect(
-    getByText(
-      /This hasn't been done due to time constraints, pending review, and ensuring proper documentation./i,
-    ),
-  ).toBeVisible();
-
-  expect(getAllByText('Joe Doe').length).toEqual(2);
-  expect(getAllByText('Team A').length).toEqual(2);
-  expect(getAllByText('21st June 2024').length).toEqual(2);
-
-  expect(getAllByText('Joe Doe')[0]!.closest('a')!.href!).toContain(
-    '/network/users/user-1',
-  );
-  expect(getAllByText('Team A')[0]!.closest('a')!.href!).toContain(
-    '/network/teams/team-a',
-  );
-});
-
-it('displays Additional Information section when present', () => {
-  const { getByRole, queryByRole, rerender } = render(
-    <ManuscriptCard {...props} />,
-  );
-  userEvent.click(getByRole('button'));
-  expect(
-    queryByRole('heading', { name: /Additional Information/i }),
-  ).not.toBeInTheDocument();
-
-  rerender(
-    <ManuscriptCard
-      {...props}
-      versions={[{ ...props.versions[0]!, otherDetails: 'Necessary info' }]}
-    />,
-  );
-
-  expect(
-    getByRole('heading', { name: /Additional Information/i }),
-  ).toBeVisible();
-});
-
-it('renders a divider between fields in Additional Information section and files section', () => {
-  const { getByRole, queryAllByRole } = render(
-    <ManuscriptCard
-      {...props}
-      versions={[
-        {
-          ...props.versions[0]!,
-          preprintDoi: '10.1101/gr.10.12.1841',
-          publicationDoi: '10.1101/gr.10.12.1842',
-          requestingApcCoverage: 'Already submitted',
-          otherDetails: 'Necessary info',
-        },
-      ]}
-    />,
-  );
-
-  userEvent.click(getByRole('button'));
-  expect(queryAllByRole('separator').length).toEqual(6);
-});
-
-it.each`
-  field                      | title                         | newValue
-  ${'preprintDoi'}           | ${'Preprint DOI'}             | ${'10.1101/gr.10.12.1841'}
-  ${'publicationDoi'}        | ${'Publication DOI'}          | ${'10.1101/gr.10.12.1841'}
-  ${'requestingApcCoverage'} | ${'Requesting APC Coverage?'} | ${'Yes'}
-  ${'otherDetails'}          | ${'Other details'}            | ${'new details'}
-`(`displays field $field when present`, async ({ field, title, newValue }) => {
-  const { getByRole, getByText, queryByText, rerender } = render(
-    <ManuscriptCard {...props} />,
-  );
-  userEvent.click(getByRole('button'));
-  expect(queryByText(title)).not.toBeInTheDocument();
-
-  rerender(
-    <ManuscriptCard
-      {...props}
-      versions={[{ ...props.versions[0]!, [field]: newValue }]}
-    />,
-  );
-
-  expect(getByText(title)).toBeVisible();
-  expect(getByText(newValue)).toBeVisible();
-});
-
-it('builds the correct href for doi fields', () => {
-  const preprintDoiValue = '10.1101/gr.10.12.1841';
-  const publicationDoiValue = '10.1101/gr.10.12.1842';
-  const expectedPreprintLink = new URL(
-    `https://doi.org/${preprintDoiValue}`,
-  ).toString();
-  const expectedPublicationLink = new URL(
-    `https://doi.org/${publicationDoiValue}`,
-  ).toString();
-
-  const { getByText, getByRole } = render(
-    <ManuscriptCard
-      {...props}
-      versions={[
-        {
-          ...props.versions[0]!,
-          preprintDoi: preprintDoiValue,
-          publicationDoi: publicationDoiValue,
-        },
-      ]}
-    />,
-  );
-  userEvent.click(getByRole('button'));
-
-  expect(getByText(preprintDoiValue)?.closest('a')).toHaveAttribute(
-    'href',
-    expectedPreprintLink,
-  );
-  expect(getByText(publicationDoiValue)?.closest('a')).toHaveAttribute(
-    'href',
-    expectedPublicationLink,
-  );
-});
-
-it('renders manuscript main file details and download link', () => {
-  const { getByText, getByRole } = render(
-    <ManuscriptCard
-      {...props}
-      versions={[
-        {
-          ...props.versions[0]!,
-          manuscriptFile: {
-            filename: 'manuscript_file.pdf',
-            url: 'https://example.com/main-file.pdf',
-            id: 'file-1',
-          },
-          keyResourceTable: undefined,
-        },
-      ]}
-    />,
-  );
-  userEvent.click(getByRole('button'));
-
-  expect(getByText('manuscript_file.pdf')).toBeVisible();
-  expect(getByText('Download').closest('a')).toHaveAttribute(
-    'href',
-    'https://example.com/main-file.pdf',
-  );
-});
-
-it('renders key resource table file details and download link', () => {
-  const { getAllByText, getByText, getByRole } = render(
-    <ManuscriptCard
-      {...props}
-      versions={[
-        {
-          ...props.versions[0]!,
-          manuscriptFile: {
-            filename: 'manuscript_file.pdf',
-            url: 'https://example.com/main-file.pdf',
-            id: 'file-1',
-          },
-          keyResourceTable: {
-            filename: 'key_resource_table.csv',
-            url: 'https://example.com/key_resource_table.csv',
-            id: 'file-2',
-          },
-        },
-      ]}
-    />,
-  );
-  userEvent.click(getByRole('button'));
-
-  expect(getByText('key_resource_table.csv')).toBeVisible();
-  expect(getAllByText('Download')[1]!.closest('a')).toHaveAttribute(
-    'href',
-    'https://example.com/key_resource_table.csv',
-  );
-});
-
-it('renders additional files details and download link when provided', () => {
-  const { getAllByText, getByText, getByRole } = render(
-    <ManuscriptCard
-      {...props}
-      versions={[
-        {
-          ...props.versions[0]!,
-          manuscriptFile: {
-            filename: 'manuscript_file.pdf',
-            url: 'https://example.com/main-file.pdf',
-            id: 'file-1',
-          },
-          keyResourceTable: undefined,
-          additionalFiles: [
-            {
-              filename: 'additional_file.pdf',
-              url: 'https://example.com/additional-file.pdf',
-              id: 'additional-file-1',
-            },
-          ],
-        },
-      ]}
-    />,
-  );
-  userEvent.click(getByRole('button'));
-
-  expect(getByText('additional_file.pdf')).toBeVisible();
-  expect(getAllByText('Download')[1]!.closest('a')).toHaveAttribute(
-    'href',
-    'https://example.com/additional-file.pdf',
-  );
+  expect(getByText(/Original Research/i)).toBeVisible();
+  expect(getByText(/Preprint, version 1/i)).toBeVisible();
 });
---
+++ packages/react-components/src/organisms/__tests__/ManuscriptVersionCard.test.tsx
@@ -0,0 +1,239 @@
+import { createManuscriptResponse } from '@asap-hub/fixtures';
+import { ManuscriptVersion } from '@asap-hub/model';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { ComponentProps } from 'react';
+import ManuscriptVersionCard from '../ManuscriptVersionCard';
+
+const props: ComponentProps<typeof ManuscriptVersionCard> = {
+  ...(createManuscriptResponse().versions[0] as ManuscriptVersion),
+};
+
+it('displays quick checks when present', () => {
+  const { getByText, queryByText, getByRole, rerender, getAllByText } = render(
+    <ManuscriptVersionCard {...props} />,
+  );
+  userEvent.click(getByRole('button'));
+
+  expect(
+    queryByText(
+      /Included ASAP as an affiliation within the author list for all ASAP-affiliated authors/i,
+    ),
+  ).not.toBeInTheDocument();
+
+  const updatedProps = {
+    ...props,
+    asapAffiliationIncludedDetails:
+      "Including ASAP as an affiliation hasn't been done due to compliance with journal guidelines, needing agreement from authors and institutions, administrative complexities, and balancing recognition with primary affiliations.",
+    codeDepositedDetails:
+      "This hasn't been done due to time constraints, pending review, and ensuring proper documentation.",
+    createdBy: {
+      id: 'user-1',
+      firstName: 'Joe',
+      lastName: 'Doe',
+      displayName: 'Joe Doe',
+      teams: [
+        {
+          id: 'team-a',
+          name: 'Team A',
+        },
+      ],
+    },
+    createdDate: '2024-06-20T11:06:58.899Z',
+    publishedAt: '2024-06-21T11:06:58.899Z',
+  };
+
+  rerender(<ManuscriptVersionCard {...updatedProps} />);
+
+  expect(
+    getByText(
+      /Included ASAP as an affiliation within the author list for all ASAP-affiliated authors/i,
+    ),
+  ).toBeVisible();
+  expect(
+    getByText(
+      /Including ASAP as an affiliation hasn't been done due to compliance with journal guidelines, needing agreement from authors and institutions, administrative complexities, and balancing recognition with primary affiliations./i,
+    ),
+  ).toBeVisible();
+
+  expect(
+    getByText(/Deposited all newly generated code and analysis scripts/i),
+  ).toBeVisible();
+  expect(
+    getByText(
+      /This hasn't been done due to time constraints, pending review, and ensuring proper documentation./i,
+    ),
+  ).toBeVisible();
+
+  expect(getAllByText('Joe Doe').length).toEqual(3);
+  expect(getAllByText('Team A').length).toEqual(3);
+  expect(getAllByText('21st June 2024').length).toEqual(2);
+  expect(getAllByText('20th June 2024').length).toEqual(1);
+
+  expect(getAllByText('Joe Doe')[0]!.closest('a')!.href!).toContain(
+    '/network/users/user-1',
+  );
+  expect(getAllByText('Team A')[0]!.closest('a')!.href!).toContain(
+    '/network/teams/team-a',
+  );
+});
+
+it('displays Additional Information section when present', () => {
+  const { getByRole, queryByRole, rerender } = render(
+    <ManuscriptVersionCard {...props} />,
+  );
+  userEvent.click(getByRole('button'));
+  expect(
+    queryByRole('heading', { name: /Additional Information/i }),
+  ).not.toBeInTheDocument();
+
+  rerender(
+    <ManuscriptVersionCard {...props} otherDetails={'Necessary info'} />,
+  );
+
+  expect(
+    getByRole('heading', { name: /Additional Information/i }),
+  ).toBeVisible();
+});
+
+it('renders a divider between fields in Additional Information section and files section', () => {
+  const { getByRole, queryAllByRole } = render(
+    <ManuscriptVersionCard
+      {...props}
+      preprintDoi={'10.1101/gr.10.12.1841'}
+      publicationDoi={'10.1101/gr.10.12.1842'}
+      requestingApcCoverage={'Already submitted'}
+      otherDetails={'Necessary info'}
+    />,
+  );
+
+  userEvent.click(getByRole('button'));
+  expect(queryAllByRole('separator').length).toEqual(6);
+});
+
+it.each`
+  field                      | title                         | newValue
+  ${'preprintDoi'}           | ${'Preprint DOI'}             | ${'10.1101/gr.10.12.1841'}
+  ${'publicationDoi'}        | ${'Publication DOI'}          | ${'10.1101/gr.10.12.1841'}
+  ${'requestingApcCoverage'} | ${'Requesting APC Coverage?'} | ${'Yes'}
+  ${'otherDetails'}          | ${'Other details'}            | ${'new details'}
+`(`displays field $field when present`, async ({ field, title, newValue }) => {
+  const { getByRole, getByText, queryByText, rerender } = render(
+    <ManuscriptVersionCard {...props} />,
+  );
+  userEvent.click(getByRole('button'));
+  expect(queryByText(title)).not.toBeInTheDocument();
+
+  const updatedProps = {
+    ...props,
+    [field]: newValue,
+  };
+
+  rerender(<ManuscriptVersionCard {...updatedProps} />);
+
+  expect(getByText(title)).toBeVisible();
+  expect(getByText(newValue)).toBeVisible();
+});
+
+it('builds the correct href for doi fields', () => {
+  const preprintDoiValue = '10.1101/gr.10.12.1841';
+  const publicationDoiValue = '10.1101/gr.10.12.1842';
+  const expectedPreprintLink = new URL(
+    `https://doi.org/${preprintDoiValue}`,
+  ).toString();
+  const expectedPublicationLink = new URL(
+    `https://doi.org/${publicationDoiValue}`,
+  ).toString();
+
+  const { getByText, getByRole } = render(
+    <ManuscriptVersionCard
+      {...props}
+      preprintDoi={preprintDoiValue}
+      publicationDoi={publicationDoiValue}
+    />,
+  );
+  userEvent.click(getByRole('button'));
+
+  expect(getByText(preprintDoiValue)?.closest('a')).toHaveAttribute(
+    'href',
+    expectedPreprintLink,
+  );
+  expect(getByText(publicationDoiValue)?.closest('a')).toHaveAttribute(
+    'href',
+    expectedPublicationLink,
+  );
+});
+
+it('renders manuscript main file details and download link', () => {
+  const { getByText, getByRole } = render(
+    <ManuscriptVersionCard
+      {...props}
+      manuscriptFile={{
+        filename: 'manuscript_file.pdf',
+        url: 'https://example.com/main-file.pdf',
+        id: 'file-1',
+      }}
+      keyResourceTable={undefined}
+    />,
+  );
+  userEvent.click(getByRole('button'));
+
+  expect(getByText('manuscript_file.pdf')).toBeVisible();
+  expect(getByText('Download').closest('a')).toHaveAttribute(
+    'href',
+    'https://example.com/main-file.pdf',
+  );
+});
+
+it('renders key resource table file details and download link', () => {
+  const { getAllByText, getByText, getByRole } = render(
+    <ManuscriptVersionCard
+      {...props}
+      manuscriptFile={{
+        filename: 'manuscript_file.pdf',
+        url: 'https://example.com/main-file.pdf',
+        id: 'file-1',
+      }}
+      keyResourceTable={{
+        filename: 'key_resource_table.csv',
+        url: 'https://example.com/key_resource_table.csv',
+        id: 'file-2',
+      }}
+    />,
+  );
+  userEvent.click(getByRole('button'));
+
+  expect(getByText('key_resource_table.csv')).toBeVisible();
+  expect(getAllByText('Download')[1]!.closest('a')).toHaveAttribute(
+    'href',
+    'https://example.com/key_resource_table.csv',
+  );
+});
+
+it('renders additional files details and download link when provided', () => {
+  const { getAllByText, getByText, getByRole } = render(
+    <ManuscriptVersionCard
+      {...props}
+      manuscriptFile={{
+        filename: 'manuscript_file.pdf',
+        url: 'https://example.com/main-file.pdf',
+        id: 'file-1',
+      }}
+      keyResourceTable={undefined}
+      additionalFiles={[
+        {
+          filename: 'additional_file.pdf',
+          url: 'https://example.com/additional-file.pdf',
+          id: 'additional-file-1',
+        },
+      ]}
+    />,
+  );
+  userEvent.click(getByRole('button'));
+
+  expect(getByText('additional_file.pdf')).toBeVisible();
+  expect(getAllByText('Download')[1]!.closest('a')).toHaveAttribute(
+    'href',
+    'https://example.com/additional-file.pdf',
+  );
+});

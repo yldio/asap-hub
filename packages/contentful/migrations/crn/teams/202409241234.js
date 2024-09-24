bbd7a0c625132a722aacfd4d6c200905ec0993a0
Quentin Leonetti <q.leonetti@gmail.com>
2024-09-24 11:30:15 +0200

    add research theme to teams

---
+++ packages/contentful/migrations/crn/teams/202409241234.js
@@ -0,0 +1,38 @@
+module.exports.description = 'Add Research Theme to Teams';
+
+module.exports.up = (migration) => {
+  const researchTheme = migration
+    .createContentType('researchTheme')
+    .name('Research Theme')
+    .description('')
+    .displayField('name');
+
+  researchTheme
+    .createField('name')
+    .name('Name')
+    .type('Symbol')
+    .localized(false)
+    .required(true)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  const teams = migration.editContentType('teams');
+
+  teams
+    .createField('researchTheme')
+    .name('Research Theme')
+    .type('Link')
+    .localized(false)
+    .required(false)
+    .disabled(false)
+    .omitted(false)
+    .linkType('Entry');
+};
+
+module.exports.down = (migration) => {
+  const teams = migration.editContentType('teams');
+  teams.deleteField('researchTheme');
+
+  migration.deleteContentType('researchTheme');
+};

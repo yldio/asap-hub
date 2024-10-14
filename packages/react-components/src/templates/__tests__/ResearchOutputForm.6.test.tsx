02bc11962c780efeff691eba7f1988ffb858dc11
Gabriela Ueno <gabiayako@gmail.com>
2024-10-14 11:34:41 -0300

    remove setup form from research output form 6

--- packages/react-components/src/templates/__tests__/ResearchOutputForm.6.test.tsx
+++ packages/react-components/src/templates/__tests__/ResearchOutputForm.6.test.tsx
@@ -4,16 +4,9 @@ import { Router } from 'react-router-dom';
 
 import {
   createResearchOutputResponse,
-  researchTagEnvironmentResponse,
   researchTagMethodResponse,
-  researchTagOrganismResponse,
 } from '@asap-hub/fixtures';
-import {
-  researchOutputDocumentTypeToType,
-  ResearchOutputPostRequest,
-  ResearchOutputResponse,
-  ResearchTagResponse,
-} from '@asap-hub/model';
+import { researchOutputDocumentTypeToType } from '@asap-hub/model';
 import { fireEvent } from '@testing-library/dom';
 import { render, screen, waitFor } from '@testing-library/react';
 import { createMemoryHistory, History } from 'history';
@@ -41,6 +34,37 @@ const defaultProps: ComponentProps<typeof ResearchOutputForm> = {
 
 jest.setTimeout(60000);
 
+const initialResearchOutputData = {
+  id: 'id',
+  created: '2020-09-07T17:36:54Z',
+  addedDate: '2020-10-08T16:35:54Z',
+  lastUpdatedPartial: '2020-11-09T20:36:54Z',
+  lastModifiedDate: '2020-12-10T20:36:54Z',
+  title: 'Output',
+  description: 'description',
+  descriptionMD: 'descriptionMD',
+  shortDescription: 'shortDescription',
+  documentType: 'Grant Document' as const,
+  authors: [],
+  teams: [],
+  publishingEntity: 'Working Group' as const,
+  workingGroups: undefined,
+  relatedEvents: [],
+  relatedResearch: [],
+  sharingStatus: 'Public' as const,
+  contactEmails: [],
+  labs: [],
+  methods: [],
+  organisms: [],
+  environments: [],
+  subtype: 'Metabolite',
+  keywords: [],
+  published: true,
+  isInReview: false,
+  versions: [],
+  link: 'http://example.com',
+};
+
 describe('on submit', () => {
   let history!: History;
   const id = '42';
@@ -51,87 +75,42 @@ describe('on submit', () => {
   const getRelatedResearchSuggestions = jest.fn();
   const getShortDescriptionFromDescription = jest.fn();
 
-  beforeEach(() => {
-    history = createMemoryHistory();
-    saveDraftFn.mockResolvedValue({ ...createResearchOutputResponse(), id });
-    saveFn.mockResolvedValue({ ...createResearchOutputResponse(), id });
-    getLabSuggestions.mockResolvedValue([]);
-    getAuthorSuggestions.mockResolvedValue([]);
-    getRelatedResearchSuggestions.mockResolvedValue([]);
-    getShortDescriptionFromDescription.mockReturnValue('short description');
-
-    // TODO: fix act error
-    jest.spyOn(console, 'error').mockImplementation();
-  });
+  history = createMemoryHistory();
+  saveDraftFn.mockResolvedValue({ ...createResearchOutputResponse(), id });
+  saveFn.mockResolvedValue({ ...createResearchOutputResponse(), id });
+  getLabSuggestions.mockResolvedValue([]);
+  getAuthorSuggestions.mockResolvedValue([]);
+  getRelatedResearchSuggestions.mockResolvedValue([]);
+  getShortDescriptionFromDescription.mockReturnValue('short description');
 
   afterEach(() => {
     jest.resetAllMocks();
   });
 
-  const expectedRequest: ResearchOutputPostRequest = {
-    documentType: 'Article',
-    doi: '10.1234',
-    link: 'http://example.com',
-    title: 'example title',
-    description: '',
-    descriptionMD: 'example description',
-    shortDescription: 'short description',
-    type: 'Preprint',
-    labs: [],
-    authors: [],
-    teams: ['TEAMID'],
-    sharingStatus: 'Network Only',
-    methods: [],
-    organisms: [],
-    environments: [],
-    usageNotes: '',
-    workingGroups: [],
-    relatedResearch: [],
-    keywords: [],
-    published: false,
-    relatedEvents: [],
-  };
-  type Data = Pick<
-    ResearchOutputPostRequest,
-    'link' | 'title' | 'descriptionMD' | 'shortDescription' | 'type'
-  >;
+  const submitForm = async () => {
+    const button = screen.getByRole('button', { name: /Publish/i });
+    userEvent.click(button);
 
-  const setupForm = async (
-    {
-      data = {
-        descriptionMD: 'example description',
-        shortDescription: 'short description',
-        title: 'example title',
-        type: 'Preprint',
-        link: 'http://example.com',
-      },
-      propOverride = {},
-      documentType = 'Article',
-      researchTags = [{ id: '1', name: 'research tag 1' }],
-      researchOutputData = undefined,
-    }: {
-      data?: Data;
-      propOverride?: Partial<ComponentProps<typeof ResearchOutputForm>>;
-      documentType?: ComponentProps<typeof ResearchOutputForm>['documentType'];
-      researchTags?: ResearchTagResponse[];
-      researchOutputData?: ResearchOutputResponse;
-    } = {
-      data: {
-        descriptionMD: 'example description',
-        shortDescription: 'short description',
-        title: 'example title',
-        type: 'Preprint',
-        link: 'http://example.com',
-      },
-      documentType: 'Article',
-      researchTags: [],
-    },
-  ) => {
+    userEvent.click(screen.getByRole('button', { name: /Publish Output/i }));
+
+    await waitFor(() => {
+      expect(screen.getByRole('button', { name: 'Publish' })).toBeEnabled();
+      expect(screen.getByRole('button', { name: /Cancel/i })).toBeEnabled();
+    });
+  };
+
+  it('can submit a method', async () => {
+    const researchTags = [researchTagMethodResponse];
+    const documentType = 'Dataset';
+    const type = 'Spectroscopy';
     render(
       <Router history={history}>
         <ResearchOutputForm
           {...defaultProps}
-          researchOutputData={researchOutputData}
+          researchOutputData={{
+            ...initialResearchOutputData,
+            type,
+          }}
           selectedTeams={[{ value: 'TEAMID', label: 'Example Team' }]}
           documentType={documentType}
           typeOptions={Array.from(
@@ -146,93 +125,51 @@ describe('on submit', () => {
             getShortDescriptionFromDescription
           }
           researchTags={researchTags}
-          {...propOverride}
+        />
+      </Router>,
+    );
+    userEvent.click(await screen.findByRole('textbox', { name: /methods/i }));
+    userEvent.click(screen.getByText('ELISA'));
+    await submitForm();
+    expect(saveFn).toHaveBeenLastCalledWith(
+      expect.objectContaining({
+        documentType,
+        type,
+        methods: ['ELISA'],
+      }),
+    );
+  });
+
+  it('resetting the type resets methods', async () => {
+    const type = 'Spectroscopy';
+    const researchTags = [researchTagMethodResponse];
+    const documentType = 'Dataset';
+    render(
+      <Router history={history}>
+        <ResearchOutputForm
+          {...defaultProps}
+          researchOutputData={{
+            ...initialResearchOutputData,
+            type,
+          }}
+          selectedTeams={[{ value: 'TEAMID', label: 'Example Team' }]}
+          documentType={documentType}
+          typeOptions={Array.from(
+            researchOutputDocumentTypeToType[documentType],
+          )}
+          onSave={saveFn}
+          onSaveDraft={saveDraftFn}
+          getLabSuggestions={getLabSuggestions}
+          getAuthorSuggestions={getAuthorSuggestions}
+          getRelatedResearchSuggestions={getRelatedResearchSuggestions}
+          getShortDescriptionFromDescription={
+            getShortDescriptionFromDescription
+          }
+          researchTags={researchTags}
         />
       </Router>,
     );
 
-    fireEvent.change(screen.getByLabelText(/url/i), {
-      target: { value: data.link },
-    });
-    fireEvent.change(screen.getByLabelText(/title/i), {
-      target: { value: data.title },
-    });
-
-    const descriptionEditor = screen.getByTestId('editor');
-    userEvent.click(descriptionEditor);
-    userEvent.tab();
-    fireEvent.input(descriptionEditor, { data: data.descriptionMD });
-    userEvent.tab();
-
-    fireEvent.change(
-      screen.getByRole('textbox', { name: /short description/i }),
-      {
-        target: { value: data.shortDescription },
-      },
-    );
-
-    const typeDropdown = screen.getByRole('textbox', {
-      name: /Select the type/i,
-    });
-    fireEvent.change(typeDropdown, {
-      target: { value: data.type },
-    });
-    fireEvent.keyDown(typeDropdown, {
-      keyCode: ENTER_KEYCODE,
-    });
-
-    const identifier = screen.getByRole('textbox', { name: /identifier/i });
-    fireEvent.change(identifier, {
-      target: { value: 'DOI' },
-    });
-    fireEvent.keyDown(identifier, {
-      keyCode: ENTER_KEYCODE,
-    });
-    fireEvent.change(screen.getByPlaceholderText('e.g. 10.5555/YFRU1371'), {
-      target: { value: '10.1234' },
-    });
-  };
-  const submitForm = async () => {
-    const button = screen.getByRole('button', { name: /Publish/i });
-    userEvent.click(button);
-    userEvent.click(screen.getByRole('button', { name: /Publish Output/i }));
-    await waitFor(() => {
-      expect(screen.getByRole('button', { name: 'Publish' })).toBeEnabled();
-      expect(screen.getByRole('button', { name: /Cancel/i })).toBeEnabled();
-    });
-  };
-
-  it.only('can submit a method', async () => {
-    const researchTags = [researchTagMethodResponse];
-    const documentType = 'Dataset';
-    const type = 'Spectroscopy';
-    await setupForm({ researchTags, documentType });
-    const typeDropdown = screen.getByRole('textbox', {
-      name: /Select the type/i,
-    });
-    fireEvent.change(typeDropdown, {
-      target: { value: type },
-    });
-    fireEvent.keyDown(typeDropdown, {
-      keyCode: ENTER_KEYCODE,
-    });
-
-    userEvent.click(await screen.findByRole('textbox', { name: /methods/i }));
-    userEvent.click(screen.getByText('ELISA'));
-    await submitForm();
-    expect(saveFn).toHaveBeenLastCalledWith({
-      ...expectedRequest,
-      documentType,
-      type,
-      methods: ['ELISA'],
-    });
-  });
-
-  it.only('resetting the type resets methods', async () => {
-    const researchTags = [researchTagMethodResponse];
-    const documentType = 'Dataset';
-    const type = 'Spectroscopy';
-    await setupForm({ researchTags, documentType });
     const typeDropdown = screen.getByRole('textbox', {
       name: /Select the type/i,
     });
@@ -248,6 +185,7 @@ describe('on submit', () => {
     userEvent.click(screen.getByText('ELISA'));
 
     expect(screen.getByText(/ELISA/i)).toBeInTheDocument();
+
     fireEvent.change(typeDropdown, {
       target: { value: 'Protein Data' },
     });

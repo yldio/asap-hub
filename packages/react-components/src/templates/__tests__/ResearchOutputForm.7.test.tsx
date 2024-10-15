a5744be1bf83a6568af5743c5cbf5dc40c1887ef
Quentin Leonetti <q.leonetti@gmail.com>
2024-10-15 11:40:22 +0200

    more splitsss

---
+++ packages/react-components/src/templates/__tests__/ResearchOutputForm.7.test.tsx
@@ -0,0 +1,251 @@
+import userEvent from '@testing-library/user-event';
+import { ComponentProps } from 'react';
+import { Router } from 'react-router-dom';
+
+import {
+  createResearchOutputResponse,
+  createUserResponse,
+  researchTagEnvironmentResponse,
+} from '@asap-hub/fixtures';
+import {
+  researchOutputDocumentTypeToType,
+  ResearchOutputPostRequest,
+} from '@asap-hub/model';
+import { fireEvent } from '@testing-library/dom';
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import { createMemoryHistory, History } from 'history';
+import ResearchOutputForm from '../ResearchOutputForm';
+import { ENTER_KEYCODE } from '../../atoms/Dropdown';
+
+const defaultProps: ComponentProps<typeof ResearchOutputForm> = {
+  onSave: jest.fn(() => Promise.resolve()),
+  onSaveDraft: jest.fn(() => Promise.resolve()),
+  published: false,
+  tagSuggestions: [],
+  researchTags: [],
+  documentType: 'Article',
+  selectedTeams: [],
+  typeOptions: Array.from(researchOutputDocumentTypeToType.Article.values()),
+  permissions: {
+    canEditResearchOutput: true,
+    canPublishResearchOutput: true,
+    canShareResearchOutput: true,
+  },
+  getRelatedResearchSuggestions: jest.fn(),
+  getRelatedEventSuggestions: jest.fn(),
+  getShortDescriptionFromDescription: jest.fn(),
+};
+
+jest.setTimeout(60000);
+
+describe('on submit', () => {
+  let history!: History;
+  const id = '42';
+  const saveDraftFn = jest.fn();
+  const saveFn = jest.fn();
+  const getLabSuggestions = jest.fn();
+  const getAuthorSuggestions = jest.fn();
+  const getRelatedResearchSuggestions = jest.fn();
+  const getShortDescriptionFromDescription = jest.fn();
+
+  beforeEach(() => {
+    history = createMemoryHistory();
+    saveDraftFn.mockResolvedValue({ ...createResearchOutputResponse(), id });
+    saveFn.mockResolvedValue({ ...createResearchOutputResponse(), id });
+    getLabSuggestions.mockResolvedValue([]);
+    getAuthorSuggestions.mockResolvedValue([]);
+    getRelatedResearchSuggestions.mockResolvedValue([]);
+    getShortDescriptionFromDescription.mockReturnValue('short description');
+
+    // TODO: fix act error
+    jest.spyOn(console, 'error').mockImplementation();
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  const expectedRequest: ResearchOutputPostRequest = {
+    documentType: 'Article',
+    doi: '10.1234',
+    link: 'http://example.com',
+    title: 'example title',
+    description: '',
+    descriptionMD: 'example description',
+    shortDescription: 'short description',
+    type: 'Preprint',
+    labs: [],
+    authors: [],
+    teams: ['TEAMID'],
+    sharingStatus: 'Network Only',
+    methods: [],
+    organisms: [],
+    environments: [],
+    usageNotes: '',
+    workingGroups: [],
+    relatedResearch: [],
+    keywords: [],
+    published: false,
+    relatedEvents: [],
+  };
+
+  const submitForm = async () => {
+    const button = screen.getByRole('button', { name: /Publish/i });
+    userEvent.click(button);
+    userEvent.click(screen.getByRole('button', { name: /Publish Output/i }));
+    await waitFor(() => {
+      expect(screen.getByRole('button', { name: 'Publish' })).toBeEnabled();
+      expect(screen.getByRole('button', { name: /Cancel/i })).toBeEnabled();
+    });
+  };
+
+  it('field tests', async () => {
+    getLabSuggestions.mockResolvedValue([
+      { label: 'One Lab', value: '1' },
+      { label: 'Two Lab', value: '2' },
+    ]);
+    getRelatedResearchSuggestions.mockResolvedValue([
+      { label: 'First Related Research', value: '1' },
+      { label: 'Second Related Research', value: '2' },
+    ]);
+    getAuthorSuggestions.mockResolvedValue([
+      {
+        author: { ...createUserResponse(), displayName: 'Chris Blue' },
+        label: 'Chris Blue',
+        value: 'u2',
+      },
+      {
+        author: {
+          id: 'external-chris',
+          displayName: 'Chris Reed',
+        },
+        label: 'Chris Reed (Non CRN)',
+        value: 'u1',
+      },
+    ]);
+
+    const researchOutputData = undefined;
+    const data = {
+      descriptionMD: 'example description',
+      shortDescription: 'short description',
+      title: 'example title',
+      type: 'Preprint',
+      link: 'http://example.com',
+    };
+    const propOverride = {};
+    const documentType = 'Article';
+    const researchTags = [{ id: '1', name: 'research tag 1' }];
+
+    await render(
+      <Router history={history}>
+        <ResearchOutputForm
+          {...defaultProps}
+          researchOutputData={researchOutputData}
+          selectedTeams={[{ value: 'TEAMID', label: 'Example Team' }]}
+          documentType={documentType}
+          typeOptions={Array.from(
+            researchOutputDocumentTypeToType[documentType],
+          )}
+          onSave={saveFn}
+          onSaveDraft={saveDraftFn}
+          getLabSuggestions={getLabSuggestions}
+          getAuthorSuggestions={getAuthorSuggestions}
+          getRelatedResearchSuggestions={getRelatedResearchSuggestions}
+          getShortDescriptionFromDescription={
+            getShortDescriptionFromDescription
+          }
+          researchTags={researchTags}
+          {...propOverride}
+        />
+      </Router>,
+    );
+
+    fireEvent.change(screen.getByLabelText(/url/i), {
+      target: { value: data.link },
+    });
+    fireEvent.change(screen.getByLabelText(/title/i), {
+      target: { value: data.title },
+    });
+
+    const descriptionEditor = screen.getByTestId('editor');
+    userEvent.click(descriptionEditor);
+    userEvent.tab();
+    fireEvent.input(descriptionEditor, { data: data.descriptionMD });
+    userEvent.tab();
+
+    fireEvent.change(
+      screen.getByRole('textbox', { name: /short description/i }),
+      {
+        target: { value: data.shortDescription },
+      },
+    );
+
+    const typeDropdown = screen.getByRole('textbox', {
+      name: /Select the type/i,
+    });
+    fireEvent.change(typeDropdown, {
+      target: { value: data.type },
+    });
+    fireEvent.keyDown(typeDropdown, {
+      keyCode: ENTER_KEYCODE,
+    });
+
+    const identifier = screen.getByRole('textbox', { name: /identifier/i });
+    fireEvent.change(identifier, {
+      target: { value: 'DOI' },
+    });
+    fireEvent.keyDown(identifier, {
+      keyCode: ENTER_KEYCODE,
+    });
+    fireEvent.change(screen.getByPlaceholderText('e.g. 10.5555/YFRU1371'), {
+      target: { value: '10.1234' },
+    });
+
+    // can submit a lab
+
+    userEvent.click(screen.getByRole('textbox', { name: /Labs/i }));
+    userEvent.click(screen.getByText('One Lab'));
+
+    // related research
+    userEvent.click(screen.getByRole('textbox', { name: /Related Outputs/i }));
+    userEvent.click(screen.getByText('First Related Research'));
+
+    // authors
+    const authors = screen.getByRole('textbox', { name: /Authors/i });
+    userEvent.click(authors);
+    userEvent.click(screen.getByText(/Chris Reed/i));
+    userEvent.click(authors);
+    userEvent.click(screen.getByText('Chris Blue'));
+    userEvent.click(authors);
+    userEvent.type(authors, 'Alex White');
+    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
+    userEvent.click(screen.getAllByText('Alex White')[1]!);
+
+    // access instructions
+    userEvent.type(
+      screen.getByRole('textbox', { name: /usage notes/i }),
+      'Access Instructions',
+    );
+
+    await submitForm();
+
+    expect(saveFn).toHaveBeenLastCalledWith({
+      ...expectedRequest,
+      labs: ['1'],
+      relatedResearch: ['1'],
+      authors: [
+        {
+          externalAuthorId: 'u1',
+        },
+        { userId: 'u2' },
+        { externalAuthorName: 'Alex White' },
+      ],
+      usageNotes: 'Access Instructions',
+    });
+  });
+});

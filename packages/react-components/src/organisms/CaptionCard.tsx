c94935e4a2cc3798dfc4ba4482bcbc49140eb5ed
Quentin Leonetti <q.leonetti@gmail.com>
2024-10-30 15:03:58 +0100

    Fix caption card ui for engagement analytics (#4428)
    * solves folding
    
    * linting

--- packages/react-components/src/organisms/CaptionCard.tsx
+++ packages/react-components/src/organisms/CaptionCard.tsx
@@ -1,8 +1,10 @@
 import { css } from '@emotion/react';
 
-import { rem, smallDesktopScreen } from '../pixels';
-import { Card } from '../atoms';
+import { largeDesktopScreen, rem } from '../pixels';
+import { Card, Paragraph } from '../atoms';
 import { contentSidePaddingWithNavigation } from '../layout';
+import { lead } from '../colors';
+import { PercentageIcon } from '../icons';
 
 const cardStyles = css({ marginBottom: rem(32) });
 
@@ -10,7 +12,7 @@ const containerStyles = css({
   padding: rem(24),
   display: 'grid',
   [`@media (max-width: calc(${
-    smallDesktopScreen.width
+    largeDesktopScreen.width
   }px + ${contentSidePaddingWithNavigation(4)}))`]: {
     gridTemplateColumns: '1fr',
   },
@@ -19,12 +21,34 @@ const containerStyles = css({
   columnGap: rem(16),
 });
 
+const captionLegend = css({
+  padding: `0 ${rem(24)} ${rem(24)}`,
+  display: 'grid',
+  gridColumn: '1 / span 2',
+  gridTemplateColumns: `${rem(20)} auto`,
+  gridTemplateRows: 'auto',
+  gap: rem(14),
+  alignItems: 'start',
+  '& p': {
+    color: lead.rgb,
+    marginBlockStart: 0,
+    marginBlockEnd: 0,
+  },
+});
+
 interface CaptionCardProps {
   children: React.ReactNode;
+  legend?: string;
 }
-const CaptionCard: React.FC<CaptionCardProps> = ({ children }) => (
+const CaptionCard: React.FC<CaptionCardProps> = ({ children, legend }) => (
   <Card padding={false} overrideStyles={cardStyles}>
     <div css={containerStyles}>{children}</div>
+    {legend && (
+      <div css={captionLegend}>
+        <PercentageIcon title="percentage" color={lead.rgb} />
+        <Paragraph>{legend}</Paragraph>
+      </div>
+    )}
   </Card>
 );
 export default CaptionCard;

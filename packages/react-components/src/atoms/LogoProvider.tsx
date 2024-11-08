c5088cb882c5adaaac4705779d3532355679233c
Gabriela Ueno <gabiayako@gmail.com>
2024-11-08 13:30:26 -0300

    trying to decrease bundle size

--- packages/react-components/src/atoms/LogoProvider.tsx
+++ packages/react-components/src/atoms/LogoProvider.tsx
@@ -1,15 +1,29 @@
 import type { EmotionJSX } from '@emotion/react/types/jsx-namespace';
-import React, { createContext, useContext } from 'react';
-import { crnLogoFull, gp2LogoFull } from '../icons';
+import React, { Suspense, lazy, createContext, useContext } from 'react';
 
+const loadCRNLogo = () =>
+  import('../icons/crn-logo-full').then((module) => ({
+    default: () => module.default,
+  }));
+const loadGP2Logo = () =>
+  import('../icons/gp2-logo-full').then((module) => ({
+    default: () => module.default,
+  }));
 const LogoContext = createContext<EmotionJSX.Element | null>(null);
+
+const LazyCRNLogo = lazy(loadCRNLogo);
+const LazyGP2Logo = lazy(loadGP2Logo);
 
 export const LogoProvider: React.FC<{ appName: 'CRN' | 'GP2' }> = ({
   appName,
   children,
 }) => {
-  const logo = appName === 'CRN' ? crnLogoFull : gp2LogoFull;
-  return <LogoContext.Provider value={logo}>{children}</LogoContext.Provider>;
+  const logo = appName === 'CRN' ? <LazyCRNLogo /> : <LazyGP2Logo />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LogoContext.Provider value={logo}>{children}</LogoContext.Provider>
+    </Suspense>
+  );
 };
 
 export const useLogo = () => useContext(LogoContext);

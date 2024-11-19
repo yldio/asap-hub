3c2288f4a70cca2e1ea782e8036fde24c5ee8777
AimeurAmin <mohammedamin.aimeur@gmail.com>
2024-11-19 10:34:16 +0000

    usage of text editor markdown instead of remark component

--- packages/react-components/src/organisms/SharedResearchDetailsTagsCard.tsx
+++ packages/react-components/src/organisms/SharedResearchDetailsTagsCard.tsx
@@ -1,7 +1,7 @@
 import { ResearchOutputResponse } from '@asap-hub/model';
 
-import { Card, Divider, Headline2, Markdown, Paragraph } from '../atoms';
 import { RichText, TagList } from '..';
+import { Card, Divider, Headline2, Paragraph, TextEditor } from '../atoms';
 import { perRem } from '../pixels';
 
 type SharedResearchDetailsTagsCardProps = Pick<
@@ -14,34 +14,41 @@ type SharedResearchDetailsTagsCardProps = Pick<
 
 const SharedResearchDetailsTagsCard: React.FC<
   SharedResearchDetailsTagsCardProps
-> = ({ tags, description = '', descriptionMD = '', displayDescription }) => (
-  <Card>
-    {displayDescription && (
-      <div css={{ paddingBottom: `${12 / perRem}em` }}>
-        <Headline2 noMargin>Description</Headline2>
-        <Markdown value={descriptionMD}></Markdown>
-        {descriptionMD === '' && <RichText poorText text={description} />}
-      </div>
-    )}
-    {displayDescription && !!tags.length && <Divider />}
-    {!!tags.length && (
-      <>
-        <Headline2 styleAsHeading={4}>Tags</Headline2>
-        <div
-          css={{
-            marginTop: `${12 / perRem}em`,
-            marginBottom: `${24 / perRem}em`,
-          }}
-        >
-          <Paragraph noMargin accent="lead">
-            Explore keywords related to skills, techniques, resources, and
-            tools.
-          </Paragraph>
+> = ({ tags, description = '', descriptionMD = '', displayDescription }) => {
+  return (
+    <Card>
+      {displayDescription && (
+        <div css={{ paddingBottom: `${12 / perRem}em` }}>
+          <Headline2 noMargin>Description</Headline2>
+          <TextEditor
+            id="description"
+            value={descriptionMD}
+            enabled={false}
+            isMarkdown
+          ></TextEditor>
+          {descriptionMD === '' && <RichText poorText text={description} />}
         </div>
-        <TagList tags={tags} />
-      </>
-    )}
-  </Card>
-);
+      )}
+      {displayDescription && !!tags.length && <Divider />}
+      {!!tags.length && (
+        <>
+          <Headline2 styleAsHeading={4}>Tags</Headline2>
+          <div
+            css={{
+              marginTop: `${12 / perRem}em`,
+              marginBottom: `${24 / perRem}em`,
+            }}
+          >
+            <Paragraph noMargin accent="lead">
+              Explore keywords related to skills, techniques, resources, and
+              tools.
+            </Paragraph>
+          </div>
+          <TagList tags={tags} />
+        </>
+      )}
+    </Card>
+  );
+};
 
 export default SharedResearchDetailsTagsCard;

db496aaa5f7238d8bdabc569ff7008279ed331a8
Gabriela Ueno <gabiayako@gmail.com>
2024-12-13 07:36:57 -0300

    ASAP-813 Allow only lab membership creation on the CMS

---
+++ packages/contentful/migrations/crn/users/20241213103301-remove-labs-link-existing-entries.js
@@ -0,0 +1,19 @@
+module.exports.description = 'Allow only creation of lab membership';
+
+module.exports.up = (migration) => {
+  const users = migration.editContentType('users');
+
+  users.changeFieldControl('labs', 'builtin', 'entryLinksEditor', {
+    showLinkEntityAction: false,
+    showCreateEntityAction: true,
+  });
+};
+
+module.exports.down = (migration) => {
+  const users = migration.editContentType('users');
+
+  users.changeFieldControl('labs', 'builtin', 'entryLinksEditor', {
+    showLinkEntityAction: false,
+    showCreateEntityAction: true,
+  });
+};

3ed7591863528dc7680cb7ddb8ca9b63ec18ec9f
AimeurAmin <mohammedamin.aimeur@gmail.com>
2025-02-10 11:54:17 +0000

    fixes failed tests

--- packages/react-components/src/templates/__tests__/ManuscriptForm.test.tsx
+++ packages/react-components/src/templates/__tests__/ManuscriptForm.test.tsx
@@ -1186,7 +1186,7 @@ describe('manuscript file', () => {
       ComponentProps<typeof ManuscriptForm>['handleFileUpload']
     > = jest.fn();
 
-    const mockFile = new File(['1'.repeat(1024 * 1024 * 25)], 'test.txt', {
+    const mockFile = new File(['1'.repeat(1024 * 1024 * 100)], 'test.txt', {
       type: 'text/plain',
     });
 
@@ -1299,7 +1299,7 @@ describe('key resource table', () => {
       ComponentProps<typeof ManuscriptForm>['handleFileUpload']
     > = jest.fn();
 
-    const mockFile = new File(['1'.repeat(1024 * 1024 * 25)], 'test.txt', {
+    const mockFile = new File(['1'.repeat(1024 * 1024 * 100)], 'test.txt', {
       type: 'text/plain',
     });
 
@@ -1488,7 +1488,7 @@ describe('additional files', () => {
       ComponentProps<typeof ManuscriptForm>['handleFileUpload']
     > = jest.fn();
 
-    const mockFile = new File(['1'.repeat(1024 * 1024 * 25)], 'test.txt', {
+    const mockFile = new File(['1'.repeat(1024 * 1024 * 100)], 'test.txt', {
       type: 'text/plain',
     });
 

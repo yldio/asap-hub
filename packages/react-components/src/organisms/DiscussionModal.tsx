efd2d390c3b7dc0960a3896a2fa4694b8df39859
AkosuaA <nattuah@gmail.com>
2025-04-09 16:05:38 +0100

    [ASAP-954] - update DiscussionModal (#4555)
    * update discussionModal
    
    * fix failing tests
    
    * set background when texteditor is disabled
    
    * minor update
    
    * update styling on DiscussionModal

--- packages/react-components/src/organisms/DiscussionModal.tsx
+++ packages/react-components/src/organisms/DiscussionModal.tsx
@@ -1,43 +1,35 @@
 import { DiscussionCreateRequest } from '@asap-hub/model';
 import { css } from '@emotion/react';
+import { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 
 import { Button, Headline3 } from '../atoms';
-import { crossIcon } from '../icons';
 import { LabeledTextEditor, LabeledTextField, Modal } from '../molecules';
-import { mobileScreen, perRem, rem } from '../pixels';
+import { mobileScreen, rem } from '../pixels';
 
 const headerStyles = css({
   padding: `${rem(32)} ${rem(24)} 0px ${rem(24)}`,
   marginBottom: rem(12),
   display: 'flex',
-  flexDirection: 'row-reverse',
-  justifyContent: 'space-between',
 });
 
-const controlsContainerStyles = css({
-  display: 'flex',
-  alignItems: 'flex-start',
-});
-
-const buttonMediaQuery = `@media (min-width: ${mobileScreen.max - 100}px)`;
+const buttonMediaQuery = `@media (min-width: ${mobileScreen.max + 50}px)`;
 
 const buttonContainerStyles = css({
-  marginTop: `${14 / perRem}em`,
   display: 'grid',
-  columnGap: `${30 / perRem}em`,
-  gridTemplateRows: 'max-content 12px max-content',
+  rowGap: rem(24),
+  gridTemplateRows: 'max-content max-content',
   [buttonMediaQuery]: {
     gridTemplateColumns: 'max-content max-content',
-    gridTemplateRows: 'auto',
     justifyContent: 'flex-end',
+    columnGap: rem(24),
   },
 });
 
 const confirmButtonStyles = css({
   display: 'flex',
   justifyContent: 'center',
-  gridRow: '1 / span 2',
+  gridRow: '1 / span 1',
   gridColumn: '1',
   [buttonMediaQuery]: {
     gridRow: '1',
@@ -47,10 +39,34 @@ const confirmButtonStyles = css({
 const dismissButtonStyles = css({
   display: 'flex',
   justifyContent: 'center',
-  gridRow: '2 / span 2',
+  gridRow: '2 / span 1',
   gridColumn: '1',
   [buttonMediaQuery]: {
     gridRow: '1',
+  },
+});
+
+const buttonOverrideStyles = css({
+  padding: `${rem(15)} ${rem(33)}}`,
+});
+
+const footerStyles = (isCancelling: boolean) =>
+  css({
+    display: 'flex',
+    flexDirection: 'column',
+    paddingTop: rem(15),
+    gap: `${isCancelling ? rem(24) : 0}`,
+    [buttonMediaQuery]: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+  });
+
+const cancelTextStyles = css({
+  fontWeight: 'bold',
+  [buttonMediaQuery]: {
+    maxHeight: rem(60),
+    maxWidth: rem(250),
   },
 });
 
@@ -77,6 +93,7 @@ const DiscussionModal: React.FC<DiscussionModalProps> = ({
       text: '',
     },
   });
+  const [isCancelling, setIsCancelling] = useState(false);
 
   const modalTitle = type === 'start' ? 'Start Discussion' : 'Reply';
 
@@ -95,11 +112,6 @@ const DiscussionModal: React.FC<DiscussionModalProps> = ({
     <form onSubmit={handleSubmit(onSubmit)}>
       <Modal padding={false}>
         <header css={headerStyles}>
-          <div css={controlsContainerStyles}>
-            <Button noMargin small onClick={onDismiss}>
-              {crossIcon}
-            </Button>
-          </div>
           <Headline3 noMargin>{modalTitle}</Headline3>
         </header>
         <div
@@ -124,7 +136,7 @@ const DiscussionModal: React.FC<DiscussionModalProps> = ({
               }) => (
                 <LabeledTextField
                   overrideStyles={css({
-                    paddingBottom: `${20 / perRem}em`,
+                    paddingBottom: rem(20),
                   })}
                   title="Title"
                   subtitle="(required)"
@@ -132,7 +144,7 @@ const DiscussionModal: React.FC<DiscussionModalProps> = ({
                   customValidationMessage={error?.message}
                   required
                   value={value || ''}
-                  enabled={!isSubmitting}
+                  enabled={!(isSubmitting || isCancelling)}
                 />
               )}
             />
@@ -152,28 +164,56 @@ const DiscussionModal: React.FC<DiscussionModalProps> = ({
                 customValidationMessage={error?.message}
                 required
                 value={value || ''}
-                enabled={!isSubmitting}
+                enabled={!(isSubmitting || isCancelling)}
               />
             )}
           />
 
-          <div css={buttonContainerStyles}>
-            <div css={dismissButtonStyles}>
-              <Button noMargin enabled onClick={onDismiss}>
-                Cancel
-              </Button>
+          <div css={footerStyles(isCancelling)}>
+            <div css={cancelTextStyles}>
+              {isCancelling && (
+                <span>
+                  Cancelling now will result in the loss of all entered data. Do
+                  you want to continue?
+                </span>
+              )}
             </div>
-            <div css={confirmButtonStyles}>
-              <Button
-                noMargin
-                primary
-                enabled={!isSubmitting && isValid}
-                submit
-                preventDefault={false}
-                data-testid="discussion-modal-submit"
-              >
-                Send
-              </Button>
+            <div css={buttonContainerStyles}>
+              <div css={dismissButtonStyles}>
+                <Button
+                  noMargin
+                  enabled
+                  overrideStyles={buttonOverrideStyles}
+                  onClick={() => setIsCancelling(!isCancelling)}
+                >
+                  {isCancelling ? 'Keep Editing' : 'Cancel'}
+                </Button>
+              </div>
+              <div css={confirmButtonStyles}>
+                {isCancelling ? (
+                  <Button
+                    warning
+                    noMargin
+                    overrideStyles={buttonOverrideStyles}
+                    enabled
+                    onClick={onDismiss}
+                  >
+                    {type === 'start' ? 'Cancel Discussion' : 'Cancel Reply'}
+                  </Button>
+                ) : (
+                  <Button
+                    noMargin
+                    primary
+                    enabled={!isSubmitting && isValid}
+                    overrideStyles={buttonOverrideStyles}
+                    submit
+                    preventDefault={false}
+                    data-testid="discussion-modal-submit"
+                  >
+                    Send
+                  </Button>
+                )}
+              </div>
             </div>
           </div>
         </div>

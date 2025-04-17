736b7447599b60134623246ab734de99a4db29a2
AkosuaA <nattuah@gmail.com>
2025-04-17 16:34:53 +0100

    added apcRequested field

--- packages/contentful/migrations/crn/manuscripts/20250417143217-add-apc-fields.js
+++ packages/contentful/migrations/crn/manuscripts/20250417143217-add-apc-fields.js
@@ -4,6 +4,18 @@ module.exports.up = (migration) => {
   const manuscripts = migration.editContentType('manuscripts');
 
   manuscripts
+    .createField('apcRequested')
+    .name('APC Requested?')
+    .type('Boolean')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .defaultValue({
+      'en-US': false,
+    })
+    .disabled(false)
+    .omitted(false);
+  manuscripts
     .createField('apcPaid')
     .name('APC Paid?')
     .type('Boolean')
@@ -26,14 +38,17 @@ module.exports.up = (migration) => {
     .disabled(true)
     .omitted(false);
 
+  manuscripts.changeFieldControl('apcRequested', 'builtin', 'boolean', {});
   manuscripts.changeFieldControl('apcPaid', 'builtin', 'boolean', {});
   manuscripts.changeFieldControl('apcAmount', 'builtin', 'numberEditor', {});
-  manuscripts.moveField('apcPaid').afterField('previousStatus');
+  manuscripts.moveField('apcRequested').afterField('previousStatus');
+  manuscripts.moveField('apcPaid').afterField('apcRequested');
   manuscripts.moveField('apcAmount').afterField('apcPaid');
 };
 
 module.exports.down = (migration) => {
   const manuscripts = migration.editContentType('manuscripts');
+  manuscripts.deleteField('apcRequested');
   manuscripts.deleteField('apcPaid');
   manuscripts.deleteField('apcAmount');
 };

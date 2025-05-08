6601399743ebb18fa3f57cfddcbf451fbd3888aa
AkosuaA <nattuah@gmail.com>
2025-05-08 13:34:58 +0100

    [ASAP-889] - add original version id field

---
+++ packages/contentful/migrations/crn/manuscriptVersions/20250501152002-add-original-version-id-field.js
@@ -0,0 +1,22 @@
+module.exports.description =
+  'Add Original Version ID field to Manuscript Version';
+
+module.exports.up = (migration) => {
+  const manuscriptVersions = migration.editContentType('manuscriptVersions');
+  manuscriptVersions
+    .createField('originalVersionId')
+    .name('Original Version Id')
+    .type('Symbol')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false);
+
+  manuscriptVersions.changeFieldControl('originalVersionId', 'app', '2finDNk15g5UtOq4DaLNxv', {});
+};
+
+module.exports.down = (migration) => {
+  const manuscriptVersions = migration.editContentType('manuscriptVersions');
+  manuscriptVersions.deleteField('originalVersionId');
+};

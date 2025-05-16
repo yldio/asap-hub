838f6e74522bfc67b5be7272aa07deb5562d9e5a
AimeurAmin <mohammedamin.aimeur@gmail.com>
2025-05-16 03:25:35 +0100

    fixes form tests

--- packages/react-components/src/templates/ManuscriptForm.tsx
+++ packages/react-components/src/templates/ManuscriptForm.tsx
@@ -1478,7 +1478,7 @@ const ManuscriptForm: React.FC<ManuscriptFormProps> = ({
                                 required: 'Please enter the details.',
                               }}
                               render={({
-                                field: { value, onChange },
+                                field: { value, onChange, onBlur },
                                 fieldState: { error },
                               }) => (
                                 <LabeledTextField
@@ -1488,8 +1488,12 @@ const ManuscriptForm: React.FC<ManuscriptFormProps> = ({
                                   description="The reason you provide must be accepted by the Open Science team."
                                   value={value || ''}
                                   customValidationMessage={error?.message}
-                                  onChange={onChange}
+                                  onChange={async (e) => {
+                                    onChange(e);
+                                    await trigger(`versions.0.${field}Details`);
+                                  }}
                                   enabled={!isEditMode && !isSubmitting}
+                                  onBlur={onBlur}
                                 />
                               )}
                             />

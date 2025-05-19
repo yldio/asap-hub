7f2512d4f61ebda3aa6af51ab5376c7db3e829f1
AimeurAmin <mohammedamin.aimeur@gmail.com>
2025-05-19 11:55:10 +0100

    adds os checklist on the manuscript form

--- packages/react-components/src/molecules/__tests__/FormCard.test.tsx
+++ packages/react-components/src/molecules/__tests__/FormCard.test.tsx
@@ -10,3 +10,12 @@ it('renders the title', () => {
   const { container } = render(<FormCard title="Title">Text</FormCard>);
   expect(container).toHaveTextContent('Title');
 });
+
+it('renders the description when it is a react node', () => {
+  const { container } = render(
+    <FormCard title="Title" description={<span>description</span>}>
+      Text
+    </FormCard>,
+  );
+  expect(container).toHaveTextContent('description');
+});

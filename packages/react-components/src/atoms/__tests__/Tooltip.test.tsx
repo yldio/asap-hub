7838e7533c58ae79a4f7f964e80b8a06397288e9
Gabriela Ueno <gabiayako@gmail.com>
2025-06-26 08:27:15 -0300

    ASAP-983 Add hover text on disabled resubmit manuscript button

--- packages/react-components/src/atoms/__tests__/Tooltip.test.tsx
+++ packages/react-components/src/atoms/__tests__/Tooltip.test.tsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import { rem } from '../../pixels';
 
 import Tooltip from '../Tooltip';
 
@@ -11,4 +12,16 @@ it('shows a tooltip with the children when shown', () => {
   const { getByRole, getByText } = render(<Tooltip shown>text</Tooltip>);
   expect(getByText('text')).toBeVisible();
   expect(getByRole('tooltip')).toHaveTextContent('text');
+});
+
+it('applies width as rem when width is a number', () => {
+  const width = 34;
+  const { getByRole } = render(
+    <Tooltip shown width={width}>
+      text
+    </Tooltip>,
+  );
+  expect(getByRole('tooltip').parentElement).toHaveStyle({
+    width: rem(width),
+  });
 });

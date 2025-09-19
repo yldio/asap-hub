96962cf688bfbfcf8239d7566bf7742ce84ab635
AimeurAmin <mohammedamin.aimeur@gmail.com>
2025-09-19 18:33:05 +0100

    adds react query devtools

--- apps/crn-frontend/src/analytics/collaboration/providers/query-client.provider.tsx
+++ apps/crn-frontend/src/analytics/collaboration/providers/query-client.provider.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactNode, useState } from 'react';
 
 interface QueryClientProviderProps {
@@ -34,9 +34,9 @@ export const AnalyticsQueryClientProvider = ({
     <QueryClientProvider client={queryClient}>
       {children}
       {/* Only show devtools in development */}
-      {/* {process.env.NODE_ENV === 'development' && (
+      {process.env.NODE_ENV === 'development' && (
         <ReactQueryDevtools initialIsOpen={false} />
-      )} */}
+      )}
     </QueryClientProvider>
   );
 };

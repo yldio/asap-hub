df0d9e6223fb0aca2d88953487ff803d6e30171f
Damian Mendez Romera <damianmr@users.noreply.github.com>
2025-10-03 13:08:35 +0100

    WIP: Improve Loading molecule

--- packages/react-components/src/molecules/Loading.tsx
+++ packages/react-components/src/molecules/Loading.tsx
@@ -1,7 +1,35 @@
+import { css } from '@emotion/react';
+import Lottie from 'react-lottie';
 import { Paragraph } from '../atoms';
+import loading from '../lotties/loading.json';
+
+const loadingContainerStyles = css({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  minHeight: '400px',
+  gap: '12px',
+});
 
 const Loading: React.FC<Record<string, never>> = () => (
-  <Paragraph>Loading...</Paragraph>
+  <div css={[loadingContainerStyles]}>
+    <div css={css({ width: 24, height: 24 })}>
+      <Lottie
+        options={{
+          loop: true,
+          autoplay: true,
+          animationData: loading,
+          rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice',
+          },
+        }}
+        height={24}
+        width={24}
+      />
+    </div>
+    <Paragraph noMargin>Loading...</Paragraph>
+  </div>
 );
 
 export default Loading;

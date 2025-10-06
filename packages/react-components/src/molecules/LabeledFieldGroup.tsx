fdb3af77ed9e7d6c27f95fa194a6457f35c63df2
Damian <damianmr@users.noreply.github.com>
2025-10-06 15:36:29 +0100

    WIP: Add LabeledFieldGroup

---
+++ packages/react-components/src/molecules/LabeledFieldGroup.tsx
@@ -0,0 +1,21 @@
+import { css } from '@emotion/react';
+import { ReactNode } from 'react';
+import { rem } from '../pixels';
+
+export type LabeledFieldGroupProps = {
+  children: ReactNode;
+};
+
+const childrenWrapStyles = css({
+  marginTop: rem(32),
+  display: 'flex',
+  flexFlow: 'column',
+  gap: rem(48),
+  background: 'red',
+});
+
+const LabeledFieldGroup: React.FC<LabeledFieldGroupProps> = ({ children }) => (
+  <div css={[childrenWrapStyles]}>{children}</div>
+);
+
+export default LabeledFieldGroup;

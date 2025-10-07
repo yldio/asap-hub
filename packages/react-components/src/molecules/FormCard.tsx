d3d695224a17b2c42c00777e9d28018cfd1e1627
Damian <damianmr@users.noreply.github.com>
2025-10-07 15:19:41 +0100

    WIP: FormCard uses FormSection

--- packages/react-components/src/molecules/FormCard.tsx
+++ packages/react-components/src/molecules/FormCard.tsx
@@ -1,12 +1,11 @@
 import { css } from '@emotion/react';
-import { Card, Headline3, Paragraph } from '../atoms';
+import { Card } from '../atoms';
 import { rem } from '../pixels';
-import LabeledFieldGroup from './LabeledFieldGroup';
+import FormSection from './FormSection';
 
 export type FormCardProps = {
   title?: string;
   description?: string | React.ReactNode;
-  borderless?: boolean;
   headerDecorator?: React.ReactNode;
 };
 
@@ -14,68 +13,25 @@ const cardStyles = css({
   padding: `${rem(32)} ${rem(24)}`,
 });
 
-const titleStyles = css({
-  display: 'flex',
-  flexFlow: 'row',
-  gap: rem(10),
-  alignItems: 'center',
-  justifyContent: 'space-between',
-});
-
-const cardStylesBorderless = css({
-  border: 'none',
-  borderRadius: 0,
-  boxShadow: 'none',
-  padding: 0,
-});
-
-const descriptionStyles = css({
-  paddingTop: rem(24),
-  paddingBottom: 0,
-});
-
-const gapStyle = css({
-  height: 0,
-  marginTop: rem(32),
-});
-
 const FormCard: React.FC<FormCardProps> = ({
   children,
   title,
   description,
-  borderless = false,
   headerDecorator,
 }) => (
   <Card
     padding={false}
     stroke={false}
-    shadow={false}
-    title={title}
-    overrideStyles={css([cardStyles, borderless && cardStylesBorderless])}
+    shadow={true}
+    overrideStyles={css([cardStyles])}
   >
-    <div role="presentation" css={[titleStyles]}>
-      <div style={{ flex: 1 }}>
-        <Headline3 noMargin>{title}</Headline3>
-      </div>
-      <div>{headerDecorator}</div>
-    </div>
-    {!!description && (
-      <div css={[descriptionStyles]}>
-        <Paragraph noMargin accent="lead">
-          {description}
-        </Paragraph>
-      </div>
-    )}
-    {title || description ? (
-      <>
-        <div css={gapStyle}>&nbsp;</div>
-        <LabeledFieldGroup>{children}</LabeledFieldGroup>
-      </>
-    ) : (
-      <>
-        <LabeledFieldGroup>{children}</LabeledFieldGroup>
-      </>
-    )}
+    <FormSection
+      title={title}
+      headerDecorator={headerDecorator}
+      description={description}
+    >
+      {children}
+    </FormSection>
   </Card>
 );
 

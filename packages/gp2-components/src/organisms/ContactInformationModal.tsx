345c5d673d8d5b65e09e0f3991db8d37e0859095
Damian <damianmr@users.noreply.github.com>
2025-10-08 09:58:22 +0100

    [ASAP-1078] - Update ContributingCohortsModal to match designs

--- packages/gp2-components/src/organisms/ContactInformationModal.tsx
+++ packages/gp2-components/src/organisms/ContactInformationModal.tsx
@@ -3,6 +3,7 @@ import {
   LabeledDropdown,
   LabeledTextField,
   pixels,
+  FormSection,
 } from '@asap-hub/react-components';
 import {
   emailExpression,
@@ -72,54 +73,56 @@ const ContactInformationModal: React.FC<ContactInformationModalProps> = ({
     >
       {({ isSaving }) => (
         <>
-          <LabeledTextField
-            title="Institutional Email"
-            description={<ContactSupport />}
-            required
-            enabled={false}
-            value={email}
-          />
-          <LabeledTextField
-            title="Alternative Email"
-            subtitle={optional}
-            description="An alternative way for members to contact you. This will not affect the way that you login."
-            enabled={!isSaving}
-            value={newAlternativeEmail}
-            onChange={setNewAlternativeEmail}
-            type={'email'}
-            pattern={emailExpression}
-            getValidationMessage={() => 'Please enter a valid email address'}
-          />
-          <div css={telephoneContainerStyles}>
-            <div css={css({ flex: `0 0 ${rem(208)}` })}>
-              <LabeledDropdown
-                title="Country Code"
+          <FormSection>
+            <LabeledTextField
+              title="Institutional Email"
+              description={<ContactSupport />}
+              required
+              enabled={false}
+              value={email}
+            />
+            <LabeledTextField
+              title="Alternative Email"
+              subtitle={optional}
+              description="An alternative way for members to contact you. This will not affect the way that you login."
+              enabled={!isSaving}
+              value={newAlternativeEmail}
+              onChange={setNewAlternativeEmail}
+              type={'email'}
+              pattern={emailExpression}
+              getValidationMessage={() => 'Please enter a valid email address'}
+            />
+            <div css={telephoneContainerStyles}>
+              <div css={css({ flex: `0 0 ${rem(208)}` })}>
+                <LabeledDropdown
+                  title="Country Code"
+                  subtitle={optional}
+                  enabled={!isSaving}
+                  value={newCountryCode}
+                  description=" "
+                  onChange={setNewCountryCode}
+                  options={countryCodeSuggestions.map(({ name, dialCode }) => ({
+                    label: `${name} (${dialCode})`,
+                    value: dialCode,
+                  }))}
+                />
+              </div>
+
+              <LabeledTextField
+                title="Telephone Number"
                 subtitle={optional}
+                description="Please note: this will only be visible to admins."
                 enabled={!isSaving}
-                value={newCountryCode}
-                description=" "
-                onChange={setNewCountryCode}
-                options={countryCodeSuggestions.map(({ name, dialCode }) => ({
-                  label: `${name} (${dialCode})`,
-                  value: dialCode,
-                }))}
+                value={newNumber}
+                onChange={setNewNumber}
+                pattern={telephoneNumberExpression}
+                getValidationMessage={() =>
+                  'Please enter a valid telephone number'
+                }
+                type={'tel'}
               />
             </div>
-
-            <LabeledTextField
-              title="Telephone Number"
-              subtitle={optional}
-              description="Please note: this will only be visible to admins."
-              enabled={!isSaving}
-              value={newNumber}
-              onChange={setNewNumber}
-              pattern={telephoneNumberExpression}
-              getValidationMessage={() =>
-                'Please enter a valid telephone number'
-              }
-              type={'tel'}
-            />
-          </div>
+          </FormSection>
           {/* Give extra space to the options rendered above */}
           <div css={{ paddingBottom: rem(80) }} />
         </>

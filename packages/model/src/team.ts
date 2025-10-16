99c1d1f43184ae052966559c036040c7c915b291
AkosuaA <nattuah@gmail.com>
2025-10-16 16:25:11 +0100

    update team integration test fixtures

--- packages/model/src/team.ts
+++ packages/model/src/team.ts
@@ -101,7 +101,7 @@ export type TeamCreateDataObject = {
   tools?: TeamTool[];
   teamId: string;
   grantId: string;
-  type: TeamType;
+  teamType: TeamType;
 };
 
 export type TeamResponse = Omit<TeamDataObject, 'researchTheme'>;

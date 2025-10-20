ffce19688a1e462404648d4e05e067252aa0032b
Damian Mendez Romera <damianmr@users.noreply.github.com>
2025-10-20 11:28:23 +0100

    [ASAP-1217] - Create a warning in yarn lint command if react-hooks don't supply a dependency array
    This can be disabled when the hooks needs to run on every render by
    using `// eslint-disable-next-line no-restricted-syntax`

--- packages/eslint-config-asap-hub/react.js
+++ packages/eslint-config-asap-hub/react.js
@@ -17,6 +17,17 @@ module.exports = {
 
     // testing-library's waitFor is sometimes used in beforeEach and requires an expect inside
     'jest/no-standalone-expect': 'off',
+
+    // Warn when hooks don't include a dependency array (even if empty)
+    'no-restricted-syntax': [
+      'warn',
+      {
+        selector:
+          'CallExpression[callee.name=/^use(Effect|LayoutEffect|ImperativeHandle|Callback|Memo)$/][arguments.length=1]',
+        message:
+          'Consider adding a dependency array to this hook. Omitting it means the effect runs after every render. Use an empty array [] if you want it to run only once on mount.',
+      },
+    ],
   },
   overrides,
 };

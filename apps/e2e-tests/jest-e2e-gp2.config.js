e31b1b4f854fbbb37e553cf2964db43cccbe77b7
AimeurAmin <mohammedamin.aimeur@gmail.com>
2025-10-21 11:57:30 +0100

    separates e2e test for gp2 and crn server

---
+++ apps/e2e-tests/jest-e2e-gp2.config.js
@@ -0,0 +1,17 @@
+const { dirname, basename } = require('path');
+
+const makeDefaultConfig = require('../../jest/make-default-config');
+
+const { testPathIgnorePatterns, ...base } = makeDefaultConfig(
+  dirname(__dirname),
+  basename(__dirname),
+);
+
+module.exports = {
+  ...base,
+
+  rootDir: `${__dirname}/tests/gp2-server`,
+  displayName: 'test-e2e-tests-gp2',
+
+  testMatch: ['**/*.e2e-test.{js,jsx,ts,tsx}'],
+};

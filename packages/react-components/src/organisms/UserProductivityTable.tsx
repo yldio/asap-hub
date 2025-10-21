60f2583b93836dbc8c02376b0b7dab510296e0eb
AimeurAmin <mohammedamin.aimeur@gmail.com>
2025-10-21 02:31:33 +0100

    changes ratio to float instead of string when saving to algolia or exporting to csv

--- packages/react-components/src/organisms/UserProductivityTable.tsx
+++ packages/react-components/src/organisms/UserProductivityTable.tsx
@@ -373,7 +373,8 @@ const UserProductivityTable: React.FC<UserProductivityTableProps> = ({
               </p>
               <span css={[titleStyles, rowTitleStyles]}>Ratio</span>
               <p css={rowValueStyles}>
-                {row.ratio} {getPerformanceIcon(row.ratio, performance.ratio)}
+                {row.ratio.toFixed(2)}{' '}
+                {getPerformanceIcon(row.ratio, performance.ratio)}
               </p>
             </div>
           ))}

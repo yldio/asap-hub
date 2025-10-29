afd06d674934c1da56a8d2f28bb68834d1efdb34
igberty@gmail.com <andrew.hilton@yld.com>
2025-10-29 16:34:42 +0000

    fix: start again, realised yarn was causing problems

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -12,7 +12,7 @@ export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
     <button
       className={className}
-      onClick={() => alert(`Hello from your ${appName} apps!`)}
+      onClick={() => alert(`Hello from your ${appName} app!`)}
     >
       {children}
     </button>

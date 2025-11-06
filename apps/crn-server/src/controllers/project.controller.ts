23ac62ec5b8cfdf7b0eeab216e03600d88f94ec4
AimeurAmin <mohammedamin.aimeur@gmail.com>
2025-11-06 19:03:28 +0000

    fixes format issues

--- apps/crn-server/src/controllers/project.controller.ts
+++ apps/crn-server/src/controllers/project.controller.ts
@@ -45,4 +45,3 @@ export default class ProjectController {
     return project;
   }
 }
-
--- apps/crn-server/src/dependencies/projects.dependencies.ts
+++ apps/crn-server/src/dependencies/projects.dependencies.ts
@@ -16,4 +16,3 @@ export const getProjectDataProvider = (): ProjectDataProvider => {
 
   return new ProjectContentfulDataProvider(contentfulGraphQLClient);
 };
-

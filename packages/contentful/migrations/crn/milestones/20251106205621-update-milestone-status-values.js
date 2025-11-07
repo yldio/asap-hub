482d7486e11bf3f7dad5b1de371e72dc2f50ea5e
AimeurAmin <mohammedamin.aimeur@gmail.com>
2025-11-07 00:17:04 +0000

    fixes linting issues

--- packages/contentful/migrations/crn/milestones/20251106205621-update-milestone-status-values.js
+++ packages/contentful/migrations/crn/milestones/20251106205621-update-milestone-status-values.js
@@ -1,17 +1,12 @@
-module.exports.description = 'Update Milestone Status to include Incomplete and Pending';
+module.exports.description =
+  'Update Milestone Status to include Incomplete and Pending';
 
 module.exports.up = (migration) => {
   const milestones = migration.editContentType('milestones');
 
   milestones.editField('status').validations([
     {
-      in: [
-        'Complete',
-        'In Progress',
-        'Pending',
-        'Incomplete',
-        'Not Started',
-      ],
+      in: ['Complete', 'In Progress', 'Pending', 'Incomplete', 'Not Started'],
     },
   ]);
 };
@@ -25,4 +20,3 @@ module.exports.down = (migration) => {
     },
   ]);
 };
-

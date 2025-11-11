15fab7e488ff6eb0f175cceed044d4f5ac3f9735
Damian Mendez Romera <damianmr@users.noreply.github.com>
2025-11-11 15:13:59 +0000

    [ASAP-1232] - Adapt guides and tutorials content to match new designs

--- packages/react-components/src/templates/DiscoverPage.tsx
+++ packages/react-components/src/templates/DiscoverPage.tsx
@@ -1,20 +1,14 @@
 import { ComponentProps } from 'react';
-import { css } from '@emotion/react';
 
-import { rem } from '../pixels';
 import DiscoverPageHeader from './DiscoverPageHeader';
-import { contentSidePaddingWithNavigation } from '../layout';
-
-const mainStyles = css({
-  padding: `${rem(36)} ${contentSidePaddingWithNavigation(8)}`,
-});
+import PageContraints from './PageConstraints';
 
 type DashboardPageProps = ComponentProps<typeof DiscoverPageHeader>;
 
 const Dashboard: React.FC<DashboardPageProps> = ({ children }) => (
   <article>
     <DiscoverPageHeader />
-    <main css={mainStyles}>{children}</main>
+    <PageContraints as="main">{children}</PageContraints>
   </article>
 );
 
--- packages/react-components/src/templates/DiscoverPageHeader.tsx
+++ packages/react-components/src/templates/DiscoverPageHeader.tsx
@@ -1,28 +1,23 @@
-import { css } from '@emotion/react';
 import { discover } from '@asap-hub/routing';
 
 import { Display, Paragraph } from '../atoms';
-import { rem } from '../pixels';
-import { paper, steel } from '../colors';
-import { contentSidePaddingWithNavigation } from '../layout';
-import { TabNav, TabLink } from '..';
-
-const containerStyles = css({
-  padding: `${rem(36)} ${contentSidePaddingWithNavigation(8)} 0`,
-  background: paper.rgb,
-  boxShadow: `0 2px 4px -2px ${steel.rgb}`,
-});
+import { TabNav, TabLink, PageInfoContainer } from '..';
 
 const DiscoverPageHeader: React.FC = () => (
-  <header css={containerStyles}>
-    <Display styleAsHeading={2}>Guides &amp; Tutorials</Display>
-    <Paragraph accent="lead">
-      Guidance and resources about ASAP’s programs and policies.
-    </Paragraph>
-    <TabNav>
-      <TabLink href={discover({}).guides({}).$}>Guides</TabLink>
-      <TabLink href={discover({}).tutorials({}).$}>Tutorials</TabLink>
-    </TabNav>
+  <header>
+    <PageInfoContainer
+      nav={
+        <TabNav>
+          <TabLink href={discover({}).guides({}).$}>Guides</TabLink>
+          <TabLink href={discover({}).tutorials({}).$}>Tutorials</TabLink>
+        </TabNav>
+      }
+    >
+      <Display styleAsHeading={2}>Guides &amp; Tutorials</Display>
+      <Paragraph accent="lead">
+        Guidance and resources about ASAP’s programs and policies.
+      </Paragraph>
+    </PageInfoContainer>
   </header>
 );
 

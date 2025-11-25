8963bf53eb43e0f68a388e0e11a74ae3eb0aff3e
AimeurAmin <mohammedamin.aimeur@gmail.com>
2025-11-25 20:29:33 +0000

    fixes manuscript submission

--- apps/crn-frontend/src/network/teams/useManuscriptToast.tsx
+++ apps/crn-frontend/src/network/teams/useManuscriptToast.tsx
@@ -2,4 +2,12 @@ import { useContext } from 'react';
 
 import { ManuscriptToastContext } from './ManuscriptToastProvider';
 
-export const useManuscriptToast = () => useContext(ManuscriptToastContext);
+export const useManuscriptToast = () => {
+  const context = useContext(ManuscriptToastContext);
+  if (!context.setFormType) {
+    console.error(
+      'useManuscriptToast must be used within a ManuscriptToastProvider',
+    );
+  }
+  return context;
+};

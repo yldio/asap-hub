d9d5d7685fc373387ee462230aef8b1bd81969d5
AimeurAmin <mohammedamin.aimeur@gmail.com>
2025-11-26 01:55:26 +0000

    fixes some linting isuues

--- apps/crn-frontend/src/network/teams/useManuscriptToast.tsx
+++ apps/crn-frontend/src/network/teams/useManuscriptToast.tsx
@@ -5,6 +5,7 @@ import { ManuscriptToastContext } from './ManuscriptToastProvider';
 export const useManuscriptToast = () => {
   const context = useContext(ManuscriptToastContext);
   if (!context.setFormType) {
+    // eslint-disable-next-line no-console
     console.error(
       'useManuscriptToast must be used within a ManuscriptToastProvider',
     );

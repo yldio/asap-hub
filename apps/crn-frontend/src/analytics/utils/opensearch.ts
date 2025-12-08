442c9bf7d4d668c1601070ddb866918de6eca0b8
Damian Mendez Romera <damianmr@users.noreply.github.com>
2025-12-08 10:08:06 +0000

    [ASAP-1292] - Add getTagSuggestions support for 'user-productivity'

--- apps/crn-frontend/src/analytics/utils/opensearch.ts
+++ apps/crn-frontend/src/analytics/utils/opensearch.ts
@@ -35,38 +35,53 @@ type AggregationBucket = {
   doc_count: number;
 };
 
+type SearchResultAggregation =
+  | {
+      filtered_names: {
+        names: {
+          buckets: AggregationBucket[];
+        };
+      };
+      buckets: never;
+    }
+  | {
+      filtered_names: never;
+      buckets: AggregationBucket[];
+    };
+
 // When there is a search query (filtered results) for tags
 type SearchQueryResultAggregations = {
-  matching_teams?: {
-    teams: {
-      buckets: AggregationBucket[];
-    };
-  };
-  matching_users?: {
-    filtered_names: {
-      names: {
-        buckets: AggregationBucket[];
-      };
-    };
-  };
+  matching_teams?:
+    | {
+        teams: SearchResultAggregation;
+      }
+    | SearchResultAggregation;
+  matching_users?:
+    | {
+        users: SearchResultAggregation;
+      }
+    | SearchResultAggregation;
 };
 
-// When there is NO search query (default results) for tags
-type QuerylessResultAggregations = {
-  teams: {
-    // Assumes there will always be some results (builders return a min number of results)
-    buckets: AggregationBucket[];
-  };
-  users: {
-    names: {
-      // Assumes there will always be some results (builders return a min number of results)
+type ResultAggregation =
+  | {
+      names: { buckets: AggregationBucket[] };
+      buckets: never;
+    }
+  | {
+      names: never;
       buckets: AggregationBucket[];
     };
-  };
+
+// When there is NO search query (default results) for tags
+// Assumes there will always be some results (builders return a min number of results)
+type EmptyQueryResultAggregations = {
+  teams: ResultAggregation;
+  users: ResultAggregation;
 };
 
 type TagSuggestionsResponse = {
-  aggregations: SearchQueryResultAggregations | QuerylessResultAggregations;
+  aggregations: EmptyQueryResultAggregations | SearchQueryResultAggregations;
 };
 
 /**
@@ -397,7 +412,10 @@ const queryBuilderByIndex: Record<
 type TagQueryBuilder = (
   searchQuery: string,
   searchScope: SearchScope,
-) => Record<string, unknown>;
+) => {
+  query: Record<string, unknown>;
+  responseTransformer: (queryResponse: TagSuggestionsResponse) => string[];
+};
 
 export const teamWithUsersRecordsTagQueryBuilder: TagQueryBuilder = (
   searchQuery: string,
@@ -430,8 +448,14 @@ export const teamWithUsersRecordsTagQueryBuilder: TagQueryBuilder = (
     }
 
     return {
-      size: 0,
-      aggs,
+      query: {
+        size: 0,
+        aggs,
+      },
+      responseTransformer: (_queryResponse) => {
+        // temporary stub
+        return [];
+      },
     };
   }
 
@@ -497,14 +521,205 @@ export const teamWithUsersRecordsTagQueryBuilder: TagQueryBuilder = (
   }
 
   return {
-    size: 0,
     query: {
-      bool: {
-        should: shouldClauses,
+      size: 0,
+      query: {
+        bool: {
+          should: shouldClauses,
+        },
+      },
+      aggs,
+    },
+    responseTransformer: (_queryResponse) => {
+      // temporary stub
+      return [];
+    },
+  };
+};
+
+export const userWithTeamsRecordsTagQueryBuilder: TagQueryBuilder = (
+  searchQuery: string,
+  searchScope: SearchScope,
+): ReturnType<TagQueryBuilder> => {
+  if (!searchQuery) {
+    const aggs: Record<string, unknown> = {
+      users: {
+        terms: {
+          field: 'name.keyword',
+          size: 5,
+        },
+      },
+    };
+
+    if (searchScope === 'extended') {
+      aggs.teams = {
+        nested: {
+          path: 'teams',
+        },
+        aggs: {
+          names: {
+            terms: {
+              field: 'teams.team.keyword',
+              size: 10,
+            },
+          },
+        },
+      };
+    }
+
+    return {
+      query: {
+        size: 0,
+        aggs,
+      },
+      responseTransformer: (queryResponse: TagSuggestionsResponse) => {
+        let teams: string[] = [];
+        let users: string[] = [];
+
+        if ('teams' in queryResponse.aggregations) {
+          const aggregations =
+            queryResponse.aggregations as EmptyQueryResultAggregations;
+          teams = aggregations.teams.names.buckets.map((b) => b.key);
+        }
+
+        if ('users' in queryResponse.aggregations) {
+          const aggregations =
+            queryResponse.aggregations as EmptyQueryResultAggregations;
+          users = aggregations.users.buckets.map((b) => b.key);
+        }
+
+        // Returning teams first because it's the current behavior with Algolia.
+        return [...teams, ...users];
+      },
+    };
+  }
+
+  const wildcardPattern = `*${searchQuery.toLowerCase()}*`; // a wildcard pattern works better in the typeahead feature
+
+  const shouldClauses: Record<string, unknown>[] = [
+    {
+      wildcard: {
+        'name.keyword': {
+          value: wildcardPattern,
+          case_insensitive: true,
+        },
       },
     },
-    aggs,
+  ];
+
+  const aggs: Record<string, unknown> = {
+    matching_users: {
+      filter: {
+        wildcard: {
+          'name.keyword': {
+            value: wildcardPattern,
+            case_insensitive: true,
+          },
+        },
+      },
+      aggs: {
+        users: {
+          terms: {
+            field: 'name.keyword',
+            size: 10,
+          },
+        },
+      },
+    },
   };
+
+  if (searchScope === 'extended') {
+    shouldClauses.push({
+      nested: {
+        path: 'teams',
+        query: {
+          wildcard: {
+            'teams.team.keyword': {
+              value: wildcardPattern,
+              case_insensitive: true,
+            },
+          },
+        },
+      },
+    });
+
+    aggs.matching_teams = {
+      nested: {
+        path: 'teams',
+      },
+      aggs: {
+        filtered_names: {
+          filter: {
+            wildcard: {
+              'teams.team.keyword': {
+                value: wildcardPattern,
+                case_insensitive: true,
+              },
+            },
+          },
+          aggs: {
+            names: {
+              terms: {
+                field: 'teams.team.keyword',
+                size: 10,
+              },
+            },
+          },
+        },
+      },
+    };
+  }
+
+  return {
+    query: {
+      size: 0,
+      query: {
+        bool: {
+          should: shouldClauses,
+        },
+      },
+      aggs,
+    },
+    responseTransformer: (queryResponse: TagSuggestionsResponse) => {
+      let teams: string[] = [];
+      let users: string[] = [];
+      if (
+        'matching_users' in queryResponse.aggregations &&
+        queryResponse.aggregations.matching_users // Just for type compliance
+      ) {
+        users =
+          'users' in queryResponse.aggregations.matching_users &&
+          queryResponse.aggregations.matching_users.users
+            ? queryResponse.aggregations.matching_users.users.buckets.map(
+                (b) => b.key,
+              )
+            : [];
+      }
+
+      if (
+        'matching_teams' in queryResponse.aggregations &&
+        queryResponse.aggregations.matching_teams // Just for type compliance
+      ) {
+        teams =
+          'filtered_names' in queryResponse.aggregations.matching_teams &&
+          queryResponse.aggregations.matching_teams.filtered_names
+            ? queryResponse.aggregations.matching_teams.filtered_names.names.buckets.map(
+                (b) => b.key,
+              )
+            : [];
+      }
+
+      // Returning teams first because it's the current behavior with Algolia.
+      return [...teams, ...users];
+    },
+  };
+};
+
+const unsupportedCall: TagQueryBuilder = (
+  _searchQuery: string,
+  _searchScope: SearchScope,
+): ReturnType<TagQueryBuilder> => {
+  throw new Error(`This Opensearch index doesn't support tag suggestions`);
 };
 
 const tagQueryBuilderByIndex: Record<OpensearchIndex, TagQueryBuilder> = {
@@ -513,11 +728,10 @@ const tagQueryBuilderByIndex: Record<OpensearchIndex, TagQueryBuilder> = {
   'preliminary-data-sharing': teamWithUsersRecordsTagQueryBuilder,
   'preprint-compliance': teamWithUsersRecordsTagQueryBuilder,
   'publication-compliance': teamWithUsersRecordsTagQueryBuilder,
-  // temporary stubs to make Typescript happy
-  'user-productivity': teamWithUsersRecordsTagQueryBuilder,
-  'user-productivity-performance': teamWithUsersRecordsTagQueryBuilder,
-  'team-productivity': teamWithUsersRecordsTagQueryBuilder,
-  'team-productivity-performance': teamWithUsersRecordsTagQueryBuilder,
+  'user-productivity': userWithTeamsRecordsTagQueryBuilder,
+  'user-productivity-performance': unsupportedCall,
+  'team-productivity': unsupportedCall,
+  'team-productivity-performance': unsupportedCall,
 };
 
 export class OpensearchClient<T> {
@@ -594,23 +808,11 @@ export class OpensearchClient<T> {
     queryText: string,
     searchScope: SearchScope = 'extended',
   ): Promise<string[]> {
-    const query = tagQueryBuilderByIndex[this.index](queryText, searchScope);
+    const { query, responseTransformer } = tagQueryBuilderByIndex[this.index](
+      queryText,
+      searchScope,
+    );
     const response = await this.request<TagSuggestionsResponse>(query);
-
-    const teams =
-      'teams' in response.aggregations
-        ? response.aggregations.teams.buckets.map((b) => b.key)
-        : (response.aggregations.matching_teams?.teams.buckets.map(
-            (b) => b.key,
-          ) ?? []);
-
-    const users =
-      'users' in response.aggregations
-        ? response.aggregations.users.names.buckets.map((b) => b.key)
-        : (response.aggregations.matching_users?.filtered_names.names.buckets.map(
-            (b) => b.key,
-          ) ?? []);
-
-    return [...teams, ...users];
+    return responseTransformer(response);
   }
 }

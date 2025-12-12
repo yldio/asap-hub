f347cb62e36efed07418d9f273a941d891c7a1f3
nihat arda dura <nihat.dura@yld.com>
2025-12-12 16:33:35 +0300

    [ASAP-1227] - update TeamProfileHeader test to match 'Contact' button text

--- packages/react-components/src/templates/__tests__/TeamProfileHeader.test.tsx
+++ packages/react-components/src/templates/__tests__/TeamProfileHeader.test.tsx
@@ -95,7 +95,7 @@ it('renders a contact button when there is a pointOfContact', () => {
     />,
   );
 
-  expect(screen.getByText('Contact PM').parentElement).toHaveAttribute(
+  expect(screen.getByText('Contact').parentElement).toHaveAttribute(
     'href',
     'mailto:test@test.com',
   );

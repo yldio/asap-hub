82f795a0c033f989150199c5f56ed10411029f5a
Damian Mendez Romera <damianmr@users.noreply.github.com>
2025-12-17 10:13:31 +0000

    [ASAP-1216] - Fix icon color

--- packages/react-components/src/icons/resource-project.tsx
+++ packages/react-components/src/icons/resource-project.tsx
@@ -9,7 +9,7 @@ interface ResourceProjectIconProps {
 const ResourceProjectIcon: React.FC<ResourceProjectIconProps> = ({
   width = 24,
   height = 24,
-  color = '#00202C',
+  color = '#4D646B',
 }) => (
   <svg
     width={width}
--- packages/react-components/src/templates/__tests__/ProjectsPageHeader.test.tsx
+++ packages/react-components/src/templates/__tests__/ProjectsPageHeader.test.tsx
@@ -207,7 +207,7 @@ describe('Tab Icons Default Colors', () => {
   it('renders ResourceProjectIcon with default color', () => {
     const { container } = render(<ResourceProjectIcon />);
     const paths = container.querySelectorAll('path');
-    expect(paths[0]).toHaveAttribute('fill', '#00202C');
+    expect(paths[0]).toHaveAttribute('fill', '#4D646B');
   });
 
   it('renders TraineeProjectIcon with default color', () => {

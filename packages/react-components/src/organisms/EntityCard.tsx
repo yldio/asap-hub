d952b05b94b7d7d81b99de9cdec5c7451ceb0212
Damian Mendez Romera <damianmr@users.noreply.github.com>
2025-12-17 11:01:23 +0000

    [ASAP-1216] - Adapt EntityCard to match current designs

--- packages/react-components/src/organisms/EntityCard.tsx
+++ packages/react-components/src/organisms/EntityCard.tsx
@@ -6,7 +6,7 @@ import { googleDriveIcon } from '../icons';
 import { LinkHeadline, TagList } from '../molecules';
 import { mobileScreen, rem } from '../pixels';
 
-const contentStyles = css({
+const titleContainerStyles = css({
   display: 'flex',
   flexDirection: 'column',
   gap: rem(16),
@@ -26,6 +26,7 @@ const titleStyle = css({
 });
 
 const googleDriveButtonStyle = css({
+  display: 'block',
   '> a': {
     backgroundColor: 'transparent',
   },
@@ -53,7 +54,18 @@ const iconStyles = css({
 const pillsStyles = css({
   display: 'flex',
   gap: rem(8),
-  height: '3rem',
+});
+
+const gap24Styles = css({
+  display: 'flex',
+  flexFlow: 'column',
+  gap: rem(24),
+});
+
+const gap16Styles = css({
+  display: 'flex',
+  flexFlow: 'column',
+  gap: rem(16),
 });
 
 type CardWrapperProps = {
@@ -73,6 +85,10 @@ type CardWrapperProps = {
   readonly isTeamCard?: boolean;
 };
 
+const containerStyles = css({
+  padding: `${rem(32)} ${rem(24)}`,
+});
+
 const EntityCard: React.FC<CardWrapperProps> = ({
   active = true,
   footer,
@@ -89,58 +105,68 @@ const EntityCard: React.FC<CardWrapperProps> = ({
   textHref,
   isTeamCard,
 }) => (
-  <Card accent={active ? 'default' : 'neutral200'}>
-    {(teamType || researchTheme || resourceType) && (
-      <div css={pillsStyles}>
-        {teamType && <Pill noMargin>{teamType}</Pill>}
-        {researchTheme && <Pill noMargin>{researchTheme}</Pill>}
-        {resourceType && <Pill noMargin>{resourceType}</Pill>}
-      </div>
-    )}
-    <div css={contentStyles}>
-      <div css={titleStyle}>
-        <LinkHeadline level={2} styleAsHeading={4} href={href} noMargin>
-          {title}
-        </LinkHeadline>
-        {!active && inactiveBadge}
-      </div>
-      {googleDrive && (
-        <span css={googleDriveButtonStyle}>
-          <Link href={googleDrive} buttonStyle small noMargin>
-            {googleDriveIcon} Access Drive
-          </Link>
-        </span>
-      )}
-      {!!text && (
-        <div css={textStyles}>
-          {textIcon && <span css={iconStyles}>{textIcon}</span>}
-          {(() => {
-            const ellipsisContent = (
-              <Ellipsis numberOfLines={2}>{text}</Ellipsis>
-            );
+  <Card
+    accent={active ? 'default' : 'neutral200'}
+    padding={false}
+    overrideStyles={containerStyles}
+  >
+    <div css={gap24Styles}>
+      <div css={gap16Styles}>
+        {(teamType || researchTheme || resourceType) && (
+          <div css={pillsStyles}>
+            {teamType && <Pill noMargin>{teamType}</Pill>}
+            {researchTheme && <Pill noMargin>{researchTheme}</Pill>}
+            {resourceType && <Pill noMargin>{resourceType}</Pill>}
+          </div>
+        )}
+        <div css={titleContainerStyles}>
+          <div css={titleStyle}>
+            <LinkHeadline level={2} styleAsHeading={4} href={href} noMargin>
+              {title}
+            </LinkHeadline>
+            {!active && inactiveBadge}
+          </div>
+          {googleDrive && (
+            <span css={googleDriveButtonStyle}>
+              <Link href={googleDrive} buttonStyle small noMargin>
+                {googleDriveIcon} Access Drive
+              </Link>
+            </span>
+          )}
+        </div>
+        {!!text && (
+          <div css={textStyles}>
+            {textIcon && <span css={iconStyles}>{textIcon}</span>}
+            {(() => {
+              const ellipsisContent = (
+                <Ellipsis numberOfLines={2}>{text}</Ellipsis>
+              );
 
-            if (isTeamCard) {
-              return textHref ? (
-                <Anchor href={textHref}>{ellipsisContent}</Anchor>
+              if (isTeamCard) {
+                return textHref ? (
+                  <Anchor href={textHref}>{ellipsisContent}</Anchor>
+                ) : (
+                  <span css={plainTextStyles}>{ellipsisContent}</span>
+                );
+              }
+
+              return href ? (
+                <Anchor href={href}>{ellipsisContent}</Anchor>
               ) : (
                 <span css={plainTextStyles}>{ellipsisContent}</span>
               );
-            }
-
-            return href ? (
-              <Anchor href={href}>{ellipsisContent}</Anchor>
-            ) : (
-              <span css={plainTextStyles}>{ellipsisContent}</span>
-            );
-          })()}
-        </div>
-      )}
-      {!!tags.length && (
-        <div css={tagsContainerStyles}>
-          <TagList max={3} tags={tags} />
-        </div>
-      )}
-      {footer}
+            })()}
+          </div>
+        )}
+      </div>
+      <div css={css(gap24Styles, { ':empty': { display: 'none' } })}>
+        {!!tags.length && (
+          <div css={tagsContainerStyles}>
+            <TagList max={3} tags={tags} />
+          </div>
+        )}
+        {footer}
+      </div>
     </div>
   </Card>
 );

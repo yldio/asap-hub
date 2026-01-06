feeb920c4dbb67f7691f04c4f491af047b3d7831
nihat arda dura <ardadura@192.168.1.102>
2026-01-06 20:29:56 +0000

    fix(ASAP-1322): fix text v2

--- apps/gp2-frontend/src/onboarding/__tests__/Preview.test.tsx
+++ apps/gp2-frontend/src/onboarding/__tests__/Preview.test.tsx
@@ -185,7 +185,13 @@ describe('Preview', () => {
       time_taken: 0,
       items: [
         {
-          name: 'ExampleInst',
+          names: [
+            {
+              value: 'ExampleInst',
+              types: ['ror_display', 'label'],
+              lang: 'en',
+            },
+          ],
           id: 'id-1',
           email_address: 'example@example.com',
           status: '',
--- apps/gp2-frontend/src/users/__tests__/UserDetail.test.tsx
+++ apps/gp2-frontend/src/users/__tests__/UserDetail.test.tsx
@@ -396,7 +396,13 @@ describe('UserDetail', () => {
         time_taken: 0,
         items: [
           {
-            name: 'ExampleInst',
+            names: [
+              {
+                value: 'ExampleInst',
+                types: ['ror_display', 'label'],
+                lang: 'en',
+              },
+            ],
             id: 'id-1',
             email_address: 'example@example.com',
             status: '',

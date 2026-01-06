4ea07e3c616ed4587f32795714076ac548919b53
Damian Mendez Romera <damianmr@users.noreply.github.com>
2026-01-06 11:41:31 +0000

    [ASAP-1227] - Fix test

--- packages/react-components/src/organisms/__tests__/TeamProfileOverview.test.tsx
+++ packages/react-components/src/organisms/__tests__/TeamProfileOverview.test.tsx
@@ -28,46 +28,4 @@ it('does not render tags when empty', () => {
   );
 
   expect(queryByText('Tags')).toBeNull();
-  expect(
-    getByRole('button', { name: /Supplement Grant/i }),
-  ).toBeInTheDocument();
-
-  expect(getByText(supplementGrant.title)).toBeInTheDocument();
-  expect(getByText(supplementGrant.description)).toBeInTheDocument();
-  expect(
-    getByRole('link', { name: /read full proposal/i }),
-  ).toBeInTheDocument();
-});
-
-it('renders original grant when user clicks on original grant tab', async () => {
-  const { queryByText, getByRole, getByText } = render(
-    <TeamProfileOverview
-      projectTitle="Original Title"
-      projectSummary="Original Summary"
-      supplementGrant={supplementGrant}
-    />,
-  );
-
-  await userEvent.click(getByRole('button', { name: /Original Grant/i }));
-
-  expect(getByText('Original Title')).toBeInTheDocument();
-  expect(getByText('Original Summary')).toBeInTheDocument();
-
-  expect(queryByText(supplementGrant.title)).not.toBeInTheDocument();
-  expect(queryByText(supplementGrant.description)).not.toBeInTheDocument();
-});
-
-it('does not show read proposal if no proposal available', () => {
-  const { rerender, queryByText } = render(
-    <TeamProfileOverview
-      projectTitle="Title"
-      projectSummary="Summary"
-      proposalURL="42"
-    />,
-  );
-  expect(queryByText('Read Proposal')).toBeDefined();
-  rerender(
-    <TeamProfileOverview projectTitle="Title" projectSummary="Summary" />,
-  );
-  expect(queryByText('Read Proposal')).toBe(null);
 });

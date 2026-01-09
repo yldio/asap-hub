83147187c799893e0557507e190b2b8fb43ddb9e
AimeurAmin <mohammedamin.aimeur@gmail.com>
2026-01-09 14:58:16 +0000

    fixes manuscript form test failing randomly due to suspense

--- packages/react-components/src/templates/__tests__/ManuscriptForm.other.test.tsx
+++ packages/react-components/src/templates/__tests__/ManuscriptForm.other.test.tsx
@@ -141,7 +141,7 @@ describe('authors', () => {
         },
       ]);
 
-      const { getByText, findByRole, findByLabelText } = render(
+      const { getByText, findByRole, findByLabelText, queryByText } = render(
         <StaticRouter location="/">
           <Suspense fallback={<div>Loading...</div>}>
             <ManuscriptForm
@@ -169,9 +169,10 @@ describe('authors', () => {
         </StaticRouter>,
       );
 
-      // Wait for the form section to be ready - findByLabelText waits for the element to appear
-      // This avoids race conditions with Suspense fallbacks in CI environments
-      const sectionInput = await findByLabelText(section);
+      const sectionInput = await findByLabelText(section, undefined, {
+        timeout: 10000,
+      });
+
       await userEvent.click(sectionInput);
       await userEvent.click(getByText('Author One'));
 

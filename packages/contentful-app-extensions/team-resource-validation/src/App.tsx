4a22cefa85ff3e36c9318bbd50e126a6e9a3c2c0
nihat arda dura <ardadura@192.168.1.117>
2026-01-22 18:01:23 +0000

    new fiedls added to the main screen

--- packages/contentful-app-extensions/team-resource-validation/src/App.tsx
+++ packages/contentful-app-extensions/team-resource-validation/src/App.tsx
@@ -1,12 +1,16 @@
 import React, { useMemo } from 'react';
 import { locations } from '@contentful/app-sdk';
 import { useSDK } from '@contentful/react-apps-toolkit';
+import Field from './locations/Field';
 import Sidebar from './locations/Sidebar';
 
 const App = () => {
   const sdk = useSDK();
 
   const Component = useMemo(() => {
+    if (sdk.location.is(locations.LOCATION_ENTRY_FIELD)) {
+      return Field;
+    }
     if (sdk.location.is(locations.LOCATION_ENTRY_SIDEBAR)) {
       return Sidebar;
     }

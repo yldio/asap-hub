001b9324e7d0188dc502b1a87cba2381b72464a7
nihat arda dura <ardadura@192.168.1.120>
2026-01-27 16:10:33 +0000

    small ui changes and code changes

--- packages/react-components/src/organisms/TeamResourcesCard.tsx
+++ packages/react-components/src/organisms/TeamResourcesCard.tsx
@@ -6,7 +6,6 @@ import { createMailTo } from '../mail';
 import { Card, Headline2, Paragraph, Link } from '../atoms';
 import { ExpandableText } from '../molecules';
 import { googleDriveIcon } from '../icons';
-import { fern, pine, steel, paper, colorWithTransparency } from '../colors';
 import { rem } from '../pixels';
 
 const cardStyles = css({
@@ -23,16 +22,11 @@ const buttonsContainerStyles = css({
   display: 'flex',
   gap: rem(12),
   flexWrap: 'wrap',
-  marginTop: rem(16),
 });
 
 const googleDriveButtonStyle = css({
   display: 'block',
   '> a': {
-    borderRadius: rem(4),
-    border: `1px solid ${steel.rgb}`,
-    backgroundColor: paper.rgb,
-    boxShadow: `0 2px 4px -2px ${colorWithTransparency(steel, 0.3).rgba}`,
     height: rem(40),
     display: 'flex',
     alignItems: 'center',
@@ -43,15 +37,15 @@ const googleDriveButtonStyle = css({
 const primaryButtonStyle = css({
   display: 'block',
   '> a': {
-    borderRadius: rem(4),
-    border: `1px solid ${pine.rgb}`,
-    backgroundColor: fern.rgb,
-    boxShadow: `0 2px 4px -2px ${colorWithTransparency(pine, 0.25).rgba}`,
     height: rem(40),
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
   },
+});
+
+const descriptionStyles = css({
+  overflowWrap: 'anywhere',
 });
 
 type TeamResourcesCardProps = Pick<
@@ -89,7 +83,9 @@ const TeamResourcesCard: React.FC<TeamResourcesCardProps> = ({
 
         {resourceDescription && (
           <ExpandableText variant="arrow">
-            <Paragraph noMargin>{resourceDescription}</Paragraph>
+            <Paragraph noMargin css={descriptionStyles}>
+              {resourceDescription}
+            </Paragraph>
           </ExpandableText>
         )}
 

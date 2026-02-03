5ffd7a24118cb47c5d45cc7380c74cff582a5e3a
nihat arda dura <ardadura@192.168.1.120>
2026-02-03 17:01:27 +0000

    new approach for helpText

--- packages/contentful/migrations/crn/projectMembership/20260203164356-helper-text-projectmembership-members-field.js
+++ packages/contentful/migrations/crn/projectMembership/20260203164356-helper-text-projectmembership-members-field.js
@@ -3,15 +3,17 @@ module.exports.description = 'Add helper text to project membership projectMembe
 module.exports.up = (migration) => {
   const contentTypes = migration.editContentType('projectMembership');
 
-  contentTypes
-    .editField('projectMember')
-    .helpText(
+  contentTypes.changeFieldControl('projectMember', 'builtin', 'entryLinkEditor', {
+    helpText:
       'User-based projects: add users only. Team-based projects: add teams only.',
-    );
+  });
 };
 
 module.exports.down = (migration) => {
   const contentTypes = migration.editContentType('projectMembership');
 
-  contentTypes.editField('projectMember').helpText('');
+  contentTypes.changeFieldControl('projectMember', 'builtin', 'entryLinkEditor', {
+    helpText:
+      '',
+  });
 };
--- packages/contentful/migrations/crn/projects/20260203155618-helper-text-project-members-field.js
+++ packages/contentful/migrations/crn/projects/20260203155618-helper-text-project-members-field.js
@@ -3,16 +3,21 @@ module.exports.description = 'Add helper text to project members field';
 module.exports.up = (migration) => {
   const contentTypes = migration.editContentType('projects');
 
-  contentTypes
-    .editField('members')
-    .name('Members (Users and Teams)')
-    .helpText(
+  contentTypes.editField('members').name('Members (Users and Teams)');
+
+  contentTypes.changeFieldControl('members', 'builtin', 'entryLinksEditor', {
+    helpText:
       'User-based projects: add users only. Team-based projects: add teams only.',
-    );
+  });
 };
 
 module.exports.down = (migration) => {
   const contentTypes = migration.editContentType('projects');
 
-  contentTypes.editField('members').name('Members').helpText('');
+  contentTypes.editField('members').name('Members');
+
+  contentTypes.changeFieldControl('members', 'builtin', 'entryLinksEditor', {
+    helpText:
+      '',
+  });
 };

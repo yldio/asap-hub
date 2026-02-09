d19866913c74559886b7c781cebd8207f4c46734
nihat arda dura <ardadura@192.168.1.117>
2026-02-09 17:27:45 +0000

    Add migration to group resource fields into collapsible fieldset
    Uses Migration API's createEditorLayout and createFieldGroup to organize
    resource fields (resourceTitle, resourceDescription, resourceButtonCopy,
    resourceContactEmail, resourceLink) into a collapsible "Resource Section"
    fieldset in the Teams content type.

---
+++ packages/contentful/migrations/crn/teams/20260209120000-group-resource-fields.js
@@ -0,0 +1,50 @@
+module.exports.description = 'Group resource fields into a collapsible fieldset';
+
+module.exports.up = (migration) => {
+  const teams = migration.editContentType('teams');
+
+  // Create editor layout with field groups
+  const editorLayout = teams.createEditorLayout();
+
+  // Create a fieldset for resource fields
+  editorLayout.createFieldGroup('resourceGroup', {
+    name: 'Resource Section',
+  });
+
+  // Change the field group control to be a collapsible fieldset
+  teams.changeFieldGroupControl('resourceGroup', 'builtin', 'fieldset', {
+    helpText: 'Fields related to team resources',
+    collapsedByDefault: false,
+  });
+
+  // Move resource fields into the group
+  editorLayout
+    .editFieldGroup('resourceGroup')
+    .moveField('resourceTitle')
+    .toTheTopOfFieldGroup();
+
+  editorLayout
+    .editFieldGroup('resourceGroup')
+    .moveField('resourceDescription')
+    .toTheBottomOfFieldGroup();
+
+  editorLayout
+    .editFieldGroup('resourceGroup')
+    .moveField('resourceButtonCopy')
+    .toTheBottomOfFieldGroup();
+
+  editorLayout
+    .editFieldGroup('resourceGroup')
+    .moveField('resourceContactEmail')
+    .toTheBottomOfFieldGroup();
+
+  editorLayout
+    .editFieldGroup('resourceGroup')
+    .moveField('resourceLink')
+    .toTheBottomOfFieldGroup();
+};
+
+module.exports.down = (migration) => {
+  const teams = migration.editContentType('teams');
+  teams.deleteEditorLayout();
+};

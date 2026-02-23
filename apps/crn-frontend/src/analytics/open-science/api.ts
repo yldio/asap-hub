cd98f892e272827523fe4d27409dc82f885ea870
AimeurAmin <mohammedamin.aimeur@gmail.com>
2026-02-23 12:55:09 +0000

    adds sort support to publication compliance api

--- apps/crn-frontend/src/analytics/open-science/api.ts
+++ apps/crn-frontend/src/analytics/open-science/api.ts
@@ -8,6 +8,7 @@ import {
   PublicationComplianceOpensearchResponse,
 } from '@asap-hub/model';
 import { OpensearchClient } from '../utils/opensearch';
+import { OpensearchSortMap } from '../utils/opensearch/types';
 
 export const getPreprintCompliance = async (
   opensearchClient: OpensearchClient<PreprintComplianceOpensearchResponse>,
@@ -23,16 +24,33 @@ export const getPreprintCompliance = async (
   });
 };
 
+const publicationComplianceOpensearchSort: OpensearchSortMap<SortPublicationCompliance> =
+  {
+    team_asc: [{ 'teamName.keyword': { order: 'asc' } }],
+    team_desc: [{ 'teamName.keyword': { order: 'desc' } }],
+    publications_asc: [{ overallCompliance: { order: 'asc' } }],
+    publications_desc: [{ overallCompliance: { order: 'desc' } }],
+    datasets_asc: [{ datasetsPercentage: { order: 'asc' } }],
+    datasets_desc: [{ datasetsPercentage: { order: 'desc' } }],
+    protocols_asc: [{ protocolsPercentage: { order: 'asc' } }],
+    protocols_desc: [{ protocolsPercentage: { order: 'desc' } }],
+    code_asc: [{ codePercentage: { order: 'asc' } }],
+    code_desc: [{ codePercentage: { order: 'desc' } }],
+    lab_materials_asc: [{ labMaterialsPercentage: { order: 'asc' } }],
+    lab_materials_desc: [{ labMaterialsPercentage: { order: 'desc' } }],
+  };
+
 export const getPublicationCompliance = async (
   opensearchClient: OpensearchClient<PublicationComplianceOpensearchResponse>,
   options: AnalyticsSearchOptionsWithFiltering<SortPublicationCompliance>,
 ): Promise<ListPublicationComplianceOpensearchResponse | undefined> => {
-  const { tags, currentPage, pageSize, timeRange } = options;
+  const { tags, currentPage, pageSize, timeRange, sort } = options;
   return opensearchClient.search({
     searchTags: tags,
     currentPage: currentPage ?? undefined,
     pageSize: pageSize ?? undefined,
     timeRange,
     searchScope: 'flat',
+    sort: sort ? publicationComplianceOpensearchSort[sort] : undefined,
   });
 };

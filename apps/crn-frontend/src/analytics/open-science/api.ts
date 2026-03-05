f6d78030e71a70b3064ffe5a3a0dbde2ab7dd484
AimeurAmin <mohammedamin.aimeur@gmail.com>
2026-03-05 12:05:58 +0000

    adds sort option to preprint compliance api

--- apps/crn-frontend/src/analytics/open-science/api.ts
+++ apps/crn-frontend/src/analytics/open-science/api.ts
@@ -10,17 +10,56 @@ import {
 import { OpensearchClient } from '../utils/opensearch';
 import { OpensearchSortMap } from '../utils/opensearch/types';
 
+const preprintComplianceOpensearchSort: OpensearchSortMap<SortPreprintCompliance> =
+  {
+    team_asc: [{ 'teamName.keyword': { order: 'asc' } }],
+    team_desc: [{ 'teamName.keyword': { order: 'desc' } }],
+    number_of_preprints_asc: [
+      {
+        numberOfPreprints: {
+          order: 'asc',
+          missing: '_last',
+        },
+      },
+    ],
+    number_of_preprints_desc: [
+      {
+        numberOfPreprints: {
+          order: 'desc',
+          missing: '_last',
+        },
+      },
+    ],
+    posted_prior_asc: [
+      {
+        postedPriorPercentage: {
+          order: 'asc',
+          missing: '_first',
+        },
+      },
+    ],
+    posted_prior_desc: [
+      {
+        postedPriorPercentage: {
+          order: 'desc',
+          missing: '_last',
+        },
+      },
+    ],
+  };
+
 export const getPreprintCompliance = async (
   opensearchClient: OpensearchClient<PreprintComplianceOpensearchResponse>,
   options: AnalyticsSearchOptionsWithFiltering<SortPreprintCompliance>,
 ): Promise<ListPreprintComplianceOpensearchResponse | undefined> => {
-  const { tags, currentPage, pageSize, timeRange } = options;
+  const { tags, currentPage, pageSize, timeRange, sort } = options;
   return opensearchClient.search({
     searchTags: tags,
     currentPage: currentPage ?? undefined,
     pageSize: pageSize ?? undefined,
     timeRange,
     searchScope: 'flat',
+    sort: sort ? preprintComplianceOpensearchSort[sort] : undefined,
   });
 };
 

7050ba0070608d028b6fa22a5fc99b860a43dc42
AimeurAmin <mohammedamin.aimeur@gmail.com>
2026-03-09 12:44:47 +0000

    creates project milestones flag

--- packages/flags/src/index.ts
+++ packages/flags/src/index.ts
@@ -4,7 +4,8 @@ export type Flag =
   | 'OPENSEARCH_METRICS'
   | 'DATA_MANAGER_ROLE_ENABLED'
   | 'PROJECT_WORKSPACE'
-  | 'PROJECT_AIMS';
+  | 'PROJECT_AIMS'
+  | 'PROJECT_MILESTONES';
 
 export type Flags = Partial<Record<Flag, boolean | string | undefined>>;
 let overrides: Flags = {

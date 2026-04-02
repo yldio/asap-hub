bd6b93bb3f082784fbfc5e9ada522ade57ad45ac
nihat arda dura <ardadura@192.168.1.102>
2026-04-02 10:06:21 +0100

    [ASAP-1442] Add Scientific Facilitator field to Projects content model

---
+++ packages/contentful/migrations/crn/projects/20260402120000-add-scientific-facilitator-field.js
@@ -0,0 +1,42 @@
+module.exports.description =
+  'Add Scientific Facilitator field to projects';
+
+module.exports.up = (migration) => {
+  const projects = migration.editContentType('projects');
+
+  projects
+    .createField('scientificFacilitator')
+    .name('Scientific Facilitator')
+    .type('Array')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Link',
+      validations: [
+        {
+          linkContentType: ['users'],
+        },
+      ],
+      linkType: 'Entry',
+    });
+
+  projects.changeFieldControl(
+    'scientificFacilitator',
+    'builtin',
+    'entryLinksEditor',
+    {
+      showLinkEntityAction: true,
+      showCreateEntityAction: false,
+    },
+  );
+
+  projects.moveField('scientificFacilitator').afterField('members');
+};
+
+module.exports.down = (migration) => {
+  const projects = migration.editContentType('projects');
+  projects.deleteField('scientificFacilitator');
+};

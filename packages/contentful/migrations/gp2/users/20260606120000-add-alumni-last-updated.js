aeb65f7d8de071b8d68e6556402f7ca5278bd124
Diego Nascimento <diegonascimento@hotmail.com.br>
2026-06-05 15:58:01 +0100

    Drop alumniSinceDate/alumniLocation (handled by #5049), keep alumniLastUpdated

---
+++ packages/contentful/migrations/gp2/users/20260606120000-add-alumni-last-updated.js
@@ -0,0 +1,31 @@
+module.exports.description = 'Add alumniLastUpdated to users';
+
+module.exports.up = (migration) => {
+  const users = migration.editContentType('users');
+
+  users
+    .createField('alumniLastUpdated')
+    .name('Alumni Last Updated')
+    .type('Date')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(true)
+    .omitted(false);
+
+  users.changeFieldControl(
+    'alumniLastUpdated',
+    'app',
+    'mqvX9KU5AthRTlnIRhNNh',
+    {
+      observedField: 'alumniSinceDate,alumniLocation',
+    },
+  );
+
+  users.moveField('alumniLastUpdated').afterField('alumniLocation');
+};
+
+module.exports.down = (migration) => {
+  const users = migration.editContentType('users');
+  users.deleteField('alumniLastUpdated');
+};

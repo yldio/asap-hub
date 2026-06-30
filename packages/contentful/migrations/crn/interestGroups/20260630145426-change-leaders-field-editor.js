3b971f5ae50347d2d8a2130e3264fa030d7e6974
Gabriela Ueno <gabiayako@gmail.com>
2026-06-30 12:51:21 -0300

    ASAP-964 Display Leaders names in CMS

---
+++ packages/contentful/migrations/crn/interestGroups/20260630145426-change-leaders-field-editor.js
@@ -0,0 +1,19 @@
+module.exports.description = 'Change leaders field editor';
+
+module.exports.up = (migration) => {
+  const interestGroups = migration.editContentType('interestGroups');
+  interestGroups.changeFieldControl('leaders', 'app', 'Yp64pYYDuRNHdvAAAJPYa', {
+    entityName: 'user',
+    showUserEmail: true,
+    showLinkEntityAction: true,
+    showCreateEntityAction: false,
+  });
+};
+
+module.exports.down = (migration) => {
+  const interestGroups = migration.editContentType('interestGroups');
+  interestGroups.changeFieldControl('leaders', 'builtin', 'entryLinksEditor', {
+    showLinkEntityAction: true,
+    showCreateEntityAction: false,
+  });
+};

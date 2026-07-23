8f40ce98f5db3f9bef23cbda6bed57db4a7f68d7
AimeurAmin <mohammedamin.aimeur@gmail.com>
2026-07-23 07:21:06 +0100

    display attendance on past event page

--- apps/crn-frontend/src/events/Event.tsx
+++ apps/crn-frontend/src/events/Event.tsx
@@ -1,4 +1,5 @@
 import {
+  EventAttendance,
   EventConversation,
   EventDetailPage,
   eventMapper,
@@ -6,16 +7,32 @@ import {
   EventPage,
   getIconForDocumentType,
   NotFoundPage,
+  noop,
   SpeakerList,
   useDateHasPassed,
   considerEndedAfter,
   PageConstraints,
 } from '@asap-hub/react-components';
-import { useFlags } from '@asap-hub/react-context';
+import { useCurrentUserCRN, useFlags } from '@asap-hub/react-context';
+import { EventResponse } from '@asap-hub/model';
 import { events, useRouteParams } from '@asap-hub/routing';
 import { Frame, useBackHref } from '@asap-hub/frontend-utils';
 
 import { useEventById, useQuietRefreshEventById } from './state';
+
+const mapAttendanceTeams = (attendance: EventResponse['attendance'] = []) =>
+  attendance.map(({ team, attended }) => ({
+    teamId: team.id,
+    teamName: team.displayName,
+    attended,
+    teamType:
+      team.teamType === 'Discovery Team'
+        ? ('discovery' as const)
+        : team.teamType === 'Resource Team'
+          ? ('resource' as const)
+          : undefined,
+    isTeamInactive: !!team.inactiveSince,
+  }));
 
 const Event: React.FC = () => {
   const { eventId } = useRouteParams(events({}).event);
@@ -23,6 +40,7 @@ const Event: React.FC = () => {
   const refreshEvent = useQuietRefreshEventById(eventId);
   const backHref = useBackHref() ?? events({}).$;
   const { isEnabled } = useFlags();
+  const user = useCurrentUserCRN();
 
   const hasFinished = useDateHasPassed(
     considerEndedAfter(event?.endDate || ''),
@@ -31,6 +49,29 @@ const Event: React.FC = () => {
   if (event) {
     const displayCalendar =
       event.interestGroup === undefined || event.interestGroup.active;
+
+    const teams = mapAttendanceTeams(event.attendance);
+    const teamsTotal = teams.length;
+    const teamsAttended = teams.filter(({ attended }) => attended).length;
+    const isEventProjectManager = !!user?.interestGroups.some(
+      (ig) =>
+        ig.id === event.interestGroup?.id && ig.role === 'Project Manager',
+    );
+    const attendance = hasFinished ? (
+      <EventAttendance
+        teamsAttended={teamsAttended}
+        teamsTotal={teamsTotal}
+        teams={teams}
+        sinceLastEvent={
+          event.previousEventAttendance && {
+            count: teamsAttended - event.previousEventAttendance.teamsAttended,
+            teamsAttended: event.previousEventAttendance.teamsAttended,
+            teamsTotal: event.previousEventAttendance.teamsTotal,
+          }
+        }
+        onAddAttendance={isEventProjectManager ? noop : undefined}
+      />
+    ) : undefined;
 
     if (isEnabled('NEW_EVENT_PAGE')) {
       return (
@@ -43,6 +84,7 @@ const Event: React.FC = () => {
             getIconForDocumentType={getIconForDocumentType}
             displayCalendar={displayCalendar}
             eventConversation={<EventConversation {...event} />}
+            eventAttendance={attendance}
           >
             {!!event.speakers.length && <SpeakerList {...event} />}
           </EventDetailPage>

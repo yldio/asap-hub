9d7e611518b065cb87d3b3c5c30b1839f0b5a4e8
Gabriela Ueno <gabiayako@gmail.com>
2026-09-07 08:36:47 -0300

    ASAP-1077 Team Leadership Metrics (#5192)

--- packages/react-components/src/index.ts
+++ packages/react-components/src/index.ts
@@ -252,6 +252,7 @@ export {
   TeamCard,
   TeamCollaborationTable,
   TeamInterestGroupsTabbedCard,
+  TeamLeadershipMetrics,
   TeamMembersSection,
   TeamMembersTabbedCard,
   TeamProductivityTable,
